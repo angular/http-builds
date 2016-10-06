50f48c54f4140e23f859ceff1884e1a5bf036e6f
Tobias Bosch <tbosch1009@gmail.com>
2016-10-06 22:25:11 +0000

    f50c1da fix(forms): properly validate blank strings with minlength (#12091)

--- bundles/http.umd.js
+++ bundles/http.umd.js
@@ -63,74 +63,6 @@
     function isString(obj) {
         return typeof obj === 'string';
     }
-    var StringWrapper = (function () {
-        function StringWrapper() {
-        }
-        StringWrapper.fromCharCode = function (code) { return String.fromCharCode(code); };
-        StringWrapper.charCodeAt = function (s, index) { return s.charCodeAt(index); };
-        StringWrapper.split = function (s, regExp) { return s.split(regExp); };
-        StringWrapper.equals = function (s, s2) { return s === s2; };
-        StringWrapper.stripLeft = function (s, charVal) {
-            if (s && s.length) {
-                var pos = 0;
-                for (var i = 0; i < s.length; i++) {
-                    if (s[i] != charVal)
-                        break;
-                    pos++;
-                }
-                s = s.substring(pos);
-            }
-            return s;
-        };
-        StringWrapper.stripRight = function (s, charVal) {
-            if (s && s.length) {
-                var pos = s.length;
-                for (var i = s.length - 1; i >= 0; i--) {
-                    if (s[i] != charVal)
-                        break;
-                    pos--;
-                }
-                s = s.substring(0, pos);
-            }
-            return s;
-        };
-        StringWrapper.replace = function (s, from, replace) {
-            return s.replace(from, replace);
-        };
-        StringWrapper.replaceAll = function (s, from, replace) {
-            return s.replace(from, replace);
-        };
-        StringWrapper.slice = function (s, from, to) {
-            if (from === void 0) { from = 0; }
-            if (to === void 0) { to = null; }
-            return s.slice(from, to === null ? undefined : to);
-        };
-        StringWrapper.replaceAllMapped = function (s, from, cb) {
-            return s.replace(from, function () {
-                var matches = [];
-                for (var _i = 0; _i < arguments.length; _i++) {
-                    matches[_i - 0] = arguments[_i];
-                }
-                // Remove offset & string from the result array
-                matches.splice(-2, 2);
-                // The callback receives match, p1, ..., pn
-                return cb(matches);
-            });
-        };
-        StringWrapper.contains = function (s, substr) { return s.indexOf(substr) != -1; };
-        StringWrapper.compare = function (a, b) {
-            if (a < b) {
-                return -1;
-            }
-            else if (a > b) {
-                return 1;
-            }
-            else {
-                return 0;
-            }
-        };
-        return StringWrapper;
-    }());
     var NumberWrapper = (function () {
         function NumberWrapper() {
         }
@@ -1086,7 +1018,7 @@
                 var callback = _dom.requestCallback(_this._id);
                 var url = req.url;
                 if (url.indexOf('=JSONP_CALLBACK&') > -1) {
-                    url = StringWrapper.replace(url, '=JSONP_CALLBACK&', "=" + callback + "&");
+                    url = url.replace('=JSONP_CALLBACK&', "=" + callback + "&");
                 }
                 else if (url.lastIndexOf('=JSONP_CALLBACK') === url.length - '=JSONP_CALLBACK'.length) {
                     url = url.substring(0, url.length - '=JSONP_CALLBACK'.length) + ("=" + callback);
@@ -1599,7 +1531,7 @@
                 var search = requestOptions.search.toString();
                 if (search.length > 0) {
                     var prefix = '?';
-                    if (StringWrapper.contains(this.url, '?')) {
+                    if (this.url.indexOf('?') != -1) {
                         prefix = (this.url[this.url.length - 1] == '&') ? '' : '&';
                     }
                     // TODO: just delete search-query-looking string in url?
