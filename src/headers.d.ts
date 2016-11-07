39012b606cd3a52b7afbc0d4c553c1de8cfae1a7
vikerman <vikerman@users.noreply.github.com>
2016-11-07 18:42:32 +0000

    89fd54e docs(url_tree):fix typo(UrlSegment path property's comment part -> path) (#12728)

--- src/headers.d.ts
+++ src/headers.d.ts
@@ -1,4 +1,11 @@
 /**
+ * @license
+ * Copyright Google Inc. All Rights Reserved.
+ *
+ * Use of this source code is governed by an MIT-style license that can be
+ * found in the LICENSE file at https://angular.io/license
+ */
+/**
  * Polyfill for [Headers](https://developer.mozilla.org/en-US/docs/Web/API/Headers/Headers), as
  * specified in the [Fetch Spec](https://fetch.spec.whatwg.org/#headers-class).
  *
