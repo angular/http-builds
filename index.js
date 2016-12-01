f7ee4f1810bfcb11f83b6fc3b7db971f2a8b909e
Alex Rickabaugh <alx+alxhub@alxandria.net>
2016-12-01 18:36:35 +0000

    5908b66 docs(changelog): move 2.2.2 to its place in the chronological order

--- index.js
+++ index.js
@@ -9,6 +9,6 @@ import { Version } from '@angular/core';
 /**
  * @stable
  */
-export var /** @type {?} */ VERSION = new Version('2.3.0-rc.0-480ef20');
+export var /** @type {?} */ VERSION = new Version('2.3.0-rc.0-5908b66');
 export { BrowserXhr, JSONPBackend, JSONPConnection, CookieXSRFStrategy, XHRBackend, XHRConnection, BaseRequestOptions, RequestOptions, BaseResponseOptions, ResponseOptions, ReadyState, RequestMethod, ResponseContentType, ResponseType, Headers, Http, Jsonp, HttpModule, JsonpModule, Connection, ConnectionBackend, XSRFStrategy, Request, Response, QueryEncoder, URLSearchParams } from './src/index';
 //# sourceMappingURL=index.js.map
