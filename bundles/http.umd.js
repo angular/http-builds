6034804e8366ff3eae30c800c2459e967d172286
Alex Rickabaugh <alx+alxhub@alxandria.net>
2016-12-05 19:50:47 +0000

    c767df0 fix(router): throw a better error message when angular 1 is not bootstraped

--- bundles/http.umd.js
+++ bundles/http.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v2.3.0-rc.0-307c469
+ * @license Angular v2.3.0-rc.0-c767df0
  * (c) 2010-2016 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1991,7 +1991,7 @@
     /**
      * @stable
      */
-    var /** @type {?} */ VERSION = new _angular_core.Version('2.3.0-rc.0-307c469');
+    var /** @type {?} */ VERSION = new _angular_core.Version('2.3.0-rc.0-c767df0');
 
     exports.VERSION = VERSION;
     exports.BrowserXhr = BrowserXhr;
--- index.js
+++ index.js
@@ -9,6 +9,6 @@ import { Version } from '@angular/core';
 /**
  * @stable
  */
-export var /** @type {?} */ VERSION = new Version('2.3.0-rc.0-307c469');
+export var /** @type {?} */ VERSION = new Version('2.3.0-rc.0-c767df0');
 export { BrowserXhr, JSONPBackend, JSONPConnection, CookieXSRFStrategy, XHRBackend, XHRConnection, BaseRequestOptions, RequestOptions, BaseResponseOptions, ResponseOptions, ReadyState, RequestMethod, ResponseContentType, ResponseType, Headers, Http, Jsonp, HttpModule, JsonpModule, Connection, ConnectionBackend, XSRFStrategy, Request, Response, QueryEncoder, URLSearchParams } from './src/index';
 //# sourceMappingURL=index.js.map
