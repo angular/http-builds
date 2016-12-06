3d661e9db6087159086c679c2f9ecaae24626479
Alex Rickabaugh <alx+alxhub@alxandria.net>
2016-12-06 18:54:18 +0000

    2893c2c fix(router): validate nested routes (#13224)

--- index.js
+++ index.js
@@ -9,6 +9,6 @@ import { Version } from '@angular/core';
 /**
  * @stable
  */
-export var /** @type {?} */ VERSION = new Version('2.3.0-rc.0-f31c947');
+export var /** @type {?} */ VERSION = new Version('2.3.0-rc.0-2893c2c');
 export { BrowserXhr, JSONPBackend, JSONPConnection, CookieXSRFStrategy, XHRBackend, XHRConnection, BaseRequestOptions, RequestOptions, BaseResponseOptions, ResponseOptions, ReadyState, RequestMethod, ResponseContentType, ResponseType, Headers, Http, Jsonp, HttpModule, JsonpModule, Connection, ConnectionBackend, XSRFStrategy, Request, Response, QueryEncoder, URLSearchParams } from './src/index';
 //# sourceMappingURL=index.js.map
