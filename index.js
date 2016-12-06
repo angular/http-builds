d4585f43ecab3baf21989effeee24b980ec27298
Alex Rickabaugh <alx+alxhub@alxandria.net>
2016-12-06 18:54:00 +0000

    f31c947 fix(compiler): short-circut expressions with an index (#13263)

--- index.js
+++ index.js
@@ -9,6 +9,6 @@ import { Version } from '@angular/core';
 /**
  * @stable
  */
-export var /** @type {?} */ VERSION = new Version('2.3.0-rc.0-4bd8f58');
+export var /** @type {?} */ VERSION = new Version('2.3.0-rc.0-f31c947');
 export { BrowserXhr, JSONPBackend, JSONPConnection, CookieXSRFStrategy, XHRBackend, XHRConnection, BaseRequestOptions, RequestOptions, BaseResponseOptions, ResponseOptions, ReadyState, RequestMethod, ResponseContentType, ResponseType, Headers, Http, Jsonp, HttpModule, JsonpModule, Connection, ConnectionBackend, XSRFStrategy, Request, Response, QueryEncoder, URLSearchParams } from './src/index';
 //# sourceMappingURL=index.js.map
