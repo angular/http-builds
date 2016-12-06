bb57673cb5dce629b8f7338b3d29844d87a9b1fd
Alex Rickabaugh <alx+alxhub@alxandria.net>
2016-12-06 18:53:47 +0000

    4bd8f58 FIX typo (#13267)

--- index.js
+++ index.js
@@ -9,6 +9,6 @@ import { Version } from '@angular/core';
 /**
  * @stable
  */
-export var /** @type {?} */ VERSION = new Version('2.3.0-rc.0-93556a5');
+export var /** @type {?} */ VERSION = new Version('2.3.0-rc.0-4bd8f58');
 export { BrowserXhr, JSONPBackend, JSONPConnection, CookieXSRFStrategy, XHRBackend, XHRConnection, BaseRequestOptions, RequestOptions, BaseResponseOptions, ResponseOptions, ReadyState, RequestMethod, ResponseContentType, ResponseType, Headers, Http, Jsonp, HttpModule, JsonpModule, Connection, ConnectionBackend, XSRFStrategy, Request, Response, QueryEncoder, URLSearchParams } from './src/index';
 //# sourceMappingURL=index.js.map
