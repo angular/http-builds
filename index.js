6188506a9fcbb9548b4d28423321173cda2209fc
Alex Rickabaugh <alx+alxhub@alxandria.net>
2016-12-06 18:09:03 +0000

    93556a5 fix(language-service): avoid throwing for invalid class declarations (#13257)

--- index.js
+++ index.js
@@ -9,6 +9,6 @@ import { Version } from '@angular/core';
 /**
  * @stable
  */
-export var /** @type {?} */ VERSION = new Version('2.3.0-rc.0-5614c4f');
+export var /** @type {?} */ VERSION = new Version('2.3.0-rc.0-93556a5');
 export { BrowserXhr, JSONPBackend, JSONPConnection, CookieXSRFStrategy, XHRBackend, XHRConnection, BaseRequestOptions, RequestOptions, BaseResponseOptions, ResponseOptions, ReadyState, RequestMethod, ResponseContentType, ResponseType, Headers, Http, Jsonp, HttpModule, JsonpModule, Connection, ConnectionBackend, XSRFStrategy, Request, Response, QueryEncoder, URLSearchParams } from './src/index';
 //# sourceMappingURL=index.js.map
