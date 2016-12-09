30a19496620df44bf290c0a827e4b8ccfa4a0915
Victor Berchet <victor@suumit.com>
2016-12-09 19:33:11 +0000

    c8baf51 style: clang-format the code

--- bundles/http.umd.js
+++ bundles/http.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v2.3.0-9a8423d
+ * @license Angular v2.3.0-c8baf51
  * (c) 2010-2016 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1995,7 +1995,7 @@
     /**
      * @stable
      */
-    var /** @type {?} */ VERSION = new _angular_core.Version('2.3.0-9a8423d');
+    var /** @type {?} */ VERSION = new _angular_core.Version('2.3.0-c8baf51');
 
     exports.BrowserXhr = BrowserXhr;
     exports.JSONPBackend = JSONPBackend;
