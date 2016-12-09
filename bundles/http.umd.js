4bbda2c267bdf03aa9eaf959bfdcafde3096cccd
Victor Berchet <victor@suumit.com>
2016-12-09 02:55:22 +0000

    5c6ec20 refactor(router): simplify regexp

--- bundles/http.umd.js
+++ bundles/http.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v2.3.0-4e3d58a
+ * @license Angular v2.3.0-5c6ec20
  * (c) 2010-2016 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1995,7 +1995,7 @@
     /**
      * @stable
      */
-    var /** @type {?} */ VERSION = new _angular_core.Version('2.3.0-4e3d58a');
+    var /** @type {?} */ VERSION = new _angular_core.Version('2.3.0-5c6ec20');
 
     exports.BrowserXhr = BrowserXhr;
     exports.JSONPBackend = JSONPBackend;
