e756c430c9af3d1e7397dcdaf33caa7e49601195
GitHub <noreply@github.com>
2016-12-09 19:35:30 +0000

    4022173 fix(compiler): fix PR 13322 (#13331)

--- bundles/http.umd.js
+++ bundles/http.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v2.3.0-c8baf51
+ * @license Angular v2.3.0-4022173
  * (c) 2010-2016 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1995,7 +1995,7 @@
     /**
      * @stable
      */
-    var /** @type {?} */ VERSION = new _angular_core.Version('2.3.0-c8baf51');
+    var /** @type {?} */ VERSION = new _angular_core.Version('2.3.0-4022173');
 
     exports.BrowserXhr = BrowserXhr;
     exports.JSONPBackend = JSONPBackend;
