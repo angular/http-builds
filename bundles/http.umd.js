50710b80e964dd97849008113060473903a6512f
Victor Berchet <victor@suumit.com>
2016-12-09 18:57:51 +0000

    f0b0762 fix(animations): always cleanup players after they have finished internally (#13334)

--- bundles/http.umd.js
+++ bundles/http.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v2.3.0-b5c4bf1
+ * @license Angular v2.3.0-f0b0762
  * (c) 2010-2016 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1995,7 +1995,7 @@
     /**
      * @stable
      */
-    var /** @type {?} */ VERSION = new _angular_core.Version('2.3.0-b5c4bf1');
+    var /** @type {?} */ VERSION = new _angular_core.Version('2.3.0-f0b0762');
 
     exports.BrowserXhr = BrowserXhr;
     exports.JSONPBackend = JSONPBackend;
