bd9c1006ed03e41d5359d89fff64ff931df8c70c
Victor Berchet <victor@suumit.com>
2016-12-12 19:29:55 +0000

    e9f307f fix(forms): fix Validators.min/maxLength with FormArray (#13095)

--- bundles/http.umd.js
+++ bundles/http.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v2.3.0-2e500cc
+ * @license Angular v2.3.0-e9f307f
  * (c) 2010-2016 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2052,7 +2052,7 @@
     /**
      * @stable
      */
-    var /** @type {?} */ VERSION = new _angular_core.Version('2.3.0-2e500cc');
+    var /** @type {?} */ VERSION = new _angular_core.Version('2.3.0-e9f307f');
 
     exports.BrowserXhr = BrowserXhr;
     exports.JSONPBackend = JSONPBackend;
