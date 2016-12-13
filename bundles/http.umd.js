69f90ae21ba1adfe518e4d32bcd6c005d44a1026
Victor Berchet <victor@suumit.com>
2016-12-13 19:43:54 +0000

    2b90cd5 fix(compiler): narrow the span reported for invalid pipes

--- bundles/http.umd.js
+++ bundles/http.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v2.3.0-9ec0a4e
+ * @license Angular v2.3.0-2b90cd5
  * (c) 2010-2016 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2052,7 +2052,7 @@
     /**
      * @stable
      */
-    var /** @type {?} */ VERSION = new _angular_core.Version('2.3.0-9ec0a4e');
+    var /** @type {?} */ VERSION = new _angular_core.Version('2.3.0-2b90cd5');
 
     exports.BrowserXhr = BrowserXhr;
     exports.JSONPBackend = JSONPBackend;
