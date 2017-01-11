398eb46ddb574bba759816b0d221dc958d4edc25
Miško Hevery <misko@hevery.com>
2017-01-11 21:36:49 +0000

    3a4b54d docs(release-schedule): add release schedule doc (#13827)

--- bundles/http.umd.js
+++ bundles/http.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.0.0-beta.2-95cbca2
+ * @license Angular v4.0.0-beta.2-3a4b54d
  * (c) 2010-2016 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2052,7 +2052,7 @@
     /**
      * @stable
      */
-    var /** @type {?} */ VERSION = new _angular_core.Version('4.0.0-beta.2-95cbca2');
+    var /** @type {?} */ VERSION = new _angular_core.Version('4.0.0-beta.2-3a4b54d');
 
     exports.BrowserXhr = BrowserXhr;
     exports.JSONPBackend = JSONPBackend;
