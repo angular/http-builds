13125a8859ccb4a55e88d4a0219ea72d5d6d745f
Miško Hevery <misko@hevery.com>
2017-02-01 05:14:45 +0000

    8d4aa82 fix(i18n): parse ICU messages while normalizing templates (#14153)

--- bundles/http.umd.js
+++ bundles/http.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v2.4.5-14e9751
+ * @license Angular v2.4.5-8d4aa82
  * (c) 2010-2016 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2019,7 +2019,7 @@
     /**
      * @stable
      */
-    var /** @type {?} */ VERSION = new _angular_core.Version('2.4.5-14e9751');
+    var /** @type {?} */ VERSION = new _angular_core.Version('2.4.5-8d4aa82');
 
     exports.BrowserXhr = BrowserXhr;
     exports.JSONPBackend = JSONPBackend;
