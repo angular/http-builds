0ea30444c65f586818bd3304f5be99204caf5e9d
Misko Hevery <misko@hevery.com>
2017-02-09 04:56:17 +0000

    e90661a docs(changelog): add changelog for 2.4.7

--- bundles/http.umd.js
+++ bundles/http.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v2.4.6-059085b
+ * @license Angular v2.4.7-e90661a
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2019,7 +2019,7 @@
     /**
      * @stable
      */
-    var /** @type {?} */ VERSION = new _angular_core.Version('2.4.6-059085b');
+    var /** @type {?} */ VERSION = new _angular_core.Version('2.4.7-e90661a');
 
     exports.BrowserXhr = BrowserXhr;
     exports.JSONPBackend = JSONPBackend;
