2b9ec7c39b175f5979bfea625e8bd709899cf87e
Miško Hevery <misko@hevery.com>
2017-02-09 18:26:28 +0000

    74cb575 fix(upgrade): correctly project content on downgraded components with structural directives (#14274)

--- bundles/http.umd.js
+++ bundles/http.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v2.4.7-e90661a
+ * @license Angular v2.4.7-74cb575
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2019,7 +2019,7 @@
     /**
      * @stable
      */
-    var /** @type {?} */ VERSION = new _angular_core.Version('2.4.7-e90661a');
+    var /** @type {?} */ VERSION = new _angular_core.Version('2.4.7-74cb575');
 
     exports.BrowserXhr = BrowserXhr;
     exports.JSONPBackend = JSONPBackend;
