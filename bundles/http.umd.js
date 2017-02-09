0cf75ec44b7563f7d738f0445d91f18afacf50b5
Misko Hevery <misko@hevery.com>
2017-02-09 04:02:45 +0000

    401ef74 docs(compiler): incorrect method reference (#14314)

--- bundles/http.umd.js
+++ bundles/http.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v2.4.6-5bb47db
+ * @license Angular v2.4.6-401ef74
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2019,7 +2019,7 @@
     /**
      * @stable
      */
-    var /** @type {?} */ VERSION = new _angular_core.Version('2.4.6-5bb47db');
+    var /** @type {?} */ VERSION = new _angular_core.Version('2.4.6-401ef74');
 
     exports.BrowserXhr = BrowserXhr;
     exports.JSONPBackend = JSONPBackend;
