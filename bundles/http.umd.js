bd2aedc818fc6b86dc7d92a5e84af6a5dd4e9a75
GitHub <noreply@github.com>
2017-02-15 06:01:44 +0000

    e9ba7aa docs: update schedule by adding one more beta (#14494)

--- bundles/http.umd.js
+++ bundles/http.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.0.0-beta.7-bb9c7ae
+ * @license Angular v4.0.0-beta.7-e9ba7aa
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2080,7 +2080,7 @@
     /**
      * @stable
      */
-    var /** @type {?} */ VERSION = new _angular_core.Version('4.0.0-beta.7-bb9c7ae');
+    var /** @type {?} */ VERSION = new _angular_core.Version('4.0.0-beta.7-e9ba7aa');
 
     exports.BrowserXhr = BrowserXhr;
     exports.JSONPBackend = JSONPBackend;
