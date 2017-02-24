f90d5eb075f0933ba279770e0c7baa58519917ce
Igor Minar <iminar@google.com>
2017-02-24 07:46:42 +0000

    1f3198c feat(aio): copy example code from angular.io

--- bundles/http.umd.js
+++ bundles/http.umd.js
@@ -1981,7 +1981,7 @@
     /**
      * @stable
      */
-    var /** @type {?} */VERSION = new _core.Version('4.0.0-beta.8-2da3844');
+    var /** @type {?} */VERSION = new _core.Version('4.0.0-beta.8-1f3198c');
 
     exports.BrowserXhr = BrowserXhr;
     exports.JSONPBackend = JSONPBackend;
