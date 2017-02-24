c507d3c7808a87f82f71cece0465b895be2d684e
Igor Minar <iminar@google.com>
2017-02-24 23:38:18 +0000

    932a02f build(language-service): fix tsconfig needed for language-service rollup (#14717)

--- bundles/http.umd.js
+++ bundles/http.umd.js
@@ -1981,7 +1981,7 @@
     /**
      * @stable
      */
-    var /** @type {?} */VERSION = new _core.Version('4.0.0-beta.8-3c9a46c');
+    var /** @type {?} */VERSION = new _core.Version('4.0.0-beta.8-932a02f');
 
     exports.BrowserXhr = BrowserXhr;
     exports.JSONPBackend = JSONPBackend;
