327ba7663482ec6673d220f16a9ada50f57831a9
Igor Minar <iminar@google.com>
2017-02-24 08:53:44 +0000

    32c2fd5 refactor(animations): change NoOp -> Noop (#14690)

--- bundles/http.umd.js
+++ bundles/http.umd.js
@@ -1981,7 +1981,7 @@
     /**
      * @stable
      */
-    var /** @type {?} */VERSION = new _core.Version('4.0.0-beta.8-1f3198c');
+    var /** @type {?} */VERSION = new _core.Version('4.0.0-beta.8-32c2fd5');
 
     exports.BrowserXhr = BrowserXhr;
     exports.JSONPBackend = JSONPBackend;
