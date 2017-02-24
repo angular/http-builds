62cd6d680c0210f0385f356af981cf03f412a876
Igor Minar <iminar@google.com>
2017-02-24 20:32:22 +0000

    436a179 fix(animations): properly cache renderer and namespace triggers (#14703)

--- bundles/http.umd.js
+++ bundles/http.umd.js
@@ -1981,7 +1981,7 @@
     /**
      * @stable
      */
-    var /** @type {?} */VERSION = new _core.Version('4.0.0-beta.8-5094aef');
+    var /** @type {?} */VERSION = new _core.Version('4.0.0-beta.8-436a179');
 
     exports.BrowserXhr = BrowserXhr;
     exports.JSONPBackend = JSONPBackend;
