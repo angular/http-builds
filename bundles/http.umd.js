d874eee125c55e931bb61ca27ed217976121a2ce
GitHub <noreply@github.com>
2017-02-24 21:26:07 +0000

    3c9a46c fix(core): fix directive instantiation (#14715)

--- bundles/http.umd.js
+++ bundles/http.umd.js
@@ -1981,7 +1981,7 @@
     /**
      * @stable
      */
-    var /** @type {?} */VERSION = new _core.Version('4.0.0-beta.8-436a179');
+    var /** @type {?} */VERSION = new _core.Version('4.0.0-beta.8-3c9a46c');
 
     exports.BrowserXhr = BrowserXhr;
     exports.JSONPBackend = JSONPBackend;
