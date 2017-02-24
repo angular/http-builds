89c7973e8d510e5088f2325a2f596f8e9832f5b3
Igor Minar <iminar@google.com>
2017-02-24 02:24:56 +0000

    ee747f7 refactor(core): change abstract classes for interfaces (#12324)

--- bundles/http.umd.js
+++ bundles/http.umd.js
@@ -1981,7 +1981,7 @@
     /**
      * @stable
      */
-    var /** @type {?} */VERSION = new _core.Version('4.0.0-beta.8-a23634d');
+    var /** @type {?} */VERSION = new _core.Version('4.0.0-beta.8-ee747f7');
 
     exports.BrowserXhr = BrowserXhr;
     exports.JSONPBackend = JSONPBackend;
