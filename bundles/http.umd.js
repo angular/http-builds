1807d34cea7a4d0857633e1005e084d68b2a5e04
Igor Minar <iminar@google.com>
2017-02-24 04:24:52 +0000

    e99d721 feat(compiler): Add a `enableLegacyTemplate` option to support `<template>`

--- bundles/http.umd.js
+++ bundles/http.umd.js
@@ -1981,7 +1981,7 @@
     /**
      * @stable
      */
-    var /** @type {?} */VERSION = new _core.Version('4.0.0-beta.8-ee747f7');
+    var /** @type {?} */VERSION = new _core.Version('4.0.0-beta.8-e99d721');
 
     exports.BrowserXhr = BrowserXhr;
     exports.JSONPBackend = JSONPBackend;
