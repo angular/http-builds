1779691e400e3a627d3eec2196303d6529dbbf0b
GitHub <noreply@github.com>
2017-02-25 02:44:04 +0000

    f32f4de docs: more changelog fixes

--- bundles/http.umd.js
+++ bundles/http.umd.js
@@ -1981,7 +1981,7 @@
     /**
      * @stable
      */
-    var /** @type {?} */VERSION = new _core.Version('4.0.0-rc.1-26ed262');
+    var /** @type {?} */VERSION = new _core.Version('4.0.0-rc.1-f32f4de');
 
     exports.BrowserXhr = BrowserXhr;
     exports.JSONPBackend = JSONPBackend;
