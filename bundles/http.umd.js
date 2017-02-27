34c837978292bc66c6c5002164bdf79f9e7be2e4
Igor Minar <iminar@google.com>
2017-02-27 19:47:11 +0000

    77682a3 fix(compiler): quote `animation` in `RendererTypeV2` and skip if empty (#14773)

--- bundles/http.umd.js
+++ bundles/http.umd.js
@@ -1981,7 +1981,7 @@
     /**
      * @stable
      */
-    var /** @type {?} */VERSION = new _core.Version('4.0.0-rc.1-213e210');
+    var /** @type {?} */VERSION = new _core.Version('4.0.0-rc.1-77682a3');
 
     exports.BrowserXhr = BrowserXhr;
     exports.JSONPBackend = JSONPBackend;
