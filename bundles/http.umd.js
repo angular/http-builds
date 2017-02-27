c6b80b0ebc36c48712b6758ca4e8c6b736b38faf
Igor Minar <iminar@google.com>
2017-02-27 19:48:24 +0000

    f356041 docs: update version in changelog to reflect RC.1 (not 0) (#14740)

--- bundles/http.umd.js
+++ bundles/http.umd.js
@@ -1981,7 +1981,7 @@
     /**
      * @stable
      */
-    var /** @type {?} */VERSION = new _core.Version('4.0.0-rc.1-77682a3');
+    var /** @type {?} */VERSION = new _core.Version('4.0.0-rc.1-f356041');
 
     exports.BrowserXhr = BrowserXhr;
     exports.JSONPBackend = JSONPBackend;
