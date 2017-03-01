f7fd597e9525e3a2bfdfe01add8a92255e6d433c
Igor Minar <iminar@google.com>
2017-03-01 15:12:02 +0000

    968995a fix(upgrade): fix upgrade component Closure optimization. (#14801)

--- bundles/http.umd.js
+++ bundles/http.umd.js
@@ -1981,7 +1981,7 @@
     /**
      * @stable
      */
-    var /** @type {?} */VERSION = new _core.Version('4.0.0-rc.1-5ba55b0');
+    var /** @type {?} */VERSION = new _core.Version('4.0.0-rc.1-968995a');
 
     exports.BrowserXhr = BrowserXhr;
     exports.JSONPBackend = JSONPBackend;
