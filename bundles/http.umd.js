8bcae1bf664fe38e250fbe6ab0c403c3b579a150
Igor Minar <iminar@google.com>
2017-03-01 16:23:19 +0000

    bc9e1de docs: changelog - fix else example (#14766)

--- bundles/http.umd.js
+++ bundles/http.umd.js
@@ -1981,7 +1981,7 @@
     /**
      * @stable
      */
-    var /** @type {?} */VERSION = new _core.Version('4.0.0-rc.1-fc9e6b2');
+    var /** @type {?} */VERSION = new _core.Version('4.0.0-rc.1-bc9e1de');
 
     exports.BrowserXhr = BrowserXhr;
     exports.JSONPBackend = JSONPBackend;
