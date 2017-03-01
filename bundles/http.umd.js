537e1ec7f3121315d5cb410ca3d74968d768a674
Igor Minar <iminar@google.com>
2017-03-01 19:38:40 +0000

    9402df9 test(platform-server): enable meta tag and title service specs for server (#14574)

--- bundles/http.umd.js
+++ bundles/http.umd.js
@@ -1981,7 +1981,7 @@
     /**
      * @stable
      */
-    var /** @type {?} */VERSION = new _core.Version('4.0.0-rc.1-47bdc2b');
+    var /** @type {?} */VERSION = new _core.Version('4.0.0-rc.1-9402df9');
 
     exports.BrowserXhr = BrowserXhr;
     exports.JSONPBackend = JSONPBackend;
