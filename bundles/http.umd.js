245787cf8227424de7648e11cd29350b260773c6
Igor Minar <iminar@google.com>
2017-03-01 16:25:39 +0000

    d1182af refactor: change remaining `<template>` to `<ng-template>` (#14706)

--- bundles/http.umd.js
+++ bundles/http.umd.js
@@ -1981,7 +1981,7 @@
     /**
      * @stable
      */
-    var /** @type {?} */VERSION = new _core.Version('4.0.0-rc.1-5753de5');
+    var /** @type {?} */VERSION = new _core.Version('4.0.0-rc.1-d1182af');
 
     exports.BrowserXhr = BrowserXhr;
     exports.JSONPBackend = JSONPBackend;
