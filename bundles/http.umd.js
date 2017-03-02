21ed3ef31fb1adbba8fc4cc0ed41277cfcfef080
Igor Minar <iminar@google.com>
2017-03-02 01:34:47 +0000

    9560ad8 fix(animations): make animations work in AOT (#14775)

--- bundles/http.umd.js
+++ bundles/http.umd.js
@@ -1981,7 +1981,7 @@
     /**
      * @stable
      */
-    var /** @type {?} */VERSION = new _core.Version('4.0.0-rc.1-3168ef7');
+    var /** @type {?} */VERSION = new _core.Version('4.0.0-rc.1-9560ad8');
 
     exports.BrowserXhr = BrowserXhr;
     exports.JSONPBackend = JSONPBackend;
