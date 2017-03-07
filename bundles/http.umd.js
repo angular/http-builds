349ed424c35e3f1ca1523cab3e0b8746f5a0092a
GitHub <noreply@github.com>
2017-03-07 02:27:41 +0000

    b7e76cc ci: re-enable publishing master artifacts (#14973)

--- bundles/http.umd.js
+++ bundles/http.umd.js
@@ -1981,7 +1981,7 @@
     /**
      * @stable
      */
-    var /** @type {?} */VERSION = new _core.Version('4.0.0-rc.2-207298c');
+    var /** @type {?} */VERSION = new _core.Version('4.0.0-rc.2-b7e76cc');
 
     exports.BrowserXhr = BrowserXhr;
     exports.JSONPBackend = JSONPBackend;
