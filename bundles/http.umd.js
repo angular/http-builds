a347c37d682d8f664fb811a7070c4d0c4f645da2
Chuck Jazdzewski <chuckj@google.com>
2017-03-07 17:53:54 +0000

    07122f0 fix(upgrade): populate upgraded component's view before creating the controller (#14289)

--- bundles/http.umd.js
+++ bundles/http.umd.js
@@ -1981,7 +1981,7 @@
     /**
      * @stable
      */
-    var /** @type {?} */VERSION = new _core.Version('4.0.0-rc.2-ebd4463');
+    var /** @type {?} */VERSION = new _core.Version('4.0.0-rc.2-07122f0');
 
     exports.BrowserXhr = BrowserXhr;
     exports.JSONPBackend = JSONPBackend;
