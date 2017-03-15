1278eba609c2488506a95ac29c4a72bf1f8d0cb4
Chuck Jazdzewski <chuckj@google.com>
2017-03-15 20:40:57 +0000

    322bf7a Revert "refactor(upgrade): don’t rely on compiler internals"

--- bundles/http.umd.js
+++ bundles/http.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.0.0-rc.3-a3e32fb
+ * @license Angular v4.0.0-rc.3-322bf7a
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2021,7 +2021,7 @@
     /**
      * @stable
      */
-    var /** @type {?} */ VERSION = new _angular_core.Version('4.0.0-rc.3-a3e32fb');
+    var /** @type {?} */ VERSION = new _angular_core.Version('4.0.0-rc.3-322bf7a');
 
     exports.BrowserXhr = BrowserXhr;
     exports.JSONPBackend = JSONPBackend;
