4fb8683e1efec89c19a60f6b0ac29530d53883ad
Chuck Jazdzewski <chuckj@google.com>
2017-03-16 20:14:12 +0000

    992aa17 build: add source maps into NPM distribution (#15159)

--- @angular/http/testing.js
+++ @angular/http/testing.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.0.0-rc.3-a4076c7
+ * @license Angular v4.0.0-rc.3-992aa17
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -212,3 +212,4 @@ MockBackend.decorators = [
 MockBackend.ctorParameters = () => [];
 
 export { MockConnection, MockBackend };
+//# sourceMappingURL=testing.js.map
--- bundles/http.umd.js
+++ bundles/http.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.0.0-rc.3-a4076c7
+ * @license Angular v4.0.0-rc.3-992aa17
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2021,7 +2021,7 @@
     /**
      * @stable
      */
-    var /** @type {?} */ VERSION = new _angular_core.Version('4.0.0-rc.3-a4076c7');
+    var /** @type {?} */ VERSION = new _angular_core.Version('4.0.0-rc.3-992aa17');
 
     exports.BrowserXhr = BrowserXhr;
     exports.JSONPBackend = JSONPBackend;
@@ -2058,3 +2058,4 @@
     exports.ɵd = jsonpFactory;
 
 }));
+//# sourceMappingURL=http.umd.js.map
