30e52ef83d2c73b36d155f07718c3b7eab33c1f1
Miško Hevery <misko@hevery.com>
2017-03-21 19:40:29 +0000

    8e6995c fix(core): stringify shouldn't throw when toString returns null/undefined (#14975)

--- @angular/http.js
+++ @angular/http.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.0.0-rc.5-2489e4b
+ * @license Angular v4.0.0-rc.5-8e6995c
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2049,7 +2049,7 @@ JsonpModule.ctorParameters = () => [];
 /**
  * \@stable
  */
-const VERSION = new Version('4.0.0-rc.5-2489e4b');
+const VERSION = new Version('4.0.0-rc.5-8e6995c');
 
 /**
  * @license
