6822dca43b41d8f41924fc954d48e4d9a4593a2d
Igor Minar <iminar@google.com>
2017-03-23 00:31:13 +0000

    26f6bd4 ci(travis): update excluded branch to g3 (#15391)

--- @angular/http.js
+++ @angular/http.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.0.0-rc.5-ea49a95
+ * @license Angular v4.0.0-rc.5-26f6bd4
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2049,7 +2049,7 @@ JsonpModule.ctorParameters = () => [];
 /**
  * \@stable
  */
-const VERSION = new Version('4.0.0-rc.5-ea49a95');
+const VERSION = new Version('4.0.0-rc.5-26f6bd4');
 
 /**
  * @license
