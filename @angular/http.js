7cef65b4413be179b525c00f4db5c404d04bf20c
Victor Berchet <victor@suumit.com>
2017-03-28 20:50:16 +0000

    d438b88 fix(compiler): ignore errors when evaluating base classes (#15560)

--- @angular/http.js
+++ @angular/http.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.0.0-6269d28
+ * @license Angular v4.0.0-d438b88
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2049,7 +2049,7 @@ JsonpModule.ctorParameters = () => [];
 /**
  * \@stable
  */
-const VERSION = new Version('4.0.0-6269d28');
+const VERSION = new Version('4.0.0-d438b88');
 
 /**
  * @license
