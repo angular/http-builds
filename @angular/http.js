baf0d3052b7778f0e3081f11e1407b4d9519d72c
Victor Berchet <victor@suumit.com>
2017-03-29 17:46:57 +0000

    9c77a7c fix(compiler): throw when a component defines both template and templateUrl (#15572)

--- @angular/http.js
+++ @angular/http.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.0.0-331b9f6
+ * @license Angular v4.0.0-9c77a7c
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2049,7 +2049,7 @@ JsonpModule.ctorParameters = () => [];
 /**
  * \@stable
  */
-const VERSION = new Version('4.0.0-331b9f6');
+const VERSION = new Version('4.0.0-9c77a7c');
 
 /**
  * @license
