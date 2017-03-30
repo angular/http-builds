20c78fa7d87b8622651a2fbfad3d5750112daab6
Victor Berchet <victor@suumit.com>
2017-03-30 00:00:12 +0000

    6b79ab5 docs: add changelog for 4.0.1

--- @angular/http.js
+++ @angular/http.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.0.0-ca66530
+ * @license Angular v4.0.1-6b79ab5
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2038,7 +2038,7 @@ JsonpModule.ctorParameters = () => [];
 /**
  * \@stable
  */
-const VERSION = new Version('4.0.0-ca66530');
+const VERSION = new Version('4.0.1-6b79ab5');
 
 /**
  * @license
