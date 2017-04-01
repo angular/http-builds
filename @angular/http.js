a1483c09515d08f1c0c227c8b8cb89a2c73bdb46
Pete Bacon Darwin <pete@bacondarwin.com>
2017-04-01 17:37:22 +0000

    8c726ea fix(aio): preserve space after anchor elements

--- @angular/http.js
+++ @angular/http.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.1.0-beta.0-c8be396
+ * @license Angular v4.1.0-beta.0-8c726ea
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2038,7 +2038,7 @@ JsonpModule.ctorParameters = () => [];
 /**
  * \@stable
  */
-const VERSION = new Version('4.1.0-beta.0-c8be396');
+const VERSION = new Version('4.1.0-beta.0-8c726ea');
 
 /**
  * @license
