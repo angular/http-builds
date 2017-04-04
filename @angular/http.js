a08eff8490be6e3dc0e0f9f9171f25395b01f819
Pete Bacon Darwin <pete@bacondarwin.com>
2017-04-04 10:36:07 +0000

    c6917d9 build(aio): update boilerplate to new folder structure

--- @angular/http.js
+++ @angular/http.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.1.0-beta.0-25132bf
+ * @license Angular v4.1.0-beta.0-c6917d9
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2038,7 +2038,7 @@ JsonpModule.ctorParameters = () => [];
 /**
  * \@stable
  */
-const VERSION = new Version('4.1.0-beta.0-25132bf');
+const VERSION = new Version('4.1.0-beta.0-c6917d9');
 
 /**
  * @license
