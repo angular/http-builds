d667be561aaca58c4d160a58eb66634e61042295
Hans <hans@hansl.ca>
2017-04-10 22:18:37 +0000

    96aa236 fix(tsc-wrapped): ensure valid path separators in metadata

--- @angular/http.js
+++ @angular/http.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.1.0-beta.0-258d539
+ * @license Angular v4.1.0-beta.0-96aa236
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2038,7 +2038,7 @@ JsonpModule.ctorParameters = () => [];
 /**
  * \@stable
  */
-const VERSION = new Version('4.1.0-beta.0-258d539');
+const VERSION = new Version('4.1.0-beta.0-96aa236');
 
 /**
  * @license
