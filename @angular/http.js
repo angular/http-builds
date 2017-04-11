faa3ba8c52a4a4dc0d4f4507f6cf8c901149c36c
Hans <hans@hansl.ca>
2017-04-11 23:16:33 +0000

    3ced940 build(aio): ability to generate plunkers

--- @angular/http.js
+++ @angular/http.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.1.0-beta.0-f5a98d9
+ * @license Angular v4.1.0-beta.0-3ced940
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2038,7 +2038,7 @@ JsonpModule.ctorParameters = () => [];
 /**
  * \@stable
  */
-const VERSION = new Version('4.1.0-beta.0-f5a98d9');
+const VERSION = new Version('4.1.0-beta.0-3ced940');
 
 /**
  * @license
