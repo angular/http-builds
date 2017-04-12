fd633d14b41d37d52e17ecd235b47e76d4a3b59e
Pete Bacon Darwin <pete@bacondarwin.com>
2017-04-12 20:25:58 +0000

    909264f hr update

--- @angular/http.js
+++ @angular/http.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.1.0-beta.0-0614754
+ * @license Angular v4.1.0-beta.0-909264f
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2038,7 +2038,7 @@ JsonpModule.ctorParameters = () => [];
 /**
  * \@stable
  */
-const VERSION = new Version('4.1.0-beta.0-0614754');
+const VERSION = new Version('4.1.0-beta.0-909264f');
 
 /**
  * @license
