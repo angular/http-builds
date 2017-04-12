44861a945a756e3183be345ca1a46d83a0d42ffe
Pete Bacon Darwin <pete@bacondarwin.com>
2017-04-12 20:18:13 +0000

    0614754 docs(aio): final migration from old site

--- bundles/http-testing.umd.js
+++ bundles/http-testing.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.1.0-beta.0-8f1359d
+ * @license Angular v4.1.0-beta.0-0614754
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -10,7 +10,7 @@
 }(this, (function (exports,_angular_core,_angular_http,rxjs_ReplaySubject,rxjs_Subject,rxjs_operator_take) { 'use strict';
 
 /**
- * @license Angular v4.1.0-beta.0-8f1359d
+ * @license Angular v4.1.0-beta.0-0614754
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
