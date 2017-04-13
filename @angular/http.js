0ee614abf2b90f84884cc976be14a91bc1b5a6fc
Tobias Bosch <tbosch1009@gmail.com>
2017-04-13 19:11:11 +0000

    70b1d6d fix(tsc-wrapped): collect new expressions with no arguments (#15908)

--- @angular/http.js
+++ @angular/http.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.1.0-beta.1-9394835
+ * @license Angular v4.1.0-beta.1-70b1d6d
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2038,7 +2038,7 @@ JsonpModule.ctorParameters = () => [];
 /**
  * \@stable
  */
-const VERSION = new Version('4.1.0-beta.1-9394835');
+const VERSION = new Version('4.1.0-beta.1-70b1d6d');
 
 /**
  * @license
