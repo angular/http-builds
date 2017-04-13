3f2f982e51b4b31040fad7d2f5d38e0cfc79eaa3
Tobias Bosch <tbosch1009@gmail.com>
2017-04-13 19:12:53 +0000

    fdb3f26 refactor(compiler): cleanup (#15960)

--- @angular/http.js
+++ @angular/http.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.1.0-beta.1-14b7dfa
+ * @license Angular v4.1.0-beta.1-fdb3f26
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2038,7 +2038,7 @@ JsonpModule.ctorParameters = () => [];
 /**
  * \@stable
  */
-const VERSION = new Version('4.1.0-beta.1-14b7dfa');
+const VERSION = new Version('4.1.0-beta.1-fdb3f26');
 
 /**
  * @license
