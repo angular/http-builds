002a81d94f11bbd92466bd7c356cfae55e2ea469
Tobias Bosch <tbosch1009@gmail.com>
2017-04-14 16:18:21 +0000

    ce47d33 fix(compiler): ignore calls to unresolved symbols in metadata (#15970)

--- @angular/http.js
+++ @angular/http.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.1.0-beta.1-2e47a0d
+ * @license Angular v4.1.0-beta.1-ce47d33
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2038,7 +2038,7 @@ JsonpModule.ctorParameters = () => [];
 /**
  * \@stable
  */
-const VERSION = new Version('4.1.0-beta.1-2e47a0d');
+const VERSION = new Version('4.1.0-beta.1-ce47d33');
 
 /**
  * @license
