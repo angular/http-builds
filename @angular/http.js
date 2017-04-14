b6beae4665a1180f36c9c7a90310e51437b55078
Matias Niemelä <matias@yearofmoo.com>
2017-04-14 18:45:18 +0000

    6f3710e feat(animations): wip: introduce a wave of new animation features

--- @angular/http.js
+++ @angular/http.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.1.0-beta.1-c664486
+ * @license Angular v4.1.0-beta.1-6f3710e
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2038,7 +2038,7 @@ JsonpModule.ctorParameters = () => [];
 /**
  * \@stable
  */
-const VERSION = new Version('4.1.0-beta.1-c664486');
+const VERSION = new Version('4.1.0-beta.1-6f3710e');
 
 /**
  * @license
