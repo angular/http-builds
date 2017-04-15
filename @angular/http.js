832640fb1f54aa4a50f27db94cf5935f7e00eb70
Pete Bacon Darwin <pete@bacondarwin.com>
2017-04-15 09:44:42 +0000

    49dfc9f feat(aio): scroll bar styling

--- @angular/http.js
+++ @angular/http.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.1.0-beta.1-49d97e1
+ * @license Angular v4.1.0-beta.1-49dfc9f
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2038,7 +2038,7 @@ JsonpModule.ctorParameters = () => [];
 /**
  * \@stable
  */
-const VERSION = new Version('4.1.0-beta.1-49d97e1');
+const VERSION = new Version('4.1.0-beta.1-49dfc9f');
 
 /**
  * @license
