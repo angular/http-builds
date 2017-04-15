fbcf06f3d526c2c69331edfc85fbbd7a4d42579b
Pete Bacon Darwin <pete@bacondarwin.com>
2017-04-15 09:51:43 +0000

    16673fa build(aio): remove unused config code

--- @angular/http.js
+++ @angular/http.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.1.0-beta.1-624b5a5
+ * @license Angular v4.1.0-beta.1-16673fa
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2038,7 +2038,7 @@ JsonpModule.ctorParameters = () => [];
 /**
  * \@stable
  */
-const VERSION = new Version('4.1.0-beta.1-624b5a5');
+const VERSION = new Version('4.1.0-beta.1-16673fa');
 
 /**
  * @license
