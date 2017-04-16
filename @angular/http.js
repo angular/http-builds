f5664dfc3611eb491145354319fc4e47d7180c10
Pete Bacon Darwin <pete@bacondarwin.com>
2017-04-16 10:48:59 +0000

    11b54f6 feat(aio): edit page link styling and placement

--- @angular/http.js
+++ @angular/http.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.1.0-beta.1-4a3c66f
+ * @license Angular v4.1.0-beta.1-11b54f6
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2038,7 +2038,7 @@ JsonpModule.ctorParameters = () => [];
 /**
  * \@stable
  */
-const VERSION = new Version('4.1.0-beta.1-4a3c66f');
+const VERSION = new Version('4.1.0-beta.1-11b54f6');
 
 /**
  * @license
