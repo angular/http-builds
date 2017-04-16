06f40b6bcbf78c267789361b36d1e6de75d23955
Peter Bacon Darwin <pete@bacondarwin.com>
2017-04-16 20:59:24 +0000

    7c418a7 style(aio): add missing semi-colon

--- @angular/http.js
+++ @angular/http.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.1.0-beta.1-58f080a
+ * @license Angular v4.1.0-beta.1-7c418a7
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2038,7 +2038,7 @@ JsonpModule.ctorParameters = () => [];
 /**
  * \@stable
  */
-const VERSION = new Version('4.1.0-beta.1-58f080a');
+const VERSION = new Version('4.1.0-beta.1-7c418a7');
 
 /**
  * @license
