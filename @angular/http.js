b23277d6feb149e8176cf7b4c3fc5b99b70bccdb
Pete Bacon Darwin <pete@bacondarwin.com>
2017-04-19 09:37:58 +0000

    8fa4453 docs(aio): Testing Guide: Second matcher parameter

--- @angular/http.js
+++ @angular/http.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.1.0-beta.1-c2fca3d
+ * @license Angular v4.1.0-beta.1-8fa4453
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2038,7 +2038,7 @@ JsonpModule.ctorParameters = () => [];
 /**
  * \@stable
  */
-const VERSION = new Version('4.1.0-beta.1-c2fca3d');
+const VERSION = new Version('4.1.0-beta.1-8fa4453');
 
 /**
  * @license
