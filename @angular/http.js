034e6b7a2d30102fed4784c27a16301d3d20b0bb
Pete Bacon Darwin <pete@bacondarwin.com>
2017-04-20 09:33:56 +0000

    f902581 fix(aio): handle clicks deep within anchor tags

--- @angular/http.js
+++ @angular/http.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.1.0-beta.1-745731e
+ * @license Angular v4.1.0-beta.1-f902581
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2038,7 +2038,7 @@ JsonpModule.ctorParameters = () => [];
 /**
  * \@stable
  */
-const VERSION = new Version('4.1.0-beta.1-745731e');
+const VERSION = new Version('4.1.0-beta.1-f902581');
 
 /**
  * @license
