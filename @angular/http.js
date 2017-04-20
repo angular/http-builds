ca6b1711311fc538016971097c516392eb8f2177
Miško Hevery <misko@hevery.com>
2017-04-20 04:30:42 +0000

    745731e fix(compiler): make I18NHtmlParser provider AoT-compliant (#15980)

--- @angular/http.js
+++ @angular/http.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.1.0-beta.1-bfd5f27
+ * @license Angular v4.1.0-beta.1-745731e
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2038,7 +2038,7 @@ JsonpModule.ctorParameters = () => [];
 /**
  * \@stable
  */
-const VERSION = new Version('4.1.0-beta.1-bfd5f27');
+const VERSION = new Version('4.1.0-beta.1-745731e');
 
 /**
  * @license
