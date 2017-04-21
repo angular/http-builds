bb33a93b78bb895edda90810e84df828caffba16
Miško Hevery <misko@hevery.com>
2017-04-21 03:33:39 +0000

    9730351 build(aio): compute which examples to include when watching guide docs (#16213)

--- @angular/http.js
+++ @angular/http.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.1.0-beta.1-5461182
+ * @license Angular v4.1.0-beta.1-9730351
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2038,7 +2038,7 @@ JsonpModule.ctorParameters = () => [];
 /**
  * \@stable
  */
-const VERSION = new Version('4.1.0-beta.1-5461182');
+const VERSION = new Version('4.1.0-beta.1-9730351');
 
 /**
  * @license
