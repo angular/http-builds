1b71502ddad38718942ee5449398a23188d59f7b
Pete Bacon Darwin <pete@bacondarwin.com>
2017-04-21 15:47:23 +0000

    87ec8d1 docs(aio): remove doble title from setup

--- @angular/http.js
+++ @angular/http.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.1.0-rc.0-b0c5d21
+ * @license Angular v4.1.0-rc.0-87ec8d1
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2038,7 +2038,7 @@ JsonpModule.ctorParameters = () => [];
 /**
  * \@stable
  */
-const VERSION = new Version('4.1.0-rc.0-b0c5d21');
+const VERSION = new Version('4.1.0-rc.0-87ec8d1');
 
 /**
  * @license
