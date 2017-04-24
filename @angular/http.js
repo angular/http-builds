c46e28fa3374469ce26699c768f8ce526a50fb01
Miško Hevery <misko@hevery.com>
2017-04-24 17:14:25 +0000

    feda017 docs(aio): Fix decorator JS feature explanation (#16214)

--- @angular/http.js
+++ @angular/http.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.1.0-rc.0-2991221
+ * @license Angular v4.1.0-rc.0-feda017
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2038,7 +2038,7 @@ JsonpModule.ctorParameters = () => [];
 /**
  * \@stable
  */
-const VERSION = new Version('4.1.0-rc.0-2991221');
+const VERSION = new Version('4.1.0-rc.0-feda017');
 
 /**
  * @license
