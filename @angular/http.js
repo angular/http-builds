ae4c594f0dce8005a0461709adc7bb9b70416a2e
Pete Bacon Darwin <pete@bacondarwin.com>
2017-04-24 14:01:54 +0000

    d7719aa perf(aio): improve unit test rebuild time

--- @angular/http.js
+++ @angular/http.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.1.0-rc.0-54e587a
+ * @license Angular v4.1.0-rc.0-d7719aa
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2038,7 +2038,7 @@ JsonpModule.ctorParameters = () => [];
 /**
  * \@stable
  */
-const VERSION = new Version('4.1.0-rc.0-54e587a');
+const VERSION = new Version('4.1.0-rc.0-d7719aa');
 
 /**
  * @license
