decb1546a3996db6329a96b7460b9417db54f613
Pete Bacon Darwin <pete@bacondarwin.com>
2017-04-26 08:15:21 +0000

    710b4a3 feat(aio): layout max width and design cleanup

--- @angular/http.js
+++ @angular/http.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.1.0-rc.0-5331fc1
+ * @license Angular v4.1.0-rc.0-710b4a3
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2038,7 +2038,7 @@ JsonpModule.ctorParameters = () => [];
 /**
  * \@stable
  */
-const VERSION = new Version('4.1.0-rc.0-5331fc1');
+const VERSION = new Version('4.1.0-rc.0-710b4a3');
 
 /**
  * @license
