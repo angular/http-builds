0aadb6c6bd8ed0655ce41d03b8aa73ee57a6a102
Miško Hevery <misko@hevery.com>
2017-04-27 22:49:40 +0000

    3f46645 feat(aio): add Table of Contents (toc) component. (#16078)

--- @angular/http.js
+++ @angular/http.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.1.0-2a7f636
+ * @license Angular v4.1.0-3f46645
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2038,7 +2038,7 @@ JsonpModule.ctorParameters = () => [];
 /**
  * \@stable
  */
-const VERSION = new Version('4.1.0-2a7f636');
+const VERSION = new Version('4.1.0-3f46645');
 
 /**
  * @license
