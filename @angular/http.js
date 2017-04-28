4a7303c066bfb4c74e96b6e0612bcbc6e3806192
Miško Hevery <misko@hevery.com>
2017-04-28 22:51:11 +0000

    9384061 docs(aio): add missing word (#16328)

--- @angular/http.js
+++ @angular/http.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.1.0-951a575
+ * @license Angular v4.1.0-9384061
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2038,7 +2038,7 @@ JsonpModule.ctorParameters = () => [];
 /**
  * \@stable
  */
-const VERSION = new Version('4.1.0-951a575');
+const VERSION = new Version('4.1.0-9384061');
 
 /**
  * @license
