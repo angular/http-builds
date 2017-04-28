3c9ecb282fce37e124815358e8a1d7ad3a7a3d88
Matias Niemelä <matias@yearofmoo.com>
2017-04-28 20:09:26 +0000

    a619991 feat(animations): introduce a wave of new animation features

--- @angular/http.js
+++ @angular/http.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.1.0-ed4eaf3
+ * @license Angular v4.1.0-a619991
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2038,7 +2038,7 @@ JsonpModule.ctorParameters = () => [];
 /**
  * \@stable
  */
-const VERSION = new Version('4.1.0-ed4eaf3');
+const VERSION = new Version('4.1.0-a619991');
 
 /**
  * @license
