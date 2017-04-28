0094dfe2d6baa106d11dd00c851ef208c7d868d9
Miško Hevery <misko@hevery.com>
2017-04-28 23:31:09 +0000

    270d694 docs(common): fix API docs for NgComponentOutlet (#16411)

--- @angular/http.js
+++ @angular/http.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.1.0-a4de214
+ * @license Angular v4.1.0-270d694
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2038,7 +2038,7 @@ JsonpModule.ctorParameters = () => [];
 /**
  * \@stable
  */
-const VERSION = new Version('4.1.0-a4de214');
+const VERSION = new Version('4.1.0-270d694');
 
 /**
  * @license
