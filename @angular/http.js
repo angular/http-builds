36e22bf3ff229eae9b02c20ceaadcc4bd7661bac
Matias Niemelä <matias@yearofmoo.com>
2017-04-28 07:40:12 +0000

    ed4eaf3 feat(animations): introduce a wave of new animation features

--- @angular/http.js
+++ @angular/http.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.1.0-rc.0-46b20be
+ * @license Angular v4.1.0-ed4eaf3
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2038,7 +2038,7 @@ JsonpModule.ctorParameters = () => [];
 /**
  * \@stable
  */
-const VERSION = new Version('4.1.0-rc.0-46b20be');
+const VERSION = new Version('4.1.0-ed4eaf3');
 
 /**
  * @license
