fc693df9df1bf7761669485b45af51b2e70c56af
Matias Niemelä <matias@yearofmoo.com>
2017-05-02 23:09:01 +0000

    c946a92 refactor(compiler): simplify AOT tests

--- @angular/http.js
+++ @angular/http.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.1.0-21c96a5
+ * @license Angular v4.1.0-c946a92
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2038,7 +2038,7 @@ JsonpModule.ctorParameters = () => [];
 /**
  * \@stable
  */
-const VERSION = new Version('4.1.0-21c96a5');
+const VERSION = new Version('4.1.0-c946a92');
 
 /**
  * @license
