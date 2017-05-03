a81f833dc09ecae7c1dac0091c57af8e8cfbf655
Pete Bacon Darwin <pete@bacondarwin.com>
2017-05-03 08:18:47 +0000

    ea9d8a6 docs(aio): move pipes guide under components

--- @angular/http.js
+++ @angular/http.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.1.0-9650ff0
+ * @license Angular v4.1.0-ea9d8a6
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2038,7 +2038,7 @@ JsonpModule.ctorParameters = () => [];
 /**
  * \@stable
  */
-const VERSION = new Version('4.1.0-9650ff0');
+const VERSION = new Version('4.1.0-ea9d8a6');
 
 /**
  * @license
