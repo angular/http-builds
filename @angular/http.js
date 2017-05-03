b598a4b95abdd425f2e6fc23d7b375576c3653ce
Matias Niemelä <matias@yearofmoo.com>
2017-05-03 16:54:13 +0000

    9684d78 build: update `concurrently` to latest version

--- @angular/http.js
+++ @angular/http.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.1.0-71f5b73
+ * @license Angular v4.1.0-9684d78
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2038,7 +2038,7 @@ JsonpModule.ctorParameters = () => [];
 /**
  * \@stable
  */
-const VERSION = new Version('4.1.0-71f5b73');
+const VERSION = new Version('4.1.0-9684d78');
 
 /**
  * @license
