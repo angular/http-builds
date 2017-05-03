49d55954038ed65b8537419bf208a142721b3975
Matias Niemelä <matias@yearofmoo.com>
2017-05-03 21:03:19 +0000

    f29c6bb docs(aio): fix missing title warnings

--- @angular/http.js
+++ @angular/http.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.1.0-464701a
+ * @license Angular v4.1.0-f29c6bb
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2038,7 +2038,7 @@ JsonpModule.ctorParameters = () => [];
 /**
  * \@stable
  */
-const VERSION = new Version('4.1.0-464701a');
+const VERSION = new Version('4.1.0-f29c6bb');
 
 /**
  * @license
