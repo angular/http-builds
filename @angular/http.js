7d654b2faba522aed20ab5ee126a1c23cc009459
Matias Niemelä <matias@yearofmoo.com>
2017-05-04 20:32:18 +0000

    1092292 test(compiler-cli): add test for missingTranslation parameter

--- @angular/http.js
+++ @angular/http.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.1.0-8931e71
+ * @license Angular v4.1.0-1092292
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2038,7 +2038,7 @@ JsonpModule.ctorParameters = () => [];
 /**
  * \@stable
  */
-const VERSION = new Version('4.1.0-8931e71');
+const VERSION = new Version('4.1.0-1092292');
 
 /**
  * @license
