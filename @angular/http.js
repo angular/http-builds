47ae6349aa23aad3c6afda017c39a5452099a12d
Matias Niemelä <matias@yearofmoo.com>
2017-05-04 23:10:26 +0000

    0361052 docs(aio): remove hardcoded TOC

--- @angular/http.js
+++ @angular/http.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.2.0-beta.0-9449eff
+ * @license Angular v4.2.0-beta.0-0361052
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2038,7 +2038,7 @@ JsonpModule.ctorParameters = () => [];
 /**
  * \@stable
  */
-const VERSION = new Version('4.2.0-beta.0-9449eff');
+const VERSION = new Version('4.2.0-beta.0-0361052');
 
 /**
  * @license
