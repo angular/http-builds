51467a4a8c2dea5a49ec669519d2175160974d0a
Pete Bacon Darwin <pete@bacondarwin.com>
2017-05-04 11:46:45 +0000

    895f47a refactor(aio): remove work-around for browsers without ServiceWorker support

--- @angular/http.js
+++ @angular/http.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.1.0-aec65de
+ * @license Angular v4.1.0-895f47a
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2038,7 +2038,7 @@ JsonpModule.ctorParameters = () => [];
 /**
  * \@stable
  */
-const VERSION = new Version('4.1.0-aec65de');
+const VERSION = new Version('4.1.0-895f47a');
 
 /**
  * @license
