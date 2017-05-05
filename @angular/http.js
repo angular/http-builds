d03cdc60c31df84a02239f3bed42341268a47c49
Pete Bacon Darwin <pete@bacondarwin.com>
2017-05-05 10:05:12 +0000

    a8379a4 docs(aio): remove toc from all marketing pages

--- @angular/http.js
+++ @angular/http.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.2.0-beta.0-b7caa3e
+ * @license Angular v4.2.0-beta.0-a8379a4
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2038,7 +2038,7 @@ JsonpModule.ctorParameters = () => [];
 /**
  * \@stable
  */
-const VERSION = new Version('4.2.0-beta.0-b7caa3e');
+const VERSION = new Version('4.2.0-beta.0-a8379a4');
 
 /**
  * @license
