96f6e5f2709e0a10d62e3e427327a47ef29e8a84
Pete Bacon Darwin <pete@bacondarwin.com>
2017-05-10 14:23:47 +0000

    f148ebe docs(aio): remove warning when testing in the webpack guide

--- bundles/http.umd.js
+++ bundles/http.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.2.0-beta.0-f795f64
+ * @license Angular v4.2.0-beta.0-f148ebe
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -15,7 +15,7 @@ var __extends = (undefined && undefined.__extends) || function (d, b) {
     d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
 };
 /**
- * @license Angular v4.2.0-beta.0-f795f64
+ * @license Angular v4.2.0-beta.0-f148ebe
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2165,7 +2165,7 @@ JsonpModule.ctorParameters = function () { return []; };
 /**
  * \@stable
  */
-var VERSION = new _angular_core.Version('4.2.0-beta.0-f795f64');
+var VERSION = new _angular_core.Version('4.2.0-beta.0-f148ebe');
 
 exports.BrowserXhr = BrowserXhr;
 exports.JSONPBackend = JSONPBackend;
