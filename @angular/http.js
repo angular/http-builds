06caff2e1a57fecec463988e550d606d1efd1f63
Pete Bacon Darwin <pete@bacondarwin.com>
2017-05-10 14:25:15 +0000

    e242e20 fix(aio): style TOC scrollbar similar to other scrollbars

--- @angular/http.js
+++ @angular/http.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.2.0-beta.0-f148ebe
+ * @license Angular v4.2.0-beta.0-e242e20
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2086,7 +2086,7 @@ JsonpModule.ctorParameters = () => [];
 /**
  * \@stable
  */
-const VERSION = new Version('4.2.0-beta.0-f148ebe');
+const VERSION = new Version('4.2.0-beta.0-e242e20');
 
 /**
  * @license
