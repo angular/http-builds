3d13a5ded0dbc99dc3406bd7fb24bf1f13db70a1
Jason Aden <jasonaden@users.noreply.github.com>
2017-05-11 17:43:37 +0000

    ce1d7c4 refactor: use view engine also for `NgModuleFactory`s (#16658)

--- bundles/http-testing.umd.js
+++ bundles/http-testing.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.2.0-beta.1-b9521b5
+ * @license Angular v4.2.0-beta.1-ce1d7c4
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -10,7 +10,7 @@
 }(this, (function (exports,_angular_core,_angular_http,rxjs_ReplaySubject,rxjs_Subject,rxjs_operator_take) { 'use strict';
 
 /**
- * @license Angular v4.2.0-beta.1-b9521b5
+ * @license Angular v4.2.0-beta.1-ce1d7c4
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
