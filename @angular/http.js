8341750aa084c5384ed11d8f12b61ded9d3b2ba1
Igor Minar <iminar@google.com>
2017-05-26 08:38:08 +0000

    db3113b build(aio): externalize @angular/cli patches into a js file

--- @angular/http.js
+++ @angular/http.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.2.0-rc.0-0c691af
+ * @license Angular v4.2.0-rc.0-db3113b
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2086,7 +2086,7 @@ JsonpModule.ctorParameters = () => [];
 /**
  * \@stable
  */
-const VERSION = new Version('4.2.0-rc.0-0c691af');
+const VERSION = new Version('4.2.0-rc.0-db3113b');
 
 /**
  * @license
