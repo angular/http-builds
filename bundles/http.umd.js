afd3aea18d7630986c9796411a3614177d0ca7b5
Chuck Jazdzewski <chuckj@google.com>
2017-05-26 23:18:39 +0000

    0018acf fix(aio): remove es2015 syntax from the onerror webmaster tools debugger (#17056)

--- bundles/http.umd.js
+++ bundles/http.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.2.0-rc.1-51d7a65
+ * @license Angular v4.2.0-rc.1-0018acf
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -36,7 +36,7 @@ function __extends(d, b) {
 }
 
 /**
- * @license Angular v4.2.0-rc.1-51d7a65
+ * @license Angular v4.2.0-rc.1-0018acf
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2186,7 +2186,7 @@ JsonpModule.ctorParameters = function () { return []; };
 /**
  * \@stable
  */
-var VERSION = new _angular_core.Version('4.2.0-rc.1-51d7a65');
+var VERSION = new _angular_core.Version('4.2.0-rc.1-0018acf');
 
 exports.BrowserXhr = BrowserXhr;
 exports.JSONPBackend = JSONPBackend;
