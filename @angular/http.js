c99fa847d49f60f18ba786eaa62bfdee47cdb4b7
GitHub <noreply@github.com>
2017-05-27 19:04:19 +0000

    6220b49 build(aio): upgrade to ngo with top-level NewExpression and CallExpression prefixing (#17063)

--- @angular/http.js
+++ @angular/http.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.2.0-rc.1-aa683a7
+ * @license Angular v4.2.0-rc.1-6220b49
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2086,7 +2086,7 @@ JsonpModule.ctorParameters = () => [];
 /**
  * \@stable
  */
-const VERSION = new Version('4.2.0-rc.1-aa683a7');
+const VERSION = new Version('4.2.0-rc.1-6220b49');
 
 /**
  * @license
