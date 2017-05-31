a7338bb12c0cebf0eff8ca9240cf75baa81c4297
Victor Berchet <victor@suumit.com>
2017-05-31 21:59:47 +0000

    c20f60b fix(router): make remove trailing slash consistent with URL params

--- @angular/http.js
+++ @angular/http.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.2.0-rc.1-1408357
+ * @license Angular v4.2.0-rc.1-c20f60b
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2086,7 +2086,7 @@ JsonpModule.ctorParameters = () => [];
 /**
  * \@stable
  */
-const VERSION = new Version('4.2.0-rc.1-1408357');
+const VERSION = new Version('4.2.0-rc.1-c20f60b');
 
 /**
  * @license
