da89b0b3bd6fccf7088d09a13b72c3f2c9a32d72
Victor Berchet <victor@suumit.com>
2017-06-01 16:57:27 +0000

    47e4fca fix(aio): swap feature headers for divs to remove anchors and fix feature image resizing (#17156)

--- @angular/http.js
+++ @angular/http.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.2.0-rc.1-665e707
+ * @license Angular v4.2.0-rc.1-47e4fca
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2086,7 +2086,7 @@ JsonpModule.ctorParameters = () => [];
 /**
  * \@stable
  */
-const VERSION = new Version('4.2.0-rc.1-665e707');
+const VERSION = new Version('4.2.0-rc.1-47e4fca');
 
 /**
  * @license
