ffb9d93c4682b381e770227661126728d7b599e5
Alex Rickabaugh <alx+alxhub@alxandria.net>
2017-06-09 23:40:56 +0000

    15090a8 docs: add changelog for 4.2.1

--- @angular/http.js
+++ @angular/http.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.2.0-90b0713
+ * @license Angular v4.2.1-15090a8
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2088,7 +2088,7 @@ JsonpModule.ctorParameters = () => [];
 /**
  * \@stable
  */
-const VERSION = new Version('4.2.0-90b0713');
+const VERSION = new Version('4.2.1-15090a8');
 
 /**
  * @license
