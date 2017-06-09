bf4dadcc616467b410873a833ce6f6aaec3cce7b
Pete Bacon Darwin <pete@bacondarwin.com>
2017-06-09 13:55:54 +0000

    a0b30e5 docs(aio): remove example specific `.gitignore` files

--- bundles/http.umd.js
+++ bundles/http.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.2.0-e894f5c
+ * @license Angular v4.2.0-a0b30e5
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -36,7 +36,7 @@ function __extends(d, b) {
 }
 
 /**
- * @license Angular v4.2.0-e894f5c
+ * @license Angular v4.2.0-a0b30e5
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2186,7 +2186,7 @@ JsonpModule.ctorParameters = function () { return []; };
 /**
  * \@stable
  */
-var VERSION = new _angular_core.Version('4.2.0-e894f5c');
+var VERSION = new _angular_core.Version('4.2.0-a0b30e5');
 
 exports.BrowserXhr = BrowserXhr;
 exports.JSONPBackend = JSONPBackend;
