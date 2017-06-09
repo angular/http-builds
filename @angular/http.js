b98a31a5e7a577d761f4f11d449ff2d441da0181
Igor Minar <iminar@google.com>
2017-06-09 22:09:50 +0000

    d56b7ed docs(aio): reorganise the sidenav menu (#16934)

--- @angular/http.js
+++ @angular/http.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.2.0-c2d31fb
+ * @license Angular v4.2.0-d56b7ed
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2088,7 +2088,7 @@ JsonpModule.ctorParameters = () => [];
 /**
  * \@stable
  */
-const VERSION = new Version('4.2.0-c2d31fb');
+const VERSION = new Version('4.2.0-d56b7ed');
 
 /**
  * @license
