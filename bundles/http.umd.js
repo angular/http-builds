72631e57c565dc057e5203d6fe9292b6e6ba4fd5
Alex Rickabaugh <alx+alxhub@alxandria.net>
2017-06-12 19:40:12 +0000

    451257a fix(animations): do not treat a `0` animation state as `void`

--- bundles/http.umd.js
+++ bundles/http.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.2.1-185075d
+ * @license Angular v4.2.1-451257a
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -36,7 +36,7 @@ function __extends(d, b) {
 }
 
 /**
- * @license Angular v4.2.1-185075d
+ * @license Angular v4.2.1-451257a
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2188,7 +2188,7 @@ JsonpModule.ctorParameters = function () { return []; };
 /**
  * \@stable
  */
-var VERSION = new _angular_core.Version('4.2.1-185075d');
+var VERSION = new _angular_core.Version('4.2.1-451257a');
 
 exports.BrowserXhr = BrowserXhr;
 exports.JSONPBackend = JSONPBackend;
