300feb8060a9099b66d336bb76805af5585cab4e
Pete Bacon Darwin <pete@bacondarwin.com>
2017-06-13 13:15:23 +0000

    052331f build(aio): add staging environment

--- @angular/http.js
+++ @angular/http.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.2.2-541c9a9
+ * @license Angular v4.2.2-052331f
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2088,7 +2088,7 @@ JsonpModule.ctorParameters = () => [];
 /**
  * \@stable
  */
-const VERSION = new Version('4.2.2-541c9a9');
+const VERSION = new Version('4.2.2-052331f');
 
 /**
  * @license
