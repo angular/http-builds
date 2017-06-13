c5bedb333b034587b695d8b3084e3b8985e416c5
Alex Rickabaugh <alx+alxhub@alxandria.net>
2017-06-13 22:21:06 +0000

    d8d21c7 ci: update github templates (#17466)

--- @angular/http.js
+++ @angular/http.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.2.2-b4cd20c
+ * @license Angular v4.2.2-d8d21c7
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2088,7 +2088,7 @@ JsonpModule.ctorParameters = () => [];
 /**
  * \@stable
  */
-const VERSION = new Version('4.2.2-b4cd20c');
+const VERSION = new Version('4.2.2-d8d21c7');
 
 /**
  * @license
