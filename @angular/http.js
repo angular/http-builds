047e330bf80a85b23b62e89d14095a1173e42af3
Hans <hans@hansl.ca>
2017-06-15 22:11:09 +0000

    e096a85 fix(animations): remove duplicate license header

--- @angular/http.js
+++ @angular/http.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.2.2-53f57d7
+ * @license Angular v4.2.2-e096a85
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2088,7 +2088,7 @@ JsonpModule.ctorParameters = () => [];
 /**
  * \@stable
  */
-const VERSION = new Version('4.2.2-53f57d7');
+const VERSION = new Version('4.2.2-e096a85');
 
 /**
  * @license
