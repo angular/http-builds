19f71ada24e1ddbe135416443c11898043bb3dd4
Pete Bacon Darwin <pete@bacondarwin.com>
2017-06-15 07:08:37 +0000

    ba0f6de build(aio): extra redirect rule

--- @angular/http.js
+++ @angular/http.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.2.2-01173b9
+ * @license Angular v4.2.2-ba0f6de
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2088,7 +2088,7 @@ JsonpModule.ctorParameters = () => [];
 /**
  * \@stable
  */
-const VERSION = new Version('4.2.2-01173b9');
+const VERSION = new Version('4.2.2-ba0f6de');
 
 /**
  * @license
