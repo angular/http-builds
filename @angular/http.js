546027efd8aa0118c9bb420ed568237b87b2c715
Hans <hans@hansl.ca>
2017-06-16 19:34:30 +0000

    f58211d fix(aio): improve no-javascript screen

--- @angular/http.js
+++ @angular/http.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.2.2-9315ab8
+ * @license Angular v4.2.2-f58211d
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2088,7 +2088,7 @@ JsonpModule.ctorParameters = () => [];
 /**
  * \@stable
  */
-const VERSION = new Version('4.2.2-9315ab8');
+const VERSION = new Version('4.2.2-f58211d');
 
 /**
  * @license
