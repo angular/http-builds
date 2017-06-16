5420bfb6f3c6b8da38733a4935d87bc8b2b88661
Hans <hans@hansl.ca>
2017-06-16 19:40:08 +0000

    5ac3919 feat(core): update zone.js to 0.8.12

--- @angular/http.js
+++ @angular/http.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.2.2-fe126cb
+ * @license Angular v4.2.2-5ac3919
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2088,7 +2088,7 @@ JsonpModule.ctorParameters = () => [];
 /**
  * \@stable
  */
-const VERSION = new Version('4.2.2-fe126cb');
+const VERSION = new Version('4.2.2-5ac3919');
 
 /**
  * @license
