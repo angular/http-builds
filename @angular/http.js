a258f0fbf99d593b6255e6629c876bff908ca336
Hans <hans@hansl.ca>
2017-06-16 21:06:46 +0000

    3093c55 build(aio): update @angular/service-worker to 1.0.0-beta.16

--- @angular/http.js
+++ @angular/http.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.2.2-5ac3919
+ * @license Angular v4.2.2-3093c55
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2088,7 +2088,7 @@ JsonpModule.ctorParameters = () => [];
 /**
  * \@stable
  */
-const VERSION = new Version('4.2.2-5ac3919');
+const VERSION = new Version('4.2.2-3093c55');
 
 /**
  * @license
