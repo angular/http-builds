e8a818a1af30e84eba1a3d580f568dd055613c88
Hans <hans@hansl.ca>
2017-06-19 22:18:50 +0000

    319ce18 docs(RELEASE_SCHEDULE): fix version numbers for August/September releases

--- @angular/http.js
+++ @angular/http.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.2.2-aa92f3a
+ * @license Angular v4.2.2-319ce18
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2088,7 +2088,7 @@ JsonpModule.ctorParameters = () => [];
 /**
  * \@stable
  */
-const VERSION = new Version('4.2.2-aa92f3a');
+const VERSION = new Version('4.2.2-319ce18');
 
 /**
  * @license
