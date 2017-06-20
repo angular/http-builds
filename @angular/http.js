2f133772f31268d26a3754cbb95dc669c1ff8979
Hans <hans@hansl.ca>
2017-06-20 21:38:48 +0000

    8c89cc4 fix(compiler-cli): find lazy routes in nested module import arrays

--- @angular/http.js
+++ @angular/http.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.2.2-00874c2
+ * @license Angular v4.2.2-8c89cc4
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2088,7 +2088,7 @@ JsonpModule.ctorParameters = () => [];
 /**
  * \@stable
  */
-const VERSION = new Version('4.2.2-00874c2');
+const VERSION = new Version('4.2.2-8c89cc4');
 
 /**
  * @license
