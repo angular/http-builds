fc92f41c8c03c9e8a76e461c3bf2e8036b532eea
Hans <hans@hansl.ca>
2017-06-20 20:29:46 +0000

    00874c2 docs(aio): update resource listing

--- @angular/http.es5.js
+++ @angular/http.es5.js
@@ -1,6 +1,6 @@
 import * as tslib_1 from "tslib";
 /**
- * @license Angular v4.2.2-009651e
+ * @license Angular v4.2.2-00874c2
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2155,7 +2155,7 @@ JsonpModule.ctorParameters = function () { return []; };
 /**
  * \@stable
  */
-var VERSION = new Version('4.2.2-009651e');
+var VERSION = new Version('4.2.2-00874c2');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
