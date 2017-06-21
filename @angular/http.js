61de5fe4aab90c7a840c3ac00b6e7f3af83756b8
Hans Larsen <hans@hansl.ca>
2017-06-21 23:52:43 +0000

    c112232 fix(compiler): avoid emitting self importing factories

--- @angular/http.js
+++ @angular/http.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.2.3-3634832
+ * @license Angular v4.2.3-c112232
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2088,7 +2088,7 @@ JsonpModule.ctorParameters = () => [];
 /**
  * \@stable
  */
-const VERSION = new Version('4.2.3-3634832');
+const VERSION = new Version('4.2.3-c112232');
 
 /**
  * @license
