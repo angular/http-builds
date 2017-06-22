1f4eb9f1dd75dec42beed912304bcf811b296572
Matias Niemelä <matias@yearofmoo.com>
2017-06-22 22:06:36 +0000

    8c129d7 docs: Fix some non-code being formatted as code in the router docs

--- @angular/http.js
+++ @angular/http.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.3.0-beta.0-5c576d3
+ * @license Angular v4.3.0-beta.0-8c129d7
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2088,7 +2088,7 @@ JsonpModule.ctorParameters = () => [];
 /**
  * \@stable
  */
-const VERSION = new Version('4.3.0-beta.0-5c576d3');
+const VERSION = new Version('4.3.0-beta.0-8c129d7');
 
 /**
  * @license
