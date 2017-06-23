bdc340b84d337ccb42abdd1e48823d96849ae25e
Matias Niemelä <matias@yearofmoo.com>
2017-06-23 22:14:27 +0000

    81734cf refactor(compiler): add tracking comment for self-import detection

--- @angular/http.js
+++ @angular/http.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.3.0-beta.0-b754e60
+ * @license Angular v4.3.0-beta.0-81734cf
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2088,7 +2088,7 @@ JsonpModule.ctorParameters = () => [];
 /**
  * \@stable
  */
-const VERSION = new Version('4.3.0-beta.0-b754e60');
+const VERSION = new Version('4.3.0-beta.0-81734cf');
 
 /**
  * @license
