c1fcee6c72a64251e63076d74e8b014fb55a9a3e
Matias Niemelä <matias@yearofmoo.com>
2017-06-23 16:42:52 +0000

    eacc36b docs(aio): update Bash for Windows info

--- @angular/http.js
+++ @angular/http.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.3.0-beta.0-8b4acbb
+ * @license Angular v4.3.0-beta.0-eacc36b
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2088,7 +2088,7 @@ JsonpModule.ctorParameters = () => [];
 /**
  * \@stable
  */
-const VERSION = new Version('4.3.0-beta.0-8b4acbb');
+const VERSION = new Version('4.3.0-beta.0-eacc36b');
 
 /**
  * @license
