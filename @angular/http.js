7bc04f23c50896d02c7f079a6cc1ee53c630e29f
Matias Niemelä <matias@yearofmoo.com>
2017-06-23 23:40:52 +0000

    43c187b refactor(forms): remove usage of deprecated Renderer

--- @angular/http.js
+++ @angular/http.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.3.0-beta.0-3097083
+ * @license Angular v4.3.0-beta.0-43c187b
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2088,7 +2088,7 @@ JsonpModule.ctorParameters = () => [];
 /**
  * \@stable
  */
-const VERSION = new Version('4.3.0-beta.0-3097083');
+const VERSION = new Version('4.3.0-beta.0-43c187b');
 
 /**
  * @license
