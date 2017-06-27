895817093a3ff791529f26efe5173a133ffcb7e9
Matias Niemelä <matias@yearofmoo.com>
2017-06-27 18:30:10 +0000

    34f3832 fix(animations): do not delay style() values before a stagger() runs

--- @angular/http.js
+++ @angular/http.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.3.0-beta.0-f162657
+ * @license Angular v4.3.0-beta.0-34f3832
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2088,7 +2088,7 @@ JsonpModule.ctorParameters = () => [];
 /**
  * \@stable
  */
-const VERSION = new Version('4.3.0-beta.0-f162657');
+const VERSION = new Version('4.3.0-beta.0-34f3832');
 
 /**
  * @license
