01def6c6eb2d93ada72b2ce0cf084ed0605480c2
Matias Niemelä <matias@yearofmoo.com>
2017-06-30 00:36:45 +0000

    40921bb docs: add changelog for 4.2.5

--- @angular/http.js
+++ @angular/http.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.3.0-beta.1-dfcca66
+ * @license Angular v4.3.0-beta.1-40921bb
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2088,7 +2088,7 @@ JsonpModule.ctorParameters = () => [];
 /**
  * \@stable
  */
-const VERSION = new Version('4.3.0-beta.1-dfcca66');
+const VERSION = new Version('4.3.0-beta.1-40921bb');
 
 /**
  * @license
