698ab8e36870314e7b360777e62c906881c5709f
Pete Bacon Darwin <pete@bacondarwin.com>
2017-07-01 14:11:46 +0000

    d5dc53e ci(aio): avoid printing too large PWA report (and restore `print-logs.sh`)

--- @angular/http.js
+++ @angular/http.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.3.0-beta.1-8e00161
+ * @license Angular v4.3.0-beta.1-d5dc53e
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2088,7 +2088,7 @@ JsonpModule.ctorParameters = () => [];
 /**
  * \@stable
  */
-const VERSION = new Version('4.3.0-beta.1-8e00161');
+const VERSION = new Version('4.3.0-beta.1-d5dc53e');
 
 /**
  * @license
