dde2f285007da1e7d69549bfdf614e3e3bd3fc98
Peter Bacon Darwin <pete@bacondarwin.com>
2017-07-03 07:56:27 +0000

    ed45830 docs(aio): update quickstart to latest cli

--- @angular/http.js
+++ @angular/http.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.2.5-37b2000
+ * @license Angular v4.2.5-ed45830
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2088,7 +2088,7 @@ JsonpModule.ctorParameters = () => [];
 /**
  * \@stable
  */
-const VERSION = new Version('4.2.5-37b2000');
+const VERSION = new Version('4.2.5-ed45830');
 
 /**
  * @license
