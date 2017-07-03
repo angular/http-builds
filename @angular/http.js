fa0aae91f131a1526d4790d7d1f2581cd52c0252
Peter Bacon Darwin <pete@bacondarwin.com>
2017-07-03 10:09:16 +0000

    408bb89 ci(aio): Fix the payload script only check for changes in aio/scripts

--- @angular/http.js
+++ @angular/http.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.2.5-ed45830
+ * @license Angular v4.2.5-408bb89
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2088,7 +2088,7 @@ JsonpModule.ctorParameters = () => [];
 /**
  * \@stable
  */
-const VERSION = new Version('4.2.5-ed45830');
+const VERSION = new Version('4.2.5-408bb89');
 
 /**
  * @license
