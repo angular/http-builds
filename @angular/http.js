41bc4d33c21afbe3b7d7537714ff40c2a64ab8ea
Jason Aden <jasonaden@users.noreply.github.com>
2017-07-06 19:30:33 +0000

    70981c6 feat(aio): serve-and-sync command (#17850)

--- @angular/http.js
+++ @angular/http.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.3.0-beta.1-b116901
+ * @license Angular v4.3.0-beta.1-70981c6
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2088,7 +2088,7 @@ JsonpModule.ctorParameters = () => [];
 /**
  * \@stable
  */
-const VERSION = new Version('4.3.0-beta.1-b116901');
+const VERSION = new Version('4.3.0-beta.1-70981c6');
 
 /**
  * @license
