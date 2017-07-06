873e9cfaaf717c26cfa651d4f6462d6588700b23
Jason Aden <jasonaden@users.noreply.github.com>
2017-07-06 18:32:22 +0000

    1bb2476 docs(aio): add instructions for example e2e tests to README (#17819)

--- @angular/http.js
+++ @angular/http.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.3.0-beta.1-6fc5174
+ * @license Angular v4.3.0-beta.1-1bb2476
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2088,7 +2088,7 @@ JsonpModule.ctorParameters = () => [];
 /**
  * \@stable
  */
-const VERSION = new Version('4.3.0-beta.1-6fc5174');
+const VERSION = new Version('4.3.0-beta.1-1bb2476');
 
 /**
  * @license
