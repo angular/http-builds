c10d399730a3eb17cfab28ecbb30d57ec1cf9136
Jason Aden <jasonaden@angular.io>
2017-07-07 19:33:59 +0000

    37797e2 feat(common): new HttpClient API

--- @angular/http.js
+++ @angular/http.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.3.0-beta.1-72747e5
+ * @license Angular v4.3.0-beta.1-37797e2
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2088,7 +2088,7 @@ JsonpModule.ctorParameters = () => [];
 /**
  * \@stable
  */
-const VERSION = new Version('4.3.0-beta.1-72747e5');
+const VERSION = new Version('4.3.0-beta.1-37797e2');
 
 /**
  * @license
