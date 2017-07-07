9e698355212c1588df99829e4d94059bbcd3a847
Jason Aden <jasonaden@users.noreply.github.com>
2017-07-07 23:50:50 +0000

    671a175 fix(compiler-cli): fix relative source paths on windows for extracted msg (#17915)

--- @angular/http.js
+++ @angular/http.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.3.0-beta.1-9c3386b
+ * @license Angular v4.3.0-beta.1-671a175
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2088,7 +2088,7 @@ JsonpModule.ctorParameters = () => [];
 /**
  * \@stable
  */
-const VERSION = new Version('4.3.0-beta.1-9c3386b');
+const VERSION = new Version('4.3.0-beta.1-671a175');
 
 /**
  * @license
