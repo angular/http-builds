b79babdbfff039c17a1cce17d31ca082321583cb
Jason Aden <jasonaden@users.noreply.github.com>
2017-07-07 23:48:48 +0000

    2ba3ada revert: "refactor(compiler-cli): remove the dependency on fs in codegen.ts (#17738)"

--- @angular/http.js
+++ @angular/http.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.3.0-beta.1-c1474f3
+ * @license Angular v4.3.0-beta.1-2ba3ada
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2088,7 +2088,7 @@ JsonpModule.ctorParameters = () => [];
 /**
  * \@stable
  */
-const VERSION = new Version('4.3.0-beta.1-c1474f3');
+const VERSION = new Version('4.3.0-beta.1-2ba3ada');
 
 /**
  * @license
