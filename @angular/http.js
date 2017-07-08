ccde9a08e6aec1ef64c943397dafdf47fb71708e
GitHub <noreply@github.com>
2017-07-08 00:33:37 +0000

    09f1609 style(http): fix linting error in http/testing (#18002)

--- @angular/http.js
+++ @angular/http.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.3.0-beta.1-c723d42
+ * @license Angular v4.3.0-beta.1-09f1609
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2088,7 +2088,7 @@ JsonpModule.ctorParameters = () => [];
 /**
  * \@stable
  */
-const VERSION = new Version('4.3.0-beta.1-c723d42');
+const VERSION = new Version('4.3.0-beta.1-09f1609');
 
 /**
  * @license
