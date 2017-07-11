b7ed0a905d413dc987b73186cd0497087307d2c6
Peter Bacon Darwin <pete@bacondarwin.com>
2017-07-11 14:33:32 +0000

    dbfd717 build(aio): add terms from heading to the search index

--- @angular/http.js
+++ @angular/http.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.2.6-3f29348
+ * @license Angular v4.2.6-dbfd717
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2088,7 +2088,7 @@ JsonpModule.ctorParameters = () => [];
 /**
  * \@stable
  */
-const VERSION = new Version('4.2.6-3f29348');
+const VERSION = new Version('4.2.6-dbfd717');
 
 /**
  * @license
