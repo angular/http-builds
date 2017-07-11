b0ffafb2d9ab428c76dc2caa11215b4b31e8d717
GitHub <noreply@github.com>
2017-07-11 23:17:31 +0000

    edb8375 docs: adjust the release schedule with the 4.3.0-rc.0 release date

--- @angular/http.js
+++ @angular/http.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.3.0-rc.0-e110a80
+ * @license Angular v4.3.0-rc.0-edb8375
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2088,7 +2088,7 @@ JsonpModule.ctorParameters = () => [];
 /**
  * \@stable
  */
-const VERSION = new Version('4.3.0-rc.0-e110a80');
+const VERSION = new Version('4.3.0-rc.0-edb8375');
 
 /**
  * @license
