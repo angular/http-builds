931d3f93bc67be3f017a9be283fb184a4e2d26dd
Igor Minar <iminar@google.com>
2017-07-11 20:13:59 +0000

    7c479f0 docs: document using Bazel (#17701)

--- @angular/http.js
+++ @angular/http.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.3.0-rc.0-bbf2133
+ * @license Angular v4.3.0-rc.0-7c479f0
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2088,7 +2088,7 @@ JsonpModule.ctorParameters = () => [];
 /**
  * \@stable
  */
-const VERSION = new Version('4.3.0-rc.0-bbf2133');
+const VERSION = new Version('4.3.0-rc.0-7c479f0');
 
 /**
  * @license
