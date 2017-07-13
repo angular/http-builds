93fb0d5f9e4ba7fc6d9c43e1a5901b5ee44f2a98
Igor Minar <iminar@google.com>
2017-07-13 22:28:48 +0000

    db96c96 fix(aio): add <hr> in between overloads

--- @angular/http.js
+++ @angular/http.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.3.0-rc.0-ce0f4f0
+ * @license Angular v4.3.0-rc.0-db96c96
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2088,7 +2088,7 @@ JsonpModule.ctorParameters = () => [];
 /**
  * \@stable
  */
-const VERSION = new Version('4.3.0-rc.0-ce0f4f0');
+const VERSION = new Version('4.3.0-rc.0-db96c96');
 
 /**
  * @license
