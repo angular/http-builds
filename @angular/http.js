c22d0932c9b4d0b8f8e5d2eea200c0d3432a96bb
Alex Rickabaugh <alx+alxhub@alxandria.net>
2017-07-14 20:02:29 +0000

    3ecc5e5 fix(common): rename HttpXsrfModule to HttpClientXsrfModule

--- @angular/http.js
+++ @angular/http.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.3.0-rc.0-f7686d4
+ * @license Angular v4.3.0-rc.0-3ecc5e5
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2088,7 +2088,7 @@ JsonpModule.ctorParameters = () => [];
 /**
  * \@stable
  */
-const VERSION = new Version('4.3.0-rc.0-f7686d4');
+const VERSION = new Version('4.3.0-rc.0-3ecc5e5');
 
 /**
  * @license
