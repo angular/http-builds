dad0f0727eff11a5c0d6fd57c6f479a7b56fea16
Alex Rickabaugh <alx+alxhub@alxandria.net>
2017-07-14 20:39:47 +0000

    a0b06be docs: add changelog for 4.3.0

--- @angular/http.js
+++ @angular/http.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.3.0-rc.0-e0fa727
+ * @license Angular v4.3.0-a0b06be
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2088,7 +2088,7 @@ JsonpModule.ctorParameters = () => [];
 /**
  * \@stable
  */
-const VERSION = new Version('4.3.0-rc.0-e0fa727');
+const VERSION = new Version('4.3.0-a0b06be');
 
 /**
  * @license
