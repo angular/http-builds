232b20735d4955bc717a61ab10e536dd11e06196
Alex Rickabaugh <alx+alxhub@alxandria.net>
2017-07-18 18:20:39 +0000

    59c23c7 feat(upgrade): propagate touched state of NgModelController

--- @angular/http.js
+++ @angular/http.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.3.0-3b588fe
+ * @license Angular v4.3.0-59c23c7
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2088,7 +2088,7 @@ JsonpModule.ctorParameters = () => [];
 /**
  * \@stable
  */
-const VERSION = new Version('4.3.0-3b588fe');
+const VERSION = new Version('4.3.0-59c23c7');
 
 /**
  * @license
