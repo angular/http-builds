ff8b67ed93998e0ef229de6d35b16c20cf51eab9
Alex Rickabaugh <alx+alxhub@alxandria.net>
2017-07-18 18:40:17 +0000

    5c62e30 fix(common): send flushed body as error instead of null

--- @angular/http.js
+++ @angular/http.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.3.0-256bc8a
+ * @license Angular v4.3.0-5c62e30
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2088,7 +2088,7 @@ JsonpModule.ctorParameters = () => [];
 /**
  * \@stable
  */
-const VERSION = new Version('4.3.0-256bc8a');
+const VERSION = new Version('4.3.0-5c62e30');
 
 /**
  * @license
