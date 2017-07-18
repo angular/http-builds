92ae9260e75d58434a9ea18faf5b2cd51f54757d
Alex Rickabaugh <alx+alxhub@alxandria.net>
2017-07-18 18:00:23 +0000

    95635c1 fix(compiler): ensure jit external id arguments names are unique

--- @angular/http.js
+++ @angular/http.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.3.0-e20cfe1
+ * @license Angular v4.3.0-95635c1
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2088,7 +2088,7 @@ JsonpModule.ctorParameters = () => [];
 /**
  * \@stable
  */
-const VERSION = new Version('4.3.0-e20cfe1');
+const VERSION = new Version('4.3.0-95635c1');
 
 /**
  * @license
