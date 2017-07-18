fc4c1784d31107b24befe5a42ee30649e12f3a79
Alex Rickabaugh <alx+alxhub@alxandria.net>
2017-07-18 18:18:07 +0000

    3b588fe docs: fix typo

--- @angular/http.js
+++ @angular/http.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.3.0-e03adb9
+ * @license Angular v4.3.0-3b588fe
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2088,7 +2088,7 @@ JsonpModule.ctorParameters = () => [];
 /**
  * \@stable
  */
-const VERSION = new Version('4.3.0-e03adb9');
+const VERSION = new Version('4.3.0-3b588fe');
 
 /**
  * @license
