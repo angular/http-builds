febca5dbb377fc4bdb60a513c0ad8ff9f80547f9
Miško Hevery <misko@hevery.com>
2017-07-21 18:34:26 +0000

    abee785 refactor(tsc-wrapped): update tsc-wrapped to pass strictNullCheck (#18160)

--- @angular/http.js
+++ @angular/http.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.0-beta.0-e0a9625
+ * @license Angular v5.0.0-beta.0-abee785
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2088,7 +2088,7 @@ JsonpModule.ctorParameters = () => [];
 /**
  * \@stable
  */
-const VERSION = new Version('5.0.0-beta.0-e0a9625');
+const VERSION = new Version('5.0.0-beta.0-abee785');
 
 /**
  * @license
