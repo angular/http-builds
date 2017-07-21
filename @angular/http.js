f18072891c92978f2b3b47f8981b2043187b6630
Miško Hevery <misko@hevery.com>
2017-07-21 17:48:16 +0000

    e0a9625 test: fix bad merge (#18267)

--- @angular/http.js
+++ @angular/http.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.0-beta.0-a6c635e
+ * @license Angular v5.0.0-beta.0-e0a9625
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2088,7 +2088,7 @@ JsonpModule.ctorParameters = () => [];
 /**
  * \@stable
  */
-const VERSION = new Version('5.0.0-beta.0-a6c635e');
+const VERSION = new Version('5.0.0-beta.0-e0a9625');
 
 /**
  * @license
