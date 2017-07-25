67e547bf52d1f97be1715f45dadc4edad752fabd
Miško Hevery <misko@hevery.com>
2017-07-25 22:54:51 +0000

    91ab39c docs(aio) - Fixed link to the glossary dash-case term (#18311)

--- @angular/http.js
+++ @angular/http.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.0-beta.0-6279e50
+ * @license Angular v5.0.0-beta.0-91ab39c
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2126,7 +2126,7 @@ JsonpModule.ctorParameters = () => [];
 /**
  * \@stable
  */
-const VERSION = new Version('5.0.0-beta.0-6279e50');
+const VERSION = new Version('5.0.0-beta.0-91ab39c');
 
 /**
  * @fileoverview added by tsickle
