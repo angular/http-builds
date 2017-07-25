4d69bb17c0bd9b05bed9978e03e54ff0a6dbfa16
Miško Hevery <misko@hevery.com>
2017-07-25 23:11:28 +0000

    b582e2b docs(aio): update examples to 4.3

--- bundles/http.umd.js
+++ bundles/http.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.0-beta.0-91ab39c
+ * @license Angular v5.0.0-beta.0-b582e2b
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -36,7 +36,7 @@ function __extends(d, b) {
 }
 
 /**
- * @license Angular v5.0.0-beta.0-91ab39c
+ * @license Angular v5.0.0-beta.0-b582e2b
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2226,7 +2226,7 @@ JsonpModule.ctorParameters = function () { return []; };
 /**
  * \@stable
  */
-var VERSION = new _angular_core.Version('5.0.0-beta.0-91ab39c');
+var VERSION = new _angular_core.Version('5.0.0-beta.0-b582e2b');
 
 exports.BrowserXhr = BrowserXhr;
 exports.JSONPBackend = JSONPBackend;
