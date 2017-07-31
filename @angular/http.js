7b1ebd14a77215927159d3ddd13a87965ed7a521
Alex Rickabaugh <alx+alxhub@alxandria.net>
2017-07-31 19:57:54 +0000

    a11542a docs(aio): fixed list format in FormArray section

--- @angular/http.js
+++ @angular/http.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.0-beta.1-b6c4af6
+ * @license Angular v5.0.0-beta.1-a11542a
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2126,7 +2126,7 @@ JsonpModule.ctorParameters = () => [];
 /**
  * \@stable
  */
-const VERSION = new Version('5.0.0-beta.1-b6c4af6');
+const VERSION = new Version('5.0.0-beta.1-a11542a');
 
 /**
  * @fileoverview added by tsickle
