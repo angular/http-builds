86dab4750552c6e2b3cf2fe9d5b87eea1965d481
Victor Berchet <victor@suumit.com>
2017-08-02 23:11:19 +0000

    9adf40a build(aio): use cli 1.3.0-rc (#18290)

--- @angular/http.js
+++ @angular/http.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.0-beta.2-89c6161
+ * @license Angular v5.0.0-beta.2-9adf40a
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2126,7 +2126,7 @@ JsonpModule.ctorParameters = () => [];
 /**
  * \@stable
  */
-const VERSION = new Version('5.0.0-beta.2-89c6161');
+const VERSION = new Version('5.0.0-beta.2-9adf40a');
 
 /**
  * @fileoverview added by tsickle
