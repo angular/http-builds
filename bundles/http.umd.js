3c4a1720e0bba6a5beea7b3851b8cb6c66a828ee
Victor Berchet <victor@suumit.com>
2017-08-03 01:37:11 +0000

    333a708 feat(forms): add updateOn blur option to FormControls (#18408)

--- bundles/http.umd.js
+++ bundles/http.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.0-beta.2-3a227a1
+ * @license Angular v5.0.0-beta.2-333a708
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -36,7 +36,7 @@ function __extends(d, b) {
 }
 
 /**
- * @license Angular v5.0.0-beta.2-3a227a1
+ * @license Angular v5.0.0-beta.2-333a708
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2226,7 +2226,7 @@ JsonpModule.ctorParameters = function () { return []; };
 /**
  * \@stable
  */
-var VERSION = new _angular_core.Version('5.0.0-beta.2-3a227a1');
+var VERSION = new _angular_core.Version('5.0.0-beta.2-333a708');
 
 exports.BrowserXhr = BrowserXhr;
 exports.JSONPBackend = JSONPBackend;
