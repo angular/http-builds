8a06363bb5ac3240b20ce95c65ced5a806ce3a4f
Victor Berchet <victor@suumit.com>
2017-08-03 21:31:04 +0000

    c7b72aa docs(aio): tech edits to form validation (PR #18495)

--- @angular/http.js
+++ @angular/http.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.3.3-e7e7622
+ * @license Angular v4.3.3-c7b72aa
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2088,7 +2088,7 @@ JsonpModule.ctorParameters = () => [];
 /**
  * \@stable
  */
-const VERSION = new Version('4.3.3-e7e7622');
+const VERSION = new Version('4.3.3-c7b72aa');
 
 /**
  * @license
--- bundles/http.umd.js
+++ bundles/http.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.3.3-e7e7622
+ * @license Angular v4.3.3-c7b72aa
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -36,7 +36,7 @@ function __extends(d, b) {
 }
 
 /**
- * @license Angular v4.3.3-e7e7622
+ * @license Angular v4.3.3-c7b72aa
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2188,7 +2188,7 @@ JsonpModule.ctorParameters = function () { return []; };
 /**
  * \@stable
  */
-var VERSION = new _angular_core.Version('4.3.3-e7e7622');
+var VERSION = new _angular_core.Version('4.3.3-c7b72aa');
 
 exports.BrowserXhr = BrowserXhr;
 exports.JSONPBackend = JSONPBackend;
