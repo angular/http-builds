b9ac74207474774c19ba4bd1278943643bf83867
Victor Berchet <victor@suumit.com>
2017-08-03 21:27:06 +0000

    9b015a9 docs(aio): tech edits to form validation

--- @angular/http.js
+++ @angular/http.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.0-beta.2-5651e4a
+ * @license Angular v5.0.0-beta.2-9b015a9
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2126,7 +2126,7 @@ JsonpModule.ctorParameters = () => [];
 /**
  * \@stable
  */
-const VERSION = new Version('5.0.0-beta.2-5651e4a');
+const VERSION = new Version('5.0.0-beta.2-9b015a9');
 
 /**
  * @fileoverview added by tsickle
