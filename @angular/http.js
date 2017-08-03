24569b839d4b60ec2aab27676b549ec63e4d0026
Victor Berchet <victor@suumit.com>
2017-08-03 00:08:59 +0000

    65d0888 fix(aio): fix layout of the webpack guide (#18493)

--- @angular/http.js
+++ @angular/http.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.3.3-afe3393
+ * @license Angular v4.3.3-65d0888
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2088,7 +2088,7 @@ JsonpModule.ctorParameters = () => [];
 /**
  * \@stable
  */
-const VERSION = new Version('4.3.3-afe3393');
+const VERSION = new Version('4.3.3-65d0888');
 
 /**
  * @license
