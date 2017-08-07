0ce48c207c1a0b4326e24fb6b0462e5a5980d397
Victor Berchet <victor@suumit.com>
2017-08-07 22:00:39 +0000

    685cc26 fix(common): don't recreate view when context shape doesn't change (#18277)

--- @angular/http.es5.js
+++ @angular/http.es5.js
@@ -1,6 +1,6 @@
 import * as tslib_1 from "tslib";
 /**
- * @license Angular v5.0.0-beta.2-5b7432b
+ * @license Angular v5.0.0-beta.2-685cc26
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2193,7 +2193,7 @@ JsonpModule.ctorParameters = function () { return []; };
 /**
  * \@stable
  */
-var VERSION = new Version('5.0.0-beta.2-5b7432b');
+var VERSION = new Version('5.0.0-beta.2-685cc26');
 /**
  * @fileoverview added by tsickle
  * @suppress {checkTypes} checked by tsc
