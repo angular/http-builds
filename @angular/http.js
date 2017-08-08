5e95368e0976ff9d5fe7bf4906319db83c996e9d
Victor Berchet <victor@suumit.com>
2017-08-08 21:28:48 +0000

    1e18331 ci(aio): fix deploying to firebase (#18590)

--- @angular/http.js
+++ @angular/http.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.0-beta.2-6f2038c
+ * @license Angular v5.0.0-beta.2-1e18331
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2126,7 +2126,7 @@ JsonpModule.ctorParameters = () => [];
 /**
  * \@stable
  */
-const VERSION = new Version('5.0.0-beta.2-6f2038c');
+const VERSION = new Version('5.0.0-beta.2-1e18331');
 
 /**
  * @fileoverview added by tsickle
