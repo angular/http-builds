34442f8a11d35393b002a71c1181b995b72c0b2e
Victor Berchet <victor@suumit.com>
2017-08-09 22:03:11 +0000

    7f2037f test(aio): fix running docs examples against local builds (#18520)

--- @angular/http.js
+++ @angular/http.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.0-beta.2-fd6ae57
+ * @license Angular v5.0.0-beta.2-7f2037f
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2126,7 +2126,7 @@ JsonpModule.ctorParameters = () => [];
 /**
  * \@stable
  */
-const VERSION = new Version('5.0.0-beta.2-fd6ae57');
+const VERSION = new Version('5.0.0-beta.2-7f2037f');
 
 /**
  * @fileoverview added by tsickle
