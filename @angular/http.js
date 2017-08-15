c7143ae1d19bb3df7d86e1e8f1121ca26ae669d6
Hans <hans@hansl.ca>
2017-08-15 23:57:46 +0000

    77747e1 refactor(core): remove toString() method from DefaultIterableDiffer

--- @angular/http.js
+++ @angular/http.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.0-beta.3-cce2ab2
+ * @license Angular v5.0.0-beta.3-77747e1
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2126,7 +2126,7 @@ JsonpModule.ctorParameters = () => [];
 /**
  * \@stable
  */
-const VERSION = new Version('5.0.0-beta.3-cce2ab2');
+const VERSION = new Version('5.0.0-beta.3-77747e1');
 
 /**
  * @fileoverview added by tsickle
