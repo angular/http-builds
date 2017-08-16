528f176f5ca9f15e8e6b1af593b4487a93d02c94
Hans <hans@hansl.ca>
2017-08-16 22:53:22 +0000

    43226cb feat(compiler): use typescript for resolving resource paths

--- @angular/http.js
+++ @angular/http.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.0-beta.4-9aa0521
+ * @license Angular v5.0.0-beta.4-43226cb
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2126,7 +2126,7 @@ JsonpModule.ctorParameters = () => [];
 /**
  * \@stable
  */
-const VERSION = new Version('5.0.0-beta.4-9aa0521');
+const VERSION = new Version('5.0.0-beta.4-43226cb');
 
 /**
  * @fileoverview added by tsickle
