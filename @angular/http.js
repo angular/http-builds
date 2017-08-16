39ed8dedcec6eaca3d83c39ec7880e7c59df7907
Hans <hans@hansl.ca>
2017-08-16 18:53:49 +0000

    bc22ff1 fix(language-service): remove tsickle dependency

--- @angular/http.js
+++ @angular/http.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.0-beta.3-75d484e
+ * @license Angular v5.0.0-beta.3-bc22ff1
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2126,7 +2126,7 @@ JsonpModule.ctorParameters = () => [];
 /**
  * \@stable
  */
-const VERSION = new Version('5.0.0-beta.3-75d484e');
+const VERSION = new Version('5.0.0-beta.3-bc22ff1');
 
 /**
  * @fileoverview added by tsickle
