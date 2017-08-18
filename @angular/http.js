0ad7c7005125076171070a7e91b5b0f79b2f07d1
Miško Hevery <misko@hevery.com>
2017-08-18 23:52:39 +0000

    f2a2a6b refactor(core): remove deprecated `Testability#findBindings` (#18782)

--- @angular/http.js
+++ @angular/http.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.0-beta.4-a56468c
+ * @license Angular v5.0.0-beta.4-f2a2a6b
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2126,7 +2126,7 @@ JsonpModule.ctorParameters = () => [];
 /**
  * \@stable
  */
-const VERSION = new Version('5.0.0-beta.4-a56468c');
+const VERSION = new Version('5.0.0-beta.4-f2a2a6b');
 
 /**
  * @fileoverview added by tsickle
