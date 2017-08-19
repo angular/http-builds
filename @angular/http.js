721ea207ab423e7df0f5d22866f1db0a2deac1b5
Miško Hevery <misko@hevery.com>
2017-08-19 04:39:51 +0000

    77ebd2b docs(aio): fix card inconsistency (#18726)

--- @angular/http.js
+++ @angular/http.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.3.5-3b571a4
+ * @license Angular v4.3.5-77ebd2b
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2088,7 +2088,7 @@ JsonpModule.ctorParameters = () => [];
 /**
  * \@stable
  */
-const VERSION = new Version('4.3.5-3b571a4');
+const VERSION = new Version('4.3.5-77ebd2b');
 
 /**
  * @license
