cb99ed8fca8759362c885710807fc720e50a1115
Miško Hevery <misko@hevery.com>
2017-08-29 00:15:34 +0000

    dfe2bad build: Add GitHub scripts for rebasing PRs (#18359)

--- @angular/http.js
+++ @angular/http.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.3.6-f09a266
+ * @license Angular v4.3.6-dfe2bad
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2088,7 +2088,7 @@ JsonpModule.ctorParameters = () => [];
 /**
  * \@stable
  */
-const VERSION = new Version('4.3.6-f09a266');
+const VERSION = new Version('4.3.6-dfe2bad');
 
 /**
  * @license
--- bundles/http.umd.js
+++ bundles/http.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.3.6-f09a266
+ * @license Angular v4.3.6-dfe2bad
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -36,7 +36,7 @@ function __extends(d, b) {
 }
 
 /**
- * @license Angular v4.3.6-f09a266
+ * @license Angular v4.3.6-dfe2bad
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2188,7 +2188,7 @@ JsonpModule.ctorParameters = function () { return []; };
 /**
  * \@stable
  */
-var VERSION = new _angular_core.Version('4.3.6-f09a266');
+var VERSION = new _angular_core.Version('4.3.6-dfe2bad');
 
 exports.BrowserXhr = BrowserXhr;
 exports.JSONPBackend = JSONPBackend;
