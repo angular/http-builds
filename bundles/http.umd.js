4c4eb254e7c07bbdde3735fc42b604910a8e99b5
Jason Aden <jasonaden@google.com>
2017-08-31 19:23:54 +0000

    56b751e Revert "refactor(compiler-cli): expose ngtools api separately (#18952)"

--- bundles/http.umd.js
+++ bundles/http.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.0-beta.5-57abe8d
+ * @license Angular v5.0.0-beta.5-56b751e
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -36,7 +36,7 @@ function __extends(d, b) {
 }
 
 /**
- * @license Angular v5.0.0-beta.5-57abe8d
+ * @license Angular v5.0.0-beta.5-56b751e
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2226,7 +2226,7 @@ JsonpModule.ctorParameters = function () { return []; };
 /**
  * @deprecated use \@angular/common/http instead
  */
-var VERSION = new _angular_core.Version('5.0.0-beta.5-57abe8d');
+var VERSION = new _angular_core.Version('5.0.0-beta.5-56b751e');
 
 exports.BrowserXhr = BrowserXhr;
 exports.JSONPBackend = JSONPBackend;
