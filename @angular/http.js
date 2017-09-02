45b67aa4ce91a3de7431311a9b97fb0a81d4e6b9
Miško Hevery <misko@hevery.com>
2017-09-02 04:21:13 +0000

    7d137d7 fix(core): complete EventEmitter in QueryList on component destroy (#18902)

--- @angular/http.js
+++ @angular/http.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.3.6-b8b551c
+ * @license Angular v4.3.6-7d137d7
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2088,7 +2088,7 @@ JsonpModule.ctorParameters = () => [];
 /**
  * \@stable
  */
-const VERSION = new Version('4.3.6-b8b551c');
+const VERSION = new Version('4.3.6-7d137d7');
 
 /**
  * @license
