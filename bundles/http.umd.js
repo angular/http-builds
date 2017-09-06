7e111c47f2c7c8d50880292961799ac2a0177aa7
Miško Hevery <misko@hevery.com>
2017-09-06 17:48:31 +0000

    3c480e4 build(aio): support rendering of constructor overloads in API docs (#19042)

--- bundles/http.umd.js
+++ bundles/http.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.0-beta.6-29762f1
+ * @license Angular v5.0.0-beta.6-3c480e4
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -36,7 +36,7 @@ function __extends(d, b) {
 }
 
 /**
- * @license Angular v5.0.0-beta.6-29762f1
+ * @license Angular v5.0.0-beta.6-3c480e4
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2250,7 +2250,7 @@ JsonpModule.ctorParameters = function () { return []; };
 /**
  * @deprecated use \@angular/common/http instead
  */
-var VERSION = new _angular_core.Version('5.0.0-beta.6-29762f1');
+var VERSION = new _angular_core.Version('5.0.0-beta.6-3c480e4');
 
 exports.BrowserXhr = BrowserXhr;
 exports.JSONPBackend = JSONPBackend;
