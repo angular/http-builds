6ec4e7946993c9a847837efe029e8e5a4b686274
Miško Hevery <misko@hevery.com>
2017-09-06 16:35:24 +0000

    29762f1 test(aio): fix docs example e2e test (#19070)

--- esm5/index.js
+++ esm5/index.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.0-beta.6-d2707f1
+ * @license Angular v5.0.0-beta.6-29762f1
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2218,7 +2218,7 @@ JsonpModule.ctorParameters = function () { return []; };
 /**
  * @deprecated use \@angular/common/http instead
  */
-var VERSION = new Version('5.0.0-beta.6-d2707f1');
+var VERSION = new Version('5.0.0-beta.6-29762f1');
 
 /**
  * @fileoverview added by tsickle
