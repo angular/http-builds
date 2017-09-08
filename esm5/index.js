0f36649501e4d7c3792da74c8f05eae5fab3ca9e
Matias Niemelä <matias@yearofmoo.com>
2017-09-08 17:38:18 +0000

    a69172f fix(compiler): fix bazel integration and make `perform-compile` more flexible

--- esm5/index.js
+++ esm5/index.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.0-beta.6-5ef6e63
+ * @license Angular v5.0.0-beta.6-a69172f
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2218,7 +2218,7 @@ JsonpModule.ctorParameters = function () { return []; };
 /**
  * @deprecated use \@angular/common/http instead
  */
-var VERSION = new Version('5.0.0-beta.6-5ef6e63');
+var VERSION = new Version('5.0.0-beta.6-a69172f');
 
 /**
  * @fileoverview added by tsickle
