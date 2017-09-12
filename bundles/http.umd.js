6195cc178f35d6922d7ffd8c17c5ac3bcbc306eb
Matias Niemelä <matias@yearofmoo.com>
2017-09-12 18:23:00 +0000

    9ab9437 build(aio): auto-link more code items

--- bundles/http.umd.js
+++ bundles/http.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.0-beta.6-ca5aeba
+ * @license Angular v5.0.0-beta.6-9ab9437
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -36,7 +36,7 @@ function __extends(d, b) {
 }
 
 /**
- * @license Angular v5.0.0-beta.6-ca5aeba
+ * @license Angular v5.0.0-beta.6-9ab9437
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2733,7 +2733,7 @@ var JsonpModule = (function () {
 /**
  * @deprecated use \@angular/common/http instead
  */
-var VERSION = new _angular_core.Version('5.0.0-beta.6-ca5aeba');
+var VERSION = new _angular_core.Version('5.0.0-beta.6-9ab9437');
 
 exports.BrowserXhr = BrowserXhr;
 exports.JSONPBackend = JSONPBackend;
