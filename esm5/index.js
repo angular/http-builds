67ddfca8ee37745dd382b38af75abaf621c6ffa3
Matias Niemelä <matias@yearofmoo.com>
2017-09-15 23:21:02 +0000

    2373186 docs: update combined changelog for 4.4 stable

--- esm5/index.js
+++ esm5/index.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.0-beta.7-b6431c6
+ * @license Angular v5.0.0-beta.7-2373186
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2695,7 +2695,7 @@ var JsonpModule = (function () {
 /**
  * @deprecated use \@angular/common/http instead
  */
-var VERSION = new Version('5.0.0-beta.7-b6431c6');
+var VERSION = new Version('5.0.0-beta.7-2373186');
 
 /**
  * @fileoverview added by tsickle
