fdab917c457a6fe9b41a2bed2bb9968bcaac6ea9
Matias Niemelä <matias@yearofmoo.com>
2017-09-15 23:06:35 +0000

    818f4a7 release: cut the 4.4.1 release

--- @angular/http.js
+++ @angular/http.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.4.0-RC.0-b20c5d2
+ * @license Angular v4.4.1-818f4a7
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2088,7 +2088,7 @@ JsonpModule.ctorParameters = () => [];
 /**
  * \@stable
  */
-const VERSION = new Version('4.4.0-RC.0-b20c5d2');
+const VERSION = new Version('4.4.1-818f4a7');
 
 /**
  * @license
