f871bf1745f65605cce74b675804952a77466adc
Matias Niemelä <matias@yearofmoo.com>
2017-09-15 21:11:01 +0000

    0bafd03 revert: test(packaging): added test for source map correctness

--- @angular/http.js
+++ @angular/http.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.4.0-RC.0-e8d1858
+ * @license Angular v4.4.0-RC.0-0bafd03
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2088,7 +2088,7 @@ JsonpModule.ctorParameters = () => [];
 /**
  * \@stable
  */
-const VERSION = new Version('4.4.0-RC.0-e8d1858');
+const VERSION = new Version('4.4.0-RC.0-0bafd03');
 
 /**
  * @license
