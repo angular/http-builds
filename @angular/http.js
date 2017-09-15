13b6883c51b946a7cded09a7381a51e011c583c3
Matias Niemelä <matias@yearofmoo.com>
2017-09-15 21:37:01 +0000

    b20c5d2 fix(upgrade): remove code setting id attribute. (#19182)

--- @angular/http.js
+++ @angular/http.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.4.0-RC.0-0bafd03
+ * @license Angular v4.4.0-RC.0-b20c5d2
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2088,7 +2088,7 @@ JsonpModule.ctorParameters = () => [];
 /**
  * \@stable
  */
-const VERSION = new Version('4.4.0-RC.0-0bafd03');
+const VERSION = new Version('4.4.0-RC.0-b20c5d2');
 
 /**
  * @license
