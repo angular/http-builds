b5ca77f4505f363fbdae07a18927de0b51cbeff4
Matias Niemelä <matias@yearofmoo.com>
2017-09-15 23:18:16 +0000

    0c72f7d docs: update combined changelog for 4.4 stable

--- @angular/http.js
+++ @angular/http.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.4.1-818f4a7
+ * @license Angular v4.4.1-0c72f7d
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2088,7 +2088,7 @@ JsonpModule.ctorParameters = () => [];
 /**
  * \@stable
  */
-const VERSION = new Version('4.4.1-818f4a7');
+const VERSION = new Version('4.4.1-0c72f7d');
 
 /**
  * @license
