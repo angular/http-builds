02bf86f5381ff9f1c6cbb5f80f18ddbb6f6cc317
Igor Minar <igor@angularjs.org>
2017-09-19 04:36:19 +0000

    49d122e release: cut the 4.4.2 release

--- @angular/http.js
+++ @angular/http.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.4.1-ff87294
+ * @license Angular v4.4.2-49d122e
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2088,7 +2088,7 @@ JsonpModule.ctorParameters = () => [];
 /**
  * \@stable
  */
-const VERSION = new Version('4.4.1-ff87294');
+const VERSION = new Version('4.4.2-49d122e');
 
 /**
  * @license
