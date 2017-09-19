ffd7f5237f7bf3a402655921e97028af48bf958c
Igor Minar <igor@angularjs.org>
2017-09-19 21:32:52 +0000

    351331a ci: update yarn to 1.0.2 (#19270)

--- bundles/http.umd.js
+++ bundles/http.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.4.2-49d122e
+ * @license Angular v4.4.2-351331a
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -36,7 +36,7 @@ function __extends(d, b) {
 }
 
 /**
- * @license Angular v4.4.2-49d122e
+ * @license Angular v4.4.2-351331a
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2188,7 +2188,7 @@ JsonpModule.ctorParameters = function () { return []; };
 /**
  * \@stable
  */
-var VERSION = new _angular_core.Version('4.4.2-49d122e');
+var VERSION = new _angular_core.Version('4.4.2-351331a');
 
 exports.BrowserXhr = BrowserXhr;
 exports.JSONPBackend = JSONPBackend;
