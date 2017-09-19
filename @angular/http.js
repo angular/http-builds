4e80bc82a8d3ef0250d04e0533e5fa4f3aa5ef0d
Igor Minar <igor@angularjs.org>
2017-09-19 22:59:43 +0000

    e2e8ba6 release: cut the 4.4.3 release

--- @angular/http.js
+++ @angular/http.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.4.2-0371538
+ * @license Angular v4.4.3-e2e8ba6
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2088,7 +2088,7 @@ JsonpModule.ctorParameters = () => [];
 /**
  * \@stable
  */
-const VERSION = new Version('4.4.2-0371538');
+const VERSION = new Version('4.4.3-e2e8ba6');
 
 /**
  * @license
