a92a076a3bd02cca231554e29827121df4615d56
Igor Minar <igor@angularjs.org>
2017-09-20 23:45:19 +0000

    9e84719 test: fix cli-integration tests that got broken by yarn update (#19297)

--- esm5/http.js
+++ esm5/http.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.0-beta.7-2c4107c
+ * @license Angular v5.0.0-beta.7-9e84719
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2695,7 +2695,7 @@ var JsonpModule = (function () {
 /**
  * @deprecated use \@angular/common/http instead
  */
-var VERSION = new Version('5.0.0-beta.7-2c4107c');
+var VERSION = new Version('5.0.0-beta.7-9e84719');
 
 /**
  * @fileoverview added by tsickle
