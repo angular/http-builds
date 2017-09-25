60288302627612a767b6efc8d6c3eb5d37551af2
Victor Berchet <victor@suumit.com>
2017-09-25 20:54:20 +0000

    13613d4 fix(compiler): skip &nbsp; when trimming / removing whitespaces (#19310)

--- esm5/http.js
+++ esm5/http.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.0-beta.7-97e02c2
+ * @license Angular v5.0.0-beta.7-13613d4
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2695,7 +2695,7 @@ var JsonpModule = (function () {
 /**
  * @deprecated use \@angular/common/http instead
  */
-var VERSION = new Version('5.0.0-beta.7-97e02c2');
+var VERSION = new Version('5.0.0-beta.7-13613d4');
 
 /**
  * @fileoverview added by tsickle
