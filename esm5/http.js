bf151e670c2035753450f002370fd5a46ed7fa75
Victor Berchet <victor@suumit.com>
2017-09-25 20:01:19 +0000

    e224e3d fix(compiler-cli): update ngtools2 EmitFlags (#19375)

--- esm5/http.js
+++ esm5/http.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.0-beta.7-9ad4b3b
+ * @license Angular v5.0.0-beta.7-e224e3d
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2695,7 +2695,7 @@ var JsonpModule = (function () {
 /**
  * @deprecated use \@angular/common/http instead
  */
-var VERSION = new Version('5.0.0-beta.7-9ad4b3b');
+var VERSION = new Version('5.0.0-beta.7-e224e3d');
 
 /**
  * @fileoverview added by tsickle
