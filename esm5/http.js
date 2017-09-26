cf31166db0175115996a7ca31101f8fcefe0586f
Victor Berchet <victor@suumit.com>
2017-09-26 17:43:17 +0000

    2e1fec1 ci(aio): temporarily disable aot-compiler example e2e tests (#19410)

--- esm5/http.js
+++ esm5/http.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.0-beta.7-f57b7df
+ * @license Angular v5.0.0-beta.7-2e1fec1
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2695,7 +2695,7 @@ var JsonpModule = (function () {
 /**
  * @deprecated use \@angular/common/http instead
  */
-var VERSION = new Version('5.0.0-beta.7-f57b7df');
+var VERSION = new Version('5.0.0-beta.7-2e1fec1');
 
 /**
  * @fileoverview added by tsickle
