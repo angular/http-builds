369b1a3130db6771f7e4fa363cd4831c0e5e1ca8
Victor Berchet <victor@suumit.com>
2017-09-27 21:04:12 +0000

    8d8995f docs(aio): edit summary and next step headers (#16962)

--- bundles/http.umd.js
+++ bundles/http.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.0-beta.7-f7c7038
+ * @license Angular v5.0.0-beta.7-8d8995f
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -36,7 +36,7 @@ function __extends(d, b) {
 }
 
 /**
- * @license Angular v5.0.0-beta.7-f7c7038
+ * @license Angular v5.0.0-beta.7-8d8995f
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2727,7 +2727,7 @@ var JsonpModule = (function () {
 /**
  * @deprecated use \@angular/common/http instead
  */
-var VERSION = new _angular_core.Version('5.0.0-beta.7-f7c7038');
+var VERSION = new _angular_core.Version('5.0.0-beta.7-8d8995f');
 
 exports.BrowserXhr = BrowserXhr;
 exports.JSONPBackend = JSONPBackend;
