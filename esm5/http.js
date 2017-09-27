14131f6f218b1bc914381f369896cd2cbc918291
Victor Berchet <victor@suumit.com>
2017-09-27 22:44:18 +0000

    334caf8 docs(aio): add copy about NgForOf (#18686)

--- esm5/http.js
+++ esm5/http.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.0-beta.7-48c6ece
+ * @license Angular v5.0.0-beta.7-334caf8
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2695,7 +2695,7 @@ var JsonpModule = (function () {
 /**
  * @deprecated use \@angular/common/http instead
  */
-var VERSION = new Version('5.0.0-beta.7-48c6ece');
+var VERSION = new Version('5.0.0-beta.7-334caf8');
 
 /**
  * @fileoverview added by tsickle
