7386f88cde88790e3095d4d63ade3c5a37aae0b8
Victor Berchet <victor@suumit.com>
2017-09-29 00:15:24 +0000

    d442b68 feat(service-worker): introduce the @angular/service-worker package (#19274)

--- esm5/http.js
+++ esm5/http.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.0-rc.0-30d5a2c
+ * @license Angular v5.0.0-beta.7-d442b68
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2695,7 +2695,7 @@ var JsonpModule = (function () {
 /**
  * @deprecated use \@angular/common/http instead
  */
-var VERSION = new Version('5.0.0-rc.0-30d5a2c');
+var VERSION = new Version('5.0.0-beta.7-d442b68');
 
 /**
  * @fileoverview added by tsickle
