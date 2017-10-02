d4b0fd06af518f55a856e6c9632b47b7e65cf54d
Alex Rickabaugh <alx+alxhub@alxandria.net>
2017-10-02 15:45:39 +0000

    745b59f perf(compiler): only emit changed files for incremental compilation

--- esm5/http.js
+++ esm5/http.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.0-rc.0-0038712
+ * @license Angular v5.0.0-rc.0-745b59f
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2695,7 +2695,7 @@ var JsonpModule = (function () {
 /**
  * @deprecated use \@angular/common/http instead
  */
-var VERSION = new Version('5.0.0-rc.0-0038712');
+var VERSION = new Version('5.0.0-rc.0-745b59f');
 
 /**
  * @fileoverview added by tsickle
