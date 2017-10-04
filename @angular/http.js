24e8ae9f0d8fb0cd69ed81a0d9257354b4ea92fa
Alex Rickabaugh <alx+alxhub@alxandria.net>
2017-10-04 22:33:14 +0000

    f98eb35 docs(aio): document Custom Async validators

--- @angular/http.js
+++ @angular/http.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.4.4-95f3b1d
+ * @license Angular v4.4.4-f98eb35
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2088,7 +2088,7 @@ JsonpModule.ctorParameters = () => [];
 /**
  * \@stable
  */
-const VERSION = new Version('4.4.4-95f3b1d');
+const VERSION = new Version('4.4.4-f98eb35');
 
 /**
  * @license
