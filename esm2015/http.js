d26b21bafc4e1998cbb00566c9300ac6c0eaada4
Chuck Jazdzewski <chuckj@google.com>
2017-10-07 01:37:37 +0000

    b1ca5d4 Revert "perf(animations): reduce size of bundle by removing AST classes (#19539)"

--- esm2015/http.js
+++ esm2015/http.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.0-rc.1-a8920eb
+ * @license Angular v5.0.0-rc.1-b1ca5d4
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2125,7 +2125,7 @@ JsonpModule.ctorParameters = () => [];
 /**
  * @deprecated use \@angular/common/http instead
  */
-const VERSION = new Version('5.0.0-rc.1-a8920eb');
+const VERSION = new Version('5.0.0-rc.1-b1ca5d4');
 
 /**
  * @fileoverview added by tsickle
--- esm5/http.js
+++ esm5/http.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.0-rc.1-a8920eb
+ * @license Angular v5.0.0-rc.1-b1ca5d4
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2695,7 +2695,7 @@ var JsonpModule = (function () {
 /**
  * @deprecated use \@angular/common/http instead
  */
-var VERSION = new Version('5.0.0-rc.1-a8920eb');
+var VERSION = new Version('5.0.0-rc.1-b1ca5d4');
 
 /**
  * @fileoverview added by tsickle
