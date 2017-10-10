75a420c0083cd583baabba38e67375d8cbb09bd0
Chuck Jazdzewski <chuckj@google.com>
2017-10-10 23:19:10 +0000

    f89ac92 test(aio): temporarily disable failing examples e2e tests (#19600)

--- @angular/http.js
+++ @angular/http.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.4.4-ccc25ee
+ * @license Angular v4.4.4-f89ac92
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2088,7 +2088,7 @@ JsonpModule.ctorParameters = () => [];
 /**
  * \@stable
  */
-const VERSION = new Version('4.4.4-ccc25ee');
+const VERSION = new Version('4.4.4-f89ac92');
 
 /**
  * @license
