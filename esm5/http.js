8e28db48fdeeffbbaa0961d88325d6b1cf66fee7
Chuck Jazdzewski <chuckj@google.com>
2017-10-10 00:46:08 +0000

    d7eac7e Revert "fix(router): navigating to the current location works (#19463)"

--- esm5/http.js
+++ esm5/http.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.0-rc.1-79deeac
+ * @license Angular v5.0.0-rc.1-d7eac7e
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2695,7 +2695,7 @@ var JsonpModule = (function () {
 /**
  * @deprecated use \@angular/common/http instead
  */
-var VERSION = new Version('5.0.0-rc.1-79deeac');
+var VERSION = new Version('5.0.0-rc.1-d7eac7e');
 
 /**
  * @fileoverview added by tsickle
