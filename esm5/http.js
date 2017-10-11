72caa7690fecd91a503c8886b9e7b3881610e938
Chuck Jazdzewski <chuckj@google.com>
2017-10-11 18:17:55 +0000

    46992b4 Revert "fix(animations): ensure animateChild() works with all inner leave animations (#19006) (#19532)"

--- esm5/http.js
+++ esm5/http.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.0-rc.1-8b57130
+ * @license Angular v5.0.0-rc.1-46992b4
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2695,7 +2695,7 @@ var JsonpModule = (function () {
 /**
  * @deprecated use \@angular/common/http instead
  */
-var VERSION = new Version('5.0.0-rc.1-8b57130');
+var VERSION = new Version('5.0.0-rc.1-46992b4');
 
 /**
  * @fileoverview added by tsickle
