395978aa348146a5ec161c9ff80d171555ad06f8
Chuck Jazdzewski <chuckj@google.com>
2017-10-11 19:19:16 +0000

    566606e ci: freeze yarn lockfile when installing dependencies (#19616)

--- esm5/http.js
+++ esm5/http.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.0-rc.1-46992b4
+ * @license Angular v5.0.0-rc.1-566606e
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2695,7 +2695,7 @@ var JsonpModule = (function () {
 /**
  * @deprecated use \@angular/common/http instead
  */
-var VERSION = new Version('5.0.0-rc.1-46992b4');
+var VERSION = new Version('5.0.0-rc.1-566606e');
 
 /**
  * @fileoverview added by tsickle
