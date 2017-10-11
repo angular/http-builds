1204171ef8b8b9cb21b93006f01f94d22d9b6513
Chuck Jazdzewski <chuckj@google.com>
2017-10-11 20:41:23 +0000

    3acf9c7 docs(aio): update banner ad (#19669)

--- esm5/http.js
+++ esm5/http.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.0-rc.1-566606e
+ * @license Angular v5.0.0-rc.1-3acf9c7
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2695,7 +2695,7 @@ var JsonpModule = (function () {
 /**
  * @deprecated use \@angular/common/http instead
  */
-var VERSION = new Version('5.0.0-rc.1-566606e');
+var VERSION = new Version('5.0.0-rc.1-3acf9c7');
 
 /**
  * @fileoverview added by tsickle
