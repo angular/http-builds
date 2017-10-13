426507fa00b6cbc5edeb26771f7ada97168c6bf4
Tobias Bosch <tbosch1009@gmail.com>
2017-10-13 23:51:12 +0000

    836c889 fix(compiler): prepare for future Bazel semantics of += (#19717)

--- bundles/http.umd.js
+++ bundles/http.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.0-rc.2-43f9d91
+ * @license Angular v5.0.0-rc.2-836c889
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -36,7 +36,7 @@ function __extends(d, b) {
 }
 
 /**
- * @license Angular v5.0.0-rc.2-43f9d91
+ * @license Angular v5.0.0-rc.2-836c889
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2727,7 +2727,7 @@ var JsonpModule = (function () {
 /**
  * @deprecated use \@angular/common/http instead
  */
-var VERSION = new _angular_core.Version('5.0.0-rc.2-43f9d91');
+var VERSION = new _angular_core.Version('5.0.0-rc.2-836c889');
 
 exports.BrowserXhr = BrowserXhr;
 exports.JSONPBackend = JSONPBackend;
--- esm5/http.js
+++ esm5/http.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.0-rc.2-43f9d91
+ * @license Angular v5.0.0-rc.2-836c889
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2695,7 +2695,7 @@ var JsonpModule = (function () {
 /**
  * @deprecated use \@angular/common/http instead
  */
-var VERSION = new Version('5.0.0-rc.2-43f9d91');
+var VERSION = new Version('5.0.0-rc.2-836c889');
 
 /**
  * @fileoverview added by tsickle
