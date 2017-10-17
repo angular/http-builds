36b008fb9c247b81f1541cc321a09630f2d37dbc
Tobias Bosch <tbosch1009@gmail.com>
2017-10-17 18:07:30 +0000

    0789601 build: fix broken path for animations in .pullapprove (#19453)

--- @angular/http.js
+++ @angular/http.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.4.5-ce0ac46
+ * @license Angular v4.4.5-0789601
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2088,7 +2088,7 @@ JsonpModule.ctorParameters = () => [];
 /**
  * \@stable
  */
-const VERSION = new Version('4.4.5-ce0ac46');
+const VERSION = new Version('4.4.5-0789601');
 
 /**
  * @license
