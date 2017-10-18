4257bcaba181ff11945dc8c60bb37cb42f7949d6
Tobias Bosch <tbosch1009@gmail.com>
2017-10-18 20:36:17 +0000

    269f5ac fix(common): attempt to JSON.parse errors for JSON responses (#19773)

--- @angular/http.js
+++ @angular/http.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.4.5-3342a82
+ * @license Angular v4.4.5-269f5ac
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2088,7 +2088,7 @@ JsonpModule.ctorParameters = () => [];
 /**
  * \@stable
  */
-const VERSION = new Version('4.4.5-3342a82');
+const VERSION = new Version('4.4.5-269f5ac');
 
 /**
  * @license
