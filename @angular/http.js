5d6059ea1a7687f0310d4178be2ecc8024e338c7
Tobias Bosch <tbosch1009@gmail.com>
2017-10-18 17:47:57 +0000

    3342a82 fix(aio): make tests less flaky (#19784) PR Close #19784

--- @angular/http.js
+++ @angular/http.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.4.5-630c19f
+ * @license Angular v4.4.5-3342a82
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2088,7 +2088,7 @@ JsonpModule.ctorParameters = () => [];
 /**
  * \@stable
  */
-const VERSION = new Version('4.4.5-630c19f');
+const VERSION = new Version('4.4.5-3342a82');
 
 /**
  * @license
