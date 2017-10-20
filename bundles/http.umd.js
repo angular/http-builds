92def9940cb51690d644642a7907c49661ac1565
Alex Rickabaugh <alx+alxhub@alxandria.net>
2017-10-20 17:02:40 +0000

    441e01c fix(aio): simplify GaService to avoid e2e test failures

--- bundles/http.umd.js
+++ bundles/http.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.0-rc.3-fc86352
+ * @license Angular v5.0.0-rc.3-441e01c
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -36,7 +36,7 @@ function __extends(d, b) {
 }
 
 /**
- * @license Angular v5.0.0-rc.3-fc86352
+ * @license Angular v5.0.0-rc.3-441e01c
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2727,7 +2727,7 @@ var JsonpModule = (function () {
 /**
  * @deprecated use \@angular/common/http instead
  */
-var VERSION = new _angular_core.Version('5.0.0-rc.3-fc86352');
+var VERSION = new _angular_core.Version('5.0.0-rc.3-441e01c');
 
 exports.BrowserXhr = BrowserXhr;
 exports.JSONPBackend = JSONPBackend;
