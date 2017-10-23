7ff0fcd25fb96e5269b4114e1e6ab10c9098f5ba
Matias Niemelä <matias@yearofmoo.com>
2017-10-23 23:05:45 +0000

    5da96c7 fix(bazel): don't console.error from the compile helper (#19879)

--- bundles/http.umd.js
+++ bundles/http.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.0-rc.3-90d1423
+ * @license Angular v5.0.0-rc.3-5da96c7
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -36,7 +36,7 @@ function __extends(d, b) {
 }
 
 /**
- * @license Angular v5.0.0-rc.3-90d1423
+ * @license Angular v5.0.0-rc.3-5da96c7
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2727,7 +2727,7 @@ var JsonpModule = (function () {
 /**
  * @deprecated use \@angular/common/http instead
  */
-var VERSION = new _angular_core.Version('5.0.0-rc.3-90d1423');
+var VERSION = new _angular_core.Version('5.0.0-rc.3-5da96c7');
 
 exports.BrowserXhr = BrowserXhr;
 exports.JSONPBackend = JSONPBackend;
