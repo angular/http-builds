b1b5dad312909116c18a342ae45769a56b726177
Matias Niemelä <matias@yearofmoo.com>
2017-10-25 20:01:38 +0000

    fde5f2f build: update to rxjs@5.5.2 (#19931)

--- esm5/http.js
+++ esm5/http.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.0-rc.5-d567246
+ * @license Angular v5.0.0-rc.5-fde5f2f
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2695,7 +2695,7 @@ var JsonpModule = (function () {
 /**
  * @deprecated use \@angular/common/http instead
  */
-var VERSION = new Version('5.0.0-rc.5-d567246');
+var VERSION = new Version('5.0.0-rc.5-fde5f2f');
 
 /**
  * @fileoverview added by tsickle
