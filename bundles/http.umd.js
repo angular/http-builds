a61be3112bba8cffe7234d212616ec3f85464360
Matias Niemelä <matias@yearofmoo.com>
2017-10-26 22:42:43 +0000

    14016c7 fix(service-worker): fix improper call of Observable.merge (#19962)

--- bundles/http.umd.js
+++ bundles/http.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.0-rc.6-47caebf
+ * @license Angular v5.0.0-rc.6-14016c7
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -36,7 +36,7 @@ function __extends(d, b) {
 }
 
 /**
- * @license Angular v5.0.0-rc.6-47caebf
+ * @license Angular v5.0.0-rc.6-14016c7
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2727,7 +2727,7 @@ var JsonpModule = (function () {
 /**
  * @deprecated use \@angular/common/http instead
  */
-var VERSION = new _angular_core.Version('5.0.0-rc.6-47caebf');
+var VERSION = new _angular_core.Version('5.0.0-rc.6-14016c7');
 
 exports.BrowserXhr = BrowserXhr;
 exports.JSONPBackend = JSONPBackend;
