0cedf5ceebfe545bc9f75c2cf2b83b9f3968eaf8
Matias Niemelä <matias@yearofmoo.com>
2017-10-28 07:12:10 +0000

    308fc8e docs: add changelog for 5.0.0-rc.8

--- esm5/http.js
+++ esm5/http.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.0-rc.7-394d951
+ * @license Angular v5.0.0-rc.8-308fc8e
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2695,7 +2695,7 @@ var JsonpModule = (function () {
 /**
  * @deprecated use \@angular/common/http instead
  */
-var VERSION = new Version('5.0.0-rc.7-394d951');
+var VERSION = new Version('5.0.0-rc.8-308fc8e');
 
 /**
  * @fileoverview added by tsickle
