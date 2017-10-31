37351b9d3f498dee693608b8475f8fe16a668752
Matias Niemelä <matias@yearofmoo.com>
2017-10-31 04:07:01 +0000

    cf5fce8 build: update rollup lint rule from bad merge (#20047)

--- bundles/http.umd.js
+++ bundles/http.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.0-rc.8-f1248b6
+ * @license Angular v5.0.0-rc.8-cf5fce8
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -36,7 +36,7 @@ function __extends(d, b) {
 }
 
 /**
- * @license Angular v5.0.0-rc.8-f1248b6
+ * @license Angular v5.0.0-rc.8-cf5fce8
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2727,7 +2727,7 @@ var JsonpModule = (function () {
 /**
  * @deprecated use \@angular/common/http instead
  */
-var VERSION = new _angular_core.Version('5.0.0-rc.8-f1248b6');
+var VERSION = new _angular_core.Version('5.0.0-rc.8-cf5fce8');
 
 exports.BrowserXhr = BrowserXhr;
 exports.JSONPBackend = JSONPBackend;
