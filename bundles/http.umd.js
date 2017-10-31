f10b718bd4a27879313cd8d52b7108c66982bb4f
Matias Niemelä <matias@yearofmoo.com>
2017-10-31 03:33:05 +0000

    13f8648 fix: add missing globals from each rollup configuration (#20028)

--- bundles/http.umd.js
+++ bundles/http.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.0-rc.8-54480f7
+ * @license Angular v5.0.0-rc.8-13f8648
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -36,7 +36,7 @@ function __extends(d, b) {
 }
 
 /**
- * @license Angular v5.0.0-rc.8-54480f7
+ * @license Angular v5.0.0-rc.8-13f8648
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2727,7 +2727,7 @@ var JsonpModule = (function () {
 /**
  * @deprecated use \@angular/common/http instead
  */
-var VERSION = new _angular_core.Version('5.0.0-rc.8-54480f7');
+var VERSION = new _angular_core.Version('5.0.0-rc.8-13f8648');
 
 exports.BrowserXhr = BrowserXhr;
 exports.JSONPBackend = JSONPBackend;
