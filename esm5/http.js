d6e869c353b9f8028215ac7372f2528e5b2d0913
Matias Niemelä <matias@yearofmoo.com>
2017-10-31 01:59:08 +0000

    c295aee build: add `release` as a valid commit message subject (#19955)

--- esm5/http.js
+++ esm5/http.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.0-rc.8-e3a16ed
+ * @license Angular v5.0.0-rc.8-c295aee
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2695,7 +2695,7 @@ var JsonpModule = (function () {
 /**
  * @deprecated use \@angular/common/http instead
  */
-var VERSION = new Version('5.0.0-rc.8-e3a16ed');
+var VERSION = new Version('5.0.0-rc.8-c295aee');
 
 /**
  * @fileoverview added by tsickle
