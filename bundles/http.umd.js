3a023947020d2b42f08c3e05829399d6cb27e92a
Matias Niemelä <matias@yearofmoo.com>
2017-10-31 04:47:15 +0000

    f5b7f2b docs: add changelog for 5.0.0-rc.9

--- bundles/http.umd.js
+++ bundles/http.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.0-rc.8-cf5fce8
+ * @license Angular v5.0.0-rc.9-f5b7f2b
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -36,7 +36,7 @@ function __extends(d, b) {
 }
 
 /**
- * @license Angular v5.0.0-rc.8-cf5fce8
+ * @license Angular v5.0.0-rc.9-f5b7f2b
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2727,7 +2727,7 @@ var JsonpModule = (function () {
 /**
  * @deprecated use \@angular/common/http instead
  */
-var VERSION = new _angular_core.Version('5.0.0-rc.8-cf5fce8');
+var VERSION = new _angular_core.Version('5.0.0-rc.9-f5b7f2b');
 
 exports.BrowserXhr = BrowserXhr;
 exports.JSONPBackend = JSONPBackend;
