a5566ec00af65739e70fecd367108b53bb9fbd67
Matias Niemelä <matias@yearofmoo.com>
2017-11-01 23:03:17 +0000

    27ae0f9 build(aio): upgrade to @angular/material@2.0.0-beta.12 (#19702)

--- esm5/http.js
+++ esm5/http.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.0-280dada
+ * @license Angular v5.0.0-27ae0f9
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2695,7 +2695,7 @@ var JsonpModule = (function () {
 /**
  * @deprecated use \@angular/common/http instead
  */
-var VERSION = new Version('5.0.0-280dada');
+var VERSION = new Version('5.0.0-27ae0f9');
 
 /**
  * @fileoverview added by tsickle
