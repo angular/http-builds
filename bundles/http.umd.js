3d7e5f757a4fd65172bca59f382675ef8406aaef
Victor Berchet <victor@suumit.com>
2017-11-02 22:39:30 +0000

    068348e build(aio): do not fail the initial doc-gen when running docs-watch

--- bundles/http.umd.js
+++ bundles/http.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.1.0-beta.0-5b16ce9
+ * @license Angular v5.1.0-beta.0-068348e
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -36,7 +36,7 @@ function __extends(d, b) {
 }
 
 /**
- * @license Angular v5.1.0-beta.0-5b16ce9
+ * @license Angular v5.1.0-beta.0-068348e
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2727,7 +2727,7 @@ var JsonpModule = (function () {
 /**
  * @deprecated use \@angular/common/http instead
  */
-var VERSION = new _angular_core.Version('5.1.0-beta.0-5b16ce9');
+var VERSION = new _angular_core.Version('5.1.0-beta.0-068348e');
 
 exports.BrowserXhr = BrowserXhr;
 exports.JSONPBackend = JSONPBackend;
