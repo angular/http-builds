22000f30dcb1e479143018406ce28b91e25f638f
Victor Berchet <victor@suumit.com>
2017-11-02 23:28:18 +0000

    25843fe build(aio): avoid building before running the local PWA tests

--- esm2015/http.js
+++ esm2015/http.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.0-37a740b
+ * @license Angular v5.0.0-25843fe
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2125,7 +2125,7 @@ JsonpModule.ctorParameters = () => [];
 /**
  * @deprecated use \@angular/common/http instead
  */
-const VERSION = new Version('5.0.0-37a740b');
+const VERSION = new Version('5.0.0-25843fe');
 
 /**
  * @fileoverview added by tsickle
