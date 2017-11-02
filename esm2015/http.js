6e397a9b8b4967054fa235cbf09f701599f862c1
Victor Berchet <victor@suumit.com>
2017-11-02 23:36:22 +0000

    a28d616 fix(core): __symbol__ should return __zone_symbol__ without zone.js loaded (#19541)

--- esm2015/http.js
+++ esm2015/http.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.1.0-beta.0-613a9e3
+ * @license Angular v5.1.0-beta.0-a28d616
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2125,7 +2125,7 @@ JsonpModule.ctorParameters = () => [];
 /**
  * @deprecated use \@angular/common/http instead
  */
-const VERSION = new Version('5.1.0-beta.0-613a9e3');
+const VERSION = new Version('5.1.0-beta.0-a28d616');
 
 /**
  * @fileoverview added by tsickle
