8a22c248875affb6493bcb572a3b76e9d523eb28
Victor Berchet <victor@suumit.com>
2017-11-02 21:45:13 +0000

    132c071 docs(aio): updated i18n guide and example (#19975)

--- bundles/http.umd.js
+++ bundles/http.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.1.0-beta.0-3db7112
+ * @license Angular v5.1.0-beta.0-132c071
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -36,7 +36,7 @@ function __extends(d, b) {
 }
 
 /**
- * @license Angular v5.1.0-beta.0-3db7112
+ * @license Angular v5.1.0-beta.0-132c071
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2727,7 +2727,7 @@ var JsonpModule = (function () {
 /**
  * @deprecated use \@angular/common/http instead
  */
-var VERSION = new _angular_core.Version('5.1.0-beta.0-3db7112');
+var VERSION = new _angular_core.Version('5.1.0-beta.0-132c071');
 
 exports.BrowserXhr = BrowserXhr;
 exports.JSONPBackend = JSONPBackend;
--- esm5/http.js
+++ esm5/http.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.1.0-beta.0-3db7112
+ * @license Angular v5.1.0-beta.0-132c071
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2695,7 +2695,7 @@ var JsonpModule = (function () {
 /**
  * @deprecated use \@angular/common/http instead
  */
-var VERSION = new Version('5.1.0-beta.0-3db7112');
+var VERSION = new Version('5.1.0-beta.0-132c071');
 
 /**
  * @fileoverview added by tsickle
