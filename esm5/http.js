9148e94e153d918eba8d613a80afcd7adb0c4b06
Victor Berchet <victor@suumit.com>
2017-11-02 22:19:49 +0000

    d550b68 docs: fix changelog for v5 (#20097)

--- esm5/http.js
+++ esm5/http.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.0-e846953
+ * @license Angular v5.0.0-d550b68
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2695,7 +2695,7 @@ var JsonpModule = (function () {
 /**
  * @deprecated use \@angular/common/http instead
  */
-var VERSION = new Version('5.0.0-e846953');
+var VERSION = new Version('5.0.0-d550b68');
 
 /**
  * @fileoverview added by tsickle
