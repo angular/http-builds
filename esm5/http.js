c1de4203635c5c2b20b52cad45c7c67dda97177b
Victor Berchet <victor@suumit.com>
2017-11-03 22:41:20 +0000

    200d92d fix(core): should support event.stopImmediatePropagation (#19222)

--- esm5/http.js
+++ esm5/http.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.1.0-beta.0-dbec3ca
+ * @license Angular v5.1.0-beta.0-200d92d
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2695,7 +2695,7 @@ var JsonpModule = (function () {
 /**
  * @deprecated use \@angular/common/http instead
  */
-var VERSION = new Version('5.1.0-beta.0-dbec3ca');
+var VERSION = new Version('5.1.0-beta.0-200d92d');
 
 /**
  * @fileoverview added by tsickle
