dc45c98df4c797267990afef78f2f4fc7a563557
Victor Berchet <victor@suumit.com>
2017-11-03 19:11:14 +0000

    26f8299 fix(compiler): don't overwrite missingTranslation's value in JIT (#19952)

--- esm5/http.js
+++ esm5/http.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.1.0-beta.0-c7fd172
+ * @license Angular v5.1.0-beta.0-26f8299
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2695,7 +2695,7 @@ var JsonpModule = (function () {
 /**
  * @deprecated use \@angular/common/http instead
  */
-var VERSION = new Version('5.1.0-beta.0-c7fd172');
+var VERSION = new Version('5.1.0-beta.0-26f8299');
 
 /**
  * @fileoverview added by tsickle
