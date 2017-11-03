626e8e5c56718f70db2dcfebbeb57ecd18e39d88
Victor Berchet <victor@suumit.com>
2017-11-03 19:32:33 +0000

    169cedd docs: Bootstrapping guide prose for CLI (#19777)

--- esm5/http.js
+++ esm5/http.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.1.0-beta.0-1d19d61
+ * @license Angular v5.1.0-beta.0-169cedd
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2695,7 +2695,7 @@ var JsonpModule = (function () {
 /**
  * @deprecated use \@angular/common/http instead
  */
-var VERSION = new Version('5.1.0-beta.0-1d19d61');
+var VERSION = new Version('5.1.0-beta.0-169cedd');
 
 /**
  * @fileoverview added by tsickle
