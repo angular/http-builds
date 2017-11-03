9b6527656a75be8ebab5771e83bfd9180f96555b
Victor Berchet <victor@suumit.com>
2017-11-03 19:14:20 +0000

    03f080b docs: Deployment guide for CLI (#19839)

--- bundles/http.umd.js
+++ bundles/http.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.1.0-beta.0-26f8299
+ * @license Angular v5.1.0-beta.0-03f080b
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -36,7 +36,7 @@ function __extends(d, b) {
 }
 
 /**
- * @license Angular v5.1.0-beta.0-26f8299
+ * @license Angular v5.1.0-beta.0-03f080b
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2727,7 +2727,7 @@ var JsonpModule = (function () {
 /**
  * @deprecated use \@angular/common/http instead
  */
-var VERSION = new _angular_core.Version('5.1.0-beta.0-26f8299');
+var VERSION = new _angular_core.Version('5.1.0-beta.0-03f080b');
 
 exports.BrowserXhr = BrowserXhr;
 exports.JSONPBackend = JSONPBackend;
--- esm5/http.js
+++ esm5/http.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.1.0-beta.0-26f8299
+ * @license Angular v5.1.0-beta.0-03f080b
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2695,7 +2695,7 @@ var JsonpModule = (function () {
 /**
  * @deprecated use \@angular/common/http instead
  */
-var VERSION = new Version('5.1.0-beta.0-26f8299');
+var VERSION = new Version('5.1.0-beta.0-03f080b');
 
 /**
  * @fileoverview added by tsickle
