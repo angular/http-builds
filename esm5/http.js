dff2219fb1e483441817a977e3c178c80957eadd
Victor Berchet <victor@suumit.com>
2017-11-06 19:50:58 +0000

    b489259 docs(common): fix mis-ordered lines (#20221)

--- esm5/http.js
+++ esm5/http.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.1.0-beta.0-6b74883
+ * @license Angular v5.1.0-beta.0-b489259
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2695,7 +2695,7 @@ var JsonpModule = (function () {
 /**
  * @deprecated use \@angular/common/http instead
  */
-var VERSION = new Version('5.1.0-beta.0-6b74883');
+var VERSION = new Version('5.1.0-beta.0-b489259');
 
 /**
  * @fileoverview added by tsickle
