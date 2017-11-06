5c4bfcd9edc924d309cba5741a2ccd9dba167620
Victor Berchet <victor@suumit.com>
2017-11-06 18:33:04 +0000

    049c896 docs(aio): update ToH for CLI (#19811)

--- esm5/http.js
+++ esm5/http.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.1.0-beta.0-8802016
+ * @license Angular v5.1.0-beta.0-049c896
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2695,7 +2695,7 @@ var JsonpModule = (function () {
 /**
  * @deprecated use \@angular/common/http instead
  */
-var VERSION = new Version('5.1.0-beta.0-8802016');
+var VERSION = new Version('5.1.0-beta.0-049c896');
 
 /**
  * @fileoverview added by tsickle
