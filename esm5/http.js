0f510781d16e099fcf5faaed3e52c29bfd3d8770
Jason Aden <jasonaden@google.com>
2017-11-10 22:14:31 +0000

    161f88f build: update to google-closure-compiler@20171023.0.1 (#20321)

--- esm5/http.js
+++ esm5/http.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.1.0-beta.0-fb4b90a
+ * @license Angular v5.1.0-beta.0-161f88f
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2695,7 +2695,7 @@ var JsonpModule = (function () {
 /**
  * @deprecated use \@angular/common/http instead
  */
-var VERSION = new Version('5.1.0-beta.0-fb4b90a');
+var VERSION = new Version('5.1.0-beta.0-161f88f');
 
 /**
  * @fileoverview added by tsickle
