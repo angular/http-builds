207628c723160e4c0ee0642ffe3a2457e5d54b30
Jason Aden <jasonaden@google.com>
2017-11-10 23:05:36 +0000

    612f508 refactor(animations): uses a loop instead Array.map() which creates and (#19910) returns a new array that is discarded.

--- esm5/http.js
+++ esm5/http.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.1-aa3d75b
+ * @license Angular v5.0.1-612f508
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2695,7 +2695,7 @@ var JsonpModule = (function () {
 /**
  * @deprecated use \@angular/common/http instead
  */
-var VERSION = new Version('5.0.1-aa3d75b');
+var VERSION = new Version('5.0.1-612f508');
 
 /**
  * @fileoverview added by tsickle
