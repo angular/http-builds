c251d0d521e794a015df8486aec4fcf5723c2f90
Jason Aden <jasonaden@google.com>
2017-11-10 00:24:15 +0000

    424a323 fix(compiler): show explanatory text in template errors (#20313)

--- esm5/http.js
+++ esm5/http.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.1-28985cb
+ * @license Angular v5.0.1-424a323
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2695,7 +2695,7 @@ var JsonpModule = (function () {
 /**
  * @deprecated use \@angular/common/http instead
  */
-var VERSION = new Version('5.0.1-28985cb');
+var VERSION = new Version('5.0.1-424a323');
 
 /**
  * @fileoverview added by tsickle
