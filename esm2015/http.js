0d8db9f5f288baebe9604f33d21449ecd604e4cb
Jason Aden <jasonaden@google.com>
2017-11-14 22:06:52 +0000

    20aafff fix(animations): ensure final state() styles are applied within @.disabled animations (#20267)

--- esm2015/http.js
+++ esm2015/http.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.1.0-beta.0-12af6d3
+ * @license Angular v5.1.0-beta.0-20aafff
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2125,7 +2125,7 @@ JsonpModule.ctorParameters = () => [];
 /**
  * @deprecated use \@angular/common/http instead
  */
-const VERSION = new Version('5.1.0-beta.0-12af6d3');
+const VERSION = new Version('5.1.0-beta.0-20aafff');
 
 /**
  * @fileoverview added by tsickle
--- esm5/http.js
+++ esm5/http.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.1.0-beta.0-12af6d3
+ * @license Angular v5.1.0-beta.0-20aafff
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2695,7 +2695,7 @@ var JsonpModule = (function () {
 /**
  * @deprecated use \@angular/common/http instead
  */
-var VERSION = new Version('5.1.0-beta.0-12af6d3');
+var VERSION = new Version('5.1.0-beta.0-20aafff');
 
 /**
  * @fileoverview added by tsickle
