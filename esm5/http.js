e2ec6aca9e92785f8fe4460096cf00f422369e58
Miško Hevery <misko@hevery.com>
2017-11-16 22:38:35 +0000

    4064cbe docs: add changelog for 5.1.0-beta.1

--- esm5/http.js
+++ esm5/http.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.1.0-beta.0-dcfffbf
+ * @license Angular v5.1.0-beta.1-4064cbe
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2730,7 +2730,7 @@ var JsonpModule = /** @class */ (function () {
 /**
  * @deprecated use \@angular/common/http instead
  */
-var VERSION = new Version('5.1.0-beta.0-dcfffbf');
+var VERSION = new Version('5.1.0-beta.1-4064cbe');
 
 /**
  * @fileoverview added by tsickle
