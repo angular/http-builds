b24f6d9a37b3691d84cbe2581694fd0cc3221acc
Miško Hevery <misko@hevery.com>
2017-11-21 05:26:38 +0000

    2a9d2ba docs: update changelog with missing feature in 5.1.0-beta.1 (#20508)

--- bundles/http.umd.js
+++ bundles/http.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.1.0-beta.1-69c53c3
+ * @license Angular v5.1.0-beta.1-2a9d2ba
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -36,7 +36,7 @@ function __extends(d, b) {
 }
 
 /**
- * @license Angular v5.1.0-beta.1-69c53c3
+ * @license Angular v5.1.0-beta.1-2a9d2ba
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2762,7 +2762,7 @@ var JsonpModule = /** @class */ (function () {
 /**
  * @deprecated use \@angular/common/http instead
  */
-var VERSION = new _angular_core.Version('5.1.0-beta.1-69c53c3');
+var VERSION = new _angular_core.Version('5.1.0-beta.1-2a9d2ba');
 
 exports.BrowserXhr = BrowserXhr;
 exports.JSONPBackend = JSONPBackend;
--- esm5/http.js
+++ esm5/http.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.1.0-beta.1-69c53c3
+ * @license Angular v5.1.0-beta.1-2a9d2ba
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2730,7 +2730,7 @@ var JsonpModule = /** @class */ (function () {
 /**
  * @deprecated use \@angular/common/http instead
  */
-var VERSION = new Version('5.1.0-beta.1-69c53c3');
+var VERSION = new Version('5.1.0-beta.1-2a9d2ba');
 
 /**
  * @fileoverview added by tsickle
