104e6cb6717036d31a097983f9eb593b9ca61bcd
Miško Hevery <misko@hevery.com>
2017-11-22 20:09:45 +0000

    ffb6dbe fix(animations): always fire start and done callbacks in order for noop animations (#20570)

--- bundles/http.umd.js
+++ bundles/http.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.1.0-beta.1-0e012c9
+ * @license Angular v5.1.0-beta.1-ffb6dbe
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -36,7 +36,7 @@ function __extends(d, b) {
 }
 
 /**
- * @license Angular v5.1.0-beta.1-0e012c9
+ * @license Angular v5.1.0-beta.1-ffb6dbe
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2762,7 +2762,7 @@ var JsonpModule = /** @class */ (function () {
 /**
  * @deprecated use \@angular/common/http instead
  */
-var VERSION = new _angular_core.Version('5.1.0-beta.1-0e012c9');
+var VERSION = new _angular_core.Version('5.1.0-beta.1-ffb6dbe');
 
 exports.BrowserXhr = BrowserXhr;
 exports.JSONPBackend = JSONPBackend;
--- esm5/http.js
+++ esm5/http.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.1.0-beta.1-0e012c9
+ * @license Angular v5.1.0-beta.1-ffb6dbe
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2730,7 +2730,7 @@ var JsonpModule = /** @class */ (function () {
 /**
  * @deprecated use \@angular/common/http instead
  */
-var VERSION = new Version('5.1.0-beta.1-0e012c9');
+var VERSION = new Version('5.1.0-beta.1-ffb6dbe');
 
 /**
  * @fileoverview added by tsickle
