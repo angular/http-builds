02b1ee7740ad4977bfbfa6c4270c07ed66f60f81
Miško Hevery <misko@hevery.com>
2017-11-27 20:03:18 +0000

    a53a040 build(aio): prevent comments in code from leaking into doc-gen code snippets (#20607)

--- bundles/http.umd.js
+++ bundles/http.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.1.0-beta.2-330bb2a
+ * @license Angular v5.1.0-beta.2-a53a040
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -36,7 +36,7 @@ function __extends(d, b) {
 }
 
 /**
- * @license Angular v5.1.0-beta.2-330bb2a
+ * @license Angular v5.1.0-beta.2-a53a040
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2762,7 +2762,7 @@ var JsonpModule = /** @class */ (function () {
 /**
  * @deprecated use \@angular/common/http instead
  */
-var VERSION = new _angular_core.Version('5.1.0-beta.2-330bb2a');
+var VERSION = new _angular_core.Version('5.1.0-beta.2-a53a040');
 
 exports.BrowserXhr = BrowserXhr;
 exports.JSONPBackend = JSONPBackend;
--- esm5/http.js
+++ esm5/http.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.1.0-beta.2-330bb2a
+ * @license Angular v5.1.0-beta.2-a53a040
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2730,7 +2730,7 @@ var JsonpModule = /** @class */ (function () {
 /**
  * @deprecated use \@angular/common/http instead
  */
-var VERSION = new Version('5.1.0-beta.2-330bb2a');
+var VERSION = new Version('5.1.0-beta.2-a53a040');
 
 /**
  * @fileoverview added by tsickle
