a7ee5345d2333f8f2d67a3f6819efcb7a73c45ec
Miško Hevery <misko@hevery.com>
2017-11-29 22:47:32 +0000

    647ca64 docs(aio): Updating with Ignite UI for Angular (#20663)

--- esm5/http.js
+++ esm5/http.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.3-c7b211c
+ * @license Angular v5.0.3-647ca64
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2695,7 +2695,7 @@ var JsonpModule = (function () {
 /**
  * @deprecated use \@angular/common/http instead
  */
-var VERSION = new Version('5.0.3-c7b211c');
+var VERSION = new Version('5.0.3-647ca64');
 
 /**
  * @fileoverview added by tsickle
