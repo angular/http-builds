39578a8a3abc3aa1304b3340040b790f7efaccb4
Miško Hevery <misko@hevery.com>
2017-11-29 00:45:50 +0000

    8bb42df fix(compiler): correctly detect when to serialze summary metadata (#20668)

--- bundles/http.umd.js
+++ bundles/http.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.1.0-beta.2-24bf3e2
+ * @license Angular v5.1.0-beta.2-8bb42df
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -36,7 +36,7 @@ function __extends(d, b) {
 }
 
 /**
- * @license Angular v5.1.0-beta.2-24bf3e2
+ * @license Angular v5.1.0-beta.2-8bb42df
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2762,7 +2762,7 @@ var JsonpModule = /** @class */ (function () {
 /**
  * @deprecated use \@angular/common/http instead
  */
-var VERSION = new _angular_core.Version('5.1.0-beta.2-24bf3e2');
+var VERSION = new _angular_core.Version('5.1.0-beta.2-8bb42df');
 
 exports.BrowserXhr = BrowserXhr;
 exports.JSONPBackend = JSONPBackend;
