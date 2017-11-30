510825ae3dc21f68994ec7cb832cbc3a631e6480
Miško Hevery <misko@hevery.com>
2017-11-30 18:24:36 +0000

    47addd1 revert: docs(aio): add service worker guide content and update nav (#20021) (#20716)

--- bundles/http.umd.js
+++ bundles/http.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.1.0-beta.2-ba6af2a
+ * @license Angular v5.1.0-beta.2-47addd1
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -36,7 +36,7 @@ function __extends(d, b) {
 }
 
 /**
- * @license Angular v5.1.0-beta.2-ba6af2a
+ * @license Angular v5.1.0-beta.2-47addd1
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2762,7 +2762,7 @@ var JsonpModule = /** @class */ (function () {
 /**
  * @deprecated use \@angular/common/http instead
  */
-var VERSION = new _angular_core.Version('5.1.0-beta.2-ba6af2a');
+var VERSION = new _angular_core.Version('5.1.0-beta.2-47addd1');
 
 exports.BrowserXhr = BrowserXhr;
 exports.JSONPBackend = JSONPBackend;
