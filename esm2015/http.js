5c13527584281308fbca1e43ddd273da85fbf77e
Miško Hevery <misko@hevery.com>
2017-11-30 03:55:52 +0000

    b9e4d62 docs(aio): remove services plurality (#20696)

--- esm2015/http.js
+++ esm2015/http.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.1.0-beta.2-ba850b3
+ * @license Angular v5.1.0-beta.2-b9e4d62
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2160,7 +2160,7 @@ JsonpModule.ctorParameters = () => [];
 /**
  * @deprecated use \@angular/common/http instead
  */
-const VERSION = new Version('5.1.0-beta.2-ba850b3');
+const VERSION = new Version('5.1.0-beta.2-b9e4d62');
 
 /**
  * @fileoverview added by tsickle
