51b9bc92b0bca752c946618f8170b8517c76ccec
Misko Hevery <misko@hevery.com>
2017-11-30 04:50:49 +0000

    9e69d97 style: broken build due to missing new lines

--- esm5/http.js
+++ esm5/http.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.3-b450c83
+ * @license Angular v5.0.3-9e69d97
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2695,7 +2695,7 @@ var JsonpModule = (function () {
 /**
  * @deprecated use \@angular/common/http instead
  */
-var VERSION = new Version('5.0.3-b450c83');
+var VERSION = new Version('5.0.3-9e69d97');
 
 /**
  * @fileoverview added by tsickle
