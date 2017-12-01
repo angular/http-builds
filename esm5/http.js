bc30c9529655a963d60b5f1e1e7c9f204b9be0b8
Misko Hevery <misko@hevery.com>
2017-12-01 05:55:30 +0000

    cbd93fe docs: add changelog for 5.0.4

--- esm5/http.js
+++ esm5/http.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.3-e95b5d7
+ * @license Angular v5.0.4-cbd93fe
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2695,7 +2695,7 @@ var JsonpModule = (function () {
 /**
  * @deprecated use \@angular/common/http instead
  */
-var VERSION = new Version('5.0.3-e95b5d7');
+var VERSION = new Version('5.0.4-cbd93fe');
 
 /**
  * @fileoverview added by tsickle
