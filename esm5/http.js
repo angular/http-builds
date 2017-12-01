317c0c85e34e5ab0af2794d11a656d029b43eb0c
Alex Rickabaugh <alx+alxhub@alxandria.net>
2017-12-01 22:45:43 +0000

    f582620 fix(service-worker): use relative path for ngsw.json

--- esm5/http.js
+++ esm5/http.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.1.0-rc.0-f841fbe
+ * @license Angular v5.1.0-rc.0-f582620
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2730,7 +2730,7 @@ var JsonpModule = /** @class */ (function () {
 /**
  * @deprecated use \@angular/common/http instead
  */
-var VERSION = new Version('5.1.0-rc.0-f841fbe');
+var VERSION = new Version('5.1.0-rc.0-f582620');
 
 /**
  * @fileoverview added by tsickle
