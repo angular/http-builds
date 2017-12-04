98af8cbba71fdda9db38d81d1feb02fd511cf518
Igor Minar <igor@angularjs.org>
2017-12-04 23:15:58 +0000

    7e7ff2e ci: fix the payload-size checking scripts (#20683)

--- bundles/http.umd.js
+++ bundles/http.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.1.0-rc.0-d507057
+ * @license Angular v5.1.0-rc.1-7e7ff2e
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -36,7 +36,7 @@ function __extends(d, b) {
 }
 
 /**
- * @license Angular v5.1.0-rc.0-d507057
+ * @license Angular v5.1.0-rc.1-7e7ff2e
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2762,7 +2762,7 @@ var JsonpModule = /** @class */ (function () {
 /**
  * @deprecated use \@angular/common/http instead
  */
-var VERSION = new _angular_core.Version('5.1.0-rc.0-d507057');
+var VERSION = new _angular_core.Version('5.1.0-rc.1-7e7ff2e');
 
 exports.BrowserXhr = BrowserXhr;
 exports.JSONPBackend = JSONPBackend;
