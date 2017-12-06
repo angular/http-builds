d24bb629a1687cb6b2fed6249baf6fd1b657a78d
Igor Minar <igor@angularjs.org>
2017-12-06 06:15:38 +0000

    6d15dd7 docs(aio): architecture review for a11y (#17848)

--- esm5/http.js
+++ esm5/http.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.5-a2fde2b
+ * @license Angular v5.0.5-6d15dd7
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2695,7 +2695,7 @@ var JsonpModule = (function () {
 /**
  * @deprecated use \@angular/common/http instead
  */
-var VERSION = new Version('5.0.5-a2fde2b');
+var VERSION = new Version('5.0.5-6d15dd7');
 
 /**
  * @fileoverview added by tsickle
