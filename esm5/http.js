6afd0217fd4cbfc463340f147a484bb3b52d2c4f
Igor Minar <igor@angularjs.org>
2017-12-06 16:45:02 +0000

    b78ada1 fix(animations): ensure DOM is cleaned up after multiple @trigger leave animations finish (#20740)

--- esm5/http.js
+++ esm5/http.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.1.0-rc.1-be99449
+ * @license Angular v5.1.0-rc.1-b78ada1
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2730,7 +2730,7 @@ var JsonpModule = /** @class */ (function () {
 /**
  * @deprecated use \@angular/common/http instead
  */
-var VERSION = new Version('5.1.0-rc.1-be99449');
+var VERSION = new Version('5.1.0-rc.1-b78ada1');
 
 /**
  * @fileoverview added by tsickle
