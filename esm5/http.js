6d3379a45e3eb2196de01eb58cabcf05d470d076
Jason Aden <jasonaden@google.com>
2017-12-08 18:28:49 +0000

    225baf4 docs(aio): fix typo for missing quote (#20888) PR Close #20888

--- esm5/http.js
+++ esm5/http.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.1.0-46aa0a1
+ * @license Angular v5.1.0-225baf4
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2730,7 +2730,7 @@ var JsonpModule = /** @class */ (function () {
 /**
  * @deprecated use \@angular/common/http instead
  */
-var VERSION = new Version('5.1.0-46aa0a1');
+var VERSION = new Version('5.1.0-225baf4');
 
 /**
  * @fileoverview added by tsickle
