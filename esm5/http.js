54f8f27017864727c50faec84a4c16c5fb9bf142
Jason Aden <jasonaden@google.com>
2017-12-08 18:35:45 +0000

    affa54d build(aio): upgrade to latest `@angular/cli` (#18428)

--- esm5/http.js
+++ esm5/http.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.1.0-53b9de3
+ * @license Angular v5.1.0-affa54d
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2730,7 +2730,7 @@ var JsonpModule = /** @class */ (function () {
 /**
  * @deprecated use \@angular/common/http instead
  */
-var VERSION = new Version('5.1.0-53b9de3');
+var VERSION = new Version('5.1.0-affa54d');
 
 /**
  * @fileoverview added by tsickle
