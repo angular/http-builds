2ee2826d41c9aeb7b6bdf058b2d382f78838104b
Jason Aden <jasonaden@google.com>
2017-12-11 19:56:25 +0000

    7363b3d build: remove bazel option --noshow_results (#20943)

--- esm5/http.js
+++ esm5/http.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.1.0-13e663c
+ * @license Angular v5.1.0-7363b3d
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2730,7 +2730,7 @@ var JsonpModule = /** @class */ (function () {
 /**
  * @deprecated use \@angular/common/http instead
  */
-var VERSION = new Version('5.1.0-13e663c');
+var VERSION = new Version('5.1.0-7363b3d');
 
 /**
  * @fileoverview added by tsickle
