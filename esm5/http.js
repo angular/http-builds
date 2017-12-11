1f8412e83b9250e61a89fe971a504391306da7b5
Jason Aden <jasonaden@google.com>
2017-12-11 23:59:34 +0000

    8bcb093 build: update node version number in .nvmrc (#20832)

--- esm5/http.js
+++ esm5/http.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.1.0-9fbf850
+ * @license Angular v5.1.0-8bcb093
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2730,7 +2730,7 @@ var JsonpModule = /** @class */ (function () {
 /**
  * @deprecated use \@angular/common/http instead
  */
-var VERSION = new Version('5.1.0-9fbf850');
+var VERSION = new Version('5.1.0-8bcb093');
 
 /**
  * @fileoverview added by tsickle
