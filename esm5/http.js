371fa6e3b4d3a7bf590b3f49fa03b6444967a5b9
Jason Aden <jasonaden@google.com>
2017-12-12 20:03:17 +0000

    8c52088 fix(compiler-cli): merge @fileoverview comments. (#20870)

--- esm5/http.js
+++ esm5/http.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.1.0-add3589
+ * @license Angular v5.1.0-8c52088
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2730,7 +2730,7 @@ var JsonpModule = /** @class */ (function () {
 /**
  * @deprecated use \@angular/common/http instead
  */
-var VERSION = new Version('5.1.0-add3589');
+var VERSION = new Version('5.1.0-8c52088');
 
 /**
  * @fileoverview added by tsickle
