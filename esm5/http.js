a3ccd6bb924912cd26213303c31eace23c9ff448
Jason Aden <jasonaden@google.com>
2017-12-12 00:24:34 +0000

    8a5f0f7 build: pin ChromeDriver version (#20940)

--- esm5/http.js
+++ esm5/http.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.1.0-8bcb093
+ * @license Angular v5.1.0-8a5f0f7
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2730,7 +2730,7 @@ var JsonpModule = /** @class */ (function () {
 /**
  * @deprecated use \@angular/common/http instead
  */
-var VERSION = new Version('5.1.0-8bcb093');
+var VERSION = new Version('5.1.0-8a5f0f7');
 
 /**
  * @fileoverview added by tsickle
