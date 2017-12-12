3b59927f30c667fbcff665b968cbe0b3ddb65959
Jason Aden <jasonaden@google.com>
2017-12-12 00:35:07 +0000

    1f46949 ci: use container version in cache key (#20952)

--- esm5/http.js
+++ esm5/http.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.1.0-8a5f0f7
+ * @license Angular v5.1.0-1f46949
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2730,7 +2730,7 @@ var JsonpModule = /** @class */ (function () {
 /**
  * @deprecated use \@angular/common/http instead
  */
-var VERSION = new Version('5.1.0-8a5f0f7');
+var VERSION = new Version('5.1.0-1f46949');
 
 /**
  * @fileoverview added by tsickle
