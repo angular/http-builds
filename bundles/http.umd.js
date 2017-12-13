4c57c4b7407aace85397e9758feb59fad2414506
Jason Aden <jasonaden@google.com>
2017-12-13 19:56:35 +0000

    7671417 docs: add changelog for 5.1.1

--- bundles/http.umd.js
+++ bundles/http.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.1.0-b3eb1db
+ * @license Angular v5.1.0-7671417
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -36,7 +36,7 @@ function __extends(d, b) {
 }
 
 /**
- * @license Angular v5.1.0-b3eb1db
+ * @license Angular v5.1.0-7671417
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2762,7 +2762,7 @@ var JsonpModule = /** @class */ (function () {
 /**
  * @deprecated use \@angular/common/http instead
  */
-var VERSION = new _angular_core.Version('5.1.0-b3eb1db');
+var VERSION = new _angular_core.Version('5.1.0-7671417');
 
 exports.BrowserXhr = BrowserXhr;
 exports.JSONPBackend = JSONPBackend;
--- esm5/http.js
+++ esm5/http.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.1.0-b3eb1db
+ * @license Angular v5.1.0-7671417
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2730,7 +2730,7 @@ var JsonpModule = /** @class */ (function () {
 /**
  * @deprecated use \@angular/common/http instead
  */
-var VERSION = new Version('5.1.0-b3eb1db');
+var VERSION = new Version('5.1.0-7671417');
 
 /**
  * @fileoverview added by tsickle
