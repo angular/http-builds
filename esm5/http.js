54c48f2899a108d0d3c4448fba8dc760831b0c5e
Alex Rickabaugh <alx+alxhub@alxandria.net>
2017-12-14 16:52:16 +0000

    6d57cb0 ci: parallelize bazel build and test

--- esm5/http.js
+++ esm5/http.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.2.0-beta.0-6e2a8a2
+ * @license Angular v5.2.0-beta.0-6d57cb0
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2730,7 +2730,7 @@ var JsonpModule = /** @class */ (function () {
 /**
  * @deprecated use \@angular/common/http instead
  */
-var VERSION = new Version('5.2.0-beta.0-6e2a8a2');
+var VERSION = new Version('5.2.0-beta.0-6d57cb0');
 
 /**
  * @fileoverview added by tsickle
