f3fd115e67bbc6e08842975492145660e4a3ac9c
Alex Rickabaugh <alx+alxhub@alxandria.net>
2017-12-14 17:07:38 +0000

    b89e7c2 ci(aio): move e2e tests to non-optional job

--- bundles/http.umd.js
+++ bundles/http.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.2.0-beta.0-cbe7e39
+ * @license Angular v5.2.0-beta.0-b89e7c2
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -36,7 +36,7 @@ function __extends(d, b) {
 }
 
 /**
- * @license Angular v5.2.0-beta.0-cbe7e39
+ * @license Angular v5.2.0-beta.0-b89e7c2
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2762,7 +2762,7 @@ var JsonpModule = /** @class */ (function () {
 /**
  * @deprecated use \@angular/common/http instead
  */
-var VERSION = new _angular_core.Version('5.2.0-beta.0-cbe7e39');
+var VERSION = new _angular_core.Version('5.2.0-beta.0-b89e7c2');
 
 exports.BrowserXhr = BrowserXhr;
 exports.JSONPBackend = JSONPBackend;
