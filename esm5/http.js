92b21601b9e5c1f8d882c157d4caa68c062954c7
Alex Rickabaugh <alx+alxhub@alxandria.net>
2017-12-15 16:18:35 +0000

    d91ff17 fix(compiler): generate the correct imports for summary type-check

--- esm5/http.js
+++ esm5/http.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.2.0-beta.0-05ff6c0
+ * @license Angular v5.2.0-beta.0-d91ff17
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2730,7 +2730,7 @@ var JsonpModule = /** @class */ (function () {
 /**
  * @deprecated use \@angular/common/http instead
  */
-var VERSION = new Version('5.2.0-beta.0-05ff6c0');
+var VERSION = new Version('5.2.0-beta.0-d91ff17');
 
 /**
  * @fileoverview added by tsickle
