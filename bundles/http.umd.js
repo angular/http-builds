e7377295290e7129500127d19ceabdcbfbfa142e
Alex Rickabaugh <alx+alxhub@alxandria.net>
2017-12-18 20:32:11 +0000

    57bed3f docs(core): move core examples into `examples/core/` directory

--- bundles/http.umd.js
+++ bundles/http.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.1.1-c011ffa
+ * @license Angular v5.1.1-57bed3f
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -36,7 +36,7 @@ function __extends(d, b) {
 }
 
 /**
- * @license Angular v5.1.1-c011ffa
+ * @license Angular v5.1.1-57bed3f
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2762,7 +2762,7 @@ var JsonpModule = /** @class */ (function () {
 /**
  * @deprecated use \@angular/common/http instead
  */
-var VERSION = new _angular_core.Version('5.1.1-c011ffa');
+var VERSION = new _angular_core.Version('5.1.1-57bed3f');
 
 exports.BrowserXhr = BrowserXhr;
 exports.JSONPBackend = JSONPBackend;
