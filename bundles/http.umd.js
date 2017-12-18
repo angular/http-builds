244bc2a21312491b3faff6a17ac82b7531b6ce05
Alex Rickabaugh <alx+alxhub@alxandria.net>
2017-12-18 20:30:38 +0000

    0575135 fix(upgrade): replaces get/setAngularLib with get/setAngularJSGlobal

--- bundles/http.umd.js
+++ bundles/http.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.2.0-beta.0-3846f19
+ * @license Angular v5.2.0-beta.0-0575135
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -36,7 +36,7 @@ function __extends(d, b) {
 }
 
 /**
- * @license Angular v5.2.0-beta.0-3846f19
+ * @license Angular v5.2.0-beta.0-0575135
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2762,7 +2762,7 @@ var JsonpModule = /** @class */ (function () {
 /**
  * @deprecated use \@angular/common/http instead
  */
-var VERSION = new _angular_core.Version('5.2.0-beta.0-3846f19');
+var VERSION = new _angular_core.Version('5.2.0-beta.0-0575135');
 
 exports.BrowserXhr = BrowserXhr;
 exports.JSONPBackend = JSONPBackend;
