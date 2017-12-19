874d16b953de48dbe131d8b7bc37cc064cc31c30
Alex Eagle <alexeagle@google.com>
2017-12-19 17:54:28 +0000

    057b357 docs: Update BAZEL.md

--- esm2015/http.js
+++ esm2015/http.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.1.0-43868d2
+ * @license Angular v5.2.0-beta.0-057b357
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2160,7 +2160,7 @@ JsonpModule.ctorParameters = () => [];
 /**
  * @deprecated use \@angular/common/http instead
  */
-const VERSION = new Version('5.1.0-43868d2');
+const VERSION = new Version('5.2.0-beta.0-057b357');
 
 /**
  * @fileoverview added by tsickle
