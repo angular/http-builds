5e71478d75322faa7bf1c3f18b7db56d18325bd0
Igor Minar <igor@angularjs.org>
2017-12-21 20:26:23 +0000

    871ece6 fix(animations): renaming issue with DOMAnimation. (#21125)

--- esm5/http.js
+++ esm5/http.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.2.0-beta.1-abca7c0
+ * @license Angular v5.2.0-beta.1-871ece6
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2730,7 +2730,7 @@ var JsonpModule = /** @class */ (function () {
 /**
  * @deprecated use \@angular/common/http instead
  */
-var VERSION = new Version('5.2.0-beta.1-abca7c0');
+var VERSION = new Version('5.2.0-beta.1-871ece6');
 
 /**
  * @fileoverview added by tsickle
