aee9bc5547017114ff4570defb7de96aad86f988
Igor Minar <igor@angularjs.org>
2017-12-21 22:24:12 +0000

    4337768 build: fix pullapprove (#21140)

--- esm5/http.js
+++ esm5/http.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.1.2-dc53248
+ * @license Angular v5.1.2-4337768
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2730,7 +2730,7 @@ var JsonpModule = /** @class */ (function () {
 /**
  * @deprecated use \@angular/common/http instead
  */
-var VERSION = new Version('5.1.2-dc53248');
+var VERSION = new Version('5.1.2-4337768');
 
 /**
  * @fileoverview added by tsickle
