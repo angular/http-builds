75644a1cfe3951cd13e60296e9e7b4ad6a282129
Igor Minar <igor@angularjs.org>
2017-12-21 20:24:31 +0000

    d1f4500 fix(animations): renaming issue with DOMAnimation. (#21125)

--- esm5/http.js
+++ esm5/http.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.1.2-6353b77
+ * @license Angular v5.1.2-d1f4500
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2730,7 +2730,7 @@ var JsonpModule = /** @class */ (function () {
 /**
  * @deprecated use \@angular/common/http instead
  */
-var VERSION = new Version('5.1.2-6353b77');
+var VERSION = new Version('5.1.2-d1f4500');
 
 /**
  * @fileoverview added by tsickle
