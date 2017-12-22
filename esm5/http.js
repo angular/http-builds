fa944c149515979f10a17851b47cf806764d93e3
Igor Minar <igor@angularjs.org>
2017-12-22 17:22:38 +0000

    fa0e8ef fix(common): handle JS floating point errors in percent pipe (#20329)

--- esm5/http.js
+++ esm5/http.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.1.2-0aa6341
+ * @license Angular v5.1.2-fa0e8ef
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2730,7 +2730,7 @@ var JsonpModule = /** @class */ (function () {
 /**
  * @deprecated use \@angular/common/http instead
  */
-var VERSION = new Version('5.1.2-0aa6341');
+var VERSION = new Version('5.1.2-fa0e8ef');
 
 /**
  * @fileoverview added by tsickle
