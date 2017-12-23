1eaaff9ccb24dd51895edd0edbcb449d05264a68
Igor Minar <igor@angular.io>
2017-12-23 06:09:54 +0000

    efd9c09 docs(changelog): fix typo in 5.1.1 (#21007)

--- bundles/http.umd.js
+++ bundles/http.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.2.0-beta.1-83d207d
+ * @license Angular v5.2.0-beta.1-efd9c09
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -36,7 +36,7 @@ function __extends(d, b) {
 }
 
 /**
- * @license Angular v5.2.0-beta.1-83d207d
+ * @license Angular v5.2.0-beta.1-efd9c09
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2751,7 +2751,7 @@ var JsonpModule = /** @class */ (function () {
 /**
  * @deprecated use \@angular/common/http instead
  */
-var VERSION = new _angular_core.Version('5.2.0-beta.1-83d207d');
+var VERSION = new _angular_core.Version('5.2.0-beta.1-efd9c09');
 
 exports.BrowserXhr = BrowserXhr;
 exports.JSONPBackend = JSONPBackend;
--- esm5/http.js
+++ esm5/http.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.2.0-beta.1-83d207d
+ * @license Angular v5.2.0-beta.1-efd9c09
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2719,7 +2719,7 @@ var JsonpModule = /** @class */ (function () {
 /**
  * @deprecated use \@angular/common/http instead
  */
-var VERSION = new Version('5.2.0-beta.1-83d207d');
+var VERSION = new Version('5.2.0-beta.1-efd9c09');
 
 /**
  * @fileoverview added by tsickle
