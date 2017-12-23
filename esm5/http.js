15d673abd2a4d233af56c5c34d255a67237281bd
Igor Minar <igor@angular.io>
2017-12-23 05:56:58 +0000

    5de91fe docs(forms): add text about min() and max() as functions (#21110)

--- esm5/http.js
+++ esm5/http.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.1.2-d787b55
+ * @license Angular v5.1.2-5de91fe
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2730,7 +2730,7 @@ var JsonpModule = /** @class */ (function () {
 /**
  * @deprecated use \@angular/common/http instead
  */
-var VERSION = new Version('5.1.2-d787b55');
+var VERSION = new Version('5.1.2-5de91fe');
 
 /**
  * @fileoverview added by tsickle
