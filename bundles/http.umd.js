24d56283c19a20788dad8a683a9d96f56aedadaf
Igor Minar <igor@angular.io>
2017-12-23 05:54:59 +0000

    d787b55 build(common): generate ts declarations for i18n locale files (#21127)

--- bundles/http.umd.js
+++ bundles/http.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.1.2-3e34fa8
+ * @license Angular v5.1.2-d787b55
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -36,7 +36,7 @@ function __extends(d, b) {
 }
 
 /**
- * @license Angular v5.1.2-3e34fa8
+ * @license Angular v5.1.2-d787b55
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2762,7 +2762,7 @@ var JsonpModule = /** @class */ (function () {
 /**
  * @deprecated use \@angular/common/http instead
  */
-var VERSION = new _angular_core.Version('5.1.2-3e34fa8');
+var VERSION = new _angular_core.Version('5.1.2-d787b55');
 
 exports.BrowserXhr = BrowserXhr;
 exports.JSONPBackend = JSONPBackend;
--- esm5/http.js
+++ esm5/http.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.1.2-3e34fa8
+ * @license Angular v5.1.2-d787b55
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2730,7 +2730,7 @@ var JsonpModule = /** @class */ (function () {
 /**
  * @deprecated use \@angular/common/http instead
  */
-var VERSION = new Version('5.1.2-3e34fa8');
+var VERSION = new Version('5.1.2-d787b55');
 
 /**
  * @fileoverview added by tsickle
