db0fe3d94069b6a75eb2dc237401de52f69abe03
Miško Hevery <misko@hevery.com>
2017-12-28 00:03:39 +0000

    bc7a6d7 build: force upstream fetch before merge (#21192)

--- bundles/http.umd.js
+++ bundles/http.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.2.0-beta.1-9f538a6
+ * @license Angular v5.2.0-beta.1-bc7a6d7
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -36,7 +36,7 @@ function __extends(d, b) {
 }
 
 /**
- * @license Angular v5.2.0-beta.1-9f538a6
+ * @license Angular v5.2.0-beta.1-bc7a6d7
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2751,7 +2751,7 @@ var JsonpModule = /** @class */ (function () {
 /**
  * @deprecated use \@angular/common/http instead
  */
-var VERSION = new _angular_core.Version('5.2.0-beta.1-9f538a6');
+var VERSION = new _angular_core.Version('5.2.0-beta.1-bc7a6d7');
 
 exports.BrowserXhr = BrowserXhr;
 exports.JSONPBackend = JSONPBackend;
--- esm5/http.js
+++ esm5/http.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.2.0-beta.1-9f538a6
+ * @license Angular v5.2.0-beta.1-bc7a6d7
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2719,7 +2719,7 @@ var JsonpModule = /** @class */ (function () {
 /**
  * @deprecated use \@angular/common/http instead
  */
-var VERSION = new Version('5.2.0-beta.1-9f538a6');
+var VERSION = new Version('5.2.0-beta.1-bc7a6d7');
 
 /**
  * @fileoverview added by tsickle
