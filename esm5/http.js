14e10d134e410d822d499c3fe31afacd0a2a4180
Miško Hevery <misko@hevery.com>
2018-01-02 21:10:19 +0000

    48a1f32 fix(language-service): ignore null metadatas (#20557)

--- esm5/http.js
+++ esm5/http.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.1.2-66043e0
+ * @license Angular v5.1.2-48a1f32
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2730,7 +2730,7 @@ var JsonpModule = /** @class */ (function () {
 /**
  * @deprecated use \@angular/common/http instead
  */
-var VERSION = new Version('5.1.2-66043e0');
+var VERSION = new Version('5.1.2-48a1f32');
 
 /**
  * @fileoverview added by tsickle
