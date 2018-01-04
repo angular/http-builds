6e5c747c023023c5b3fb1e6745a774385fb2497b
Kara Erickson <karakara@google.com>
2018-01-04 22:39:20 +0000

    cc0b0a4 fix(aio): do not redirect pages on "next" deployment (#21027)

--- esm5/http.js
+++ esm5/http.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.1.3-b9d8ac5
+ * @license Angular v5.1.3-cc0b0a4
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2730,7 +2730,7 @@ var JsonpModule = /** @class */ (function () {
 /**
  * @deprecated use \@angular/common/http instead
  */
-var VERSION = new Version('5.1.3-b9d8ac5');
+var VERSION = new Version('5.1.3-cc0b0a4');
 
 /**
  * @fileoverview added by tsickle
