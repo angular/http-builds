3c48aa37b718130330f0d62a4489a32e366718eb
Kara Erickson <karakara@google.com>
2018-01-04 23:00:25 +0000

    d79a0ba docs(aio): add missing imports to interceptor example (#21259)

--- bundles/http.umd.js
+++ bundles/http.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.1.3-b7307c9
+ * @license Angular v5.1.3-d79a0ba
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -36,7 +36,7 @@ function __extends(d, b) {
 }
 
 /**
- * @license Angular v5.1.3-b7307c9
+ * @license Angular v5.1.3-d79a0ba
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2762,7 +2762,7 @@ var JsonpModule = /** @class */ (function () {
 /**
  * @deprecated use \@angular/common/http instead
  */
-var VERSION = new _angular_core.Version('5.1.3-b7307c9');
+var VERSION = new _angular_core.Version('5.1.3-d79a0ba');
 
 exports.BrowserXhr = BrowserXhr;
 exports.JSONPBackend = JSONPBackend;
