85fb20e64b9aea9faeb257e4d8842700ecaaa311
Kara Erickson <karakara@google.com>
2018-01-04 19:46:16 +0000

    b9d8ac5 fix(aio): don't use Object.keys on NamedNodeMap to prevent SEO errors (#21305)

--- bundles/http.umd.js
+++ bundles/http.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.1.3-fc217f5
+ * @license Angular v5.1.3-b9d8ac5
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -36,7 +36,7 @@ function __extends(d, b) {
 }
 
 /**
- * @license Angular v5.1.3-fc217f5
+ * @license Angular v5.1.3-b9d8ac5
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2762,7 +2762,7 @@ var JsonpModule = /** @class */ (function () {
 /**
  * @deprecated use \@angular/common/http instead
  */
-var VERSION = new _angular_core.Version('5.1.3-fc217f5');
+var VERSION = new _angular_core.Version('5.1.3-b9d8ac5');
 
 exports.BrowserXhr = BrowserXhr;
 exports.JSONPBackend = JSONPBackend;
--- esm5/http.js
+++ esm5/http.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.1.3-fc217f5
+ * @license Angular v5.1.3-b9d8ac5
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2730,7 +2730,7 @@ var JsonpModule = /** @class */ (function () {
 /**
  * @deprecated use \@angular/common/http instead
  */
-var VERSION = new Version('5.1.3-fc217f5');
+var VERSION = new Version('5.1.3-b9d8ac5');
 
 /**
  * @fileoverview added by tsickle
