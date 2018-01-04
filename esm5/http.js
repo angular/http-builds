916e565bdda8dc6eb68ede4122e8895a750bf81b
Kara Erickson <karakara@google.com>
2018-01-04 01:28:57 +0000

    fc217f5 refactor(aio): use one argument for `DocViewer` error reporting (#21293)

--- esm5/http.js
+++ esm5/http.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.1.3-d138b38
+ * @license Angular v5.1.3-fc217f5
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2730,7 +2730,7 @@ var JsonpModule = /** @class */ (function () {
 /**
  * @deprecated use \@angular/common/http instead
  */
-var VERSION = new Version('5.1.3-d138b38');
+var VERSION = new Version('5.1.3-fc217f5');
 
 /**
  * @fileoverview added by tsickle
