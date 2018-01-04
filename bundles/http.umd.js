3472dc072c8bbcbf90f0f96e5be78fb3e806c588
Misko Hevery <misko@hevery.com>
2018-01-04 00:49:40 +0000

    cdc66f6 docs: add changelog for 5.2.0-rc.0

--- bundles/http.umd.js
+++ bundles/http.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.2.0-beta.1-3e47ea2
+ * @license Angular v5.2.0-rc.0-cdc66f6
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -36,7 +36,7 @@ function __extends(d, b) {
 }
 
 /**
- * @license Angular v5.2.0-beta.1-3e47ea2
+ * @license Angular v5.2.0-rc.0-cdc66f6
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2751,7 +2751,7 @@ var JsonpModule = /** @class */ (function () {
 /**
  * @deprecated use \@angular/common/http instead
  */
-var VERSION = new _angular_core.Version('5.2.0-beta.1-3e47ea2');
+var VERSION = new _angular_core.Version('5.2.0-rc.0-cdc66f6');
 
 exports.BrowserXhr = BrowserXhr;
 exports.JSONPBackend = JSONPBackend;
