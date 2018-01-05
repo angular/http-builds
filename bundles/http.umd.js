0db0fbda74aa5129dc51c08c3c9572b0953164ac
Kara Erickson <karakara@google.com>
2018-01-05 22:51:29 +0000

    db55e86 fix(core): make all render3 tests pass in IE9 (#21279)

--- bundles/http.umd.js
+++ bundles/http.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.2.0-rc.0-fecf768
+ * @license Angular v5.2.0-rc.0-db55e86
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -36,7 +36,7 @@ function __extends(d, b) {
 }
 
 /**
- * @license Angular v5.2.0-rc.0-fecf768
+ * @license Angular v5.2.0-rc.0-db55e86
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2751,7 +2751,7 @@ var JsonpModule = /** @class */ (function () {
 /**
  * @deprecated use \@angular/common/http instead
  */
-var VERSION = new _angular_core.Version('5.2.0-rc.0-fecf768');
+var VERSION = new _angular_core.Version('5.2.0-rc.0-db55e86');
 
 exports.BrowserXhr = BrowserXhr;
 exports.JSONPBackend = JSONPBackend;
--- esm5/http.js
+++ esm5/http.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.2.0-rc.0-fecf768
+ * @license Angular v5.2.0-rc.0-db55e86
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2719,7 +2719,7 @@ var JsonpModule = /** @class */ (function () {
 /**
  * @deprecated use \@angular/common/http instead
  */
-var VERSION = new Version('5.2.0-rc.0-fecf768');
+var VERSION = new Version('5.2.0-rc.0-db55e86');
 
 /**
  * @fileoverview added by tsickle
