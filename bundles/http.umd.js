4afcde6c483681ea89a0bcd09247fa2f5c18acd5
Kara Erickson <karakara@google.com>
2018-01-05 23:46:13 +0000

    8f3ea33 build(core): update payload size of hello-world in patch branch (#21343)

--- bundles/http.umd.js
+++ bundles/http.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.1.3-839d24e
+ * @license Angular v5.1.3-8f3ea33
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -36,7 +36,7 @@ function __extends(d, b) {
 }
 
 /**
- * @license Angular v5.1.3-839d24e
+ * @license Angular v5.1.3-8f3ea33
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2762,7 +2762,7 @@ var JsonpModule = /** @class */ (function () {
 /**
  * @deprecated use \@angular/common/http instead
  */
-var VERSION = new _angular_core.Version('5.1.3-839d24e');
+var VERSION = new _angular_core.Version('5.1.3-8f3ea33');
 
 exports.BrowserXhr = BrowserXhr;
 exports.JSONPBackend = JSONPBackend;
