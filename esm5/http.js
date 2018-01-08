1aadcc1779ad7ef8b9fdb0bc7f6bf8a738bebd77
Kara Erickson <karakara@google.com>
2018-01-08 18:38:20 +0000

    b15d50a build(core): improve payload size failure message (#21346)

--- esm5/http.js
+++ esm5/http.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.2.0-rc.0-db55e86
+ * @license Angular v5.2.0-rc.0-b15d50a
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2719,7 +2719,7 @@ var JsonpModule = /** @class */ (function () {
 /**
  * @deprecated use \@angular/common/http instead
  */
-var VERSION = new Version('5.2.0-rc.0-db55e86');
+var VERSION = new Version('5.2.0-rc.0-b15d50a');
 
 /**
  * @fileoverview added by tsickle
