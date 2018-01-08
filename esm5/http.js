3f12222030836dd65c4839b4737d7c51e94c1833
Kara Erickson <karakara@google.com>
2018-01-08 19:33:12 +0000

    7269fe5 ci: reformat itegration/_payload-limits.json to be easier to read (#21344)

--- esm5/http.js
+++ esm5/http.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.2.0-rc.0-e994752
+ * @license Angular v5.2.0-rc.0-7269fe5
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2719,7 +2719,7 @@ var JsonpModule = /** @class */ (function () {
 /**
  * @deprecated use \@angular/common/http instead
  */
-var VERSION = new Version('5.2.0-rc.0-e994752');
+var VERSION = new Version('5.2.0-rc.0-7269fe5');
 
 /**
  * @fileoverview added by tsickle
