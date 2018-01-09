f82e12842a2194629e779d3633bb31c71e429996
Kara Erickson <karakara@google.com>
2018-01-09 18:50:30 +0000

    39f81b2 build: upgrade Bazel to 0.9.0 (#21335)

--- esm5/http.js
+++ esm5/http.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.1.3-0602aaa
+ * @license Angular v5.1.3-39f81b2
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2730,7 +2730,7 @@ var JsonpModule = /** @class */ (function () {
 /**
  * @deprecated use \@angular/common/http instead
  */
-var VERSION = new Version('5.1.3-0602aaa');
+var VERSION = new Version('5.1.3-39f81b2');
 
 /**
  * @fileoverview added by tsickle
