8b95b73193ec78def484f868471dc858faf0cde1
Kara Erickson <karakara@google.com>
2018-01-09 20:43:30 +0000

    229b76c ci: temporary work-around for Travis issue (#21422)

--- bundles/http.umd.js
+++ bundles/http.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.2.0-rc.0-80a5b91
+ * @license Angular v5.2.0-rc.0-229b76c
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -36,7 +36,7 @@ function __extends(d, b) {
 }
 
 /**
- * @license Angular v5.2.0-rc.0-80a5b91
+ * @license Angular v5.2.0-rc.0-229b76c
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2751,7 +2751,7 @@ var JsonpModule = /** @class */ (function () {
 /**
  * @deprecated use \@angular/common/http instead
  */
-var VERSION = new _angular_core.Version('5.2.0-rc.0-80a5b91');
+var VERSION = new _angular_core.Version('5.2.0-rc.0-229b76c');
 
 exports.BrowserXhr = BrowserXhr;
 exports.JSONPBackend = JSONPBackend;
