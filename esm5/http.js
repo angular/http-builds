9887f84a079626d46e75558ea3c5892032ba0fe7
Kara Erickson <karakara@google.com>
2018-01-09 00:32:55 +0000

    27196b6 docs(aio): fix punctuation for clarity (#21325) PR Close #21325

--- esm5/http.js
+++ esm5/http.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.2.0-rc.0-6040ee3
+ * @license Angular v5.2.0-rc.0-27196b6
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2719,7 +2719,7 @@ var JsonpModule = /** @class */ (function () {
 /**
  * @deprecated use \@angular/common/http instead
  */
-var VERSION = new Version('5.2.0-rc.0-6040ee3');
+var VERSION = new Version('5.2.0-rc.0-27196b6');
 
 /**
  * @fileoverview added by tsickle
