4c1aec08c5f47415de8517b8f6c244c4a3145e18
Alex Eagle <alexeagle@google.com>
2018-01-10 23:40:16 +0000

    aad1126 docs: update the release schedule with v6 info (#21435)

--- esm2015/http.js
+++ esm2015/http.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.2.0-7b463df
+ * @license Angular v5.2.0-aad1126
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2149,7 +2149,7 @@ JsonpModule.ctorParameters = () => [];
 /**
  * @deprecated use \@angular/common/http instead
  */
-const VERSION = new Version('5.2.0-7b463df');
+const VERSION = new Version('5.2.0-aad1126');
 
 /**
  * @fileoverview added by tsickle
