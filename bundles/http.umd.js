1e4d340b1269838d4786fa39da8baa054086b683
Alex Eagle <alexeagle@google.com>
2018-01-10 23:50:41 +0000

    a33ff2c docs(ivy): add Ivy as a recognized label (#21428)

--- bundles/http.umd.js
+++ bundles/http.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.2.0-92984ba
+ * @license Angular v5.2.0-a33ff2c
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -36,7 +36,7 @@ function __extends(d, b) {
 }
 
 /**
- * @license Angular v5.2.0-92984ba
+ * @license Angular v5.2.0-a33ff2c
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2751,7 +2751,7 @@ var JsonpModule = /** @class */ (function () {
 /**
  * @deprecated use \@angular/common/http instead
  */
-var VERSION = new _angular_core.Version('5.2.0-92984ba');
+var VERSION = new _angular_core.Version('5.2.0-a33ff2c');
 
 exports.BrowserXhr = BrowserXhr;
 exports.JSONPBackend = JSONPBackend;
--- esm5/http.js
+++ esm5/http.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.2.0-92984ba
+ * @license Angular v5.2.0-a33ff2c
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2719,7 +2719,7 @@ var JsonpModule = /** @class */ (function () {
 /**
  * @deprecated use \@angular/common/http instead
  */
-var VERSION = new Version('5.2.0-92984ba');
+var VERSION = new Version('5.2.0-a33ff2c');
 
 /**
  * @fileoverview added by tsickle
