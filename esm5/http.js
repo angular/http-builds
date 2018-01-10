ef8012355cd1631e1317b3380b0830dae386de24
Alex Eagle <alexeagle@google.com>
2018-01-10 21:03:34 +0000

    0de219a docs(bazel): document an installation issue (#21401)

--- esm5/http.js
+++ esm5/http.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.2.0-ff6047c
+ * @license Angular v5.2.0-0de219a
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2719,7 +2719,7 @@ var JsonpModule = /** @class */ (function () {
 /**
  * @deprecated use \@angular/common/http instead
  */
-var VERSION = new Version('5.2.0-ff6047c');
+var VERSION = new Version('5.2.0-0de219a');
 
 /**
  * @fileoverview added by tsickle
