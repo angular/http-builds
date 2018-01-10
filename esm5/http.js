69b29526cfd4710bc656888ae9ab44d05829d787
Alex Eagle <alexeagle@google.com>
2018-01-10 20:39:36 +0000

    51eb3d4 fix(service-worker): properly handle invalid hashes in all scenarios (#21288)

--- esm5/http.js
+++ esm5/http.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.2.0-48c1898
+ * @license Angular v5.2.0-51eb3d4
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2719,7 +2719,7 @@ var JsonpModule = /** @class */ (function () {
 /**
  * @deprecated use \@angular/common/http instead
  */
-var VERSION = new Version('5.2.0-48c1898');
+var VERSION = new Version('5.2.0-51eb3d4');
 
 /**
  * @fileoverview added by tsickle
