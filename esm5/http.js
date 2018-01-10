691e7debae6ff4eca9624bb3c794d4a6f44f6d28
Alex Eagle <alexeagle@google.com>
2018-01-10 20:25:34 +0000

    d2aa8ac fix(animations): fix increment/decrement aliases example (#18323)

--- esm5/http.js
+++ esm5/http.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.2.0-ef956a2
+ * @license Angular v5.2.0-d2aa8ac
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2719,7 +2719,7 @@ var JsonpModule = /** @class */ (function () {
 /**
  * @deprecated use \@angular/common/http instead
  */
-var VERSION = new Version('5.2.0-ef956a2');
+var VERSION = new Version('5.2.0-d2aa8ac');
 
 /**
  * @fileoverview added by tsickle
