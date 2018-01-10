ed361e8c1fc9c13e61806b7f1acbdad3e5ede272
Alex Eagle <alexeagle@google.com>
2018-01-10 20:19:09 +0000

    ef956a2 build: add bazel rulse for benchmarks (#21436)

--- esm5/http.js
+++ esm5/http.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.2.0-fc3e7e0
+ * @license Angular v5.2.0-ef956a2
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2719,7 +2719,7 @@ var JsonpModule = /** @class */ (function () {
 /**
  * @deprecated use \@angular/common/http instead
  */
-var VERSION = new Version('5.2.0-fc3e7e0');
+var VERSION = new Version('5.2.0-ef956a2');
 
 /**
  * @fileoverview added by tsickle
