8f9ed39e51c1eb3ecab291bcd7356857ec0c067c
Alex Eagle <alexeagle@google.com>
2018-01-10 22:49:18 +0000

    7b463df fix(aio): preserve static Observable methods (#21351)

--- esm5/http.js
+++ esm5/http.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.2.0-650f5fb
+ * @license Angular v5.2.0-7b463df
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2719,7 +2719,7 @@ var JsonpModule = /** @class */ (function () {
 /**
  * @deprecated use \@angular/common/http instead
  */
-var VERSION = new Version('5.2.0-650f5fb');
+var VERSION = new Version('5.2.0-7b463df');
 
 /**
  * @fileoverview added by tsickle
