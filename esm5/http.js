309e4dc4e9d6e6f9ca1b02cae23a4fb9bb5f9fd5
Alex Eagle <alexeagle@google.com>
2018-01-10 20:45:54 +0000

    a76ab1f docs(router): remove extra brackets (#21294)

--- esm5/http.js
+++ esm5/http.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.2.0-72246d4
+ * @license Angular v5.2.0-a76ab1f
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2719,7 +2719,7 @@ var JsonpModule = /** @class */ (function () {
 /**
  * @deprecated use \@angular/common/http instead
  */
-var VERSION = new Version('5.2.0-72246d4');
+var VERSION = new Version('5.2.0-a76ab1f');
 
 /**
  * @fileoverview added by tsickle
