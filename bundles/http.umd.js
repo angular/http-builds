c99246b8b41e2fb5ee7e1584db5ee809bd77bced
Kara Erickson <karakara@google.com>
2018-01-10 19:33:09 +0000

    908129e docs(core): provide note for unit test changes with the `ng-star-inserted` className (#21450)

--- bundles/http.umd.js
+++ bundles/http.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.2.0-04dd971
+ * @license Angular v5.2.0-908129e
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -36,7 +36,7 @@ function __extends(d, b) {
 }
 
 /**
- * @license Angular v5.2.0-04dd971
+ * @license Angular v5.2.0-908129e
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2751,7 +2751,7 @@ var JsonpModule = /** @class */ (function () {
 /**
  * @deprecated use \@angular/common/http instead
  */
-var VERSION = new _angular_core.Version('5.2.0-04dd971');
+var VERSION = new _angular_core.Version('5.2.0-908129e');
 
 exports.BrowserXhr = BrowserXhr;
 exports.JSONPBackend = JSONPBackend;
--- esm5/http.js
+++ esm5/http.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.2.0-04dd971
+ * @license Angular v5.2.0-908129e
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2719,7 +2719,7 @@ var JsonpModule = /** @class */ (function () {
 /**
  * @deprecated use \@angular/common/http instead
  */
-var VERSION = new Version('5.2.0-04dd971');
+var VERSION = new Version('5.2.0-908129e');
 
 /**
  * @fileoverview added by tsickle
