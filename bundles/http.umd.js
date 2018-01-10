436bdb7915d42bb7c43d91079f1f3459289081ee
Alex Eagle <alexeagle@google.com>
2018-01-10 21:50:09 +0000

    c32e833 fix(compiler): cache external reference resolution (#21359)

--- bundles/http.umd.js
+++ bundles/http.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.2.0-7bdd9ae
+ * @license Angular v5.2.0-c32e833
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -36,7 +36,7 @@ function __extends(d, b) {
 }
 
 /**
- * @license Angular v5.2.0-7bdd9ae
+ * @license Angular v5.2.0-c32e833
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2751,7 +2751,7 @@ var JsonpModule = /** @class */ (function () {
 /**
  * @deprecated use \@angular/common/http instead
  */
-var VERSION = new _angular_core.Version('5.2.0-7bdd9ae');
+var VERSION = new _angular_core.Version('5.2.0-c32e833');
 
 exports.BrowserXhr = BrowserXhr;
 exports.JSONPBackend = JSONPBackend;
