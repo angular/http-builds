9d7d10650c46817dcd51cbec0eb74e9a4a5304b5
Alex Eagle <alexeagle@google.com>
2018-01-10 21:28:23 +0000

    5ede67c build: move repeated tsconfig attributes to a macro (#20964)

--- bundles/http.umd.js
+++ bundles/http.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.2.0-31b671a
+ * @license Angular v5.2.0-5ede67c
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -36,7 +36,7 @@ function __extends(d, b) {
 }
 
 /**
- * @license Angular v5.2.0-31b671a
+ * @license Angular v5.2.0-5ede67c
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2751,7 +2751,7 @@ var JsonpModule = /** @class */ (function () {
 /**
  * @deprecated use \@angular/common/http instead
  */
-var VERSION = new _angular_core.Version('5.2.0-31b671a');
+var VERSION = new _angular_core.Version('5.2.0-5ede67c');
 
 exports.BrowserXhr = BrowserXhr;
 exports.JSONPBackend = JSONPBackend;
