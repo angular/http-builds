20a4abcfebd37e506bd388228770232706a5b049
Alex Eagle <alexeagle@google.com>
2018-01-11 15:04:46 +0000

    55fd82e docs: fix release schedule date (#21469) PR Close #21469

--- esm2015/http.js
+++ esm2015/http.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.2.0-7f3d0bb
+ * @license Angular v5.2.0-55fd82e
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2149,7 +2149,7 @@ JsonpModule.ctorParameters = () => [];
 /**
  * @deprecated use \@angular/common/http instead
  */
-const VERSION = new Version('5.2.0-7f3d0bb');
+const VERSION = new Version('5.2.0-55fd82e');
 
 /**
  * @fileoverview added by tsickle
