213da21978e0a8792d3c55c1ac2d24ed63b81889
Alex Eagle <alexeagle@google.com>
2018-01-11 15:22:43 +0000

    00300f6 docs(forms): clarify note in reactive forms doc (#21134)

--- esm5/http.js
+++ esm5/http.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.2.0-5eaaac3
+ * @license Angular v5.2.0-00300f6
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2719,7 +2719,7 @@ var JsonpModule = /** @class */ (function () {
 /**
  * @deprecated use \@angular/common/http instead
  */
-var VERSION = new Version('5.2.0-5eaaac3');
+var VERSION = new Version('5.2.0-00300f6');
 
 /**
  * @fileoverview added by tsickle
