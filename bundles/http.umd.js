ec2e8a167937918e96ebbb555f67503cca71a866
Alex Eagle <alexeagle@google.com>
2018-01-11 00:47:58 +0000

    3db02d2 docs: fix lazy-loading example (#20306)

--- bundles/http.umd.js
+++ bundles/http.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.2.0-a931a41
+ * @license Angular v5.2.0-3db02d2
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -36,7 +36,7 @@ function __extends(d, b) {
 }
 
 /**
- * @license Angular v5.2.0-a931a41
+ * @license Angular v5.2.0-3db02d2
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2751,7 +2751,7 @@ var JsonpModule = /** @class */ (function () {
 /**
  * @deprecated use \@angular/common/http instead
  */
-var VERSION = new _angular_core.Version('5.2.0-a931a41');
+var VERSION = new _angular_core.Version('5.2.0-3db02d2');
 
 exports.BrowserXhr = BrowserXhr;
 exports.JSONPBackend = JSONPBackend;
--- esm5/http.js
+++ esm5/http.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.2.0-a931a41
+ * @license Angular v5.2.0-3db02d2
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2719,7 +2719,7 @@ var JsonpModule = /** @class */ (function () {
 /**
  * @deprecated use \@angular/common/http instead
  */
-var VERSION = new Version('5.2.0-a931a41');
+var VERSION = new Version('5.2.0-3db02d2');
 
 /**
  * @fileoverview added by tsickle
