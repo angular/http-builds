dc06e59cc86604254affab4eb5ab77800ffa8f05
Alex Eagle <alexeagle@google.com>
2018-01-11 22:16:59 +0000

    6af3672 fix(ivy): Add workaround for AJD in google3 (#21488)

--- esm5/http.js
+++ esm5/http.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.2.0-9b84a32
+ * @license Angular v5.2.0-6af3672
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2719,7 +2719,7 @@ var JsonpModule = /** @class */ (function () {
 /**
  * @deprecated use \@angular/common/http instead
  */
-var VERSION = new Version('5.2.0-9b84a32');
+var VERSION = new Version('5.2.0-6af3672');
 
 /**
  * @fileoverview added by tsickle
