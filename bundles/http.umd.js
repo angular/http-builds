c22a8ae35fdbc0746ad082acc11e3ac479f97805
Alex Eagle <alexeagle@google.com>
2018-01-12 23:03:53 +0000

    43e1520 fix(language-service): Clear caches when program changes (#21337)

--- bundles/http.umd.js
+++ bundles/http.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.2.0-2d44a2a
+ * @license Angular v5.2.0-43e1520
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -36,7 +36,7 @@ function __extends(d, b) {
 }
 
 /**
- * @license Angular v5.2.0-2d44a2a
+ * @license Angular v5.2.0-43e1520
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2751,7 +2751,7 @@ var JsonpModule = /** @class */ (function () {
 /**
  * @deprecated use \@angular/common/http instead
  */
-var VERSION = new _angular_core.Version('5.2.0-2d44a2a');
+var VERSION = new _angular_core.Version('5.2.0-43e1520');
 
 exports.BrowserXhr = BrowserXhr;
 exports.JSONPBackend = JSONPBackend;
