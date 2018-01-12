95f8b9b83c98e62850769ae8fdaf04d34b54a3fb
Alex Eagle <alexeagle@google.com>
2018-01-12 21:54:39 +0000

    05208b8 fix(common): set correct timezone for ISO8601 dates in Safari (#21506)

--- esm5/http.js
+++ esm5/http.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.2.0-bb62458
+ * @license Angular v5.2.0-05208b8
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2719,7 +2719,7 @@ var JsonpModule = /** @class */ (function () {
 /**
  * @deprecated use \@angular/common/http instead
  */
-var VERSION = new Version('5.2.0-bb62458');
+var VERSION = new Version('5.2.0-05208b8');
 
 /**
  * @fileoverview added by tsickle
