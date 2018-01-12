d642acb2f1a375fa43632fb02df46e896d06cedf
Alex Eagle <alexeagle@google.com>
2018-01-12 22:20:15 +0000

    977978e test(common): make date pipe tests work in more timezones (#21379)

--- esm2015/http.js
+++ esm2015/http.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.2.0-eb70966
+ * @license Angular v5.2.0-977978e
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2149,7 +2149,7 @@ JsonpModule.ctorParameters = () => [];
 /**
  * @deprecated use \@angular/common/http instead
  */
-const VERSION = new Version('5.2.0-eb70966');
+const VERSION = new Version('5.2.0-977978e');
 
 /**
  * @fileoverview added by tsickle
--- esm5/http.js
+++ esm5/http.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.2.0-eb70966
+ * @license Angular v5.2.0-977978e
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2719,7 +2719,7 @@ var JsonpModule = /** @class */ (function () {
 /**
  * @deprecated use \@angular/common/http instead
  */
-var VERSION = new Version('5.2.0-eb70966');
+var VERSION = new Version('5.2.0-977978e');
 
 /**
  * @fileoverview added by tsickle
