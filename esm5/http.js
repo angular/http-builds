2a142007d496181081cc1b567a0aea4c8ba8d207
Alex Eagle <alexeagle@google.com>
2018-01-12 19:06:12 +0000

    9b5a485 perf(ivy): add missing dom element in render3_function tree benchmark (#21476)

--- esm5/http.js
+++ esm5/http.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.2.0-50ca01e
+ * @license Angular v5.2.0-9b5a485
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2719,7 +2719,7 @@ var JsonpModule = /** @class */ (function () {
 /**
  * @deprecated use \@angular/common/http instead
  */
-var VERSION = new Version('5.2.0-50ca01e');
+var VERSION = new Version('5.2.0-9b5a485');
 
 /**
  * @fileoverview added by tsickle
