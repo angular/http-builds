382db1b2ed3683def00311e84dae3b76d353484e
Alex Eagle <alexeagle@google.com>
2018-01-16 15:30:37 +0000

    d6ba9f9 build(aio): move file cleaning to later in the doc gen (#21540)

--- esm2015/http.js
+++ esm2015/http.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.2.0-54bf179
+ * @license Angular v5.2.0-d6ba9f9
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2149,7 +2149,7 @@ JsonpModule.ctorParameters = () => [];
 /**
  * @deprecated use \@angular/common/http instead
  */
-const VERSION = new Version('5.2.0-54bf179');
+const VERSION = new Version('5.2.0-d6ba9f9');
 
 /**
  * @fileoverview added by tsickle
