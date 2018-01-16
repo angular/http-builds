8c63d27052213a14a640b2ff6ba07a37b77db09b
Alex Eagle <alexeagle@google.com>
2018-01-16 15:39:29 +0000

    3bcc0e6 refactor(core): refactor WrappedValue (#20997)

--- esm2015/http.js
+++ esm2015/http.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.2.0-d6ba9f9
+ * @license Angular v5.2.0-3bcc0e6
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2149,7 +2149,7 @@ JsonpModule.ctorParameters = () => [];
 /**
  * @deprecated use \@angular/common/http instead
  */
-const VERSION = new Version('5.2.0-d6ba9f9');
+const VERSION = new Version('5.2.0-3bcc0e6');
 
 /**
  * @fileoverview added by tsickle
--- esm5/http.js
+++ esm5/http.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.2.0-d6ba9f9
+ * @license Angular v5.2.0-3bcc0e6
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2719,7 +2719,7 @@ var JsonpModule = /** @class */ (function () {
 /**
  * @deprecated use \@angular/common/http instead
  */
-var VERSION = new Version('5.2.0-d6ba9f9');
+var VERSION = new Version('5.2.0-3bcc0e6');
 
 /**
  * @fileoverview added by tsickle
