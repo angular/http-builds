c9dfa5694259946c921a58aaefd6710b22c597f3
Alex Eagle <alexeagle@google.com>
2018-01-17 15:35:18 +0000

    54e7576 fix(core): fix chained http call (#20924)

--- esm5/http.js
+++ esm5/http.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.2.0-d3333f0
+ * @license Angular v5.2.0-54e7576
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2719,7 +2719,7 @@ var JsonpModule = /** @class */ (function () {
 /**
  * @deprecated use \@angular/common/http instead
  */
-var VERSION = new Version('5.2.0-d3333f0');
+var VERSION = new Version('5.2.0-54e7576');
 
 /**
  * @fileoverview added by tsickle
