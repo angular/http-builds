171cfdd417a670ba3c911670243b7e76b3f67be3
Alex Eagle <alexeagle@google.com>
2018-01-17 15:46:13 +0000

    d3bf54b feat(core): add binding name to content changed error (#20352)

--- esm2015/http.js
+++ esm2015/http.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.2.0-7e3f9a4
+ * @license Angular v5.2.0-d3bf54b
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2149,7 +2149,7 @@ JsonpModule.ctorParameters = () => [];
 /**
  * @deprecated use \@angular/common/http instead
  */
-const VERSION = new Version('5.2.0-7e3f9a4');
+const VERSION = new Version('5.2.0-d3bf54b');
 
 /**
  * @fileoverview added by tsickle
