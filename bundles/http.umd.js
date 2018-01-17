f4f3c28f7b43b2484d551f8068a0eb2bcd393416
GitHub <noreply@github.com>
2018-01-17 23:41:38 +0000

    0eabd07 fix: avoid triggering a cli bug (#21611)

--- bundles/http.umd.js
+++ bundles/http.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-beta.0-8503cc1
+ * @license Angular v5.9.9-6-beta.0-0eabd07
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -36,7 +36,7 @@ function __extends(d, b) {
 }
 
 /**
- * @license Angular v6.0.0-beta.0-8503cc1
+ * @license Angular v5.9.9-6-beta.0-0eabd07
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2751,7 +2751,7 @@ var JsonpModule = /** @class */ (function () {
 /**
  * @deprecated use \@angular/common/http instead
  */
-var VERSION = new _angular_core.Version('6.0.0-beta.0-8503cc1');
+var VERSION = new _angular_core.Version('5.9.9-6-beta.0-0eabd07');
 
 exports.BrowserXhr = BrowserXhr;
 exports.JSONPBackend = JSONPBackend;
