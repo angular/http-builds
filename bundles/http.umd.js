eb34e44799ebc48c216690c851eecc00170b121f
Alex Eagle <alexeagle@google.com>
2018-01-17 15:59:44 +0000

    4556532 feat(core): add binding name to content changed error (#20352)

--- bundles/http.umd.js
+++ bundles/http.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.2.0-54e7576
+ * @license Angular v5.2.0-4556532
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -36,7 +36,7 @@ function __extends(d, b) {
 }
 
 /**
- * @license Angular v5.2.0-54e7576
+ * @license Angular v5.2.0-4556532
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2751,7 +2751,7 @@ var JsonpModule = /** @class */ (function () {
 /**
  * @deprecated use \@angular/common/http instead
  */
-var VERSION = new _angular_core.Version('5.2.0-54e7576');
+var VERSION = new _angular_core.Version('5.2.0-4556532');
 
 exports.BrowserXhr = BrowserXhr;
 exports.JSONPBackend = JSONPBackend;
