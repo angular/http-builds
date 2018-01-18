d0f3620a6fef517618a8a28cb4b41393d38185c8
Miško Hevery <misko@hevery.com>
2018-01-18 20:02:43 +0000

    879756d fix(common): fallback to last defined value for named date and time formats (#21299)

--- esm5/http.js
+++ esm5/http.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.9.9-6-beta.0-2cf9d6d
+ * @license Angular v5.9.9-6-beta.0-879756d
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2719,7 +2719,7 @@ var JsonpModule = /** @class */ (function () {
 /**
  * @deprecated use \@angular/common/http instead
  */
-var VERSION = new Version('5.9.9-6-beta.0-2cf9d6d');
+var VERSION = new Version('5.9.9-6-beta.0-879756d');
 
 /**
  * @fileoverview added by tsickle
