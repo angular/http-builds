7871d4d70be721f9a820287c2b8b93e2148b08d8
Chuck Jazdzewski <chuckj@google.com>
2018-01-18 01:23:40 +0000

    e79e98a docs: fix lazy loading example dir name (#21475)

--- esm2015/http.js
+++ esm2015/http.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.9.9-6-beta.0-47b7898
+ * @license Angular v5.9.9-6-beta.0-e79e98a
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2149,7 +2149,7 @@ JsonpModule.ctorParameters = () => [];
 /**
  * @deprecated use \@angular/common/http instead
  */
-const VERSION = new Version('5.9.9-6-beta.0-47b7898');
+const VERSION = new Version('5.9.9-6-beta.0-e79e98a');
 
 /**
  * @fileoverview added by tsickle
