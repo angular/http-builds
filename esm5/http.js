c3506a0946f97725c3d3f996ac9766fcab5812f2
Alex Eagle <alexeagle@google.com>
2018-01-19 20:56:11 +0000

    1104d17 refactor(bazel): pass around tsconfig as a file, not a path (#21614)

--- esm5/http.js
+++ esm5/http.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.9.9-6-beta.0-a1492a7
+ * @license Angular v5.9.9-6-beta.0-1104d17
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2719,7 +2719,7 @@ var JsonpModule = /** @class */ (function () {
 /**
  * @deprecated use \@angular/common/http instead
  */
-var VERSION = new Version('5.9.9-6-beta.0-a1492a7');
+var VERSION = new Version('5.9.9-6-beta.0-1104d17');
 
 /**
  * @fileoverview added by tsickle
