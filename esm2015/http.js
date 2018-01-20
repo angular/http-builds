53c3ff7b5c7c045c02ec87a7b262a0df45db7adc
Misko Hevery <misko@hevery.com>
2018-01-20 05:01:00 +0000

    d7dbdc5 docs: fix stray div and reformat paragraph (#21676)

--- esm2015/http.js
+++ esm2015/http.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.2.1-554129d
+ * @license Angular v5.2.1-d7dbdc5
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2149,7 +2149,7 @@ JsonpModule.ctorParameters = () => [];
 /**
  * @deprecated use \@angular/common/http instead
  */
-const VERSION = new Version('5.2.1-554129d');
+const VERSION = new Version('5.2.1-d7dbdc5');
 
 /**
  * @fileoverview added by tsickle
