5835dad65a23ad62632277341c866b59a544f956
Miško Hevery <misko@hevery.com>
2018-01-23 21:54:49 +0000

    52970c0 fix(compiler-cli): do not fold errors past calls in the collector (#21708)

--- bundles/http.umd.js
+++ bundles/http.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.2.1-21f7669
+ * @license Angular v5.2.1-52970c0
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -36,7 +36,7 @@ function __extends(d, b) {
 }
 
 /**
- * @license Angular v5.2.1-21f7669
+ * @license Angular v5.2.1-52970c0
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2751,7 +2751,7 @@ var JsonpModule = /** @class */ (function () {
 /**
  * @deprecated use \@angular/common/http instead
  */
-var VERSION = new _angular_core.Version('5.2.1-21f7669');
+var VERSION = new _angular_core.Version('5.2.1-52970c0');
 
 exports.BrowserXhr = BrowserXhr;
 exports.JSONPBackend = JSONPBackend;
