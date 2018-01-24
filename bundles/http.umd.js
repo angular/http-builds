0529ac1c302f8d0fd9d7c0bdd584368e86d731fd
Miško Hevery <misko@hevery.com>
2018-01-24 20:59:59 +0000

    c7c5214 build: merge PR to all branches per target: label (#21739)

--- bundles/http.umd.js
+++ bundles/http.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.9.9-6-beta.0-12f801e
+ * @license Angular v5.9.9-6-beta.0-c7c5214
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -36,7 +36,7 @@ function __extends(d, b) {
 }
 
 /**
- * @license Angular v5.9.9-6-beta.0-12f801e
+ * @license Angular v5.9.9-6-beta.0-c7c5214
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2751,7 +2751,7 @@ var JsonpModule = /** @class */ (function () {
 /**
  * @deprecated use \@angular/common/http instead
  */
-var VERSION = new _angular_core.Version('5.9.9-6-beta.0-12f801e');
+var VERSION = new _angular_core.Version('5.9.9-6-beta.0-c7c5214');
 
 exports.BrowserXhr = BrowserXhr;
 exports.JSONPBackend = JSONPBackend;
