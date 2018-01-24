08af7622009d4332684d2dd2c5ddf262314c536f
Miško Hevery <misko@hevery.com>
2018-01-24 21:04:37 +0000

    5423882 build: merge PR to all branches per target: label (#21739)

--- bundles/http.umd.js
+++ bundles/http.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.2.1-de6c644
+ * @license Angular v5.2.1-5423882
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -36,7 +36,7 @@ function __extends(d, b) {
 }
 
 /**
- * @license Angular v5.2.1-de6c644
+ * @license Angular v5.2.1-5423882
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2751,7 +2751,7 @@ var JsonpModule = /** @class */ (function () {
 /**
  * @deprecated use \@angular/common/http instead
  */
-var VERSION = new _angular_core.Version('5.2.1-de6c644');
+var VERSION = new _angular_core.Version('5.2.1-5423882');
 
 exports.BrowserXhr = BrowserXhr;
 exports.JSONPBackend = JSONPBackend;
--- esm5/http.js
+++ esm5/http.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.2.1-de6c644
+ * @license Angular v5.2.1-5423882
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2719,7 +2719,7 @@ var JsonpModule = /** @class */ (function () {
 /**
  * @deprecated use \@angular/common/http instead
  */
-var VERSION = new Version('5.2.1-de6c644');
+var VERSION = new Version('5.2.1-5423882');
 
 /**
  * @fileoverview added by tsickle
