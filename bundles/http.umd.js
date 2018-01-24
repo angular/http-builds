4c84d9866942b526cb165054c84c265e73e95ec9
Miško Hevery <misko@hevery.com>
2018-01-24 05:53:33 +0000

    2f61d3c fix(aio): remove remaining `plnkr` references (#20165)

--- bundles/http.umd.js
+++ bundles/http.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.2.1-8b3fbb5
+ * @license Angular v5.2.1-2f61d3c
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -36,7 +36,7 @@ function __extends(d, b) {
 }
 
 /**
- * @license Angular v5.2.1-8b3fbb5
+ * @license Angular v5.2.1-2f61d3c
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2751,7 +2751,7 @@ var JsonpModule = /** @class */ (function () {
 /**
  * @deprecated use \@angular/common/http instead
  */
-var VERSION = new _angular_core.Version('5.2.1-8b3fbb5');
+var VERSION = new _angular_core.Version('5.2.1-2f61d3c');
 
 exports.BrowserXhr = BrowserXhr;
 exports.JSONPBackend = JSONPBackend;
