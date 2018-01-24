eb70697425cabdb3a4b31652bb1ec5980e4eae5a
Miško Hevery <misko@hevery.com>
2018-01-24 05:58:50 +0000

    925e654 fix(router): don't use ParamsInheritanceStrategy in declarations (#21574)

--- esm5/http.js
+++ esm5/http.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.9.9-6-beta.0-1e9cd95
+ * @license Angular v5.9.9-6-beta.0-925e654
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2719,7 +2719,7 @@ var JsonpModule = /** @class */ (function () {
 /**
  * @deprecated use \@angular/common/http instead
  */
-var VERSION = new Version('5.9.9-6-beta.0-1e9cd95');
+var VERSION = new Version('5.9.9-6-beta.0-925e654');
 
 /**
  * @fileoverview added by tsickle
