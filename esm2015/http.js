b604a465f3bd76895095fee3560d71bcc632bb0a
Misko Hevery <misko@hevery.com>
2018-01-25 05:58:37 +0000

    3aaf43f docs: add changelog for 5.2.2

--- esm2015/http.js
+++ esm2015/http.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.2.1-da9e57b
+ * @license Angular v5.2.2-3aaf43f
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2149,7 +2149,7 @@ JsonpModule.ctorParameters = () => [];
 /**
  * @deprecated use \@angular/common/http instead
  */
-const VERSION = new Version('5.2.1-da9e57b');
+const VERSION = new Version('5.2.2-3aaf43f');
 
 /**
  * @fileoverview added by tsickle
