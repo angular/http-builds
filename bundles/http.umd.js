2483be757ac29e9334932ef76e4873730c4c8060
Misko Hevery <misko@hevery.com>
2018-01-26 07:04:14 +0000

    bbb8f38 feat(ivy): implement template variables (#21760)

--- bundles/http.umd.js
+++ bundles/http.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-beta.1-08aa54e
+ * @license Angular v6.0.0-beta.1-bbb8f38
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -36,7 +36,7 @@ function __extends(d, b) {
 }
 
 /**
- * @license Angular v6.0.0-beta.1-08aa54e
+ * @license Angular v6.0.0-beta.1-bbb8f38
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2751,7 +2751,7 @@ var JsonpModule = /** @class */ (function () {
 /**
  * @deprecated use \@angular/common/http instead
  */
-var VERSION = new _angular_core.Version('6.0.0-beta.1-08aa54e');
+var VERSION = new _angular_core.Version('6.0.0-beta.1-bbb8f38');
 
 exports.BrowserXhr = BrowserXhr;
 exports.JSONPBackend = JSONPBackend;
