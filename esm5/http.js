b2bf1c592152f61d1f7226d21ef7ce88676a6bb8
Jason Aden <jasonaden@google.com>
2018-01-26 23:10:17 +0000

    676d9c2 fix(language-service): ensure correct paths are passed to TypeScript (#21812)

--- esm5/http.js
+++ esm5/http.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-beta.1-3b7bab7
+ * @license Angular v6.0.0-beta.1-676d9c2
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2719,7 +2719,7 @@ var JsonpModule = /** @class */ (function () {
 /**
  * @deprecated use \@angular/common/http instead
  */
-var VERSION = new Version('6.0.0-beta.1-3b7bab7');
+var VERSION = new Version('6.0.0-beta.1-676d9c2');
 
 /**
  * @fileoverview added by tsickle
