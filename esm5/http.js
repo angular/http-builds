f5149c26c21496ed81572b5bcf489b921bab5be1
Jason Aden <jasonaden@google.com>
2018-01-26 23:56:52 +0000

    81d64d6 fix(core): fix retrieving the binding name when an expression changes (#21814)

--- esm5/http.js
+++ esm5/http.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.2.2-7410941
+ * @license Angular v5.2.2-81d64d6
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2719,7 +2719,7 @@ var JsonpModule = /** @class */ (function () {
 /**
  * @deprecated use \@angular/common/http instead
  */
-var VERSION = new Version('5.2.2-7410941');
+var VERSION = new Version('5.2.2-81d64d6');
 
 /**
  * @fileoverview added by tsickle
