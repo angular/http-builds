68671ada687ec65c1b854a368652091dbc110328
Miško Hevery <misko@hevery.com>
2018-01-26 19:03:41 +0000

    ba045e8 docs: add notes on email used for CLA (#21754)

--- esm5/http.js
+++ esm5/http.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.2.2-35a0721
+ * @license Angular v5.2.2-ba045e8
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2719,7 +2719,7 @@ var JsonpModule = /** @class */ (function () {
 /**
  * @deprecated use \@angular/common/http instead
  */
-var VERSION = new Version('5.2.2-35a0721');
+var VERSION = new Version('5.2.2-ba045e8');
 
 /**
  * @fileoverview added by tsickle
