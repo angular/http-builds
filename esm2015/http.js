a893934f9272b30bb73a605779efc25b0fba3d1c
Misko Hevery <misko@hevery.com>
2018-01-26 06:36:48 +0000

    5713faa build: autosquashes SHAs as part of merge-pr script (#21791)

--- esm2015/http.js
+++ esm2015/http.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-beta.1-23596b3
+ * @license Angular v6.0.0-beta.1-5713faa
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2149,7 +2149,7 @@ JsonpModule.ctorParameters = () => [];
 /**
  * @deprecated use \@angular/common/http instead
  */
-const VERSION = new Version('6.0.0-beta.1-23596b3');
+const VERSION = new Version('6.0.0-beta.1-5713faa');
 
 /**
  * @fileoverview added by tsickle
