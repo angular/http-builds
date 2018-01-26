f2d9fff7cd27443ae8915147eb70b13fd69ef668
Jason Aden <jasonaden@google.com>
2018-01-26 23:13:42 +0000

    d159ad8 build(aio): prevent Windows error on `serve-and-sync` (#21806)

--- bundles/http.umd.js
+++ bundles/http.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.2.2-ba045e8
+ * @license Angular v5.2.2-d159ad8
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -36,7 +36,7 @@ function __extends(d, b) {
 }
 
 /**
- * @license Angular v5.2.2-ba045e8
+ * @license Angular v5.2.2-d159ad8
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2751,7 +2751,7 @@ var JsonpModule = /** @class */ (function () {
 /**
  * @deprecated use \@angular/common/http instead
  */
-var VERSION = new _angular_core.Version('5.2.2-ba045e8');
+var VERSION = new _angular_core.Version('5.2.2-d159ad8');
 
 exports.BrowserXhr = BrowserXhr;
 exports.JSONPBackend = JSONPBackend;
--- esm5/http.js
+++ esm5/http.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.2.2-ba045e8
+ * @license Angular v5.2.2-d159ad8
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2719,7 +2719,7 @@ var JsonpModule = /** @class */ (function () {
 /**
  * @deprecated use \@angular/common/http instead
  */
-var VERSION = new Version('5.2.2-ba045e8');
+var VERSION = new Version('5.2.2-d159ad8');
 
 /**
  * @fileoverview added by tsickle
