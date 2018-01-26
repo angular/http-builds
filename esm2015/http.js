ea80e7209b4eeb4076e30bb0b0d4bd26b1bfc268
Jason Aden <jasonaden@google.com>
2018-01-26 23:14:13 +0000

    250c8da fix(language-service): ensure correct paths are passed to TypeScript (#21812)

--- esm2015/http.js
+++ esm2015/http.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.2.2-d159ad8
+ * @license Angular v5.2.2-250c8da
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2149,7 +2149,7 @@ JsonpModule.ctorParameters = () => [];
 /**
  * @deprecated use \@angular/common/http instead
  */
-const VERSION = new Version('5.2.2-d159ad8');
+const VERSION = new Version('5.2.2-250c8da');
 
 /**
  * @fileoverview added by tsickle
--- esm5/http.js
+++ esm5/http.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.2.2-d159ad8
+ * @license Angular v5.2.2-250c8da
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2719,7 +2719,7 @@ var JsonpModule = /** @class */ (function () {
 /**
  * @deprecated use \@angular/common/http instead
  */
-var VERSION = new Version('5.2.2-d159ad8');
+var VERSION = new Version('5.2.2-250c8da');
 
 /**
  * @fileoverview added by tsickle
