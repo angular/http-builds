f4f75e7f3f598a2cb326d5479f9e0b3b2512d3e4
Miško Hevery <misko@hevery.com>
2018-01-26 06:19:10 +0000

    23596b3 docs(aio): fix missing stylesheet in component-styles example (#21772)

--- esm2015/http.js
+++ esm2015/http.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-beta.1-f8504f4
+ * @license Angular v6.0.0-beta.1-23596b3
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2149,7 +2149,7 @@ JsonpModule.ctorParameters = () => [];
 /**
  * @deprecated use \@angular/common/http instead
  */
-const VERSION = new Version('6.0.0-beta.1-f8504f4');
+const VERSION = new Version('6.0.0-beta.1-23596b3');
 
 /**
  * @fileoverview added by tsickle
