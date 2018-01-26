edf094039e15343f012249b6db809b48c22a8370
Misko Hevery <misko@hevery.com>
2018-01-26 06:43:57 +0000

    170885c fix(forms): allow FormBuilder to create controls with any formState type (#20917)

--- bundles/http.umd.js
+++ bundles/http.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-beta.1-5713faa
+ * @license Angular v6.0.0-beta.1-170885c
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -36,7 +36,7 @@ function __extends(d, b) {
 }
 
 /**
- * @license Angular v6.0.0-beta.1-5713faa
+ * @license Angular v6.0.0-beta.1-170885c
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2751,7 +2751,7 @@ var JsonpModule = /** @class */ (function () {
 /**
  * @deprecated use \@angular/common/http instead
  */
-var VERSION = new _angular_core.Version('6.0.0-beta.1-5713faa');
+var VERSION = new _angular_core.Version('6.0.0-beta.1-170885c');
 
 exports.BrowserXhr = BrowserXhr;
 exports.JSONPBackend = JSONPBackend;
