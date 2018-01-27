e3dd5776e5ee880afe93324b52eb0503fa88d830
Misko Hevery <misko@hevery.com>
2018-01-27 19:19:49 +0000

    6a7689d build: update to latest bazel rules (#21821)

--- esm5/http.js
+++ esm5/http.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.2.2-696ba01
+ * @license Angular v5.2.2-6a7689d
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2719,7 +2719,7 @@ var JsonpModule = /** @class */ (function () {
 /**
  * @deprecated use \@angular/common/http instead
  */
-var VERSION = new Version('5.2.2-696ba01');
+var VERSION = new Version('5.2.2-6a7689d');
 
 /**
  * @fileoverview added by tsickle
