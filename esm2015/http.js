525769241185c0b2835a2d6c757d9795c14c39ed
Jason Aden <jasonaden@google.com>
2018-01-27 00:29:36 +0000

    c3fb820 fix(aio): don't set noindex metatag in the static index.html (#21816)

--- esm2015/http.js
+++ esm2015/http.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-beta.1-2af19c9
+ * @license Angular v6.0.0-beta.1-c3fb820
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2149,7 +2149,7 @@ JsonpModule.ctorParameters = () => [];
 /**
  * @deprecated use \@angular/common/http instead
  */
-const VERSION = new Version('6.0.0-beta.1-2af19c9');
+const VERSION = new Version('6.0.0-beta.1-c3fb820');
 
 /**
  * @fileoverview added by tsickle
