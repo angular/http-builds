0c60f93714cf44db0311776ee0f78107ffafa0b2
Misko Hevery <misko@hevery.com>
2018-01-27 19:10:56 +0000

    5778bb8 fix(ivy): fix issues found producing "Hello, World" example (#21790)

--- esm5/http.js
+++ esm5/http.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-beta.1-d364117
+ * @license Angular v6.0.0-beta.1-5778bb8
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2719,7 +2719,7 @@ var JsonpModule = /** @class */ (function () {
 /**
  * @deprecated use \@angular/common/http instead
  */
-var VERSION = new Version('6.0.0-beta.1-d364117');
+var VERSION = new Version('6.0.0-beta.1-5778bb8');
 
 /**
  * @fileoverview added by tsickle
