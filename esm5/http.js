089927da164465ad4e89aee14c4173e7fd9db840
Jason Aden <jasonaden@google.com>
2018-01-29 20:01:37 +0000

    72265f7 fix(aio): missing plural s in preserveWhiteSpaces example (#21854)

--- esm5/http.js
+++ esm5/http.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-beta.1-6e8bc31
+ * @license Angular v6.0.0-beta.1-72265f7
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2719,7 +2719,7 @@ var JsonpModule = /** @class */ (function () {
 /**
  * @deprecated use \@angular/common/http instead
  */
-var VERSION = new Version('6.0.0-beta.1-6e8bc31');
+var VERSION = new Version('6.0.0-beta.1-72265f7');
 
 /**
  * @fileoverview added by tsickle
