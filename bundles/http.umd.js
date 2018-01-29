04e107179353b5e876e059be92ab58c0be3f5a83
Jason Aden <jasonaden@google.com>
2018-01-29 20:07:37 +0000

    18174e5 feat(ivy): support ng-content projection in the ivy compiler (#21764)

--- bundles/http.umd.js
+++ bundles/http.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-beta.1-72265f7
+ * @license Angular v6.0.0-beta.1-18174e5
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -36,7 +36,7 @@ function __extends(d, b) {
 }
 
 /**
- * @license Angular v6.0.0-beta.1-72265f7
+ * @license Angular v6.0.0-beta.1-18174e5
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2751,7 +2751,7 @@ var JsonpModule = /** @class */ (function () {
 /**
  * @deprecated use \@angular/common/http instead
  */
-var VERSION = new _angular_core.Version('6.0.0-beta.1-72265f7');
+var VERSION = new _angular_core.Version('6.0.0-beta.1-18174e5');
 
 exports.BrowserXhr = BrowserXhr;
 exports.JSONPBackend = JSONPBackend;
