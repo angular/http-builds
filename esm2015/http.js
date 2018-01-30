1547289aed2fcf10c67be8b9a665ba7fd3b961b5
Jason Aden <jasonaden@google.com>
2018-01-30 20:04:46 +0000

    b62739a fix(common): generate closure-locale data file with exported plural functions (#21873)

--- esm2015/http.js
+++ esm2015/http.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-beta.1-bb577c6
+ * @license Angular v6.0.0-beta.1-b62739a
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2149,7 +2149,7 @@ JsonpModule.ctorParameters = () => [];
 /**
  * @deprecated use \@angular/common/http instead
  */
-const VERSION = new Version('6.0.0-beta.1-bb577c6');
+const VERSION = new Version('6.0.0-beta.1-b62739a');
 
 /**
  * @fileoverview added by tsickle
