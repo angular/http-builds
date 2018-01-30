51953ad625040e6d68eeab71eead5a867319bdeb
Jason Aden <jasonaden@google.com>
2018-01-30 00:33:23 +0000

    11b1267 docs: document debugging a Node test in VSCode/Bazel (#21868)

--- esm2015/http.js
+++ esm2015/http.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-beta.1-dd48df1
+ * @license Angular v6.0.0-beta.1-11b1267
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2149,7 +2149,7 @@ JsonpModule.ctorParameters = () => [];
 /**
  * @deprecated use \@angular/common/http instead
  */
-const VERSION = new Version('6.0.0-beta.1-dd48df1');
+const VERSION = new Version('6.0.0-beta.1-11b1267');
 
 /**
  * @fileoverview added by tsickle
