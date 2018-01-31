dcc8d4ecdd449f6c5cf324ad0807187fc62904a8
Jason Aden <jasonaden@google.com>
2018-01-31 18:40:15 +0000

    81306c1 feat(ivy): add support for content query (#21912)

--- esm5/http.js
+++ esm5/http.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-beta.1-407b5cf
+ * @license Angular v6.0.0-beta.1-81306c1
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2719,7 +2719,7 @@ var JsonpModule = /** @class */ (function () {
 /**
  * @deprecated use \@angular/common/http instead
  */
-var VERSION = new Version('6.0.0-beta.1-407b5cf');
+var VERSION = new Version('6.0.0-beta.1-81306c1');
 
 /**
  * @fileoverview added by tsickle
