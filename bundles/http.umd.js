7a4422c81ed4c268391105e4b4936b3e0b0d0479
Jason Aden <jasonaden@google.com>
2018-01-31 18:45:30 +0000

    c83c416 build(aio): upgrade to dgeni-packages 0.24.0 (#21802)

--- bundles/http.umd.js
+++ bundles/http.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-beta.1-9dca5f2
+ * @license Angular v6.0.0-beta.1-c83c416
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -36,7 +36,7 @@ function __extends(d, b) {
 }
 
 /**
- * @license Angular v6.0.0-beta.1-9dca5f2
+ * @license Angular v6.0.0-beta.1-c83c416
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2751,7 +2751,7 @@ var JsonpModule = /** @class */ (function () {
 /**
  * @deprecated use \@angular/common/http instead
  */
-var VERSION = new _angular_core.Version('6.0.0-beta.1-9dca5f2');
+var VERSION = new _angular_core.Version('6.0.0-beta.1-c83c416');
 
 exports.BrowserXhr = BrowserXhr;
 exports.JSONPBackend = JSONPBackend;
