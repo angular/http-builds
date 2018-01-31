d4c89f8862e8adbad965ef1b08746990f0329397
Jason Aden <jasonaden@google.com>
2018-01-31 19:19:24 +0000

    2aa460b docs: add http guide sample and adjust text (#21326)

--- esm2015/http.js
+++ esm2015/http.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.2.2-e0022ae
+ * @license Angular v5.2.2-2aa460b
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2149,7 +2149,7 @@ JsonpModule.ctorParameters = () => [];
 /**
  * @deprecated use \@angular/common/http instead
  */
-const VERSION = new Version('5.2.2-e0022ae');
+const VERSION = new Version('5.2.2-2aa460b');
 
 /**
  * @fileoverview added by tsickle
