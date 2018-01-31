fefc6c39f0d8b3cc08662e900d2cf1e8c22495e9
Jason Aden <jasonaden@google.com>
2018-01-31 21:52:37 +0000

    61abba4 docs: add changelog for 6.6.0-beta.2

--- esm2015/http.js
+++ esm2015/http.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-beta.1-3db75b3
+ * @license Angular v6.0.0-beta.2-61abba4
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2149,7 +2149,7 @@ JsonpModule.ctorParameters = () => [];
 /**
  * @deprecated use \@angular/common/http instead
  */
-const VERSION = new Version('6.0.0-beta.1-3db75b3');
+const VERSION = new Version('6.0.0-beta.2-61abba4');
 
 /**
  * @fileoverview added by tsickle
