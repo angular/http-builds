186dc853c25a0ce8fd33612638d9cdc4873cae01
Jason Aden <jasonaden@google.com>
2018-01-31 19:15:34 +0000

    db3e65f docs(service-worker): describe safety-worker.js in SW guide (#21921)

--- esm2015/http.js
+++ esm2015/http.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-beta.1-ccd0298
+ * @license Angular v6.0.0-beta.1-db3e65f
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2149,7 +2149,7 @@ JsonpModule.ctorParameters = () => [];
 /**
  * @deprecated use \@angular/common/http instead
  */
-const VERSION = new Version('6.0.0-beta.1-ccd0298');
+const VERSION = new Version('6.0.0-beta.1-db3e65f');
 
 /**
  * @fileoverview added by tsickle
