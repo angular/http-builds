a4d67dca32f8ea5c277c19a11a15a0c55729c5de
Alex Rickabaugh <alx+alxhub@alxandria.net>
2018-02-01 17:03:43 +0000

    8b14488 fix(common): don't convert null to a string when flushing a mock request (#21417)

--- esm2015/http.js
+++ esm2015/http.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-beta.2-eb8ddd2
+ * @license Angular v6.0.0-beta.2-8b14488
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2149,7 +2149,7 @@ JsonpModule.ctorParameters = () => [];
 /**
  * @deprecated use \@angular/common/http instead
  */
-const VERSION = new Version('6.0.0-beta.2-eb8ddd2');
+const VERSION = new Version('6.0.0-beta.2-8b14488');
 
 /**
  * @fileoverview added by tsickle
