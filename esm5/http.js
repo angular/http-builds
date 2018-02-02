2d59b92635ec65527143a873c564e1976c016175
Alex Rickabaugh <alx+alxhub@alxandria.net>
2018-02-02 16:11:12 +0000

    965eecc build(aio): move zip and live-example generation to `yarn predocs` task (#21970)

--- esm5/http.js
+++ esm5/http.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.2.3-c4fb696
+ * @license Angular v5.2.3-965eecc
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2719,7 +2719,7 @@ var JsonpModule = /** @class */ (function () {
 /**
  * @deprecated use \@angular/common/http instead
  */
-var VERSION = new Version('5.2.3-c4fb696');
+var VERSION = new Version('5.2.3-965eecc');
 
 /**
  * @fileoverview added by tsickle
