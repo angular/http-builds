bb70deb329cda120488fe614ed4579121b123823
Alex Rickabaugh <alx+alxhub@alxandria.net>
2018-02-02 16:54:09 +0000

    3a86940 fix(core): should check Zone existance when scheduleMicroTask (#20656)

--- esm2015/http.js
+++ esm2015/http.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-beta.2-7b120b5
+ * @license Angular v6.0.0-beta.2-3a86940
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2149,7 +2149,7 @@ JsonpModule.ctorParameters = () => [];
 /**
  * @deprecated use \@angular/common/http instead
  */
-const VERSION = new Version('6.0.0-beta.2-7b120b5');
+const VERSION = new Version('6.0.0-beta.2-3a86940');
 
 /**
  * @fileoverview added by tsickle
