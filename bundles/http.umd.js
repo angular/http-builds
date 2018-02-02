f612fc42193b3c5f6fd2fde649e226a30f02c184
Alex Rickabaugh <alx+alxhub@alxandria.net>
2018-02-02 16:49:47 +0000

    aa9ba7f fix(core): should check Zone existance when scheduleMicroTask (#20656)

--- bundles/http.umd.js
+++ bundles/http.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.2.3-11ec80a
+ * @license Angular v5.2.3-aa9ba7f
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -36,7 +36,7 @@ function __extends(d, b) {
 }
 
 /**
- * @license Angular v5.2.3-11ec80a
+ * @license Angular v5.2.3-aa9ba7f
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2751,7 +2751,7 @@ var JsonpModule = /** @class */ (function () {
 /**
  * @deprecated use \@angular/common/http instead
  */
-var VERSION = new _angular_core.Version('5.2.3-11ec80a');
+var VERSION = new _angular_core.Version('5.2.3-aa9ba7f');
 
 exports.BrowserXhr = BrowserXhr;
 exports.JSONPBackend = JSONPBackend;
