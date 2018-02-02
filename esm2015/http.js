52fa667ef05abaa49628139de17af35323883fc7
Alex Rickabaugh <alx+alxhub@alxandria.net>
2018-02-02 16:14:47 +0000

    75eecdc docs: add missing underline (#21892) PR Close #21892

--- esm2015/http.js
+++ esm2015/http.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.2.3-965eecc
+ * @license Angular v5.2.3-75eecdc
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2149,7 +2149,7 @@ JsonpModule.ctorParameters = () => [];
 /**
  * @deprecated use \@angular/common/http instead
  */
-const VERSION = new Version('5.2.3-965eecc');
+const VERSION = new Version('5.2.3-75eecdc');
 
 /**
  * @fileoverview added by tsickle
--- esm5/http.js
+++ esm5/http.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.2.3-965eecc
+ * @license Angular v5.2.3-75eecdc
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2719,7 +2719,7 @@ var JsonpModule = /** @class */ (function () {
 /**
  * @deprecated use \@angular/common/http instead
  */
-var VERSION = new Version('5.2.3-965eecc');
+var VERSION = new Version('5.2.3-75eecdc');
 
 /**
  * @fileoverview added by tsickle
