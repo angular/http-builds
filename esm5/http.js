9b105fcad9bf117ba74df31b0201cfd5a16ae4c8
Alex Rickabaugh <alx+alxhub@alxandria.net>
2018-02-05 22:04:43 +0000

    adb1d62 docs: clarify npm/yarn commands, add blank lines to mix md/html in table (#21606)

--- esm5/http.js
+++ esm5/http.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.2.3-664f7fa
+ * @license Angular v5.2.3-adb1d62
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2719,7 +2719,7 @@ var JsonpModule = /** @class */ (function () {
 /**
  * @deprecated use \@angular/common/http instead
  */
-var VERSION = new Version('5.2.3-664f7fa');
+var VERSION = new Version('5.2.3-adb1d62');
 
 /**
  * @fileoverview added by tsickle
