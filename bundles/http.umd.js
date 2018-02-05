ea0b83e3c3fa1690423cbf31da8e232641674bb1
Alex Rickabaugh <alx+alxhub@alxandria.net>
2018-02-05 21:52:56 +0000

    0c9ec37 ci: mark PRs with rejection as not green (#21922) PR Close #21922

--- bundles/http.umd.js
+++ bundles/http.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-beta.2-9a0700f
+ * @license Angular v6.0.0-beta.2-0c9ec37
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -36,7 +36,7 @@ function __extends(d, b) {
 }
 
 /**
- * @license Angular v6.0.0-beta.2-9a0700f
+ * @license Angular v6.0.0-beta.2-0c9ec37
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2751,7 +2751,7 @@ var JsonpModule = /** @class */ (function () {
 /**
  * @deprecated use \@angular/common/http instead
  */
-var VERSION = new _angular_core.Version('6.0.0-beta.2-9a0700f');
+var VERSION = new _angular_core.Version('6.0.0-beta.2-0c9ec37');
 
 exports.BrowserXhr = BrowserXhr;
 exports.JSONPBackend = JSONPBackend;
