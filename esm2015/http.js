746d6e1118dfe2eac655ef783b4c988e594beafe
Alex Rickabaugh <alx+alxhub@alxandria.net>
2018-02-05 21:46:24 +0000

    9a0700f build(aio): add API static members to search index (#21988)

--- esm2015/http.js
+++ esm2015/http.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-beta.2-ae7bc22
+ * @license Angular v6.0.0-beta.2-9a0700f
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2149,7 +2149,7 @@ JsonpModule.ctorParameters = () => [];
 /**
  * @deprecated use \@angular/common/http instead
  */
-const VERSION = new Version('6.0.0-beta.2-ae7bc22');
+const VERSION = new Version('6.0.0-beta.2-9a0700f');
 
 /**
  * @fileoverview added by tsickle
