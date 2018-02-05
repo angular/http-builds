9ca847e95395906622256323b6b565566cc5a7ff
Alex Rickabaugh <alx+alxhub@alxandria.net>
2018-02-05 21:19:57 +0000

    ce51ea9 fix(core): fix #20582, don't need to wrap zone in location change listener (#22007)

--- esm5/http.js
+++ esm5/http.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.2.3-102d06b
+ * @license Angular v5.2.3-ce51ea9
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2719,7 +2719,7 @@ var JsonpModule = /** @class */ (function () {
 /**
  * @deprecated use \@angular/common/http instead
  */
-var VERSION = new Version('5.2.3-102d06b');
+var VERSION = new Version('5.2.3-ce51ea9');
 
 /**
  * @fileoverview added by tsickle
