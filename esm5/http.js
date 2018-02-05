febea3e20854526d0a732f99a0360d2acf5ff9d7
Alex Rickabaugh <alx+alxhub@alxandria.net>
2018-02-05 22:00:22 +0000

    664f7fa build(aio): add API static members to search index (#21988)

--- esm5/http.js
+++ esm5/http.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.2.3-973607f
+ * @license Angular v5.2.3-664f7fa
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2719,7 +2719,7 @@ var JsonpModule = /** @class */ (function () {
 /**
  * @deprecated use \@angular/common/http instead
  */
-var VERSION = new Version('5.2.3-973607f');
+var VERSION = new Version('5.2.3-664f7fa');
 
 /**
  * @fileoverview added by tsickle
