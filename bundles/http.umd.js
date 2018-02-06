48e2e9595a03469f39c9c232fdcf8137b479ed83
Alex Rickabaugh <alx+alxhub@alxandria.net>
2018-02-06 16:25:26 +0000

    7e51e52 perf(ivy): improve Uglify configuration in hello world integration test (#21985)

--- bundles/http.umd.js
+++ bundles/http.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-beta.2-e81606c
+ * @license Angular v6.0.0-beta.2-7e51e52
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -36,7 +36,7 @@ function __extends(d, b) {
 }
 
 /**
- * @license Angular v6.0.0-beta.2-e81606c
+ * @license Angular v6.0.0-beta.2-7e51e52
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2751,7 +2751,7 @@ var JsonpModule = /** @class */ (function () {
 /**
  * @deprecated use \@angular/common/http instead
  */
-var VERSION = new _angular_core.Version('6.0.0-beta.2-e81606c');
+var VERSION = new _angular_core.Version('6.0.0-beta.2-7e51e52');
 
 exports.BrowserXhr = BrowserXhr;
 exports.JSONPBackend = JSONPBackend;
--- esm2015/http.js
+++ esm2015/http.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-beta.2-e81606c
+ * @license Angular v6.0.0-beta.2-7e51e52
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2149,7 +2149,7 @@ JsonpModule.ctorParameters = () => [];
 /**
  * @deprecated use \@angular/common/http instead
  */
-const VERSION = new Version('6.0.0-beta.2-e81606c');
+const VERSION = new Version('6.0.0-beta.2-7e51e52');
 
 /**
  * @fileoverview added by tsickle
