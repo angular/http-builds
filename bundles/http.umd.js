069bab561004719a35027489eac2e7a58117ee12
Miško Hevery <misko@hevery.com>
2018-02-07 20:28:34 +0000

    7e928db docs(forms): Custom Validator example selector name incorrect. (#20464)

--- bundles/http.umd.js
+++ bundles/http.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-beta.3-545fdf1
+ * @license Angular v6.0.0-beta.3-7e928db
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -36,7 +36,7 @@ function __extends(d, b) {
 }
 
 /**
- * @license Angular v6.0.0-beta.3-545fdf1
+ * @license Angular v6.0.0-beta.3-7e928db
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2751,7 +2751,7 @@ var JsonpModule = /** @class */ (function () {
 /**
  * @deprecated use \@angular/common/http instead
  */
-var VERSION = new _angular_core.Version('6.0.0-beta.3-545fdf1');
+var VERSION = new _angular_core.Version('6.0.0-beta.3-7e928db');
 
 exports.BrowserXhr = BrowserXhr;
 exports.JSONPBackend = JSONPBackend;
--- esm2015/http.js
+++ esm2015/http.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-beta.3-545fdf1
+ * @license Angular v6.0.0-beta.3-7e928db
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2149,7 +2149,7 @@ JsonpModule.ctorParameters = () => [];
 /**
  * @deprecated use \@angular/common/http instead
  */
-const VERSION = new Version('6.0.0-beta.3-545fdf1');
+const VERSION = new Version('6.0.0-beta.3-7e928db');
 
 /**
  * @fileoverview added by tsickle
