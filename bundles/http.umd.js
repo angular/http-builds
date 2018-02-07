dbe23fdd6e8a8bb3a2c17e5a0bba16e5babbdc25
Miško Hevery <misko@hevery.com>
2018-02-07 21:08:49 +0000

    9d02db3 fix(aio): ignore `.header-link` when selecting the heading text (#21695)

--- bundles/http.umd.js
+++ bundles/http.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.2.4-56b9591
+ * @license Angular v5.2.4-9d02db3
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -36,7 +36,7 @@ function __extends(d, b) {
 }
 
 /**
- * @license Angular v5.2.4-56b9591
+ * @license Angular v5.2.4-9d02db3
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2751,7 +2751,7 @@ var JsonpModule = /** @class */ (function () {
 /**
  * @deprecated use \@angular/common/http instead
  */
-var VERSION = new _angular_core.Version('5.2.4-56b9591');
+var VERSION = new _angular_core.Version('5.2.4-9d02db3');
 
 exports.BrowserXhr = BrowserXhr;
 exports.JSONPBackend = JSONPBackend;
--- esm5/http.js
+++ esm5/http.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.2.4-56b9591
+ * @license Angular v5.2.4-9d02db3
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2719,7 +2719,7 @@ var JsonpModule = /** @class */ (function () {
 /**
  * @deprecated use \@angular/common/http instead
  */
-var VERSION = new Version('5.2.4-56b9591');
+var VERSION = new Version('5.2.4-9d02db3');
 
 /**
  * @fileoverview added by tsickle
