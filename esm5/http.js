f541a1ccbd6961c10901e677c43334b6b97bc2f1
Alex Rickabaugh <alx+alxhub@alxandria.net>
2018-02-07 19:41:32 +0000

    530b824 docs: add changelog for 5.2.4

--- esm5/http.js
+++ esm5/http.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.2.3-c6645e7
+ * @license Angular v5.2.4-530b824
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2719,7 +2719,7 @@ var JsonpModule = /** @class */ (function () {
 /**
  * @deprecated use \@angular/common/http instead
  */
-var VERSION = new Version('5.2.3-c6645e7');
+var VERSION = new Version('5.2.4-530b824');
 
 /**
  * @fileoverview added by tsickle
