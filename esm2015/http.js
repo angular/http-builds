329a2f02036e10754296900faac7ee45743fadb0
Miško Hevery <misko@hevery.com>
2018-02-07 22:05:56 +0000

    b58c352 test(ivy): add canonical spec for object literals (#22045)

--- esm2015/http.js
+++ esm2015/http.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-beta.3-efc67ee
+ * @license Angular v6.0.0-beta.3-b58c352
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2149,7 +2149,7 @@ JsonpModule.ctorParameters = () => [];
 /**
  * @deprecated use \@angular/common/http instead
  */
-const VERSION = new Version('6.0.0-beta.3-efc67ee');
+const VERSION = new Version('6.0.0-beta.3-b58c352');
 
 /**
  * @fileoverview added by tsickle
