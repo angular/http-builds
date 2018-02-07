6436fe08ce1cf04da266dfc7ba9dc61695c0d7b4
Miško Hevery <misko@hevery.com>
2018-02-07 21:40:56 +0000

    47b73fd fix(core): ensure initial value of QueryList length (#21980) (#21982)

--- bundles/http.umd.js
+++ bundles/http.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.2.4-07769e5
+ * @license Angular v5.2.4-47b73fd
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -36,7 +36,7 @@ function __extends(d, b) {
 }
 
 /**
- * @license Angular v5.2.4-07769e5
+ * @license Angular v5.2.4-47b73fd
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2751,7 +2751,7 @@ var JsonpModule = /** @class */ (function () {
 /**
  * @deprecated use \@angular/common/http instead
  */
-var VERSION = new _angular_core.Version('5.2.4-07769e5');
+var VERSION = new _angular_core.Version('5.2.4-47b73fd');
 
 exports.BrowserXhr = BrowserXhr;
 exports.JSONPBackend = JSONPBackend;
--- esm5/http.js
+++ esm5/http.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.2.4-07769e5
+ * @license Angular v5.2.4-47b73fd
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2719,7 +2719,7 @@ var JsonpModule = /** @class */ (function () {
 /**
  * @deprecated use \@angular/common/http instead
  */
-var VERSION = new Version('5.2.4-07769e5');
+var VERSION = new Version('5.2.4-47b73fd');
 
 /**
  * @fileoverview added by tsickle
