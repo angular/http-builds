3c69db563907e0f70e19d4cf2a56d3b2244aeaef
Miško Hevery <misko@pangular.mtv.corp.google.com>
2018-02-08 18:05:34 +0000

    de561f3 docs(common): add `HttpParamsOptions` to the public API (#20332)

--- esm5/http.js
+++ esm5/http.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.2.4-47b71d9
+ * @license Angular v5.2.4-de561f3
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2719,7 +2719,7 @@ var JsonpModule = /** @class */ (function () {
 /**
  * @deprecated use \@angular/common/http instead
  */
-var VERSION = new Version('5.2.4-47b71d9');
+var VERSION = new Version('5.2.4-de561f3');
 
 /**
  * @fileoverview added by tsickle
