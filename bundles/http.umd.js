e917aa4adb4e970eeb95d77b31c72c7559182878
Miško Hevery <misko@pangular.mtv.corp.google.com>
2018-02-08 00:29:46 +0000

    ffcb5dd build(aio): update examples to CLI to 1.6.5 (#21222)

--- bundles/http.umd.js
+++ bundles/http.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.2.4-75897d4
+ * @license Angular v5.2.4-ffcb5dd
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -36,7 +36,7 @@ function __extends(d, b) {
 }
 
 /**
- * @license Angular v5.2.4-75897d4
+ * @license Angular v5.2.4-ffcb5dd
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2751,7 +2751,7 @@ var JsonpModule = /** @class */ (function () {
 /**
  * @deprecated use \@angular/common/http instead
  */
-var VERSION = new _angular_core.Version('5.2.4-75897d4');
+var VERSION = new _angular_core.Version('5.2.4-ffcb5dd');
 
 exports.BrowserXhr = BrowserXhr;
 exports.JSONPBackend = JSONPBackend;
--- esm5/http.js
+++ esm5/http.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.2.4-75897d4
+ * @license Angular v5.2.4-ffcb5dd
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2719,7 +2719,7 @@ var JsonpModule = /** @class */ (function () {
 /**
  * @deprecated use \@angular/common/http instead
  */
-var VERSION = new Version('5.2.4-75897d4');
+var VERSION = new Version('5.2.4-ffcb5dd');
 
 /**
  * @fileoverview added by tsickle
