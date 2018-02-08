07c1b9cfb8851622ef9e846338fe201f93fbc00d
Miško Hevery <misko@pangular.mtv.corp.google.com>
2018-02-08 17:20:23 +0000

    d9ae70c test(ivy): normalize template names in canonical spec (#21815)

--- esm2015/http.js
+++ esm2015/http.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-beta.3-a751649
+ * @license Angular v6.0.0-beta.3-d9ae70c
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2149,7 +2149,7 @@ JsonpModule.ctorParameters = () => [];
 /**
  * @deprecated use \@angular/common/http instead
  */
-const VERSION = new Version('6.0.0-beta.3-a751649');
+const VERSION = new Version('6.0.0-beta.3-d9ae70c');
 
 /**
  * @fileoverview added by tsickle
