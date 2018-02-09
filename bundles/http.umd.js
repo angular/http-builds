953e4612b1b4a273faf3e2f4a443a92f4ebe7821
Miško Hevery <misko@hevery.com>
2018-02-09 21:46:35 +0000

    5fc77c9 fix(aio): do not rewrite /styleguide URL in Service Worker (#22085)

--- bundles/http.umd.js
+++ bundles/http.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-beta.3-c348445
+ * @license Angular v6.0.0-beta.3-5fc77c9
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -36,7 +36,7 @@ function __extends(d, b) {
 }
 
 /**
- * @license Angular v6.0.0-beta.3-c348445
+ * @license Angular v6.0.0-beta.3-5fc77c9
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2751,7 +2751,7 @@ var JsonpModule = /** @class */ (function () {
 /**
  * @deprecated use \@angular/common/http instead
  */
-var VERSION = new _angular_core.Version('6.0.0-beta.3-c348445');
+var VERSION = new _angular_core.Version('6.0.0-beta.3-5fc77c9');
 
 exports.BrowserXhr = BrowserXhr;
 exports.JSONPBackend = JSONPBackend;
