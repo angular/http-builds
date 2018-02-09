39eae51ff2d234a7fd98830003e091f9547b69dd
Miško Hevery <misko@hevery.com>
2018-02-09 21:59:22 +0000

    ac815f7 docs: fix typo in http.md (#22058)

--- bundles/http.umd.js
+++ bundles/http.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.2.4-854f0ff
+ * @license Angular v5.2.4-ac815f7
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -36,7 +36,7 @@ function __extends(d, b) {
 }
 
 /**
- * @license Angular v5.2.4-854f0ff
+ * @license Angular v5.2.4-ac815f7
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2751,7 +2751,7 @@ var JsonpModule = /** @class */ (function () {
 /**
  * @deprecated use \@angular/common/http instead
  */
-var VERSION = new _angular_core.Version('5.2.4-854f0ff');
+var VERSION = new _angular_core.Version('5.2.4-ac815f7');
 
 exports.BrowserXhr = BrowserXhr;
 exports.JSONPBackend = JSONPBackend;
--- esm5/http.js
+++ esm5/http.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.2.4-854f0ff
+ * @license Angular v5.2.4-ac815f7
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2719,7 +2719,7 @@ var JsonpModule = /** @class */ (function () {
 /**
  * @deprecated use \@angular/common/http instead
  */
-var VERSION = new Version('5.2.4-854f0ff');
+var VERSION = new Version('5.2.4-ac815f7');
 
 /**
  * @fileoverview added by tsickle
