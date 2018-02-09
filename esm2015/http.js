1fa857449b4fca791ed3c308c56904641dd61dae
Miško Hevery <misko@hevery.com>
2018-02-09 21:38:09 +0000

    fbef94a feat(aio): enable data driven homepage announcements (#22043)

--- esm2015/http.js
+++ esm2015/http.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-beta.3-aa456ed
+ * @license Angular v6.0.0-beta.3-fbef94a
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2149,7 +2149,7 @@ JsonpModule.ctorParameters = () => [];
 /**
  * @deprecated use \@angular/common/http instead
  */
-const VERSION = new Version('6.0.0-beta.3-aa456ed');
+const VERSION = new Version('6.0.0-beta.3-fbef94a');
 
 /**
  * @fileoverview added by tsickle
