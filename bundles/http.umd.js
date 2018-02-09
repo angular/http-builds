b5c3c0f6e254028e9db86fdfe7ce6b6fe1e3741b
Miško Hevery <misko@hevery.com>
2018-02-09 21:37:44 +0000

    aa456ed refactor(ivy): validate that identifier identity in emitted output (#21877)

--- bundles/http.umd.js
+++ bundles/http.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-beta.3-bc1e229
+ * @license Angular v6.0.0-beta.3-aa456ed
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -36,7 +36,7 @@ function __extends(d, b) {
 }
 
 /**
- * @license Angular v6.0.0-beta.3-bc1e229
+ * @license Angular v6.0.0-beta.3-aa456ed
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2751,7 +2751,7 @@ var JsonpModule = /** @class */ (function () {
 /**
  * @deprecated use \@angular/common/http instead
  */
-var VERSION = new _angular_core.Version('6.0.0-beta.3-bc1e229');
+var VERSION = new _angular_core.Version('6.0.0-beta.3-aa456ed');
 
 exports.BrowserXhr = BrowserXhr;
 exports.JSONPBackend = JSONPBackend;
--- esm5/http.js
+++ esm5/http.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-beta.3-bc1e229
+ * @license Angular v6.0.0-beta.3-aa456ed
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2719,7 +2719,7 @@ var JsonpModule = /** @class */ (function () {
 /**
  * @deprecated use \@angular/common/http instead
  */
-var VERSION = new Version('6.0.0-beta.3-bc1e229');
+var VERSION = new Version('6.0.0-beta.3-aa456ed');
 
 /**
  * @fileoverview added by tsickle
