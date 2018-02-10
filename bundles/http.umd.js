e33da990f360685c1536032ef834d54d7036d9ca
Miško Hevery <misko@hevery.com>
2018-02-10 01:36:33 +0000

    c878d55 docs: add VSCode interaction issue to bazel docs (#22128)

--- bundles/http.umd.js
+++ bundles/http.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-beta.3-263a2ec
+ * @license Angular v6.0.0-beta.3-c878d55
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -36,7 +36,7 @@ function __extends(d, b) {
 }
 
 /**
- * @license Angular v6.0.0-beta.3-263a2ec
+ * @license Angular v6.0.0-beta.3-c878d55
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2751,7 +2751,7 @@ var JsonpModule = /** @class */ (function () {
 /**
  * @deprecated use \@angular/common/http instead
  */
-var VERSION = new _angular_core.Version('6.0.0-beta.3-263a2ec');
+var VERSION = new _angular_core.Version('6.0.0-beta.3-c878d55');
 
 exports.BrowserXhr = BrowserXhr;
 exports.JSONPBackend = JSONPBackend;
