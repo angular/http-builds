f1127a45dcc2d7f500ea87d9e3c1d380c6dc0ee7
Miško Hevery <misko@hevery.com>
2018-02-13 00:17:16 +0000

    c6bdc83 fix(common): weaken AsyncPipe transform signature (#22169)

--- bundles/http.umd.js
+++ bundles/http.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.2.4-1a897e4
+ * @license Angular v5.2.4-c6bdc83
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -36,7 +36,7 @@ function __extends(d, b) {
 }
 
 /**
- * @license Angular v5.2.4-1a897e4
+ * @license Angular v5.2.4-c6bdc83
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2751,7 +2751,7 @@ var JsonpModule = /** @class */ (function () {
 /**
  * @deprecated use \@angular/common/http instead
  */
-var VERSION = new _angular_core.Version('5.2.4-1a897e4');
+var VERSION = new _angular_core.Version('5.2.4-c6bdc83');
 
 exports.BrowserXhr = BrowserXhr;
 exports.JSONPBackend = JSONPBackend;
