d6177e7a9d1a979130a58de6fe9385aa5bea3f3f
Miško Hevery <misko@hevery.com>
2018-02-13 20:04:05 +0000

    6435ecd fix(platform-browser): support 0/false/null values in transfer_state (#22179)

--- esm5/http.js
+++ esm5/http.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-beta.3-16d1700
+ * @license Angular v6.0.0-beta.3-6435ecd
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2719,7 +2719,7 @@ var JsonpModule = /** @class */ (function () {
 /**
  * @deprecated use \@angular/common/http instead
  */
-var VERSION = new Version('6.0.0-beta.3-16d1700');
+var VERSION = new Version('6.0.0-beta.3-6435ecd');
 
 /**
  * @fileoverview added by tsickle
