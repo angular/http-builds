d15c77560bfde6dc3090fc0a4d7a01c147dae16f
Miško Hevery <misko@hevery.com>
2018-02-13 20:12:10 +0000

    20a900b test: Add bundle symbol extractor tool (#22002)

--- esm5/http.js
+++ esm5/http.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-beta.3-6435ecd
+ * @license Angular v6.0.0-beta.3-20a900b
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2719,7 +2719,7 @@ var JsonpModule = /** @class */ (function () {
 /**
  * @deprecated use \@angular/common/http instead
  */
-var VERSION = new Version('6.0.0-beta.3-6435ecd');
+var VERSION = new Version('6.0.0-beta.3-20a900b');
 
 /**
  * @fileoverview added by tsickle
