b02ed1e4fe080b505c8a99638427320898d21778
Miško Hevery <misko@hevery.com>
2018-02-13 00:19:14 +0000

    b333919 build(bazel): allow ng_modules to elide .ngsummary.closure.js files (#22107)

--- esm5/http.js
+++ esm5/http.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-beta.3-be59c3a
+ * @license Angular v6.0.0-beta.3-b333919
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2719,7 +2719,7 @@ var JsonpModule = /** @class */ (function () {
 /**
  * @deprecated use \@angular/common/http instead
  */
-var VERSION = new Version('6.0.0-beta.3-be59c3a');
+var VERSION = new Version('6.0.0-beta.3-b333919');
 
 /**
  * @fileoverview added by tsickle
