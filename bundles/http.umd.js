9ee0ffcc84274f21004028e7e73e342231000c3d
Matias Niemelä <matias@yearofmoo.com>
2018-02-14 22:49:13 +0000

    5bb9fca build: comment-out chromium version checking code temporarily (#22232)

--- bundles/http.umd.js
+++ bundles/http.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-beta.4-fa7d890
+ * @license Angular v6.0.0-beta.4-5bb9fca
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -36,7 +36,7 @@ function __extends(d, b) {
 }
 
 /**
- * @license Angular v6.0.0-beta.4-fa7d890
+ * @license Angular v6.0.0-beta.4-5bb9fca
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2751,7 +2751,7 @@ var JsonpModule = /** @class */ (function () {
 /**
  * @deprecated use \@angular/common/http instead
  */
-var VERSION = new _angular_core.Version('6.0.0-beta.4-fa7d890');
+var VERSION = new _angular_core.Version('6.0.0-beta.4-5bb9fca');
 
 exports.BrowserXhr = BrowserXhr;
 exports.JSONPBackend = JSONPBackend;
