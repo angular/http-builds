faf72efd7489fff3d864142ea0bde59a521cbeac
Matias Niemelä <matias@yearofmoo.com>
2018-02-14 22:48:39 +0000

    0b8b06e build: comment-out chromium version checking code temporarily (#22232)

--- esm5/http.js
+++ esm5/http.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.2.5-106b435
+ * @license Angular v5.2.5-0b8b06e
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2719,7 +2719,7 @@ var JsonpModule = /** @class */ (function () {
 /**
  * @deprecated use \@angular/common/http instead
  */
-var VERSION = new Version('5.2.5-106b435');
+var VERSION = new Version('5.2.5-0b8b06e');
 
 /**
  * @fileoverview added by tsickle
