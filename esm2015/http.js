8f7f8bf34e54b25e68ccdd363f8702be35d8532e
Matias Niemelä <matias@yearofmoo.com>
2018-02-15 23:13:34 +0000

    f628797 Revert "refactor(ivy): assertion (#22189)"

--- esm2015/http.js
+++ esm2015/http.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-beta.4-ba9cd5b
+ * @license Angular v6.0.0-beta.4-f628797
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2149,7 +2149,7 @@ JsonpModule.ctorParameters = () => [];
 /**
  * @deprecated use \@angular/common/http instead
  */
-const VERSION = new Version('6.0.0-beta.4-ba9cd5b');
+const VERSION = new Version('6.0.0-beta.4-f628797');
 
 /**
  * @fileoverview added by tsickle
