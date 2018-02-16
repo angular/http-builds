c53b0eb662a6d6f1ae740736db4dd1a275cc3ea0
Victor Berchet <victor@suumit.com>
2018-02-16 23:11:15 +0000

    6b45784 test(aio): increase `docs-test` timeouts to prevent flakes on Travis (#22261)

--- esm2015/http.js
+++ esm2015/http.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.2.5-5f52ea3
+ * @license Angular v5.2.5-6b45784
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2149,7 +2149,7 @@ JsonpModule.ctorParameters = () => [];
 /**
  * @deprecated use \@angular/common/http instead
  */
-const VERSION = new Version('5.2.5-5f52ea3');
+const VERSION = new Version('5.2.5-6b45784');
 
 /**
  * @fileoverview added by tsickle
