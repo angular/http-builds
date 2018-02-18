6a2385c274146e3feb6a8a03f56c076e6395e856
Victor Berchet <victor@suumit.com>
2018-02-18 21:46:53 +0000

    228eb9f fix(aio): improve announcement-bar layout with wide logos (#22272)

--- bundles/http.umd.js
+++ bundles/http.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.2.5-396bc0d
+ * @license Angular v5.2.5-228eb9f
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -36,7 +36,7 @@ function __extends(d, b) {
 }
 
 /**
- * @license Angular v5.2.5-396bc0d
+ * @license Angular v5.2.5-228eb9f
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2751,7 +2751,7 @@ var JsonpModule = /** @class */ (function () {
 /**
  * @deprecated use \@angular/common/http instead
  */
-var VERSION = new _angular_core.Version('5.2.5-396bc0d');
+var VERSION = new _angular_core.Version('5.2.5-228eb9f');
 
 exports.BrowserXhr = BrowserXhr;
 exports.JSONPBackend = JSONPBackend;
