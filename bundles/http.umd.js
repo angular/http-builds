912520f8e2a0578fe4aae854ea9e136619126982
Victor Berchet <victor@suumit.com>
2018-02-19 05:17:50 +0000

    bf07837 test(common): fix ngIf tests

--- bundles/http.umd.js
+++ bundles/http.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.2.5-cdfedc1
+ * @license Angular v5.2.5-bf07837
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -36,7 +36,7 @@ function __extends(d, b) {
 }
 
 /**
- * @license Angular v5.2.5-cdfedc1
+ * @license Angular v5.2.5-bf07837
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2751,7 +2751,7 @@ var JsonpModule = /** @class */ (function () {
 /**
  * @deprecated use \@angular/common/http instead
  */
-var VERSION = new _angular_core.Version('5.2.5-cdfedc1');
+var VERSION = new _angular_core.Version('5.2.5-bf07837');
 
 exports.BrowserXhr = BrowserXhr;
 exports.JSONPBackend = JSONPBackend;
