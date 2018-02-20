81affe1db056464d62c2c93bbee72c74985e8c65
Victor Berchet <victor@suumit.com>
2018-02-20 18:32:11 +0000

    f6120c0 docs(aio): add Nx and Angular Enterprise Playbook to resources (#22321) PR Close #22321

--- bundles/http.umd.js
+++ bundles/http.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-beta.4-e2bdef4
+ * @license Angular v6.0.0-beta.4-f6120c0
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -36,7 +36,7 @@ function __extends(d, b) {
 }
 
 /**
- * @license Angular v6.0.0-beta.4-e2bdef4
+ * @license Angular v6.0.0-beta.4-f6120c0
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2751,7 +2751,7 @@ var JsonpModule = /** @class */ (function () {
 /**
  * @deprecated use \@angular/common/http instead
  */
-var VERSION = new _angular_core.Version('6.0.0-beta.4-e2bdef4');
+var VERSION = new _angular_core.Version('6.0.0-beta.4-f6120c0');
 
 exports.BrowserXhr = BrowserXhr;
 exports.JSONPBackend = JSONPBackend;
--- esm2015/http.js
+++ esm2015/http.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-beta.4-e2bdef4
+ * @license Angular v6.0.0-beta.4-f6120c0
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2149,7 +2149,7 @@ JsonpModule.ctorParameters = () => [];
 /**
  * @deprecated use \@angular/common/http instead
  */
-const VERSION = new Version('6.0.0-beta.4-e2bdef4');
+const VERSION = new Version('6.0.0-beta.4-f6120c0');
 
 /**
  * @fileoverview added by tsickle
