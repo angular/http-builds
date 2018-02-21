110dd0d737ab63cc039bcb4855870964048df9f8
Victor Berchet <victor@suumit.com>
2018-02-21 00:46:26 +0000

    1c1cbba docs: add ngStyle to cheat sheet (#22070)

--- esm2015/http.js
+++ esm2015/http.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-beta.4-3b692a5
+ * @license Angular v6.0.0-beta.4-1c1cbba
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2149,7 +2149,7 @@ JsonpModule.ctorParameters = () => [];
 /**
  * @deprecated use \@angular/common/http instead
  */
-const VERSION = new Version('6.0.0-beta.4-3b692a5');
+const VERSION = new Version('6.0.0-beta.4-1c1cbba');
 
 /**
  * @fileoverview added by tsickle
