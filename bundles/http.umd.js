d28292d5830feeb90860101d78d16fa440fc29fc
Victor Berchet <victor@suumit.com>
2018-02-21 23:13:13 +0000

    b42921b docs(aio): updates directive event hooks real capabilities (#16654)

--- bundles/http.umd.js
+++ bundles/http.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-beta.4-722dec1
+ * @license Angular v6.0.0-beta.4-b42921b
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -36,7 +36,7 @@ function __extends(d, b) {
 }
 
 /**
- * @license Angular v6.0.0-beta.4-722dec1
+ * @license Angular v6.0.0-beta.4-b42921b
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2751,7 +2751,7 @@ var JsonpModule = /** @class */ (function () {
 /**
  * @deprecated use \@angular/common/http instead
  */
-var VERSION = new _angular_core.Version('6.0.0-beta.4-722dec1');
+var VERSION = new _angular_core.Version('6.0.0-beta.4-b42921b');
 
 exports.BrowserXhr = BrowserXhr;
 exports.JSONPBackend = JSONPBackend;
