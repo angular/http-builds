78e6d7e34cff30ac628e63113ff9154ee37d0308
Victor Berchet <victor@suumit.com>
2018-02-21 23:14:48 +0000

    612cfec docs(aio): updates directive event hooks real capabilities (#16654)

--- esm2015/http.js
+++ esm2015/http.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.2.5-dfdade2
+ * @license Angular v5.2.5-612cfec
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2149,7 +2149,7 @@ JsonpModule.ctorParameters = () => [];
 /**
  * @deprecated use \@angular/common/http instead
  */
-const VERSION = new Version('5.2.5-dfdade2');
+const VERSION = new Version('5.2.5-612cfec');
 
 /**
  * @fileoverview added by tsickle
--- esm5/http.js
+++ esm5/http.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.2.5-dfdade2
+ * @license Angular v5.2.5-612cfec
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2719,7 +2719,7 @@ var JsonpModule = /** @class */ (function () {
 /**
  * @deprecated use \@angular/common/http instead
  */
-var VERSION = new Version('5.2.5-dfdade2');
+var VERSION = new Version('5.2.5-612cfec');
 
 /**
  * @fileoverview added by tsickle
