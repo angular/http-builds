ffedeafe9fe30facfb13e6e95fafed04ad5eba84
Victor Berchet <victor@suumit.com>
2018-02-21 00:02:40 +0000

    5bdb3ac build: update tsickle dep from compiler-cli (#22295) PR Close #22295

--- bundles/http.umd.js
+++ bundles/http.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.2.5-a346d28
+ * @license Angular v5.2.5-5bdb3ac
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -36,7 +36,7 @@ function __extends(d, b) {
 }
 
 /**
- * @license Angular v5.2.5-a346d28
+ * @license Angular v5.2.5-5bdb3ac
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2751,7 +2751,7 @@ var JsonpModule = /** @class */ (function () {
 /**
  * @deprecated use \@angular/common/http instead
  */
-var VERSION = new _angular_core.Version('5.2.5-a346d28');
+var VERSION = new _angular_core.Version('5.2.5-5bdb3ac');
 
 exports.BrowserXhr = BrowserXhr;
 exports.JSONPBackend = JSONPBackend;
