f82f87104a25be2a642e10251edb0a03a5e235ef
Victor Berchet <victor@suumit.com>
2018-02-21 00:18:35 +0000

    a2cb010 docs(aio): Fix name of component (#22332) PR Close #22332

--- esm5/http.js
+++ esm5/http.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.2.5-d20a08b
+ * @license Angular v5.2.5-a2cb010
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2719,7 +2719,7 @@ var JsonpModule = /** @class */ (function () {
 /**
  * @deprecated use \@angular/common/http instead
  */
-var VERSION = new Version('5.2.5-d20a08b');
+var VERSION = new Version('5.2.5-a2cb010');
 
 /**
  * @fileoverview added by tsickle
