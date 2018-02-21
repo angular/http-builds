e177ad31fd46b3795e1fb8d14696db002d338e4a
Victor Berchet <victor@suumit.com>
2018-02-21 19:32:10 +0000

    435f6ee build: make git revert messages valid (#22339)

--- esm2015/http.js
+++ esm2015/http.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-beta.4-9e6268b
+ * @license Angular v6.0.0-beta.4-435f6ee
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2149,7 +2149,7 @@ JsonpModule.ctorParameters = () => [];
 /**
  * @deprecated use \@angular/common/http instead
  */
-const VERSION = new Version('6.0.0-beta.4-9e6268b');
+const VERSION = new Version('6.0.0-beta.4-435f6ee');
 
 /**
  * @fileoverview added by tsickle
