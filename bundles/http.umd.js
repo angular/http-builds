f63d5652dc26f45c45b3617aa74c7b0219e84fb9
Victor Berchet <victor@suumit.com>
2018-02-21 00:32:17 +0000

    3b692a5 docs(aio): fix incorrect quote mark usage (#22335) PR Close #22335

--- bundles/http.umd.js
+++ bundles/http.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-beta.4-69a0578
+ * @license Angular v6.0.0-beta.4-3b692a5
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -36,7 +36,7 @@ function __extends(d, b) {
 }
 
 /**
- * @license Angular v6.0.0-beta.4-69a0578
+ * @license Angular v6.0.0-beta.4-3b692a5
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2751,7 +2751,7 @@ var JsonpModule = /** @class */ (function () {
 /**
  * @deprecated use \@angular/common/http instead
  */
-var VERSION = new _angular_core.Version('6.0.0-beta.4-69a0578');
+var VERSION = new _angular_core.Version('6.0.0-beta.4-3b692a5');
 
 exports.BrowserXhr = BrowserXhr;
 exports.JSONPBackend = JSONPBackend;
