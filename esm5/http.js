5c1e7a327b0a0b343bfbb78c713c119d1d5bb299
Victor Berchet <victor@suumit.com>
2018-02-21 19:37:13 +0000

    dfdade2 docs(aio): Wrong code example. Form status field was added later in the guide. (#21275)

--- esm5/http.js
+++ esm5/http.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.2.5-c2f78e1
+ * @license Angular v5.2.5-dfdade2
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2719,7 +2719,7 @@ var JsonpModule = /** @class */ (function () {
 /**
  * @deprecated use \@angular/common/http instead
  */
-var VERSION = new Version('5.2.5-c2f78e1');
+var VERSION = new Version('5.2.5-dfdade2');
 
 /**
  * @fileoverview added by tsickle
