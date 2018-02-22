f6b64637b37b601deead06513449c9d821611f16
Victor Berchet <victor@suumit.com>
2018-02-22 22:21:07 +0000

    45eff4c fix(router): don't mutate route configs (#22358)

--- esm2015/http.js
+++ esm2015/http.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-beta.5-b3ffeaa
+ * @license Angular v6.0.0-beta.5-45eff4c
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2149,7 +2149,7 @@ JsonpModule.ctorParameters = () => [];
 /**
  * @deprecated use \@angular/common/http instead
  */
-const VERSION = new Version('6.0.0-beta.5-b3ffeaa');
+const VERSION = new Version('6.0.0-beta.5-45eff4c');
 
 /**
  * @fileoverview added by tsickle
