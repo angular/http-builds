264282e20b42c5203dacc342a03a57655f4d8a6d
Victor Berchet <victor@suumit.com>
2018-02-22 19:41:25 +0000

    da1b4d5 docs: fix ngmodules-jsmodules pre-req (#22316)

--- bundles/http.umd.js
+++ bundles/http.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.2.6-aa100f6
+ * @license Angular v5.2.6-da1b4d5
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -36,7 +36,7 @@ function __extends(d, b) {
 }
 
 /**
- * @license Angular v5.2.6-aa100f6
+ * @license Angular v5.2.6-da1b4d5
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2751,7 +2751,7 @@ var JsonpModule = /** @class */ (function () {
 /**
  * @deprecated use \@angular/common/http instead
  */
-var VERSION = new _angular_core.Version('5.2.6-aa100f6');
+var VERSION = new _angular_core.Version('5.2.6-da1b4d5');
 
 exports.BrowserXhr = BrowserXhr;
 exports.JSONPBackend = JSONPBackend;
