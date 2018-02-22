30a5c9e88d267e67e4aac07d3a9161145363763c
Victor Berchet <victor@suumit.com>
2018-02-22 21:55:47 +0000

    f194d00 build: disable bazel-out symlink (#22375)

--- bundles/http.umd.js
+++ bundles/http.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-beta.5-b7544cc
+ * @license Angular v6.0.0-beta.5-f194d00
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -36,7 +36,7 @@ function __extends(d, b) {
 }
 
 /**
- * @license Angular v6.0.0-beta.5-b7544cc
+ * @license Angular v6.0.0-beta.5-f194d00
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2751,7 +2751,7 @@ var JsonpModule = /** @class */ (function () {
 /**
  * @deprecated use \@angular/common/http instead
  */
-var VERSION = new _angular_core.Version('6.0.0-beta.5-b7544cc');
+var VERSION = new _angular_core.Version('6.0.0-beta.5-f194d00');
 
 exports.BrowserXhr = BrowserXhr;
 exports.JSONPBackend = JSONPBackend;
--- esm2015/http.js
+++ esm2015/http.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-beta.5-b7544cc
+ * @license Angular v6.0.0-beta.5-f194d00
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2149,7 +2149,7 @@ JsonpModule.ctorParameters = () => [];
 /**
  * @deprecated use \@angular/common/http instead
  */
-const VERSION = new Version('6.0.0-beta.5-b7544cc');
+const VERSION = new Version('6.0.0-beta.5-f194d00');
 
 /**
  * @fileoverview added by tsickle
