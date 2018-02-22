d3a27b9e5b8f8782b55509dcb56c26eb2d5671c3
Victor Berchet <victor@suumit.com>
2018-02-22 01:10:23 +0000

    5c89d6b feat(core): support metadata reflection for native class types (#22356)

--- esm5/http.js
+++ esm5/http.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-beta.4-3e6a86f
+ * @license Angular v6.0.0-beta.4-5c89d6b
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2719,7 +2719,7 @@ var JsonpModule = /** @class */ (function () {
 /**
  * @deprecated use \@angular/common/http instead
  */
-var VERSION = new Version('6.0.0-beta.4-3e6a86f');
+var VERSION = new Version('6.0.0-beta.4-5c89d6b');
 
 /**
  * @fileoverview added by tsickle
