f5048069863513fb7912e7e9b94e27a9d8722b95
Victor Berchet <victor@suumit.com>
2018-02-23 23:53:06 +0000

    4180912 feat(common): export functions to format numbers, percents, currencies & dates (#22423)

--- esm5/http.js
+++ esm5/http.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-beta.5-094666d
+ * @license Angular v6.0.0-beta.5-4180912
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2719,7 +2719,7 @@ var JsonpModule = /** @class */ (function () {
 /**
  * @deprecated use \@angular/common/http instead
  */
-var VERSION = new Version('6.0.0-beta.5-094666d');
+var VERSION = new Version('6.0.0-beta.5-4180912');
 
 /**
  * @fileoverview added by tsickle
