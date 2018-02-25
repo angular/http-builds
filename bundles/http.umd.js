d1643f765c4507446b0ac6aca26dd0cd9e759e55
Victor Berchet <victor@suumit.com>
2018-02-25 18:26:54 +0000

    13ab91e fix(upgrade): fix empty transclusion content with AngularJS@>=1.5.8 (#22167)

--- bundles/http.umd.js
+++ bundles/http.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-beta.5-a403229
+ * @license Angular v6.0.0-beta.5-13ab91e
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -36,7 +36,7 @@ function __extends(d, b) {
 }
 
 /**
- * @license Angular v6.0.0-beta.5-a403229
+ * @license Angular v6.0.0-beta.5-13ab91e
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2751,7 +2751,7 @@ var JsonpModule = /** @class */ (function () {
 /**
  * @deprecated use \@angular/common/http instead
  */
-var VERSION = new _angular_core.Version('6.0.0-beta.5-a403229');
+var VERSION = new _angular_core.Version('6.0.0-beta.5-13ab91e');
 
 exports.BrowserXhr = BrowserXhr;
 exports.JSONPBackend = JSONPBackend;
--- esm2015/http.js
+++ esm2015/http.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-beta.5-a403229
+ * @license Angular v6.0.0-beta.5-13ab91e
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2149,7 +2149,7 @@ JsonpModule.ctorParameters = () => [];
 /**
  * @deprecated use \@angular/common/http instead
  */
-const VERSION = new Version('6.0.0-beta.5-a403229');
+const VERSION = new Version('6.0.0-beta.5-13ab91e');
 
 /**
  * @fileoverview added by tsickle
