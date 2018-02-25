039f9ed7c4616dd77429d2b303cc29bcb0b0e7b9
Victor Berchet <victor@suumit.com>
2018-02-25 18:30:53 +0000

    a9a0e27 fix(upgrade): fix empty transclusion content with AngularJS@>=1.5.8 (#22167)

--- esm5/http.js
+++ esm5/http.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.2.6-4a08745
+ * @license Angular v5.2.6-a9a0e27
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2719,7 +2719,7 @@ var JsonpModule = /** @class */ (function () {
 /**
  * @deprecated use \@angular/common/http instead
  */
-var VERSION = new Version('5.2.6-4a08745');
+var VERSION = new Version('5.2.6-a9a0e27');
 
 /**
  * @fileoverview added by tsickle
