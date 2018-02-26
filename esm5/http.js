d7faa5a114a247ca27ff2dca1d9227c0dea31c90
Alex Eagle <alexeagle@google.com>
2018-02-26 21:56:17 +0000

    2a38d93 docs(aio): fix doc typo referring to `httpOptions` (#22456)

--- esm5/http.js
+++ esm5/http.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.2.6-a9a0e27
+ * @license Angular v5.2.6-2a38d93
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2719,7 +2719,7 @@ var JsonpModule = /** @class */ (function () {
 /**
  * @deprecated use \@angular/common/http instead
  */
-var VERSION = new Version('5.2.6-a9a0e27');
+var VERSION = new Version('5.2.6-2a38d93');
 
 /**
  * @fileoverview added by tsickle
