2c95df514095d420d93202ab4407190a4d714df4
Alex Eagle <alexeagle@google.com>
2018-02-26 21:57:38 +0000

    5a32d7e build(aio): render default value for optional parameters (#22435)

--- esm2015/http.js
+++ esm2015/http.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-beta.5-1f59981
+ * @license Angular v6.0.0-beta.5-5a32d7e
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2149,7 +2149,7 @@ JsonpModule.ctorParameters = () => [];
 /**
  * @deprecated use \@angular/common/http instead
  */
-const VERSION = new Version('6.0.0-beta.5-1f59981');
+const VERSION = new Version('6.0.0-beta.5-5a32d7e');
 
 /**
  * @fileoverview added by tsickle
