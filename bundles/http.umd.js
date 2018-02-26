59b08f4bc19915a68f1ba1455b0218ebad9023d2
Alex Eagle <alexeagle@google.com>
2018-02-26 23:19:59 +0000

    d3827a0 feat(platform-server): bump Domino to v2.0 (#22411)

--- bundles/http.umd.js
+++ bundles/http.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-beta.5-a7e1f23
+ * @license Angular v6.0.0-beta.5-d3827a0
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -36,7 +36,7 @@ function __extends(d, b) {
 }
 
 /**
- * @license Angular v6.0.0-beta.5-a7e1f23
+ * @license Angular v6.0.0-beta.5-d3827a0
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2751,7 +2751,7 @@ var JsonpModule = /** @class */ (function () {
 /**
  * @deprecated use \@angular/common/http instead
  */
-var VERSION = new _angular_core.Version('6.0.0-beta.5-a7e1f23');
+var VERSION = new _angular_core.Version('6.0.0-beta.5-d3827a0');
 
 exports.BrowserXhr = BrowserXhr;
 exports.JSONPBackend = JSONPBackend;
