1e94c0efe512431afcf70aafc0822a10f1ae9e85
Alex Eagle <alexeagle@google.com>
2018-02-27 19:45:36 +0000

    79656e7 docs(aio): add Observable and Rx docs (#21423)

--- bundles/http.umd.js
+++ bundles/http.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-beta.5-4bd3e5f
+ * @license Angular v6.0.0-beta.5-79656e7
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -36,7 +36,7 @@ function __extends(d, b) {
 }
 
 /**
- * @license Angular v6.0.0-beta.5-4bd3e5f
+ * @license Angular v6.0.0-beta.5-79656e7
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2751,7 +2751,7 @@ var JsonpModule = /** @class */ (function () {
 /**
  * @deprecated use \@angular/common/http instead
  */
-var VERSION = new _angular_core.Version('6.0.0-beta.5-4bd3e5f');
+var VERSION = new _angular_core.Version('6.0.0-beta.5-79656e7');
 
 exports.BrowserXhr = BrowserXhr;
 exports.JSONPBackend = JSONPBackend;
