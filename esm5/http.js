1f5de69fb5357a9fe1b4473cd2c36c5c52449c6a
Alex Eagle <alexeagle@google.com>
2018-02-27 02:07:19 +0000

    de02a7a fix(platform-server): generate correct stylings for camel case names (#22263)

--- esm5/http.js
+++ esm5/http.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.2.6-2a38d93
+ * @license Angular v5.2.6-de02a7a
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2719,7 +2719,7 @@ var JsonpModule = /** @class */ (function () {
 /**
  * @deprecated use \@angular/common/http instead
  */
-var VERSION = new Version('5.2.6-2a38d93');
+var VERSION = new Version('5.2.6-de02a7a');
 
 /**
  * @fileoverview added by tsickle
