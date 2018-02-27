f2a43cbd68e20aee28a8ff7fd3daf0797d2a0fef
Alex Eagle <alexeagle@google.com>
2018-02-27 02:16:24 +0000

    82a7912 docs: update i18n guide for projects that don't use the cli (#21767)

--- esm5/http.js
+++ esm5/http.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-beta.5-ef99126
+ * @license Angular v6.0.0-beta.5-82a7912
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2719,7 +2719,7 @@ var JsonpModule = /** @class */ (function () {
 /**
  * @deprecated use \@angular/common/http instead
  */
-var VERSION = new Version('6.0.0-beta.5-ef99126');
+var VERSION = new Version('6.0.0-beta.5-82a7912');
 
 /**
  * @fileoverview added by tsickle
