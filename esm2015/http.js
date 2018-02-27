e1d126dd24c036fbead09bf857dbce887fcd6c23
Alex Eagle <alexeagle@google.com>
2018-02-27 02:12:35 +0000

    d033106 docs: update i18n guide for projects that don't use the cli (#21767)

--- esm2015/http.js
+++ esm2015/http.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.2.6-de02a7a
+ * @license Angular v5.2.6-d033106
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2149,7 +2149,7 @@ JsonpModule.ctorParameters = () => [];
 /**
  * @deprecated use \@angular/common/http instead
  */
-const VERSION = new Version('5.2.6-de02a7a');
+const VERSION = new Version('5.2.6-d033106');
 
 /**
  * @fileoverview added by tsickle
