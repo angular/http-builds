10cd10e936b71d9f4f79647f8434135d08635b8a
Alex Eagle <alexeagle@google.com>
2018-02-27 02:11:51 +0000

    ef99126 test(aio): move forgotten e2e test to the correct folder (#22445)

--- esm2015/http.js
+++ esm2015/http.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-beta.5-40ba009
+ * @license Angular v6.0.0-beta.5-ef99126
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2149,7 +2149,7 @@ JsonpModule.ctorParameters = () => [];
 /**
  * @deprecated use \@angular/common/http instead
  */
-const VERSION = new Version('6.0.0-beta.5-40ba009');
+const VERSION = new Version('6.0.0-beta.5-ef99126');
 
 /**
  * @fileoverview added by tsickle
