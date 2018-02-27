c22b5fca9cb2a2de00ce13d1c5154e17baa90e43
Alex Eagle <alexeagle@google.com>
2018-02-27 02:08:41 +0000

    40ba009 fix(platform-server): generate correct stylings for camel case names (#22263)

--- bundles/http.umd.js
+++ bundles/http.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-beta.5-d3827a0
+ * @license Angular v6.0.0-beta.5-40ba009
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -36,7 +36,7 @@ function __extends(d, b) {
 }
 
 /**
- * @license Angular v6.0.0-beta.5-d3827a0
+ * @license Angular v6.0.0-beta.5-40ba009
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2751,7 +2751,7 @@ var JsonpModule = /** @class */ (function () {
 /**
  * @deprecated use \@angular/common/http instead
  */
-var VERSION = new _angular_core.Version('6.0.0-beta.5-d3827a0');
+var VERSION = new _angular_core.Version('6.0.0-beta.5-40ba009');
 
 exports.BrowserXhr = BrowserXhr;
 exports.JSONPBackend = JSONPBackend;
