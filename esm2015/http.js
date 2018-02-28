3559c984fe7467f1a1a3788a3cb68240bb8dd059
Alex Eagle <alexeagle@google.com>
2018-02-28 19:27:53 +0000

    c82cef8 docs: fix dynamic component loader example (#22181)

--- esm2015/http.js
+++ esm2015/http.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-beta.5-7d65356
+ * @license Angular v6.0.0-beta.5-c82cef8
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2149,7 +2149,7 @@ JsonpModule.ctorParameters = () => [];
 /**
  * @deprecated use \@angular/common/http instead
  */
-const VERSION = new Version('6.0.0-beta.5-7d65356');
+const VERSION = new Version('6.0.0-beta.5-c82cef8');
 
 /**
  * @fileoverview added by tsickle
