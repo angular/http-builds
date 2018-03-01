ac5e124d4257c29f6dd238cc05b0111e43549e96
Alex Eagle <alexeagle@google.com>
2018-03-01 17:09:54 +0000

    dd53447 fix(compiler): allow tree-shakeable injectables to depend on string tokens (#22376)

--- bundles/http.umd.js
+++ bundles/http.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-beta.6-0e311e3
+ * @license Angular v6.0.0-beta.6-dd53447
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -36,7 +36,7 @@ function __extends(d, b) {
 }
 
 /**
- * @license Angular v6.0.0-beta.6-0e311e3
+ * @license Angular v6.0.0-beta.6-dd53447
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2751,7 +2751,7 @@ var JsonpModule = /** @class */ (function () {
 /**
  * @deprecated use \@angular/common/http instead
  */
-var VERSION = new _angular_core.Version('6.0.0-beta.6-0e311e3');
+var VERSION = new _angular_core.Version('6.0.0-beta.6-dd53447');
 
 exports.BrowserXhr = BrowserXhr;
 exports.JSONPBackend = JSONPBackend;
