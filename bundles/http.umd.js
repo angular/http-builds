56804593ce57655754fd8cf0d117c27c513c5c9a
Alex Eagle <alexeagle@google.com>
2018-03-01 17:04:57 +0000

    fe0d53f build: Add support for bazelOptions.maxCacheSizeMb in ngc-wrapped. (#22511)

--- bundles/http.umd.js
+++ bundles/http.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.2.7-f8e70fb
+ * @license Angular v5.2.7-fe0d53f
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -36,7 +36,7 @@ function __extends(d, b) {
 }
 
 /**
- * @license Angular v5.2.7-f8e70fb
+ * @license Angular v5.2.7-fe0d53f
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2751,7 +2751,7 @@ var JsonpModule = /** @class */ (function () {
 /**
  * @deprecated use \@angular/common/http instead
  */
-var VERSION = new _angular_core.Version('5.2.7-f8e70fb');
+var VERSION = new _angular_core.Version('5.2.7-fe0d53f');
 
 exports.BrowserXhr = BrowserXhr;
 exports.JSONPBackend = JSONPBackend;
--- esm5/http.js
+++ esm5/http.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.2.7-f8e70fb
+ * @license Angular v5.2.7-fe0d53f
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2719,7 +2719,7 @@ var JsonpModule = /** @class */ (function () {
 /**
  * @deprecated use \@angular/common/http instead
  */
-var VERSION = new Version('5.2.7-f8e70fb');
+var VERSION = new Version('5.2.7-fe0d53f');
 
 /**
  * @fileoverview added by tsickle
