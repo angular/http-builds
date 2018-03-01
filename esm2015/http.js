e502879d8be9349967251577ef151b28317b0cfc
Alex Eagle <alexeagle@google.com>
2018-03-01 21:26:14 +0000

    49f074f feat(ivy): support array and object literals in binding expressions (#22336)

--- esm2015/http.js
+++ esm2015/http.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-beta.6-ec445b5
+ * @license Angular v6.0.0-beta.6-49f074f
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2149,7 +2149,7 @@ JsonpModule.ctorParameters = () => [];
 /**
  * @deprecated use \@angular/common/http instead
  */
-const VERSION = new Version('6.0.0-beta.6-ec445b5');
+const VERSION = new Version('6.0.0-beta.6-49f074f');
 
 /**
  * @fileoverview added by tsickle
