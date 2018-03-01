c99d336ec282118afab3bc19a4132932f40c1aa8
Alex Eagle <alexeagle@google.com>
2018-03-01 21:39:16 +0000

    0451fd9 feat(ivy): support generating view and content queries (#22330)

--- esm2015/http.js
+++ esm2015/http.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-beta.6-49f074f
+ * @license Angular v6.0.0-beta.6-0451fd9
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2149,7 +2149,7 @@ JsonpModule.ctorParameters = () => [];
 /**
  * @deprecated use \@angular/common/http instead
  */
-const VERSION = new Version('6.0.0-beta.6-49f074f');
+const VERSION = new Version('6.0.0-beta.6-0451fd9');
 
 /**
  * @fileoverview added by tsickle
