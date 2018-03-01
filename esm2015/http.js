e80efc2a47052309f37585ff44e726c457578726
Alex Eagle <alexeagle@google.com>
2018-03-01 17:50:52 +0000

    ec445b5 ci: speed up lint job on CircleCI (#22526)

--- esm2015/http.js
+++ esm2015/http.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-beta.6-ab790f3
+ * @license Angular v6.0.0-beta.6-ec445b5
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2149,7 +2149,7 @@ JsonpModule.ctorParameters = () => [];
 /**
  * @deprecated use \@angular/common/http instead
  */
-const VERSION = new Version('6.0.0-beta.6-ab790f3');
+const VERSION = new Version('6.0.0-beta.6-ec445b5');
 
 /**
  * @fileoverview added by tsickle
