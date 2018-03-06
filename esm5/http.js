e10d38661688bc96faa04a3d532c55c3807bcfdf
Alex Eagle <alexeagle@google.com>
2018-03-06 15:17:20 +0000

    2c75acc feat(ivy): add support for the ngProjectAs attribute (#22498)

--- esm5/http.js
+++ esm5/http.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-beta.6-f86d8ae
+ * @license Angular v6.0.0-beta.6-2c75acc
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2719,7 +2719,7 @@ var JsonpModule = /** @class */ (function () {
 /**
  * @deprecated use \@angular/common/http instead
  */
-var VERSION = new Version('6.0.0-beta.6-f86d8ae');
+var VERSION = new Version('6.0.0-beta.6-2c75acc');
 
 /**
  * @fileoverview added by tsickle
