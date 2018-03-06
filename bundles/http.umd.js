5f4f0e0bde738ffd57e061d1c8bfbe874df46e47
Alex Eagle <alexeagle@google.com>
2018-03-06 18:02:03 +0000

    d0db9de docs: fix cli-quickstart doc and specs (#22338)

--- bundles/http.umd.js
+++ bundles/http.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-beta.6-4c40812
+ * @license Angular v6.0.0-beta.6-d0db9de
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -36,7 +36,7 @@ function __extends(d, b) {
 }
 
 /**
- * @license Angular v6.0.0-beta.6-4c40812
+ * @license Angular v6.0.0-beta.6-d0db9de
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2751,7 +2751,7 @@ var JsonpModule = /** @class */ (function () {
 /**
  * @deprecated use \@angular/common/http instead
  */
-var VERSION = new _angular_core.Version('6.0.0-beta.6-4c40812');
+var VERSION = new _angular_core.Version('6.0.0-beta.6-d0db9de');
 
 exports.BrowserXhr = BrowserXhr;
 exports.JSONPBackend = JSONPBackend;
