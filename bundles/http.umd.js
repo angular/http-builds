b5a67646e761e7a3c8c42c78bc6056c257ce228d
Alex Eagle <alexeagle@google.com>
2018-03-07 19:12:48 +0000

    4aeb04d docs: update RELEASE_SCHEDULE.md by pushing out v6 rc by one week

--- bundles/http.umd.js
+++ bundles/http.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.2.7-5876fb0
+ * @license Angular v5.2.7-4aeb04d
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -36,7 +36,7 @@ function __extends(d, b) {
 }
 
 /**
- * @license Angular v5.2.7-5876fb0
+ * @license Angular v5.2.7-4aeb04d
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2751,7 +2751,7 @@ var JsonpModule = /** @class */ (function () {
 /**
  * @deprecated use \@angular/common/http instead
  */
-var VERSION = new _angular_core.Version('5.2.7-5876fb0');
+var VERSION = new _angular_core.Version('5.2.7-4aeb04d');
 
 exports.BrowserXhr = BrowserXhr;
 exports.JSONPBackend = JSONPBackend;
