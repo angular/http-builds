e6245b977771c28a684548c7b1ec6fe37d6e2109
Alex Eagle <alexeagle@google.com>
2018-03-07 22:58:59 +0000

    ad8fb84 test: include router in public_api_guard tests (#22628)

--- esm2015/http.js
+++ esm2015/http.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-beta.6-7816791
+ * @license Angular v6.0.0-beta.6-ad8fb84
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2149,7 +2149,7 @@ JsonpModule.ctorParameters = () => [];
 /**
  * @deprecated use \@angular/common/http instead
  */
-const VERSION = new Version('6.0.0-beta.6-7816791');
+const VERSION = new Version('6.0.0-beta.6-ad8fb84');
 
 /**
  * @fileoverview added by tsickle
