b72d90fd7208540eae60979dc0fe0caea17e145c
Kara Erickson <karakara@google.com>
2018-03-08 02:43:00 +0000

    3112320 docs: add HeroService to code tabs and fix headers (#22373)

--- esm2015/http.js
+++ esm2015/http.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.2.7-4aeb04d
+ * @license Angular v5.2.8-3112320
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2149,7 +2149,7 @@ JsonpModule.ctorParameters = () => [];
 /**
  * @deprecated use \@angular/common/http instead
  */
-const VERSION = new Version('5.2.7-4aeb04d');
+const VERSION = new Version('5.2.8-3112320');
 
 /**
  * @fileoverview added by tsickle
--- esm5/http.js
+++ esm5/http.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.2.7-4aeb04d
+ * @license Angular v5.2.8-3112320
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2719,7 +2719,7 @@ var JsonpModule = /** @class */ (function () {
 /**
  * @deprecated use \@angular/common/http instead
  */
-var VERSION = new Version('5.2.7-4aeb04d');
+var VERSION = new Version('5.2.8-3112320');
 
 /**
  * @fileoverview added by tsickle
