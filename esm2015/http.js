e98b29ec144f450c6bc1ee269e0d4890225de662
Kara Erickson <karakara@google.com>
2018-03-08 05:21:31 +0000

    a1efc27 ci: double our cores on CircleCI (#22641)

--- esm2015/http.js
+++ esm2015/http.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.2.8-3112320
+ * @license Angular v5.2.8-a1efc27
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2149,7 +2149,7 @@ JsonpModule.ctorParameters = () => [];
 /**
  * @deprecated use \@angular/common/http instead
  */
-const VERSION = new Version('5.2.8-3112320');
+const VERSION = new Version('5.2.8-a1efc27');
 
 /**
  * @fileoverview added by tsickle
--- esm5/http.js
+++ esm5/http.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.2.8-3112320
+ * @license Angular v5.2.8-a1efc27
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2719,7 +2719,7 @@ var JsonpModule = /** @class */ (function () {
 /**
  * @deprecated use \@angular/common/http instead
  */
-var VERSION = new Version('5.2.8-3112320');
+var VERSION = new Version('5.2.8-a1efc27');
 
 /**
  * @fileoverview added by tsickle
