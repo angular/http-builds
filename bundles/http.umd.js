652095f27244b8773d68dc95c7c0c12925d674aa
Kara Erickson <karakara@google.com>
2018-03-08 19:23:18 +0000

    2ca77d8 docs: refactor revert() and call to lifecylce hook, edit doc to changes (#22094)

--- bundles/http.umd.js
+++ bundles/http.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-beta.7-73c203f
+ * @license Angular v6.0.0-beta.7-2ca77d8
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -36,7 +36,7 @@ function __extends(d, b) {
 }
 
 /**
- * @license Angular v6.0.0-beta.7-73c203f
+ * @license Angular v6.0.0-beta.7-2ca77d8
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2751,7 +2751,7 @@ var JsonpModule = /** @class */ (function () {
 /**
  * @deprecated use \@angular/common/http instead
  */
-var VERSION = new _angular_core.Version('6.0.0-beta.7-73c203f');
+var VERSION = new _angular_core.Version('6.0.0-beta.7-2ca77d8');
 
 exports.BrowserXhr = BrowserXhr;
 exports.JSONPBackend = JSONPBackend;
