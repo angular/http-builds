33f563fd116ed5753f97b363fd781a69c834a004
Kara Erickson <karakara@google.com>
2018-03-08 05:17:18 +0000

    5d4fa7f test(ivy): add canonical examples of bindings on elements (#22403)

--- esm2015/http.js
+++ esm2015/http.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-beta.7-8fb34bc
+ * @license Angular v6.0.0-beta.7-5d4fa7f
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2149,7 +2149,7 @@ JsonpModule.ctorParameters = () => [];
 /**
  * @deprecated use \@angular/common/http instead
  */
-const VERSION = new Version('6.0.0-beta.7-8fb34bc');
+const VERSION = new Version('6.0.0-beta.7-5d4fa7f');
 
 /**
  * @fileoverview added by tsickle
