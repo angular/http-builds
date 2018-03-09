3e91134b8a71cc82b7ef0f64ebabd7c5e496ec13
Kara Erickson <karakara@google.com>
2018-03-09 01:06:28 +0000

    5412e10 docs(core): replace ancient live demos (#22665)

--- esm2015/http.js
+++ esm2015/http.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-beta.7-489fec1
+ * @license Angular v6.0.0-beta.7-5412e10
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2149,7 +2149,7 @@ JsonpModule.ctorParameters = () => [];
 /**
  * @deprecated use \@angular/common/http instead
  */
-const VERSION = new Version('6.0.0-beta.7-489fec1');
+const VERSION = new Version('6.0.0-beta.7-5412e10');
 
 /**
  * @fileoverview added by tsickle
--- esm5/http.js
+++ esm5/http.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-beta.7-489fec1
+ * @license Angular v6.0.0-beta.7-5412e10
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2719,7 +2719,7 @@ var JsonpModule = /** @class */ (function () {
 /**
  * @deprecated use \@angular/common/http instead
  */
-var VERSION = new Version('6.0.0-beta.7-489fec1');
+var VERSION = new Version('6.0.0-beta.7-5412e10');
 
 /**
  * @fileoverview added by tsickle
