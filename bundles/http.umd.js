b501ec8a5c08a67e519a729f833b1670a05d10c1
Kara Erickson <karakara@google.com>
2018-03-09 07:02:54 +0000

    b0b9ca3 feat(ivy): produce Renderer2 back-patching and factories. (#22506)

--- bundles/http.umd.js
+++ bundles/http.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-beta.7-5412e10
+ * @license Angular v6.0.0-beta.7-b0b9ca3
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -36,7 +36,7 @@ function __extends(d, b) {
 }
 
 /**
- * @license Angular v6.0.0-beta.7-5412e10
+ * @license Angular v6.0.0-beta.7-b0b9ca3
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2751,7 +2751,7 @@ var JsonpModule = /** @class */ (function () {
 /**
  * @deprecated use \@angular/common/http instead
  */
-var VERSION = new _angular_core.Version('6.0.0-beta.7-5412e10');
+var VERSION = new _angular_core.Version('6.0.0-beta.7-b0b9ca3');
 
 exports.BrowserXhr = BrowserXhr;
 exports.JSONPBackend = JSONPBackend;
