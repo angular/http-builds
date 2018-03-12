1d51590eb055fa883d779c151d0209336da0a773
Kara Erickson <karakara@google.com>
2018-03-12 05:40:51 +0000

    aa7dba2 feat(ivy): support checkNoChanges (#22710)

--- esm5/http.js
+++ esm5/http.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-beta.7-40315be
+ * @license Angular v6.0.0-beta.7-aa7dba2
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2719,7 +2719,7 @@ var JsonpModule = /** @class */ (function () {
 /**
  * @deprecated use \@angular/common/http instead
  */
-var VERSION = new Version('6.0.0-beta.7-40315be');
+var VERSION = new Version('6.0.0-beta.7-aa7dba2');
 
 /**
  * @fileoverview added by tsickle
