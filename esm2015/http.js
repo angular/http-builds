826652e38889a7d0e3b94a96345ce1b338b58de3
Kara Erickson <karakara@google.com>
2018-03-12 16:44:54 +0000

    0bf6fa5 fix(router): correct over-encoding of URL fragment (#22687)

--- esm2015/http.js
+++ esm2015/http.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-beta.7-aa7dba2
+ * @license Angular v6.0.0-beta.7-0bf6fa5
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2149,7 +2149,7 @@ JsonpModule.ctorParameters = () => [];
 /**
  * @deprecated use \@angular/common/http instead
  */
-const VERSION = new Version('6.0.0-beta.7-aa7dba2');
+const VERSION = new Version('6.0.0-beta.7-0bf6fa5');
 
 /**
  * @fileoverview added by tsickle
