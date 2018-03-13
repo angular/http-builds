0f2536f86cdb04619cb0adabeae52f43c174b32e
Kara Erickson <karakara@google.com>
2018-03-13 17:06:07 +0000

    58b94e6 feat(animations): expose `element` and `params` within transition matchers (#22693)

--- esm2015/http.js
+++ esm2015/http.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-beta.7-db56836
+ * @license Angular v6.0.0-beta.7-58b94e6
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2149,7 +2149,7 @@ JsonpModule.ctorParameters = () => [];
 /**
  * @deprecated use \@angular/common/http instead
  */
-const VERSION = new Version('6.0.0-beta.7-db56836');
+const VERSION = new Version('6.0.0-beta.7-58b94e6');
 
 /**
  * @fileoverview added by tsickle
