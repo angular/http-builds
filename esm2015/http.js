2a48250482635e58e27ac13c4b074b296fce9499
Kara Erickson <karakara@google.com>
2018-03-14 19:48:51 +0000

    f5a98f4 fix(core): remove core animation import symbols (#22692)

--- esm2015/http.js
+++ esm2015/http.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-beta.7-c09bd67
+ * @license Angular v6.0.0-beta.7-f5a98f4
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2149,7 +2149,7 @@ JsonpModule.ctorParameters = () => [];
 /**
  * @deprecated use \@angular/common/http instead
  */
-const VERSION = new Version('6.0.0-beta.7-c09bd67');
+const VERSION = new Version('6.0.0-beta.7-f5a98f4');
 
 /**
  * @fileoverview added by tsickle
