b2e8ffe4d2fcd0974ea4cc5c608815d396ad8052
Kara Erickson <karakara@google.com>
2018-03-14 19:31:06 +0000

    c09bd67 fix(ivy): fix views manipulation logic (#22656)

--- esm5/http.js
+++ esm5/http.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-beta.7-9df13ad
+ * @license Angular v6.0.0-beta.7-c09bd67
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2719,7 +2719,7 @@ var JsonpModule = /** @class */ (function () {
 /**
  * @deprecated use \@angular/common/http instead
  */
-var VERSION = new Version('6.0.0-beta.7-9df13ad');
+var VERSION = new Version('6.0.0-beta.7-c09bd67');
 
 /**
  * @fileoverview added by tsickle
