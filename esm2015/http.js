e6002058da0d34fc8178bd874a0351939e09264d
Kara Erickson <karakara@google.com>
2018-03-14 05:07:40 +0000

    4ac606b docs(compiler): fix spelling errors (#22704)

--- esm2015/http.js
+++ esm2015/http.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-beta.7-51027d7
+ * @license Angular v6.0.0-beta.7-4ac606b
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2149,7 +2149,7 @@ JsonpModule.ctorParameters = () => [];
 /**
  * @deprecated use \@angular/common/http instead
  */
-const VERSION = new Version('6.0.0-beta.7-51027d7');
+const VERSION = new Version('6.0.0-beta.7-4ac606b');
 
 /**
  * @fileoverview added by tsickle
