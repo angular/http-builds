a3a4ebee2a04e364ab59e4b0b4e7ca970b6e09ca
Kara Erickson <karakara@google.com>
2018-03-14 03:17:40 +0000

    48636f3 build(aio): compute stability and deprecate `@stable` tag (#22674)

--- bundles/http.umd.js
+++ bundles/http.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-beta.7-bd9d4df
+ * @license Angular v6.0.0-beta.7-48636f3
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -36,7 +36,7 @@ function __extends(d, b) {
 }
 
 /**
- * @license Angular v6.0.0-beta.7-bd9d4df
+ * @license Angular v6.0.0-beta.7-48636f3
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2751,7 +2751,7 @@ var JsonpModule = /** @class */ (function () {
 /**
  * @deprecated use \@angular/common/http instead
  */
-var VERSION = new _angular_core.Version('6.0.0-beta.7-bd9d4df');
+var VERSION = new _angular_core.Version('6.0.0-beta.7-48636f3');
 
 exports.BrowserXhr = BrowserXhr;
 exports.JSONPBackend = JSONPBackend;
