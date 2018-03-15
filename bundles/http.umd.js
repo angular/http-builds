0103091a5e9565caba9ce7209915362fd7c39087
Miško Hevery <misko@hevery.com>
2018-03-15 22:42:57 +0000

    44de10e feat: mark angular packages as side-effect free (#22785)

--- bundles/http.umd.js
+++ bundles/http.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-beta.7-0ebd577
+ * @license Angular v6.0.0-beta.7-44de10e
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -36,7 +36,7 @@ function __extends(d, b) {
 }
 
 /**
- * @license Angular v6.0.0-beta.7-0ebd577
+ * @license Angular v6.0.0-beta.7-44de10e
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2751,7 +2751,7 @@ var JsonpModule = /** @class */ (function () {
 /**
  * @deprecated use \@angular/common/http instead
  */
-var VERSION = new _angular_core.Version('6.0.0-beta.7-0ebd577');
+var VERSION = new _angular_core.Version('6.0.0-beta.7-44de10e');
 
 exports.BrowserXhr = BrowserXhr;
 exports.JSONPBackend = JSONPBackend;
