380e1b5209641d0fed36d07d7f82bf452a6d755a
Miško Hevery <misko@hevery.com>
2018-03-15 19:11:35 +0000

    7d9de17 build: add release helper scripts (#22378) (#22781)

--- esm5/http.js
+++ esm5/http.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.2.9-db0afa9
+ * @license Angular v5.2.9-7d9de17
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2719,7 +2719,7 @@ var JsonpModule = /** @class */ (function () {
 /**
  * @deprecated use \@angular/common/http instead
  */
-var VERSION = new Version('5.2.9-db0afa9');
+var VERSION = new Version('5.2.9-7d9de17');
 
 /**
  * @fileoverview added by tsickle
