b945e009ceb7c76b0dd6c1c869ecec550394cdf7
Miško Hevery <misko@hevery.com>
2018-03-16 04:40:44 +0000

    b149424 build(bazel): make ng_package auto generate package.json for secondary entry-points (#22806)

--- bundles/http.umd.js
+++ bundles/http.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-beta.7-f9247e4
+ * @license Angular v6.0.0-beta.7-b149424
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -36,7 +36,7 @@ function __extends(d, b) {
 }
 
 /**
- * @license Angular v6.0.0-beta.7-f9247e4
+ * @license Angular v6.0.0-beta.7-b149424
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2751,7 +2751,7 @@ var JsonpModule = /** @class */ (function () {
 /**
  * @deprecated use \@angular/common/http instead
  */
-var VERSION = new _angular_core.Version('6.0.0-beta.7-f9247e4');
+var VERSION = new _angular_core.Version('6.0.0-beta.7-b149424');
 
 exports.BrowserXhr = BrowserXhr;
 exports.JSONPBackend = JSONPBackend;
--- esm2015/http.js
+++ esm2015/http.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-beta.7-f9247e4
+ * @license Angular v6.0.0-beta.7-b149424
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2149,7 +2149,7 @@ JsonpModule.ctorParameters = () => [];
 /**
  * @deprecated use \@angular/common/http instead
  */
-const VERSION = new Version('6.0.0-beta.7-f9247e4');
+const VERSION = new Version('6.0.0-beta.7-b149424');
 
 /**
  * @fileoverview added by tsickle
