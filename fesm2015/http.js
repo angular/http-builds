2858bdeca3323f64d270185a8069316ce4baf67e
Igor Minar <igor@angular.io>
2018-03-18 21:31:14 +0000

    0bede54 style: fix formatting issues on the master branch (#22854)

--- fesm2015/http.js
+++ fesm2015/http.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-beta.7-e27cfd6
+ * @license Angular v6.0.0-beta.7-0bede54
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2149,7 +2149,7 @@ JsonpModule.ctorParameters = () => [];
 /**
  * @deprecated use \@angular/common/http instead
  */
-const VERSION = new Version('6.0.0-beta.7-e27cfd6');
+const VERSION = new Version('6.0.0-beta.7-0bede54');
 
 /**
  * @fileoverview added by tsickle
