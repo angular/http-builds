a2e41feda88fe44d7875b4b7c71e70b9de7a0840
Misko Hevery <misko@hevery.com>
2018-03-18 19:04:28 +0000

    4f21d37 refactor(ivy): move hostBindings calls out of template (#22833)

--- bundles/http.umd.js
+++ bundles/http.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-beta.7-ce63dc6
+ * @license Angular v6.0.0-beta.7-4f21d37
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -36,7 +36,7 @@ function __extends(d, b) {
 }
 
 /**
- * @license Angular v6.0.0-beta.7-ce63dc6
+ * @license Angular v6.0.0-beta.7-4f21d37
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2751,7 +2751,7 @@ var JsonpModule = /** @class */ (function () {
 /**
  * @deprecated use \@angular/common/http instead
  */
-var VERSION = new _angular_core.Version('6.0.0-beta.7-ce63dc6');
+var VERSION = new _angular_core.Version('6.0.0-beta.7-4f21d37');
 
 exports.BrowserXhr = BrowserXhr;
 exports.JSONPBackend = JSONPBackend;
