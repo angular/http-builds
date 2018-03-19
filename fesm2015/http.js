f76fcbdbd07b9430299bf30672d0a16ea50e12bf
Miško Hevery <misko@misko-ubi.cbf.corp.google.com>
2018-03-19 21:52:46 +0000

    1a0cb21 refactor(ivy): remove unnecessary binding from hello world (#22848)

--- fesm2015/http.js
+++ fesm2015/http.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-beta.7-0bede54
+ * @license Angular v6.0.0-beta.7-1a0cb21
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2149,7 +2149,7 @@ JsonpModule.ctorParameters = () => [];
 /**
  * @deprecated use \@angular/common/http instead
  */
-const VERSION = new Version('6.0.0-beta.7-0bede54');
+const VERSION = new Version('6.0.0-beta.7-1a0cb21');
 
 /**
  * @fileoverview added by tsickle
