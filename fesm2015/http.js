d8abdd0545ab6ca4a3c87bc87e38be2a3d301a3b
Miško Hevery <misko@misko-ubi.cbf.corp.google.com>
2018-03-19 21:58:37 +0000

    bfe077a fix(bazel): correct expected outs for external sources in ng_module (#22755)

--- fesm2015/http.js
+++ fesm2015/http.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-beta.7-1a0cb21
+ * @license Angular v6.0.0-beta.7-bfe077a
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2149,7 +2149,7 @@ JsonpModule.ctorParameters = () => [];
 /**
  * @deprecated use \@angular/common/http instead
  */
-const VERSION = new Version('6.0.0-beta.7-1a0cb21');
+const VERSION = new Version('6.0.0-beta.7-bfe077a');
 
 /**
  * @fileoverview added by tsickle
