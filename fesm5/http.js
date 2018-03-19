251795ef36eb1733d3115ac8a6ce69af4ca7468e
Miško Hevery <misko@hevery.com>
2018-03-19 23:22:39 +0000

    fc50c77 test(ivy): switch HelloWorld to ivy compiler (#22788)

--- fesm5/http.js
+++ fesm5/http.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-beta.7-bfe077a
+ * @license Angular v6.0.0-beta.7-fc50c77
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2719,7 +2719,7 @@ var JsonpModule = /** @class */ (function () {
 /**
  * @deprecated use \@angular/common/http instead
  */
-var VERSION = new Version('6.0.0-beta.7-bfe077a');
+var VERSION = new Version('6.0.0-beta.7-fc50c77');
 
 /**
  * @fileoverview added by tsickle
