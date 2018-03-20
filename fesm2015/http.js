e4424fb75a3c7a39ec0a78d9960e16d09ff3cf5e
Igor Minar <igor@angular.io>
2018-03-20 21:01:58 +0000

    e6c731f fix(router): don't use spread operator to workaround an issue in closure compiler (#22884)

--- fesm2015/http.js
+++ fesm2015/http.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-beta.7-7d095b9
+ * @license Angular v6.0.0-beta.7-e6c731f
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2150,7 +2150,7 @@ JsonpModule.ctorParameters = () => [];
 /**
  * @deprecated use \@angular/common/http instead
  */
-const VERSION = new Version('6.0.0-beta.7-7d095b9');
+const VERSION = new Version('6.0.0-beta.7-e6c731f');
 
 /**
  * @fileoverview added by tsickle
