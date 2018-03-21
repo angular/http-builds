4c95b4d4aa9f73dad6be7006cfd4297b5a8e3d28
Igor Minar <igor@angular.io>
2018-03-21 21:04:23 +0000

    64efcf1 build: add the FESM files back to ng_package (#22889)

--- bundles/http.umd.js
+++ bundles/http.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-rc.0-b12ea30
+ * @license Angular v6.0.0-rc.0-64efcf1
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -36,7 +36,7 @@ function __extends(d, b) {
 }
 
 /**
- * @license Angular v6.0.0-rc.0-b12ea30
+ * @license Angular v6.0.0-rc.0-64efcf1
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2752,7 +2752,7 @@ var JsonpModule = /** @class */ (function () {
 /**
  * @deprecated use \@angular/common/http instead
  */
-var VERSION = new _angular_core.Version('6.0.0-rc.0-b12ea30');
+var VERSION = new _angular_core.Version('6.0.0-rc.0-64efcf1');
 
 exports.BrowserXhr = BrowserXhr;
 exports.JSONPBackend = JSONPBackend;
