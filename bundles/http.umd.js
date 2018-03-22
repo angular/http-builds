a94d0cc3ae4d82941739d68c114641034191d9c4
Matias Niemelä <matsko@matskocloud.c.googlers.com>
2018-03-22 19:49:38 +0000

    32105c8 test: temporarily disable ng_package test (#22933)

--- bundles/http.umd.js
+++ bundles/http.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-rc.0-f461f43
+ * @license Angular v6.0.0-rc.0-32105c8
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -36,7 +36,7 @@ function __extends(d, b) {
 }
 
 /**
- * @license Angular v6.0.0-rc.0-f461f43
+ * @license Angular v6.0.0-rc.0-32105c8
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2752,7 +2752,7 @@ var JsonpModule = /** @class */ (function () {
 /**
  * @deprecated use \@angular/common/http instead
  */
-var VERSION = new _angular_core.Version('6.0.0-rc.0-f461f43');
+var VERSION = new _angular_core.Version('6.0.0-rc.0-32105c8');
 
 exports.BrowserXhr = BrowserXhr;
 exports.JSONPBackend = JSONPBackend;
