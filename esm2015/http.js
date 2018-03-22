14815f3e5731c646857746ae290c0fac2a4748f6
Matias Niemelä <matsko@matskocloud.c.googlers.com>
2018-03-22 22:23:28 +0000

    641cc49 fix(animations): avoid animation insertions during router back/refresh (#21977)

--- esm2015/http.js
+++ esm2015/http.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.2.9-a846abb
+ * @license Angular v5.2.9-641cc49
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2149,7 +2149,7 @@ JsonpModule.ctorParameters = () => [];
 /**
  * @deprecated use \@angular/common/http instead
  */
-const VERSION = new Version('5.2.9-a846abb');
+const VERSION = new Version('5.2.9-641cc49');
 
 /**
  * @fileoverview added by tsickle
