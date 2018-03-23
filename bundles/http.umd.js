960986fd0792588581be87b0cb1a8c5fea8d51df
Matias Niemelä <matias@yearofmoo.com>
2018-03-23 01:50:28 +0000

    5ab9d4d test(compiler): allow asserting matching identifier names (#22835)

--- bundles/http.umd.js
+++ bundles/http.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-rc.0-49396ca
+ * @license Angular v6.0.0-rc.0-5ab9d4d
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -36,7 +36,7 @@ function __extends(d, b) {
 }
 
 /**
- * @license Angular v6.0.0-rc.0-49396ca
+ * @license Angular v6.0.0-rc.0-5ab9d4d
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2752,7 +2752,7 @@ var JsonpModule = /** @class */ (function () {
 /**
  * @deprecated use \@angular/common/http instead
  */
-var VERSION = new _angular_core.Version('6.0.0-rc.0-49396ca');
+var VERSION = new _angular_core.Version('6.0.0-rc.0-5ab9d4d');
 
 exports.BrowserXhr = BrowserXhr;
 exports.JSONPBackend = JSONPBackend;
