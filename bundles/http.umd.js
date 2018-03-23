f678fb128b49fbfc56b000935fec364757ea228e
Matias Niemelä <matias@yearofmoo.com>
2018-03-23 17:32:03 +0000

    16f021c docs(aio): fix TS warning error  - filter expects a boolean function param (#22954)

--- bundles/http.umd.js
+++ bundles/http.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-rc.0-3a30f5d
+ * @license Angular v6.0.0-rc.0-16f021c
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -36,7 +36,7 @@ function __extends(d, b) {
 }
 
 /**
- * @license Angular v6.0.0-rc.0-3a30f5d
+ * @license Angular v6.0.0-rc.0-16f021c
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2752,7 +2752,7 @@ var JsonpModule = /** @class */ (function () {
 /**
  * @deprecated use \@angular/common/http instead
  */
-var VERSION = new _angular_core.Version('6.0.0-rc.0-3a30f5d');
+var VERSION = new _angular_core.Version('6.0.0-rc.0-16f021c');
 
 exports.BrowserXhr = BrowserXhr;
 exports.JSONPBackend = JSONPBackend;
