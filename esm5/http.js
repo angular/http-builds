91f6f283b8a4167eff78cb2a840a71479fafe86e
Alex Rickabaugh <alx+alxhub@alxandria.net>
2018-03-28 17:23:22 +0000

    0d4fe38 fix(service-worker): ignore invalid `only-if-cached` requests (#22883)

--- esm5/http.js
+++ esm5/http.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.2.9-776bb82
+ * @license Angular v5.2.9-0d4fe38
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2719,7 +2719,7 @@ var JsonpModule = /** @class */ (function () {
 /**
  * @deprecated use \@angular/common/http instead
  */
-var VERSION = new Version('5.2.9-776bb82');
+var VERSION = new Version('5.2.9-0d4fe38');
 
 /**
  * @fileoverview added by tsickle
