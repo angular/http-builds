d187b22f1169bdc30f109305d83bb32b0c232a1d
Alex Rickabaugh <alx+alxhub@alxandria.net>
2018-03-30 23:03:32 +0000

    4473da7 ci(aio): add monitoring for angular.io (#23093)

--- bundles/http.umd.js
+++ bundles/http.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.2.9-4f7c369
+ * @license Angular v5.2.9-4473da7
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -36,7 +36,7 @@ function __extends(d, b) {
 }
 
 /**
- * @license Angular v5.2.9-4f7c369
+ * @license Angular v5.2.9-4473da7
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2751,7 +2751,7 @@ var JsonpModule = /** @class */ (function () {
 /**
  * @deprecated use \@angular/common/http instead
  */
-var VERSION = new _angular_core.Version('5.2.9-4f7c369');
+var VERSION = new _angular_core.Version('5.2.9-4473da7');
 
 exports.BrowserXhr = BrowserXhr;
 exports.JSONPBackend = JSONPBackend;
