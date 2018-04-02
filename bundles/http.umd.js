9f89bb5cce74bd48afa5fdb487cfa9de4d751b51
Alex Rickabaugh <alx+alxhub@alxandria.net>
2018-04-02 21:43:18 +0000

    ae76eec fix(upgrade): propagate return value of resumeBootstrap (#22754)

--- bundles/http.umd.js
+++ bundles/http.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.2.9-f43fba6
+ * @license Angular v5.2.9-ae76eec
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -36,7 +36,7 @@ function __extends(d, b) {
 }
 
 /**
- * @license Angular v5.2.9-f43fba6
+ * @license Angular v5.2.9-ae76eec
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2751,7 +2751,7 @@ var JsonpModule = /** @class */ (function () {
 /**
  * @deprecated use \@angular/common/http instead
  */
-var VERSION = new _angular_core.Version('5.2.9-f43fba6');
+var VERSION = new _angular_core.Version('5.2.9-ae76eec');
 
 exports.BrowserXhr = BrowserXhr;
 exports.JSONPBackend = JSONPBackend;
