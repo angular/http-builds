611fbe876615b05e487e75170de18c9f13b13043
Alex Rickabaugh <alx+alxhub@alxandria.net>
2018-04-03 18:30:34 +0000

    18ac228 docs: change examples within services to be tree-shakeable (#23070)

--- bundles/http.umd.js
+++ bundles/http.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-rc.1-fab6b39
+ * @license Angular v6.0.0-rc.1-18ac228
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -36,7 +36,7 @@ function __extends(d, b) {
 }
 
 /**
- * @license Angular v6.0.0-rc.1-fab6b39
+ * @license Angular v6.0.0-rc.1-18ac228
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2752,7 +2752,7 @@ var JsonpModule = /** @class */ (function () {
 /**
  * @deprecated use \@angular/common/http instead
  */
-var VERSION = new _angular_core.Version('6.0.0-rc.1-fab6b39');
+var VERSION = new _angular_core.Version('6.0.0-rc.1-18ac228');
 
 exports.BrowserXhr = BrowserXhr;
 exports.JSONPBackend = JSONPBackend;
