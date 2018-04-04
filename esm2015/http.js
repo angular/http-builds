2baba42c0c7ba3c6d9acea926801b4526433ca4f
Alex Rickabaugh <alx+alxhub@alxandria.net>
2018-04-04 15:45:48 +0000

    dd61595 fix(forms): improve error message for invalid value accessors (#22731)

--- esm2015/http.js
+++ esm2015/http.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.2.9-ae76eec
+ * @license Angular v5.2.9-dd61595
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2149,7 +2149,7 @@ JsonpModule.ctorParameters = () => [];
 /**
  * @deprecated use \@angular/common/http instead
  */
-const VERSION = new Version('5.2.9-ae76eec');
+const VERSION = new Version('5.2.9-dd61595');
 
 /**
  * @fileoverview added by tsickle
