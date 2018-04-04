399ac5725a19fce690529d851e30c4cee983d49e
Alex Rickabaugh <alx+alxhub@alxandria.net>
2018-04-04 17:55:08 +0000

    32a41bc test(ivy): fixes in the TodoMVC example (#23161)

--- esm2015/src/version.js
+++ esm2015/src/version.js
@@ -13,5 +13,5 @@ import { Version } from '@angular/core';
 /**
  * @deprecated use \@angular/common/http instead
  */
-export const /** @type {?} */ VERSION = new Version('6.0.0-rc.1-f48e215');
+export const /** @type {?} */ VERSION = new Version('6.0.0-rc.1-32a41bc');
 //# sourceMappingURL=version.js.map
