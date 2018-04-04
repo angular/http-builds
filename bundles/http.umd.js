a47aaa36c3abb43e4a5348e1440cfb3d2ac43e97
Alex Rickabaugh <alx+alxhub@alxandria.net>
2018-04-04 16:50:38 +0000

    5c8340a refactor(ivy): misc refactoring (#23154)

--- bundles/http.umd.js
+++ bundles/http.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-rc.1-46eadb5
+ * @license Angular v6.0.0-rc.1-5c8340a
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -36,7 +36,7 @@ function __extends(d, b) {
 }
 
 /**
- * @license Angular v6.0.0-rc.1-46eadb5
+ * @license Angular v6.0.0-rc.1-5c8340a
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2752,7 +2752,7 @@ var JsonpModule = /** @class */ (function () {
 /**
  * @deprecated use \@angular/common/http instead
  */
-var VERSION = new _angular_core.Version('6.0.0-rc.1-46eadb5');
+var VERSION = new _angular_core.Version('6.0.0-rc.1-5c8340a');
 
 exports.BrowserXhr = BrowserXhr;
 exports.JSONPBackend = JSONPBackend;
