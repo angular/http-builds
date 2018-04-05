78608bbc274b0594ed887f1a02d72f068af81340
Igor Minar <igor@angular.io>
2018-04-05 23:53:49 +0000

    b5f41f2 docs: fixed live example for the lifecycle hooks. (#23201)

--- bundles/http.umd.js
+++ bundles/http.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-rc.2-629629d
+ * @license Angular v6.0.0-rc.2-b5f41f2
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -36,7 +36,7 @@ function __extends(d, b) {
 }
 
 /**
- * @license Angular v6.0.0-rc.2-629629d
+ * @license Angular v6.0.0-rc.2-b5f41f2
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2752,7 +2752,7 @@ var JsonpModule = /** @class */ (function () {
 /**
  * @deprecated use \@angular/common/http instead
  */
-var VERSION = new _angular_core.Version('6.0.0-rc.2-629629d');
+var VERSION = new _angular_core.Version('6.0.0-rc.2-b5f41f2');
 
 exports.BrowserXhr = BrowserXhr;
 exports.JSONPBackend = JSONPBackend;
