2e1746a4b7d37befb189c2d7e969c0fbbd17a70f
Igor Minar <igor@angular.io>
2018-04-05 17:29:37 +0000

    8aa49ac fix(aio): update trusted GitHub teams (angular-core --> team) (#23181)

--- esm2015/http.js
+++ esm2015/http.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.2.9-3900c36
+ * @license Angular v5.2.9-8aa49ac
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2149,7 +2149,7 @@ JsonpModule.ctorParameters = () => [];
 /**
  * @deprecated use \@angular/common/http instead
  */
-const VERSION = new Version('5.2.9-3900c36');
+const VERSION = new Version('5.2.9-8aa49ac');
 
 /**
  * @fileoverview added by tsickle
