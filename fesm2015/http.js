592ff1621024c769557003af5bc0b94a309199f5
Igor Minar <igor@angular.io>
2018-04-06 22:03:46 +0000

    11ea3a3 fix(compiler-cli): don't lower expressions in flat module metadata (#23226)

--- fesm2015/http.js
+++ fesm2015/http.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-rc.2-195193b
+ * @license Angular v6.0.0-rc.2-11ea3a3
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2150,7 +2150,7 @@ JsonpModule.ctorParameters = () => [];
 /**
  * @deprecated use \@angular/common/http instead
  */
-const VERSION = new Version('6.0.0-rc.2-195193b');
+const VERSION = new Version('6.0.0-rc.2-11ea3a3');
 
 /**
  * @fileoverview added by tsickle
