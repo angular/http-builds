17e93e844528056f965381914acb67dd40d9781a
Igor Minar <igor@angular.io>
2018-04-07 00:13:19 +0000

    33f8120 docs: add changelog for 6.0.0-rc.3

---
+++ esm2015/http.js
@@ -0,0 +1,12 @@
+/**
+ * @fileoverview added by tsickle
+ * @suppress {checkTypes} checked by tsc
+ */
+/**
+ * Generated bundle index. Do not edit.
+ */
+export { BrowserXhr, JSONPBackend, JSONPConnection, CookieXSRFStrategy, XHRBackend, XHRConnection, BaseRequestOptions, RequestOptions, BaseResponseOptions, ResponseOptions, ReadyState, RequestMethod, ResponseContentType, ResponseType, Headers, Http, Jsonp, HttpModule, JsonpModule, Connection, ConnectionBackend, XSRFStrategy, Request, Response, QueryEncoder, URLSearchParams, VERSION } from './public_api';
+export { BrowserJsonp as ɵe } from './src/backends/browser_jsonp';
+export { Body as ɵf } from './src/body';
+export { _createDefaultCookieXSRFStrategy as ɵa, httpFactory as ɵb, jsonpFactory as ɵc } from './src/http_module';
+//# sourceMappingURL=http.js.map
