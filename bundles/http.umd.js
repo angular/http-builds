221189982281e0458a76bf4a5f242677bbf9ab46
Igor Minar <igor@angular.io>
2018-04-07 00:11:29 +0000

    33f8120 docs: add changelog for 6.0.0-rc.3

--- bundles/http.umd.js
+++ bundles/http.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-rc.2-da58a55
+ * @license Angular v6.0.0-rc.3-33f8120
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -36,7 +36,7 @@ function __extends(d, b) {
 }
 
 /**
- * @license Angular v6.0.0-rc.2-da58a55
+ * @license Angular v6.0.0-rc.3-33f8120
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2752,7 +2752,7 @@ var JsonpModule = /** @class */ (function () {
 /**
  * @deprecated use \@angular/common/http instead
  */
-var VERSION = new _angular_core.Version('6.0.0-rc.2-da58a55');
+var VERSION = new _angular_core.Version('6.0.0-rc.3-33f8120');
 
 exports.BrowserXhr = BrowserXhr;
 exports.JSONPBackend = JSONPBackend;
