8ac115dca301021c3700e643950fea5f286b6a39
Igor Minar <igor@angular.io>
2018-04-10 20:38:44 +0000

    f1db789 test(ivy): update todo app to http://todomvc.com (#23168)

--- bundles/http.umd.js
+++ bundles/http.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-rc.3-8555a3a
+ * @license Angular v6.0.0-rc.3-f1db789
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -36,7 +36,7 @@ function __extends(d, b) {
 }
 
 /**
- * @license Angular v6.0.0-rc.3-8555a3a
+ * @license Angular v6.0.0-rc.3-f1db789
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2752,7 +2752,7 @@ var JsonpModule = /** @class */ (function () {
 /**
  * @deprecated use \@angular/common/http instead
  */
-var VERSION = new _angular_core.Version('6.0.0-rc.3-8555a3a');
+var VERSION = new _angular_core.Version('6.0.0-rc.3-f1db789');
 
 exports.BrowserXhr = BrowserXhr;
 exports.JSONPBackend = JSONPBackend;
