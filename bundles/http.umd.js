89eedc2ec75a15ae8a77f7cf069c0eaf5e8c5467
Igor Minar <igor@angular.io>
2018-04-11 21:12:05 +0000

    764760b docs(aio): add new sections to issue template (#23304)

--- bundles/http.umd.js
+++ bundles/http.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-rc.3-b3a10e0
+ * @license Angular v6.0.0-rc.3-764760b
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -36,7 +36,7 @@ function __extends(d, b) {
 }
 
 /**
- * @license Angular v6.0.0-rc.3-b3a10e0
+ * @license Angular v6.0.0-rc.3-764760b
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2752,7 +2752,7 @@ var JsonpModule = /** @class */ (function () {
 /**
  * @deprecated use \@angular/common/http instead
  */
-var VERSION = new _angular_core.Version('6.0.0-rc.3-b3a10e0');
+var VERSION = new _angular_core.Version('6.0.0-rc.3-764760b');
 
 exports.BrowserXhr = BrowserXhr;
 exports.JSONPBackend = JSONPBackend;
