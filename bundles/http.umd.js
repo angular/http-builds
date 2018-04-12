0032ca44874f54e92a6c3742c041c264a5813d2f
Igor Minar <igor@angular.io>
2018-04-12 06:39:53 +0000

    7e8cee6 build: fix aio size tracking, we need to use node_modules local to aio (#23328)

--- bundles/http.umd.js
+++ bundles/http.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-rc.3-37d2cb4
+ * @license Angular v6.0.0-rc.3-7e8cee6
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -36,7 +36,7 @@ function __extends(d, b) {
 }
 
 /**
- * @license Angular v6.0.0-rc.3-37d2cb4
+ * @license Angular v6.0.0-rc.3-7e8cee6
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2752,7 +2752,7 @@ var JsonpModule = /** @class */ (function () {
 /**
  * @deprecated use \@angular/common/http instead
  */
-var VERSION = new _angular_core.Version('6.0.0-rc.3-37d2cb4');
+var VERSION = new _angular_core.Version('6.0.0-rc.3-7e8cee6');
 
 exports.BrowserXhr = BrowserXhr;
 exports.JSONPBackend = JSONPBackend;
