917134dbd53ab2e1a3db83029d292fbdf7fddacc
Igor Minar <igor@angular.io>
2018-04-13 20:46:26 +0000

    21f3301 refactor: ensure all 'TODO's are consistent (#23252)

--- bundles/http.umd.js
+++ bundles/http.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.2.9-a3204f8
+ * @license Angular v5.2.9-21f3301
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -36,7 +36,7 @@ function __extends(d, b) {
 }
 
 /**
- * @license Angular v5.2.9-a3204f8
+ * @license Angular v5.2.9-21f3301
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2751,7 +2751,7 @@ var JsonpModule = /** @class */ (function () {
 /**
  * @deprecated use \@angular/common/http instead
  */
-var VERSION = new _angular_core.Version('5.2.9-a3204f8');
+var VERSION = new _angular_core.Version('5.2.9-21f3301');
 
 exports.BrowserXhr = BrowserXhr;
 exports.JSONPBackend = JSONPBackend;
