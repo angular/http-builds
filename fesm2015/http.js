b8390b700a5763b7a7ecd139fc2e78da6c035356
Igor Minar <igor@angular.io>
2018-04-13 21:42:52 +0000

    490772e docs(aio): Update file tree to match Angular CLI V6 structure (#23367)

--- fesm2015/http.js
+++ fesm2015/http.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-rc.4-e7ef027
+ * @license Angular v6.0.0-rc.4-490772e
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2150,7 +2150,7 @@ JsonpModule.ctorParameters = () => [];
 /**
  * @deprecated use \@angular/common/http instead
  */
-const VERSION = new Version('6.0.0-rc.4-e7ef027');
+const VERSION = new Version('6.0.0-rc.4-490772e');
 
 /**
  * @fileoverview added by tsickle
