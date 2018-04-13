610d6fbfca50cd58b680e7bbf03386200846a3bf
Igor Minar <igor@angular.io>
2018-04-13 07:30:31 +0000

    a113330 refactor(language-service): fix typo on type.ts language-service

--- esm5/http.js
+++ esm5/http.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.2.9-3cf5719
+ * @license Angular v5.2.9-a113330
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2719,7 +2719,7 @@ var JsonpModule = /** @class */ (function () {
 /**
  * @deprecated use \@angular/common/http instead
  */
-var VERSION = new Version('5.2.9-3cf5719');
+var VERSION = new Version('5.2.9-a113330');
 
 /**
  * @fileoverview added by tsickle
