7d99bdc4e4f31c21e8f0edf35c1a2cfab21b50e5
Igor Minar <igor@angular.io>
2018-04-13 23:52:52 +0000

    d1177c7 refactor: update CLDR data for closure locale & use a const for undefined (#23372) PR Close #23372

--- fesm2015/http.js
+++ fesm2015/http.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-rc.4-12f90ef
+ * @license Angular v6.0.0-rc.4-d1177c7
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2150,7 +2150,7 @@ JsonpModule.ctorParameters = () => [];
 /**
  * @deprecated use \@angular/common/http instead
  */
-const VERSION = new Version('6.0.0-rc.4-12f90ef');
+const VERSION = new Version('6.0.0-rc.4-d1177c7');
 
 /**
  * @fileoverview added by tsickle
