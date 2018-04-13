d0516b51fc9108211c3523ef0fc866f452762658
Igor Minar <igor@angular.io>
2018-04-13 15:36:15 +0000

    61bddeb fix(aio): remove additional 'googlebot' reference (#23249)

--- esm5/http.js
+++ esm5/http.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.2.9-a113330
+ * @license Angular v5.2.9-61bddeb
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2719,7 +2719,7 @@ var JsonpModule = /** @class */ (function () {
 /**
  * @deprecated use \@angular/common/http instead
  */
-var VERSION = new Version('5.2.9-a113330');
+var VERSION = new Version('5.2.9-61bddeb');
 
 /**
  * @fileoverview added by tsickle
