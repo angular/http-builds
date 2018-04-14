af3692b50cc1f0d1c44553a3d029fb1f017a1736
Igor Minar <igor@angular.io>
2018-04-14 06:29:33 +0000

    674c3de revert: refactor(ivy): make return value of define(Component|Directive|Pipe|Injector|Injectable) private (#23371)

--- bundles/http.umd.js
+++ bundles/http.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-rc.4-33630dd
+ * @license Angular v6.0.0-rc.4-674c3de
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -36,7 +36,7 @@ function __extends(d, b) {
 }
 
 /**
- * @license Angular v6.0.0-rc.4-33630dd
+ * @license Angular v6.0.0-rc.4-674c3de
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2752,7 +2752,7 @@ var JsonpModule = /** @class */ (function () {
 /**
  * @deprecated use \@angular/common/http instead
  */
-var VERSION = new _angular_core.Version('6.0.0-rc.4-33630dd');
+var VERSION = new _angular_core.Version('6.0.0-rc.4-674c3de');
 
 exports.BrowserXhr = BrowserXhr;
 exports.JSONPBackend = JSONPBackend;
