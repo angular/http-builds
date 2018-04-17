7e13a88b2f3bc575c9697867c8236bf7aeb0be9e
Jason Aden <jasonaden@google.com>
2018-04-17 21:11:17 +0000

    7f612fc fix(ivy): generate bind calls for property bindings (#23403)

--- esm2015/src/version.js
+++ esm2015/src/version.js
@@ -13,5 +13,5 @@ import { Version } from '@angular/core';
 /**
  * @deprecated use \@angular/common/http instead
  */
-export const /** @type {?} */ VERSION = new Version('6.0.0-rc.5-99f8e10');
+export const /** @type {?} */ VERSION = new Version('6.0.0-rc.5-7f612fc');
 //# sourceMappingURL=version.js.map
