1d660fc10fdd3df64ed816ca665267a283b3f88f
Jason Aden <jasonaden@google.com>
2018-04-17 21:45:06 +0000

    402f452 docs(aio): add front page campaign for the ng-conf live stream (#23391)

--- esm2015/http.js
+++ esm2015/http.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.2.10-d2e7c99
+ * @license Angular v5.2.10-402f452
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2149,7 +2149,7 @@ JsonpModule.ctorParameters = () => [];
 /**
  * @deprecated use \@angular/common/http instead
  */
-const VERSION = new Version('5.2.10-d2e7c99');
+const VERSION = new Version('5.2.10-402f452');
 
 /**
  * @fileoverview added by tsickle
