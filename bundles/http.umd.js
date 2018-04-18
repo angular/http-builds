a8c37a887bdf9861ce55ef70007026e5fd38ea05
Victor Berchet <victor@suumit.com>
2018-04-18 22:13:19 +0000

    acf6781 test(core): add a symbols test for renderer2 code (#23436)

--- bundles/http.umd.js
+++ bundles/http.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-rc.5-fd48e53
+ * @license Angular v6.0.0-rc.5-acf6781
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -36,7 +36,7 @@ function __extends(d, b) {
 }
 
 /**
- * @license Angular v6.0.0-rc.5-fd48e53
+ * @license Angular v6.0.0-rc.5-acf6781
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2752,7 +2752,7 @@ var JsonpModule = /** @class */ (function () {
 /**
  * @deprecated use \@angular/common/http instead
  */
-var VERSION = new _angular_core.Version('6.0.0-rc.5-fd48e53');
+var VERSION = new _angular_core.Version('6.0.0-rc.5-acf6781');
 
 exports.BrowserXhr = BrowserXhr;
 exports.JSONPBackend = JSONPBackend;
