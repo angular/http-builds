47ba33202db1dffb3088559d08983eece0be3e3a
Victor Berchet <victor@suumit.com>
2018-04-19 23:55:52 +0000

    9757347 feat(ivy): add an IVY local the compiler which avoids analyzeModule (#23441)

--- fesm2015/http.js
+++ fesm2015/http.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-rc.5-1d1e75e
+ * @license Angular v6.0.0-rc.5-9757347
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2150,7 +2150,7 @@ JsonpModule.ctorParameters = () => [];
 /**
  * @deprecated use \@angular/common/http instead
  */
-const VERSION = new Version('6.0.0-rc.5-1d1e75e');
+const VERSION = new Version('6.0.0-rc.5-9757347');
 
 /**
  * @fileoverview added by tsickle
