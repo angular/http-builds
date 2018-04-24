1c6305686b5557744c671013d62f5b45fe54b9b9
Victor Berchet <victor@suumit.com>
2018-04-24 18:37:18 +0000

    540626a build(common): mark locales files as side-effect-full (#23509)

--- esm2015/src/version.js
+++ esm2015/src/version.js
@@ -13,5 +13,5 @@ import { Version } from '@angular/core';
 /**
  * @deprecated use \@angular/common/http instead
  */
-export const /** @type {?} */ VERSION = new Version('6.0.0-rc.5-391bfce');
+export const /** @type {?} */ VERSION = new Version('6.0.0-rc.5-540626a');
 //# sourceMappingURL=version.js.map
