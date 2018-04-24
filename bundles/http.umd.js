e339c39bbaf409df45ac7dadeee525b8fc205997
Victor Berchet <victor@suumit.com>
2018-04-24 21:59:12 +0000

    f721b06 style: format code

--- bundles/http.umd.js
+++ bundles/http.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-rc.5-0bc8443
+ * @license Angular v6.0.0-rc.5-f721b06
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -36,7 +36,7 @@ function __extends(d, b) {
 }
 
 /**
- * @license Angular v6.0.0-rc.5-0bc8443
+ * @license Angular v6.0.0-rc.5-f721b06
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2752,7 +2752,7 @@ var JsonpModule = /** @class */ (function () {
 /**
  * @deprecated use \@angular/common/http instead
  */
-var VERSION = new _angular_core.Version('6.0.0-rc.5-0bc8443');
+var VERSION = new _angular_core.Version('6.0.0-rc.5-f721b06');
 
 exports.BrowserXhr = BrowserXhr;
 exports.JSONPBackend = JSONPBackend;
