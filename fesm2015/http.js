cfba778ca36772f683acd02ea3ea408fe68ac574
Victor Berchet <victor@suumit.com>
2018-04-24 22:01:21 +0000

    811a7f2 docs(benchpress): fix typo in README (#23471) (#23488)

--- fesm2015/http.js
+++ fesm2015/http.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-rc.5+79.sha-9ed5fb6
+ * @license Angular v6.0.0-rc.5+80.sha-811a7f2
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2151,7 +2151,7 @@ JsonpModule.ctorParameters = () => [];
 /**
  * @deprecated use \@angular/common/http instead
  */
-const VERSION = new Version('6.0.0-rc.5+79.sha-9ed5fb6');
+const VERSION = new Version('6.0.0-rc.5+80.sha-811a7f2');
 
 /**
  * @fileoverview added by tsickle
