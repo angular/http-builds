88ad778b47e342a47b0361bfd6d729052688b074
Victor Berchet <victor@suumit.com>
2018-04-24 22:11:18 +0000

    04c18ac docs: fix typo (#23514)

--- esm2015/src/version.js
+++ esm2015/src/version.js
@@ -13,5 +13,5 @@ import { Version } from '@angular/core';
 /**
  * @deprecated use \@angular/common/http instead
  */
-export const /** @type {?} */ VERSION = new Version('6.0.0-rc.5-1b26dd8');
+export const /** @type {?} */ VERSION = new Version('6.0.0-rc.5-04c18ac');
 //# sourceMappingURL=version.js.map
