00226cdc52b14e2f6e1c49e260b36da98c7ce3a9
Victor Berchet <victor@suumit.com>
2018-04-24 22:04:48 +0000

    43a49d3 docs: fix typo (#23514)

--- fesm2015/http.js
+++ fesm2015/http.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-rc.5+80.sha-811a7f2
+ * @license Angular v6.0.0-rc.5+81.sha-43a49d3
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2151,7 +2151,7 @@ JsonpModule.ctorParameters = () => [];
 /**
  * @deprecated use \@angular/common/http instead
  */
-const VERSION = new Version('6.0.0-rc.5+80.sha-811a7f2');
+const VERSION = new Version('6.0.0-rc.5+81.sha-43a49d3');
 
 /**
  * @fileoverview added by tsickle
