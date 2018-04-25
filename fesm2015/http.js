f5ad38833b7b9123e9b4a93a75a98d5c1dcb8d9a
Igor Minar <igor@angular.io>
2018-04-25 20:50:39 +0000

    ab5bc42 feat(ivy): first steps towards ngtsc mode (#23455)

--- fesm2015/http.js
+++ fesm2015/http.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-rc.5+82.sha-8d0ee34
+ * @license Angular v6.0.0-rc.5+84.sha-ab5bc42
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2151,7 +2151,7 @@ JsonpModule.ctorParameters = () => [];
 /**
  * @deprecated use \@angular/common/http instead
  */
-const VERSION = new Version('6.0.0-rc.5+82.sha-8d0ee34');
+const VERSION = new Version('6.0.0-rc.5+84.sha-ab5bc42');
 
 /**
  * @fileoverview added by tsickle
