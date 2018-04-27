38457f83ab12db356cb1ec3e407b92a9bc16b010
Igor Minar <igor@angular.io>
2018-04-27 14:48:15 +0000

    5021bdd ci: add Brandon Roberts as an aio approver (#23417)

--- fesm2015/http.js
+++ fesm2015/http.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-rc.5-64bf6ed
+ * @license Angular v6.0.0-rc.5-5021bdd
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2150,7 +2150,7 @@ JsonpModule.ctorParameters = () => [];
 /**
  * @deprecated use \@angular/common/http instead
  */
-const VERSION = new Version('6.0.0-rc.5-64bf6ed');
+const VERSION = new Version('6.0.0-rc.5-5021bdd');
 
 /**
  * @fileoverview added by tsickle
