f15f4ced261f779b9b7fb3e24669b8e38e9f1c33
Igor Minar <igor@angular.io>
2018-05-03 22:30:18 +0000

    b19216d fix(aio): correct project id for deployment of archive sites

--- @angular/http.js
+++ @angular/http.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.4.7-84fc1a3
+ * @license Angular v4.4.7-b19216d
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2088,7 +2088,7 @@ JsonpModule.ctorParameters = () => [];
 /**
  * \@stable
  */
-const VERSION = new Version('4.4.7-84fc1a3');
+const VERSION = new Version('4.4.7-b19216d');
 
 /**
  * @license
