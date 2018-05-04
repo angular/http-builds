2f073c4534e33cc9bcb012ed9c389d3c17db90d7
Igor Minar <igor@angular.io>
2018-05-04 23:10:47 +0000

    83f12f3 build: update to latest nodejs bazel rules (#23683)

--- bundles/http.umd.js
+++ bundles/http.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-bbc416c
+ * @license Angular v6.0.0-83f12f3
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -36,7 +36,7 @@ function __extends(d, b) {
 }
 
 /**
- * @license Angular v6.0.0-bbc416c
+ * @license Angular v6.0.0-83f12f3
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2752,7 +2752,7 @@ var JsonpModule = /** @class */ (function () {
 /**
  * @deprecated use \@angular/common/http instead
  */
-var VERSION = new _angular_core.Version('6.0.0-bbc416c');
+var VERSION = new _angular_core.Version('6.0.0-83f12f3');
 
 exports.BrowserXhr = BrowserXhr;
 exports.JSONPBackend = JSONPBackend;
