8cfde4886510f6f2045e907c5f1a84b3f57e6902
Igor Minar <igor@angular.io>
2018-05-05 15:38:55 +0000

    e351896 style(animations): fix short param names (#23668)

--- fesm2015/http.js
+++ fesm2015/http.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-rc.5+136.sha-7e9649b
+ * @license Angular v6.0.0-rc.5+138.sha-e351896
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2151,7 +2151,7 @@ JsonpModule.ctorParameters = () => [];
 /**
  * @deprecated use \@angular/common/http instead
  */
-const VERSION = new Version('6.0.0-rc.5+136.sha-7e9649b');
+const VERSION = new Version('6.0.0-rc.5+138.sha-e351896');
 
 /**
  * @fileoverview added by tsickle
