e691df40987664508c0c45df8527520ec5d57115
Igor Minar <igor@angular.io>
2018-05-09 19:32:11 +0000

    2254ac2 fix(service-worker): correctly handle requests with empty `clientId` (#23625)

--- fesm2015/http.js
+++ fesm2015/http.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0+38.sha-2b31b6d
+ * @license Angular v6.0.0+37.sha-2254ac2
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2151,7 +2151,7 @@ JsonpModule.ctorParameters = () => [];
 /**
  * @deprecated use \@angular/common/http instead
  */
-const VERSION = new Version('6.0.0+38.sha-2b31b6d');
+const VERSION = new Version('6.0.0+37.sha-2254ac2');
 
 /**
  * @fileoverview added by tsickle
