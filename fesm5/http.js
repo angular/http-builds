3b188d3b6ac0c4bdae49d1ffbcc5e02d8db9c4ac
Igor Minar <igor@angular.io>
2018-05-09 19:27:44 +0000

    c5ca5c0 build(bazel): update to rules_typescript 0.12.3 (#23617)

--- fesm5/http.js
+++ fesm5/http.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-rc.5+147.sha-6117085
+ * @license Angular v6.0.0-rc.5+148.sha-c5ca5c0
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2290,7 +2290,7 @@ var JsonpModule = /** @class */ (function () {
 /**
  * @deprecated use @angular/common/http instead
  */
-var VERSION = new Version('6.0.0-rc.5+147.sha-6117085');
+var VERSION = new Version('6.0.0-rc.5+148.sha-c5ca5c0');
 
 /**
  * @license
