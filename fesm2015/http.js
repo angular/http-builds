c1d11b57aa93ee926c2c33cd9010cc7a16dc2d02
Alex Rickabaugh <alx+alxhub@alxandria.net>
2018-05-14 17:57:25 +0000

    bc20638 build(aio): ensure usageNotes are copied into decorator API docs (#23901)

--- fesm2015/http.js
+++ fesm2015/http.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0+56.sha-2a528fc
+ * @license Angular v6.0.0+57.sha-bc20638
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2151,7 +2151,7 @@ JsonpModule.ctorParameters = () => [];
 /**
  * @deprecated use \@angular/common/http instead
  */
-const VERSION = new Version('6.0.0+56.sha-2a528fc');
+const VERSION = new Version('6.0.0+57.sha-bc20638');
 
 /**
  * @fileoverview added by tsickle
--- fesm5/http.js
+++ fesm5/http.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0+56.sha-2a528fc
+ * @license Angular v6.0.0+57.sha-bc20638
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2290,7 +2290,7 @@ var JsonpModule = /** @class */ (function () {
 /**
  * @deprecated use @angular/common/http instead
  */
-var VERSION = new Version('6.0.0+56.sha-2a528fc');
+var VERSION = new Version('6.0.0+57.sha-bc20638');
 
 /**
  * @license
