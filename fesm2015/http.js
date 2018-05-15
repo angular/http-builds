4eb728510e5472b60361869b2ce59740b19a970d
Alex Rickabaugh <alx+alxhub@alxandria.net>
2018-05-15 22:59:13 +0000

    95fca24 feat(aio): add brand and concept icons, img style class more flexible (#23589)

--- fesm2015/http.js
+++ fesm2015/http.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.2
+ * @license Angular v6.0.2+1.sha-95fca24
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2151,7 +2151,7 @@ JsonpModule.ctorParameters = () => [];
 /**
  * @deprecated use \@angular/common/http instead
  */
-const VERSION = new Version('6.0.2');
+const VERSION = new Version('6.0.2+1.sha-95fca24');
 
 /**
  * @fileoverview added by tsickle
