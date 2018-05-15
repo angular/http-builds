a8ef7e0a74cb0e82b29849d7a00fe951a16fd688
Alex Rickabaugh <alx+alxhub@alxandria.net>
2018-05-15 19:03:19 +0000

    ad6052e test: switch to ts_web_test_suite (#23859)

--- fesm5/http.js
+++ fesm5/http.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0+60.sha-e9d1709
+ * @license Angular v6.0.0+61.sha-ad6052e
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2290,7 +2290,7 @@ var JsonpModule = /** @class */ (function () {
 /**
  * @deprecated use @angular/common/http instead
  */
-var VERSION = new Version('6.0.0+60.sha-e9d1709');
+var VERSION = new Version('6.0.0+61.sha-ad6052e');
 
 /**
  * @license
