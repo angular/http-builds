f6b59fcf8e4e6b114fd8cf51950b6224fef71efd
Matias Niemelä <matias@yearofmoo.com>
2018-05-21 20:47:10 +0000

    db2329e docs(aio): Add missing dependencies and files to testing zip file download (#23948)

--- bundles/http.umd.js
+++ bundles/http.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-rc.5+204.sha-de267e9
+ * @license Angular v6.0.0-rc.5+205.sha-db2329e
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2352,7 +2352,7 @@ var JsonpModule = /** @class */ (function () {
 /**
  * @deprecated use @angular/common/http instead
  */
-var VERSION = new core.Version('6.0.0-rc.5+204.sha-de267e9');
+var VERSION = new core.Version('6.0.0-rc.5+205.sha-db2329e');
 
 /**
  * @license
