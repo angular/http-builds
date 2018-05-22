8c42eefcb4298c4492cbc9cc5a317c5854fb9f00
Matias Niemelä <matias@yearofmoo.com>
2018-05-22 19:32:14 +0000

    0ee5b7e fix(service-worker): check platformBrowser before accessing navigator.serviceWorker (#21231)

--- fesm2015/http.js
+++ fesm2015/http.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.2+19.sha-23f2a70
+ * @license Angular v6.0.2+21.sha-0ee5b7e
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2151,7 +2151,7 @@ JsonpModule.ctorParameters = () => [];
 /**
  * @deprecated use \@angular/common/http instead
  */
-const VERSION = new Version('6.0.2+19.sha-23f2a70');
+const VERSION = new Version('6.0.2+21.sha-0ee5b7e');
 
 /**
  * @fileoverview added by tsickle
