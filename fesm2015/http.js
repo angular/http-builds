5932d688d4f0a5a051160294c40e9131a1295e9d
Matias Niemelä <matias@yearofmoo.com>
2018-05-22 21:56:05 +0000

    469b1e4 docs: add doc to event-management api (#23656)

--- fesm2015/http.js
+++ fesm2015/http.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.2+22.sha-7fee1fd
+ * @license Angular v6.0.2+23.sha-469b1e4
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2151,7 +2151,7 @@ JsonpModule.ctorParameters = () => [];
 /**
  * @deprecated use \@angular/common/http instead
  */
-const VERSION = new Version('6.0.2+22.sha-7fee1fd');
+const VERSION = new Version('6.0.2+23.sha-469b1e4');
 
 /**
  * @fileoverview added by tsickle
