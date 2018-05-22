e58847c37c6d08794e3e687835077085b1b55700
Matias Niemelä <matias@yearofmoo.com>
2018-05-22 17:59:43 +0000

    23f2a70 docs: fix typo (#23998)

--- fesm2015/http.js
+++ fesm2015/http.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.2+20.sha-afff84c
+ * @license Angular v6.0.2+19.sha-23f2a70
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2151,7 +2151,7 @@ JsonpModule.ctorParameters = () => [];
 /**
  * @deprecated use \@angular/common/http instead
  */
-const VERSION = new Version('6.0.2+20.sha-afff84c');
+const VERSION = new Version('6.0.2+19.sha-23f2a70');
 
 /**
  * @fileoverview added by tsickle
