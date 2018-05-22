0958f11d3801a60915d8011c65826ac5709b829c
Matias Niemelä <matias@yearofmoo.com>
2018-05-22 19:31:31 +0000

    0bdd30e fix(service-worker): check platformBrowser before accessing navigator.serviceWorker (#21231)

--- fesm2015/http.js
+++ fesm2015/http.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-rc.5+211.sha-373fa78
+ * @license Angular v6.0.0-rc.5+212.sha-0bdd30e
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2145,7 +2145,7 @@ JsonpModule.decorators = [
 /**
  * @deprecated use \@angular/common/http instead
  */
-const VERSION = new Version('6.0.0-rc.5+211.sha-373fa78');
+const VERSION = new Version('6.0.0-rc.5+212.sha-0bdd30e');
 
 /**
  * @fileoverview added by tsickle
--- fesm5/http.js
+++ fesm5/http.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-rc.5+211.sha-373fa78
+ * @license Angular v6.0.0-rc.5+212.sha-0bdd30e
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2284,7 +2284,7 @@ var JsonpModule = /** @class */ (function () {
 /**
  * @deprecated use @angular/common/http instead
  */
-var VERSION = new Version('6.0.0-rc.5+211.sha-373fa78');
+var VERSION = new Version('6.0.0-rc.5+212.sha-0bdd30e');
 
 /**
  * @license
