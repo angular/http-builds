d705a330b50066c1665f0721ebea44a53d4e5b11
Matias Niemelä <matias@yearofmoo.com>
2018-05-25 18:18:11 +0000

    f206eb9 docs(aio): add Juri Strumpflohner to GDE resources (#24086)

--- fesm2015/http.js
+++ fesm2015/http.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.3+1.sha-3d6e82e
+ * @license Angular v6.0.3+2.sha-f206eb9
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2151,7 +2151,7 @@ JsonpModule.ctorParameters = () => [];
 /**
  * @deprecated use \@angular/common/http instead
  */
-const VERSION = new Version('6.0.3+1.sha-3d6e82e');
+const VERSION = new Version('6.0.3+2.sha-f206eb9');
 
 /**
  * @fileoverview added by tsickle
