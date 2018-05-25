64b6fc85e4c7adf8ba108054bdb668304076cd18
Matias Niemelä <matias@yearofmoo.com>
2018-05-25 18:23:31 +0000

    a638f50 docs(aio): add material community components (#24042)

--- bundles/http.umd.js
+++ bundles/http.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.3+3.sha-a29c756
+ * @license Angular v6.0.3+4.sha-a638f50
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2358,7 +2358,7 @@ var JsonpModule = /** @class */ (function () {
 /**
  * @deprecated use @angular/common/http instead
  */
-var VERSION = new core.Version('6.0.3+3.sha-a29c756');
+var VERSION = new core.Version('6.0.3+4.sha-a638f50');
 
 /**
  * @license
