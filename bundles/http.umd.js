24099ae371660e4aa6180e2ef31905869675a80b
Matias Niemelä <matias@yearofmoo.com>
2018-05-29 21:20:43 +0000

    7698afe docs(http): correct spelling error (#23675)

--- bundles/http.umd.js
+++ bundles/http.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.3+7.sha-84b43da
+ * @license Angular v6.0.3+10.sha-7698afe
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2358,7 +2358,7 @@ var JsonpModule = /** @class */ (function () {
 /**
  * @deprecated use @angular/common/http instead
  */
-var VERSION = new core.Version('6.0.3+7.sha-84b43da');
+var VERSION = new core.Version('6.0.3+10.sha-7698afe');
 
 /**
  * @license
