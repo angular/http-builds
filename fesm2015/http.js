dd8a15a83037a283d4afa98257832354c37140e3
Matias Niemelä <matias@yearofmoo.com>
2018-05-29 23:13:33 +0000

    31795b6 style(compiler): fix typo and formatting (#23729)

--- fesm2015/http.js
+++ fesm2015/http.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-rc.5+245.sha-41cd8f3
+ * @license Angular v6.0.0-rc.5+246.sha-31795b6
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2145,7 +2145,7 @@ JsonpModule.decorators = [
 /**
  * @deprecated use \@angular/common/http instead
  */
-const VERSION = new Version('6.0.0-rc.5+245.sha-41cd8f3');
+const VERSION = new Version('6.0.0-rc.5+246.sha-31795b6');
 
 /**
  * @fileoverview added by tsickle
