9c232cf424172a52729c90841df0abb9032b236d
Victor Berchet <victor@suumit.com>
2018-05-30 23:17:13 +0000

    accda00 test(platform-server): update the symbol lists (#24209)

--- fesm2015/http.js
+++ fesm2015/http.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-rc.5+255.sha-d6595eb
+ * @license Angular v6.0.0-rc.5+272.sha-accda00
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2145,7 +2145,7 @@ JsonpModule.decorators = [
 /**
  * @deprecated use \@angular/common/http instead
  */
-const VERSION = new Version('6.0.0-rc.5+255.sha-d6595eb');
+const VERSION = new Version('6.0.0-rc.5+272.sha-accda00');
 
 /**
  * @fileoverview added by tsickle
