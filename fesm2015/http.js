d9a1927a60a4cf6b38180711fd5caae92484447e
Victor Berchet <victor@suumit.com>
2018-05-30 23:36:22 +0000

    a634a5a Revert "docs: update docs to use HttpClientModule instead of HttpModule (#22727)"

--- fesm2015/http.js
+++ fesm2015/http.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.3+34.sha-4122502
+ * @license Angular v6.0.3+35.sha-a634a5a
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2151,7 +2151,7 @@ JsonpModule.ctorParameters = () => [];
 /**
  * @deprecated use \@angular/common/http instead
  */
-const VERSION = new Version('6.0.3+34.sha-4122502');
+const VERSION = new Version('6.0.3+35.sha-a634a5a');
 
 /**
  * @fileoverview added by tsickle
