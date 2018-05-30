1fba2322d004c371a27a1c2c28db9523161ca2b6
Matias Niemelä <matias@yearofmoo.com>
2018-05-30 06:04:56 +0000

    e53179e refactor(ivy): move parent from LNode to TNode (#24189)

--- fesm2015/http.js
+++ fesm2015/http.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-rc.5+246.sha-31795b6
+ * @license Angular v6.0.0-rc.5+247.sha-e53179e
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2145,7 +2145,7 @@ JsonpModule.decorators = [
 /**
  * @deprecated use \@angular/common/http instead
  */
-const VERSION = new Version('6.0.0-rc.5+246.sha-31795b6');
+const VERSION = new Version('6.0.0-rc.5+247.sha-e53179e');
 
 /**
  * @fileoverview added by tsickle
