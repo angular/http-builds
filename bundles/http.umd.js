21a08fc989806881f8adc0b3e69e77afd1a533ed
Victor Berchet <victor@suumit.com>
2018-05-30 19:27:37 +0000

    d249852 build: update rules_webtesting (#24198)

--- bundles/http.umd.js
+++ bundles/http.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.3+27.sha-707dd59
+ * @license Angular v6.0.3+28.sha-d249852
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2358,7 +2358,7 @@ var JsonpModule = /** @class */ (function () {
 /**
  * @deprecated use @angular/common/http instead
  */
-var VERSION = new core.Version('6.0.3+27.sha-707dd59');
+var VERSION = new core.Version('6.0.3+28.sha-d249852');
 
 /**
  * @license
