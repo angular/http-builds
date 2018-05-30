8b43ae6a31c6a350738054b3e740ff517929008e
Victor Berchet <victor@suumit.com>
2018-05-30 17:34:55 +0000

    b87d650 refactor(ivy): rename `PipeDef.n` to `PipeDef.factory` (#23883)

--- fesm2015/http.js
+++ fesm2015/http.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-rc.5+250.sha-f6f44ed
+ * @license Angular v6.0.0-rc.5+248.sha-b87d650
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2145,7 +2145,7 @@ JsonpModule.decorators = [
 /**
  * @deprecated use \@angular/common/http instead
  */
-const VERSION = new Version('6.0.0-rc.5+250.sha-f6f44ed');
+const VERSION = new Version('6.0.0-rc.5+248.sha-b87d650');
 
 /**
  * @fileoverview added by tsickle
