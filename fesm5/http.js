92b4a1e133e9929841dd4c9a20de312b7a8d2a99
Victor Berchet <victor@suumit.com>
2018-05-30 17:52:58 +0000

    d6595eb feat(platform-server): use EventManagerPlugin on the server (#24132)

--- fesm5/http.js
+++ fesm5/http.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-rc.5+253.sha-3ed7fc6
+ * @license Angular v6.0.0-rc.5+255.sha-d6595eb
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2284,7 +2284,7 @@ var JsonpModule = /** @class */ (function () {
 /**
  * @deprecated use @angular/common/http instead
  */
-var VERSION = new Version('6.0.0-rc.5+253.sha-3ed7fc6');
+var VERSION = new Version('6.0.0-rc.5+255.sha-d6595eb');
 
 /**
  * @license
