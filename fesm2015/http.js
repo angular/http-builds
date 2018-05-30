3de9eee5ba2c988e71f452b0e5952768dfc4c112
Victor Berchet <victor@suumit.com>
2018-05-30 17:37:32 +0000

    2e21690 feat(ivy): support renderer.destroy and renderer.destroyNode hooks (#24049)

--- fesm2015/http.js
+++ fesm2015/http.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-rc.5+252.sha-7c1bd71
+ * @license Angular v6.0.0-rc.5+251.sha-2e21690
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2145,7 +2145,7 @@ JsonpModule.decorators = [
 /**
  * @deprecated use \@angular/common/http instead
  */
-const VERSION = new Version('6.0.0-rc.5+252.sha-7c1bd71');
+const VERSION = new Version('6.0.0-rc.5+251.sha-2e21690');
 
 /**
  * @fileoverview added by tsickle
