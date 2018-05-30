36b37e40c8a59efc082cce708e9d5ca51684c23a
Victor Berchet <victor@suumit.com>
2018-05-30 17:35:44 +0000

    90bf5d8 feat(ivy): separate attributes for directive matching purposes (#23991)

--- fesm5/http.js
+++ fesm5/http.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-rc.5+248.sha-b87d650
+ * @license Angular v6.0.0-rc.5+249.sha-90bf5d8
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2284,7 +2284,7 @@ var JsonpModule = /** @class */ (function () {
 /**
  * @deprecated use @angular/common/http instead
  */
-var VERSION = new Version('6.0.0-rc.5+248.sha-b87d650');
+var VERSION = new Version('6.0.0-rc.5+249.sha-90bf5d8');
 
 /**
  * @license
