1bf77bb24f3274dbdae949e99888893f647f7848
Victor Berchet <victor@suumit.com>
2018-05-31 20:43:35 +0000

    7e3f8f7 refactor(ivy): replace LView.child with TView.childIndex lookup (#24211)

--- fesm5/http.js
+++ fesm5/http.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-rc.5+281.sha-b86d4de
+ * @license Angular v6.0.0-rc.5+288.sha-7e3f8f7
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2284,7 +2284,7 @@ var JsonpModule = /** @class */ (function () {
 /**
  * @deprecated use @angular/common/http instead
  */
-var VERSION = new Version('6.0.0-rc.5+281.sha-b86d4de');
+var VERSION = new Version('6.0.0-rc.5+288.sha-7e3f8f7');
 
 /**
  * @license
