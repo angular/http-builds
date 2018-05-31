de1a10421064fef14b5e764a6620255530eea1b8
Victor Berchet <victor@suumit.com>
2018-05-31 00:03:08 +0000

    c9eb491 fix(animations): Fix browser detection logic (#24188)

--- fesm2015/http.js
+++ fesm2015/http.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.3+35.sha-a634a5a
+ * @license Angular v6.0.3+36.sha-c9eb491
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2151,7 +2151,7 @@ JsonpModule.ctorParameters = () => [];
 /**
  * @deprecated use \@angular/common/http instead
  */
-const VERSION = new Version('6.0.3+35.sha-a634a5a');
+const VERSION = new Version('6.0.3+36.sha-c9eb491');
 
 /**
  * @fileoverview added by tsickle
