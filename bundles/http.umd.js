a7da673af56f28a299ad27394ec753730333b486
Victor Berchet <victor@suumit.com>
2018-05-31 17:44:10 +0000

    c17098d fix(platform-server): don't reflect innerHTML property to attibute (#24213)

--- bundles/http.umd.js
+++ bundles/http.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.3+45.sha-f28878f
+ * @license Angular v6.0.3+47.sha-c17098d
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2358,7 +2358,7 @@ var JsonpModule = /** @class */ (function () {
 /**
  * @deprecated use @angular/common/http instead
  */
-var VERSION = new core.Version('6.0.3+45.sha-f28878f');
+var VERSION = new core.Version('6.0.3+47.sha-c17098d');
 
 /**
  * @license
