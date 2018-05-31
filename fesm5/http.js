0e8d3b76b60d1d54601921e02391129f27181bb3
Victor Berchet <victor@suumit.com>
2018-05-31 00:56:13 +0000

    49d5de6 docs(aio): Add GDE Kim Maida to contributors

--- fesm5/http.js
+++ fesm5/http.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-rc.5+283.sha-4ab70fb
+ * @license Angular v6.0.0-rc.5+284.sha-49d5de6
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2284,7 +2284,7 @@ var JsonpModule = /** @class */ (function () {
 /**
  * @deprecated use @angular/common/http instead
  */
-var VERSION = new Version('6.0.0-rc.5+283.sha-4ab70fb');
+var VERSION = new Version('6.0.0-rc.5+284.sha-49d5de6');
 
 /**
  * @license
