b647797ef28720095dcb0ce1a6d26336663c59d2
Victor Berchet <victor@suumit.com>
2018-06-03 02:55:54 +0000

    cb65724 refactor(ivy): combine lifecycleStage with LViewFlags (#24263)

--- bundles/http.umd.js
+++ bundles/http.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-rc.5+299.sha-0561b66
+ * @license Angular v6.0.0-rc.5+302.sha-cb65724
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1927,7 +1927,7 @@ var JsonpModule = /** @class */ (function () {
 /**
  * @deprecated use @angular/common/http instead
  */
-var VERSION = new core.Version('6.0.0-rc.5+299.sha-0561b66');
+var VERSION = new core.Version('6.0.0-rc.5+302.sha-cb65724');
 
 /**
  * @license
