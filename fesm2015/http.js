9ce22f8351da632202773905a3e4eb400ab4f8fd
Victor Berchet <victor@suumit.com>
2018-06-04 17:59:01 +0000

    efe49c1 docs(aio): clean up frequent ng-modules (#24025)

--- fesm2015/http.js
+++ fesm2015/http.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.3+54.sha-a3d9878
+ * @license Angular v6.0.3+55.sha-efe49c1
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2145,7 +2145,7 @@ JsonpModule.decorators = [
 /**
  * @deprecated see https://angular.io/guide/http
  */
-const VERSION = new Version('6.0.3+54.sha-a3d9878');
+const VERSION = new Version('6.0.3+55.sha-efe49c1');
 
 /**
  * @fileoverview added by tsickle
