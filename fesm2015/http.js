a52a217aae39e4a033558233bfcac133e49550a5
Victor Berchet <victor@suumit.com>
2018-06-05 20:59:45 +0000

    b750919 feat(ivy): implement ViewContainerRef.remove (#24221)

--- fesm2015/http.js
+++ fesm2015/http.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-rc.5+311.sha-9c40375
+ * @license Angular v6.0.0-rc.5+312.sha-b750919
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2145,7 +2145,7 @@ JsonpModule.decorators = [
 /**
  * @deprecated see https://angular.io/guide/http
  */
-const VERSION = new Version('6.0.0-rc.5+311.sha-9c40375');
+const VERSION = new Version('6.0.0-rc.5+312.sha-b750919');
 
 /**
  * @fileoverview added by tsickle
