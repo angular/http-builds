44b033d06bf428e05d179aa1a371be10eae56f74
Victor Berchet <victor@suumit.com>
2018-06-05 21:03:59 +0000

    ea3127e build(bazel): ran format (#24279)

--- bundles/http.umd.js
+++ bundles/http.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.3+58.sha-faf60d9
+ * @license Angular v6.0.3+62.sha-ea3127e
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1927,7 +1927,7 @@ var JsonpModule = /** @class */ (function () {
 /**
  * @deprecated see https://angular.io/guide/http
  */
-var VERSION = new core.Version('6.0.3+58.sha-faf60d9');
+var VERSION = new core.Version('6.0.3+62.sha-ea3127e');
 
 /**
  * @license
--- fesm2015/http.js
+++ fesm2015/http.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.3+58.sha-faf60d9
+ * @license Angular v6.0.3+62.sha-ea3127e
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2145,7 +2145,7 @@ JsonpModule.decorators = [
 /**
  * @deprecated see https://angular.io/guide/http
  */
-const VERSION = new Version('6.0.3+58.sha-faf60d9');
+const VERSION = new Version('6.0.3+62.sha-ea3127e');
 
 /**
  * @fileoverview added by tsickle
