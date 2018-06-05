7b8fa5d194a68c2ef575b0c920c8fa1438549e40
Victor Berchet <victor@suumit.com>
2018-06-05 00:47:29 +0000

    faf60d9 docs: rename the "aio" component to "docs-infra" (#24295)

--- fesm5/http.js
+++ fesm5/http.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.3+57.sha-0639cb9
+ * @license Angular v6.0.3+58.sha-faf60d9
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1859,7 +1859,7 @@ var JsonpModule = /** @class */ (function () {
 /**
  * @deprecated see https://angular.io/guide/http
  */
-var VERSION = new Version('6.0.3+57.sha-0639cb9');
+var VERSION = new Version('6.0.3+58.sha-faf60d9');
 
 /**
  * @license
