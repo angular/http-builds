5f1bb6390da4cac7fd84959a69fce2b57543f1d9
Victor Berchet <victor@suumit.com>
2018-06-05 00:48:19 +0000

    83a0686 docs: rename the "aio" component to "docs-infra" (#24295)

--- bundles/http.umd.js
+++ bundles/http.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-rc.5+309.sha-08a18b8
+ * @license Angular v6.0.0-rc.5+310.sha-83a0686
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1927,7 +1927,7 @@ var JsonpModule = /** @class */ (function () {
 /**
  * @deprecated see https://angular.io/guide/http
  */
-var VERSION = new core.Version('6.0.0-rc.5+309.sha-08a18b8');
+var VERSION = new core.Version('6.0.0-rc.5+310.sha-83a0686');
 
 /**
  * @license
