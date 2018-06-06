20666f659345507622c2d43abedb7b26e2d13533
Victor Berchet <victor@suumit.com>
2018-06-06 01:55:06 +0000

    19deca1 fix(animations): retain trigger-state for nodes that are moved around (#24238)

--- bundles/http.umd.js
+++ bundles/http.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.3+64.sha-dc3e8aa
+ * @license Angular v6.0.3+65.sha-19deca1
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1927,7 +1927,7 @@ var JsonpModule = /** @class */ (function () {
 /**
  * @deprecated see https://angular.io/guide/http
  */
-var VERSION = new core.Version('6.0.3+64.sha-dc3e8aa');
+var VERSION = new core.Version('6.0.3+65.sha-19deca1');
 
 /**
  * @license
--- fesm2015/http.js
+++ fesm2015/http.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.3+64.sha-dc3e8aa
+ * @license Angular v6.0.3+65.sha-19deca1
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2145,7 +2145,7 @@ JsonpModule.decorators = [
 /**
  * @deprecated see https://angular.io/guide/http
  */
-const VERSION = new Version('6.0.3+64.sha-dc3e8aa');
+const VERSION = new Version('6.0.3+65.sha-19deca1');
 
 /**
  * @fileoverview added by tsickle
