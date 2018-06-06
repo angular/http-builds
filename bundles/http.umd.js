258236902e9d5dd5847e08c2d6e18739265dbe81
Victor Berchet <victor@suumit.com>
2018-06-06 17:49:49 +0000

    700e55c build(docs-infra): log warning rather than error if content errors are not fatal (#24320)

--- bundles/http.umd.js
+++ bundles/http.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-rc.5+324.sha-c494d3c
+ * @license Angular v6.0.0-rc.5+337.sha-700e55c
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1927,7 +1927,7 @@ var JsonpModule = /** @class */ (function () {
 /**
  * @deprecated see https://angular.io/guide/http
  */
-var VERSION = new core.Version('6.0.0-rc.5+324.sha-c494d3c');
+var VERSION = new core.Version('6.0.0-rc.5+337.sha-700e55c');
 
 /**
  * @license
