9260e028a4db77c55e9880348eeb3c309ec207d3
Victor Berchet <victor@suumit.com>
2018-06-06 17:50:05 +0000

    0ae8ea2 build(aio): ensure the correct decorator properties are merged (#24289)

--- fesm2015/http.js
+++ fesm2015/http.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.3+65.sha-19deca1
+ * @license Angular v6.0.3+66.sha-0ae8ea2
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2145,7 +2145,7 @@ JsonpModule.decorators = [
 /**
  * @deprecated see https://angular.io/guide/http
  */
-const VERSION = new Version('6.0.3+65.sha-19deca1');
+const VERSION = new Version('6.0.3+66.sha-0ae8ea2');
 
 /**
  * @fileoverview added by tsickle
--- fesm5/http.js
+++ fesm5/http.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.3+65.sha-19deca1
+ * @license Angular v6.0.3+66.sha-0ae8ea2
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1859,7 +1859,7 @@ var JsonpModule = /** @class */ (function () {
 /**
  * @deprecated see https://angular.io/guide/http
  */
-var VERSION = new Version('6.0.3+65.sha-19deca1');
+var VERSION = new Version('6.0.3+66.sha-0ae8ea2');
 
 /**
  * @license
