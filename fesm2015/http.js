5df6f0898b0511f5ff5941bfddf8897d36e4abaa
Victor Berchet <victor@suumit.com>
2018-06-06 19:00:35 +0000

    acf270d feat(ivy): added new namespace and element instructions to JIT environment (#23899)

--- fesm2015/http.js
+++ fesm2015/http.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-rc.5+324.sha-c494d3c
+ * @license Angular v6.0.0-rc.5+335.sha-acf270d
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2145,7 +2145,7 @@ JsonpModule.decorators = [
 /**
  * @deprecated see https://angular.io/guide/http
  */
-const VERSION = new Version('6.0.0-rc.5+324.sha-c494d3c');
+const VERSION = new Version('6.0.0-rc.5+335.sha-acf270d');
 
 /**
  * @fileoverview added by tsickle
