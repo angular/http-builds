fe37e1ef9803367d4ab5ad19f4b8ac52648ce8ad
Miško Hevery <misko@hevery.com>
2018-06-07 23:12:19 +0000

    a6e797b build(bazel): fix ng_package rollup root dir for fesm2015 output (#24298)

--- bundles/http.umd.js
+++ bundles/http.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.0-beta.0+14.sha-f781f74
+ * @license Angular v6.1.0-beta.0+16.sha-a6e797b
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1927,7 +1927,7 @@ var JsonpModule = /** @class */ (function () {
 /**
  * @deprecated see https://angular.io/guide/http
  */
-var VERSION = new core.Version('6.1.0-beta.0+14.sha-f781f74');
+var VERSION = new core.Version('6.1.0-beta.0+16.sha-a6e797b');
 
 /**
  * @license
--- fesm5/http.js
+++ fesm5/http.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.0-beta.0+14.sha-f781f74
+ * @license Angular v6.1.0-beta.0+16.sha-a6e797b
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1859,7 +1859,7 @@ var JsonpModule = /** @class */ (function () {
 /**
  * @deprecated see https://angular.io/guide/http
  */
-var VERSION = new Version('6.1.0-beta.0+14.sha-f781f74');
+var VERSION = new Version('6.1.0-beta.0+16.sha-a6e797b');
 
 /**
  * @license
