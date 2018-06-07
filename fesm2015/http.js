ea5d0e995eca191f9c0642cde18a731cb892e63b
Miško Hevery <misko@hevery.com>
2018-06-07 23:10:45 +0000

    7031972 docs(aio): Added resource link to Amexio Canvas Web Based IDE (#24336)

--- fesm2015/http.js
+++ fesm2015/http.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.4+1.sha-cfa5b65
+ * @license Angular v6.0.4+2.sha-7031972
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2145,7 +2145,7 @@ JsonpModule.decorators = [
 /**
  * @deprecated see https://angular.io/guide/http
  */
-const VERSION = new Version('6.0.4+1.sha-cfa5b65');
+const VERSION = new Version('6.0.4+2.sha-7031972');
 
 /**
  * @fileoverview added by tsickle
