5d34049dbf16ebbe1ea631cb809f0de997d6ee47
Miško Hevery <misko@hevery.com>
2018-06-07 21:05:57 +0000

    f781f74 refactor(ivy): remove need for LContainer.template (#24335)

--- bundles/http.umd.js
+++ bundles/http.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.0-beta.0+13.sha-bd02b27
+ * @license Angular v6.1.0-beta.0+14.sha-f781f74
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1927,7 +1927,7 @@ var JsonpModule = /** @class */ (function () {
 /**
  * @deprecated see https://angular.io/guide/http
  */
-var VERSION = new core.Version('6.1.0-beta.0+13.sha-bd02b27');
+var VERSION = new core.Version('6.1.0-beta.0+14.sha-f781f74');
 
 /**
  * @license
