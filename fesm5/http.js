33574df1a52805d7b76dde48fed47af1217268a4
Miško Hevery <misko@hevery.com>
2018-06-08 21:28:47 +0000

    e876535 fix(docs-infra): use script nomodule to load IE polyfills, skip other polyfills (#24317)

--- fesm5/http.js
+++ fesm5/http.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.4+3.sha-0d81151
+ * @license Angular v6.0.4+5.sha-e876535
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1859,7 +1859,7 @@ var JsonpModule = /** @class */ (function () {
 /**
  * @deprecated see https://angular.io/guide/http
  */
-var VERSION = new Version('6.0.4+3.sha-0d81151');
+var VERSION = new Version('6.0.4+5.sha-e876535');
 
 /**
  * @license
