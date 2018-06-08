7cb67dd4e89bec9c93d8c0ba3aba1944bf87fa8d
Miško Hevery <misko@hevery.com>
2018-06-08 21:27:43 +0000

    0d81151 docs(aio): add mix and connect to front page campaigns (#24357)

--- fesm2015/http.js
+++ fesm2015/http.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.4+2.sha-7031972
+ * @license Angular v6.0.4+3.sha-0d81151
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2145,7 +2145,7 @@ JsonpModule.decorators = [
 /**
  * @deprecated see https://angular.io/guide/http
  */
-const VERSION = new Version('6.0.4+2.sha-7031972');
+const VERSION = new Version('6.0.4+3.sha-0d81151');
 
 /**
  * @fileoverview added by tsickle
