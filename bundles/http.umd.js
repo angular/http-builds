1dca45236a7ca10c735af540102fa6df5cf788d1
Miško Hevery <misko@hevery.com>
2018-06-08 23:05:49 +0000

    1b253e1 fix(ivy): special case [style] and [class] bindings for future use (#23232)

--- bundles/http.umd.js
+++ bundles/http.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.0-beta.0+25.sha-8c1ac28
+ * @license Angular v6.1.0-beta.0+26.sha-1b253e1
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1927,7 +1927,7 @@ var JsonpModule = /** @class */ (function () {
 /**
  * @deprecated see https://angular.io/guide/http
  */
-var VERSION = new core.Version('6.1.0-beta.0+25.sha-8c1ac28');
+var VERSION = new core.Version('6.1.0-beta.0+26.sha-1b253e1');
 
 /**
  * @license
