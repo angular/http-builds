3241f191767c0ee9ad23cfc8e8fda5e6ab557eea
Miško Hevery <misko@hevery.com>
2018-06-08 23:00:15 +0000

    5ef7a07 docs(ivy): add `<ng-container>` to the remaining work items (#24381)

--- fesm2015/http.js
+++ fesm2015/http.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.0-beta.0+23.sha-1135563
+ * @license Angular v6.1.0-beta.0+24.sha-5ef7a07
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2145,7 +2145,7 @@ JsonpModule.decorators = [
 /**
  * @deprecated see https://angular.io/guide/http
  */
-const VERSION = new Version('6.1.0-beta.0+23.sha-1135563');
+const VERSION = new Version('6.1.0-beta.0+24.sha-5ef7a07');
 
 /**
  * @fileoverview added by tsickle
