62e78e372798c948c4cabe441a90d6aff46e826e
Miško Hevery <misko@hevery.com>
2018-06-09 00:06:49 +0000

    f3c9954 docs: adds information about the VSCode clang-format extension (#24351)

--- bundles/http.umd.js
+++ bundles/http.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.4+4.sha-9e61ad8
+ * @license Angular v6.0.4+6.sha-f3c9954
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1927,7 +1927,7 @@ var JsonpModule = /** @class */ (function () {
 /**
  * @deprecated see https://angular.io/guide/http
  */
-var VERSION = new core.Version('6.0.4+4.sha-9e61ad8');
+var VERSION = new core.Version('6.0.4+6.sha-f3c9954');
 
 /**
  * @license
