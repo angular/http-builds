dbc2fb0a98bce60d51e647ab67008ad5189c4a72
Miško Hevery <misko@hevery.com>
2018-06-09 00:08:57 +0000

    0139173 fix(animations): always render end-state styles for orphaned DOM nodes (#24236)

--- fesm2015/http.js
+++ fesm2015/http.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.4+6.sha-f3c9954
+ * @license Angular v6.0.4+7.sha-0139173
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2145,7 +2145,7 @@ JsonpModule.decorators = [
 /**
  * @deprecated see https://angular.io/guide/http
  */
-const VERSION = new Version('6.0.4+6.sha-f3c9954');
+const VERSION = new Version('6.0.4+7.sha-0139173');
 
 /**
  * @fileoverview added by tsickle
--- fesm5/http.js
+++ fesm5/http.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.4+6.sha-f3c9954
+ * @license Angular v6.0.4+7.sha-0139173
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1859,7 +1859,7 @@ var JsonpModule = /** @class */ (function () {
 /**
  * @deprecated see https://angular.io/guide/http
  */
-var VERSION = new Version('6.0.4+6.sha-f3c9954');
+var VERSION = new Version('6.0.4+7.sha-0139173');
 
 /**
  * @license
