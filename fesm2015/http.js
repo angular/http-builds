1f0f3ea1e9b9bf541e9aff65d78b3a9b8e62da3d
Miško Hevery <misko@hevery.com>
2018-06-11 18:31:50 +0000

    ea2987c fix(service-worker): fix `SwPush.unsubscribe()` (#24162)

--- fesm2015/http.js
+++ fesm2015/http.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.4+7.sha-0139173
+ * @license Angular v6.0.4+13.sha-ea2987c
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2145,7 +2145,7 @@ JsonpModule.decorators = [
 /**
  * @deprecated see https://angular.io/guide/http
  */
-const VERSION = new Version('6.0.4+7.sha-0139173');
+const VERSION = new Version('6.0.4+13.sha-ea2987c');
 
 /**
  * @fileoverview added by tsickle
