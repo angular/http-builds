1f698011e0563afebb565115d79d9f0cbbdeb816
Miško Hevery <misko@hevery.com>
2018-06-12 19:18:19 +0000

    c2b5ebf build: update buildifier to latest (#24296)

--- fesm2015/http.js
+++ fesm2015/http.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.0-beta.0+40.sha-1316024
+ * @license Angular v6.1.0-beta.0+42.sha-c2b5ebf
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2145,7 +2145,7 @@ JsonpModule.decorators = [
 /**
  * @deprecated see https://angular.io/guide/http
  */
-const VERSION = new Version('6.1.0-beta.0+40.sha-1316024');
+const VERSION = new Version('6.1.0-beta.0+42.sha-c2b5ebf');
 
 /**
  * @fileoverview added by tsickle
