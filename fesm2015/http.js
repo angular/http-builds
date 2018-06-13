3ddde20115931e0919ea103313282f3f7da847c7
Miško Hevery <misko@hevery.com>
2018-06-13 21:13:19 +0000

    503a524 docs: change capitalization for css hex color values (#23511)

--- fesm2015/http.js
+++ fesm2015/http.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.0-beta.0+55.sha-a577c9e
+ * @license Angular v6.1.0-beta.0+56.sha-503a524
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2145,7 +2145,7 @@ JsonpModule.decorators = [
 /**
  * @deprecated see https://angular.io/guide/http
  */
-const VERSION = new Version('6.1.0-beta.0+55.sha-a577c9e');
+const VERSION = new Version('6.1.0-beta.0+56.sha-503a524');
 
 /**
  * @fileoverview added by tsickle
