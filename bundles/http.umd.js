de08534ffe8ae65713f9b2b1ba8a7a4268670bd9
Miško Hevery <misko@hevery.com>
2018-06-13 21:14:35 +0000

    503a524 docs: change capitalization for css hex color values (#23511)

--- bundles/http.umd.js
+++ bundles/http.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.0-beta.0+55.sha-a577c9e
+ * @license Angular v6.1.0-beta.0+56.sha-503a524
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1915,7 +1915,7 @@ var JsonpModule = /** @class */ (function () {
 /**
  * @deprecated see https://angular.io/guide/http
  */
-var VERSION = new core.Version('6.1.0-beta.0+55.sha-a577c9e');
+var VERSION = new core.Version('6.1.0-beta.0+56.sha-503a524');
 
 /**
  * @license
