588354ded98fda59ed7b01570a9e1b4d934a2620
Miško Hevery <misko@hevery.com>
2018-06-14 21:46:39 +0000

    6d246d6 fix(ivy): allow view and content queries to match the same element (#24507)

--- bundles/http.umd.js
+++ bundles/http.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.0-beta.1+25.sha-7c8159b
+ * @license Angular v6.1.0-beta.1+26.sha-6d246d6
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1927,7 +1927,7 @@ var JsonpModule = /** @class */ (function () {
 /**
  * @deprecated see https://angular.io/guide/http
  */
-var VERSION = new core.Version('6.1.0-beta.1+25.sha-7c8159b');
+var VERSION = new core.Version('6.1.0-beta.1+26.sha-6d246d6');
 
 /**
  * @license
