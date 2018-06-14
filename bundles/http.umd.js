584b2ec8fab26e3891ed24527f8c5952c45827d8
Miško Hevery <misko@hevery.com>
2018-06-14 21:49:32 +0000

    0f7e4fa style(ivy): defeat clang format issue (#24479)

--- bundles/http.umd.js
+++ bundles/http.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.0-beta.1+26.sha-6d246d6
+ * @license Angular v6.1.0-beta.1+29.sha-0f7e4fa
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1915,7 +1915,7 @@ var JsonpModule = /** @class */ (function () {
 /**
  * @deprecated see https://angular.io/guide/http
  */
-var VERSION = new core.Version('6.1.0-beta.1+26.sha-6d246d6');
+var VERSION = new core.Version('6.1.0-beta.1+29.sha-0f7e4fa');
 
 /**
  * @license
