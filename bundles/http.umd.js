1e94ae2e658228e20948e23a09e434a6acd4969a
Miško Hevery <misko@hevery.com>
2018-06-14 00:26:13 +0000

    39af314 build(aio): add github links to API doc members (#24000)

--- bundles/http.umd.js
+++ bundles/http.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.0-beta.1+1.sha-03f93b3
+ * @license Angular v6.1.0-beta.1+22.sha-39af314
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1927,7 +1927,7 @@ var JsonpModule = /** @class */ (function () {
 /**
  * @deprecated see https://angular.io/guide/http
  */
-var VERSION = new core.Version('6.1.0-beta.1+1.sha-03f93b3');
+var VERSION = new core.Version('6.1.0-beta.1+22.sha-39af314');
 
 /**
  * @license
