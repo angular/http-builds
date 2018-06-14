c59cf9871bfc089defc98f6fc8fb685cd51abc72
Miško Hevery <misko@hevery.com>
2018-06-14 22:08:15 +0000

    27bc7dc feat(ivy): ngtsc compiles @Component, @Directive, @NgModule (#24427)

--- fesm5/http.js
+++ fesm5/http.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.0-beta.1+29.sha-0f7e4fa
+ * @license Angular v6.1.0-beta.1+30.sha-27bc7dc
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1859,7 +1859,7 @@ var JsonpModule = /** @class */ (function () {
 /**
  * @deprecated see https://angular.io/guide/http
  */
-var VERSION = new Version('6.1.0-beta.1+29.sha-0f7e4fa');
+var VERSION = new Version('6.1.0-beta.1+30.sha-27bc7dc');
 
 /**
  * @license
