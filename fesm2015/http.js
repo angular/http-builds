d2f69278bea30e3d0c5d788562bd2cfe64a398ac
Miško Hevery <misko@hevery.com>
2018-06-14 21:48:20 +0000

    0f7e4fa style(ivy): defeat clang format issue (#24479)

--- fesm2015/http.js
+++ fesm2015/http.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.0-beta.1+26.sha-6d246d6
+ * @license Angular v6.1.0-beta.1+29.sha-0f7e4fa
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2145,7 +2145,7 @@ JsonpModule.decorators = [
 /**
  * @deprecated see https://angular.io/guide/http
  */
-const VERSION = new Version('6.1.0-beta.1+26.sha-6d246d6');
+const VERSION = new Version('6.1.0-beta.1+29.sha-0f7e4fa');
 
 /**
  * @fileoverview added by tsickle
