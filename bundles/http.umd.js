76a9a5d003699e65112b07bc08f6399428ebc982
Miško Hevery <misko@hevery.com>
2018-06-15 17:18:17 +0000

    aa66e84 docs: push animation api doc changes back to stable (#24525)

--- bundles/http.umd.js
+++ bundles/http.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.5+26.sha-e1bc3f5
+ * @license Angular v6.0.5+27.sha-aa66e84
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1927,7 +1927,7 @@ var JsonpModule = /** @class */ (function () {
 /**
  * @deprecated see https://angular.io/guide/http
  */
-var VERSION = new core.Version('6.0.5+26.sha-e1bc3f5');
+var VERSION = new core.Version('6.0.5+27.sha-aa66e84');
 
 /**
  * @license
--- fesm2015/http.js
+++ fesm2015/http.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.5+26.sha-e1bc3f5
+ * @license Angular v6.0.5+27.sha-aa66e84
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2145,7 +2145,7 @@ JsonpModule.decorators = [
 /**
  * @deprecated see https://angular.io/guide/http
  */
-const VERSION = new Version('6.0.5+26.sha-e1bc3f5');
+const VERSION = new Version('6.0.5+27.sha-aa66e84');
 
 /**
  * @fileoverview added by tsickle
