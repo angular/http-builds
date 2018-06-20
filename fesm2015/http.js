1c1c534433084d97e9f874104d913dc6c192dabc
Miško Hevery <misko@hevery.com>
2018-06-20 22:10:50 +0000

    e8354ed test(animations): properly reference body node for SSR environments (#23300)

--- fesm2015/http.js
+++ fesm2015/http.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.0-beta.1+32.sha-f3625e4
+ * @license Angular v6.1.0-beta.1+42.sha-e8354ed
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2145,7 +2145,7 @@ JsonpModule.decorators = [
 /**
  * @deprecated see https://angular.io/guide/http
  */
-const VERSION = new Version('6.1.0-beta.1+32.sha-f3625e4');
+const VERSION = new Version('6.1.0-beta.1+42.sha-e8354ed');
 
 /**
  * @fileoverview added by tsickle
