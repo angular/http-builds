4a1e29e47e570bb26b9ffed26f07b3bb11e64c62
Miško Hevery <misko@hevery.com>
2018-06-21 20:45:06 +0000

    84272e2 feat(ivy): runtime i18n (#24037)

--- fesm5/http.js
+++ fesm5/http.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.0-beta.2+6.sha-cb31381
+ * @license Angular v6.1.0-beta.2+7.sha-84272e2
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1859,7 +1859,7 @@ var JsonpModule = /** @class */ (function () {
 /**
  * @deprecated see https://angular.io/guide/http
  */
-var VERSION = new Version('6.1.0-beta.2+6.sha-cb31381');
+var VERSION = new Version('6.1.0-beta.2+7.sha-84272e2');
 
 /**
  * @license
