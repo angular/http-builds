0fad5a762158d48d4ce7eecf6f3b1b2797bf07ae
Miško Hevery <misko@hevery.com>
2018-06-21 00:28:46 +0000

    1ce5a49 docs(aio): add elana olson to contributor.json file (#24579)

--- fesm2015/http.js
+++ fesm2015/http.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.6+1.sha-1f532aa
+ * @license Angular v6.0.6+2.sha-1ce5a49
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2145,7 +2145,7 @@ JsonpModule.decorators = [
 /**
  * @deprecated see https://angular.io/guide/http
  */
-const VERSION = new Version('6.0.6+1.sha-1f532aa');
+const VERSION = new Version('6.0.6+2.sha-1ce5a49');
 
 /**
  * @fileoverview added by tsickle
--- fesm5/http.js
+++ fesm5/http.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.6+1.sha-1f532aa
+ * @license Angular v6.0.6+2.sha-1ce5a49
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1859,7 +1859,7 @@ var JsonpModule = /** @class */ (function () {
 /**
  * @deprecated see https://angular.io/guide/http
  */
-var VERSION = new Version('6.0.6+1.sha-1f532aa');
+var VERSION = new Version('6.0.6+2.sha-1ce5a49');
 
 /**
  * @license
