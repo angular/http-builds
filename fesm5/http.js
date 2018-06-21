db3bcad0b0ca88c9724b3e4488ba430fa6ad05fc
Miško Hevery <misko@hevery.com>
2018-06-21 00:10:15 +0000

    9782736 release: cut the v6.0.6 release

--- fesm5/http.js
+++ fesm5/http.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.0-beta.1+42.sha-e8354ed
+ * @license Angular v6.1.0-beta.1+43.sha-9782736
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1859,7 +1859,7 @@ var JsonpModule = /** @class */ (function () {
 /**
  * @deprecated see https://angular.io/guide/http
  */
-var VERSION = new Version('6.1.0-beta.1+42.sha-e8354ed');
+var VERSION = new Version('6.1.0-beta.1+43.sha-9782736');
 
 /**
  * @license
