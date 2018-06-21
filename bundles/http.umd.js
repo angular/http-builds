90212b58d017803472d026149c08283ff4e89195
Miško Hevery <misko@hevery.com>
2018-06-21 20:48:46 +0000

    10da6a4 refactor(ivy): first pass at extracting ReflectionHost for abstract reflection (#24541)

--- bundles/http.umd.js
+++ bundles/http.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.0-beta.2+7.sha-84272e2
+ * @license Angular v6.1.0-beta.2+8.sha-10da6a4
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1915,7 +1915,7 @@ var JsonpModule = /** @class */ (function () {
 /**
  * @deprecated see https://angular.io/guide/http
  */
-var VERSION = new core.Version('6.1.0-beta.2+7.sha-84272e2');
+var VERSION = new core.Version('6.1.0-beta.2+8.sha-10da6a4');
 
 /**
  * @license
