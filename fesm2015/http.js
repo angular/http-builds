12eb36a26744a5ec41408ce60d81313c7980c746
Miško Hevery <misko@hevery.com>
2018-06-21 00:32:15 +0000

    cb31381 build(docs-infra): redirect removed webpack guide to `v5.angular.io` (#24595)

--- fesm2015/http.js
+++ fesm2015/http.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.0-beta.2+5.sha-3e1a3b2
+ * @license Angular v6.1.0-beta.2+6.sha-cb31381
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2145,7 +2145,7 @@ JsonpModule.decorators = [
 /**
  * @deprecated see https://angular.io/guide/http
  */
-const VERSION = new Version('6.1.0-beta.2+5.sha-3e1a3b2');
+const VERSION = new Version('6.1.0-beta.2+6.sha-cb31381');
 
 /**
  * @fileoverview added by tsickle
