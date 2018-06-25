e9102beeaeedcf9a41139ea4c7ab904d56e7fadb
George Kalpakas <kalpakas.g@gmail.com>
2018-06-25 20:18:16 +0000

    a5799e6 build(ivy): fix bazel `srcs` for ngcc

--- fesm2015/http.js
+++ fesm2015/http.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.0-beta.1+45.sha-db21dbf
+ * @license Angular v6.1.0-beta.1+46.sha-a5799e6
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2145,7 +2145,7 @@ JsonpModule.decorators = [
 /**
  * @deprecated see https://angular.io/guide/http
  */
-const VERSION = new Version('6.1.0-beta.1+45.sha-db21dbf');
+const VERSION = new Version('6.1.0-beta.1+46.sha-a5799e6');
 
 /**
  * @fileoverview added by tsickle
