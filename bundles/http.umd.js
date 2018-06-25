d099e272d3ec1c13a1584e09b92bc4b15bbb66e3
Miško Hevery <misko@hevery.com>
2018-06-25 15:56:38 +0000

    8919577 docs: update Angular CLI option for sourcemaps (#24437)

--- bundles/http.umd.js
+++ bundles/http.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.6+4.sha-84eff42
+ * @license Angular v6.0.6+5.sha-8919577
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1927,7 +1927,7 @@ var JsonpModule = /** @class */ (function () {
 /**
  * @deprecated see https://angular.io/guide/http
  */
-var VERSION = new core.Version('6.0.6+4.sha-84eff42');
+var VERSION = new core.Version('6.0.6+5.sha-8919577');
 
 /**
  * @license
--- fesm2015/http.js
+++ fesm2015/http.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.6+4.sha-84eff42
+ * @license Angular v6.0.6+5.sha-8919577
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2145,7 +2145,7 @@ JsonpModule.decorators = [
 /**
  * @deprecated see https://angular.io/guide/http
  */
-const VERSION = new Version('6.0.6+4.sha-84eff42');
+const VERSION = new Version('6.0.6+5.sha-8919577');
 
 /**
  * @fileoverview added by tsickle
