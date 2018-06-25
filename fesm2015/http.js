64b12a8688f911b301f735f71756923b15dffa27
Jason Aden <jasonaden@google.com>
2018-06-25 17:13:52 +0000

    65744e4 docs: test api doc for pipes (#24141)

--- fesm2015/http.js
+++ fesm2015/http.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.6+10.sha-56bc869
+ * @license Angular v6.0.6+11.sha-65744e4
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2145,7 +2145,7 @@ JsonpModule.decorators = [
 /**
  * @deprecated see https://angular.io/guide/http
  */
-const VERSION = new Version('6.0.6+10.sha-56bc869');
+const VERSION = new Version('6.0.6+11.sha-65744e4');
 
 /**
  * @fileoverview added by tsickle
