695d1fcbfa50b3b6d4378de6d83abe9c40bff442
Miško Hevery <misko@hevery.com>
2018-06-25 16:10:26 +0000

    1e139d4 refactor(ivy): rename, limit usage of global vars (#24604)

--- fesm2015/http.js
+++ fesm2015/http.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.0-beta.2+19.sha-fba3f10
+ * @license Angular v6.1.0-beta.2+20.sha-1e139d4
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2145,7 +2145,7 @@ JsonpModule.decorators = [
 /**
  * @deprecated see https://angular.io/guide/http
  */
-const VERSION = new Version('6.1.0-beta.2+19.sha-fba3f10');
+const VERSION = new Version('6.1.0-beta.2+20.sha-1e139d4');
 
 /**
  * @fileoverview added by tsickle
--- fesm5/http.js
+++ fesm5/http.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.0-beta.2+19.sha-fba3f10
+ * @license Angular v6.1.0-beta.2+20.sha-1e139d4
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1859,7 +1859,7 @@ var JsonpModule = /** @class */ (function () {
 /**
  * @deprecated see https://angular.io/guide/http
  */
-var VERSION = new Version('6.1.0-beta.2+19.sha-fba3f10');
+var VERSION = new Version('6.1.0-beta.2+20.sha-1e139d4');
 
 /**
  * @license
