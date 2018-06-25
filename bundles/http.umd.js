04ab5505ea909a520c66c0ae7d2dd45a685ddad0
Miško Hevery <misko@hevery.com>
2018-06-25 16:04:23 +0000

    39c7769 docs(core): typo in static injector tests (#24548)

--- bundles/http.umd.js
+++ bundles/http.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.0-beta.2+16.sha-8c51ce6
+ * @license Angular v6.1.0-beta.2+17.sha-39c7769
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1915,7 +1915,7 @@ var JsonpModule = /** @class */ (function () {
 /**
  * @deprecated see https://angular.io/guide/http
  */
-var VERSION = new core.Version('6.1.0-beta.2+16.sha-8c51ce6');
+var VERSION = new core.Version('6.1.0-beta.2+17.sha-39c7769');
 
 /**
  * @license
