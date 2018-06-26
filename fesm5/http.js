d1a87363edef6276a17f5aa77fabc2b3f0953212
Jason Aden <jasonaden@google.com>
2018-06-26 18:38:31 +0000

    5c0e681 docs(aio): fix adding to template driven forms (#23743)

--- fesm5/http.js
+++ fesm5/http.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.0-beta.2+33.sha-cf0968f
+ * @license Angular v6.1.0-beta.2+38.sha-5c0e681
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1840,7 +1840,7 @@ var JsonpModule = /** @class */ (function () {
 /**
  * @deprecated see https://angular.io/guide/http
  */
-var VERSION = new Version('6.1.0-beta.2+33.sha-cf0968f');
+var VERSION = new Version('6.1.0-beta.2+38.sha-5c0e681');
 
 /**
  * @license
