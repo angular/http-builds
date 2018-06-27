5595935fe60b2e02aa09c7cd11538fbd073862f3
Jason Aden <jasonaden@google.com>
2018-06-27 22:14:38 +0000

    32da3e1 docs: add explanation for `enableResourceInlining` (#24644)

--- fesm5/http.js
+++ fesm5/http.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.0-beta.2+41.sha-6d68f3e
+ * @license Angular v6.1.0-beta.2+42.sha-32da3e1
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1840,7 +1840,7 @@ var JsonpModule = /** @class */ (function () {
 /**
  * @deprecated see https://angular.io/guide/http
  */
-var VERSION = new Version('6.1.0-beta.2+41.sha-6d68f3e');
+var VERSION = new Version('6.1.0-beta.2+42.sha-32da3e1');
 
 /**
  * @license
