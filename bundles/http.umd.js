178fb55047e2fdae0bc6775b919dd048db8a5240
Jason Aden <jasonaden@google.com>
2018-06-27 22:02:34 +0000

    50fb13f fix(ivy): report results to appropriate content queries (#24673)

--- bundles/http.umd.js
+++ bundles/http.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.0-beta.2+39.sha-fe8fcc8
+ * @license Angular v6.1.0-beta.2+40.sha-50fb13f
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1915,7 +1915,7 @@ var JsonpModule = /** @class */ (function () {
 /**
  * @deprecated see https://angular.io/guide/http
  */
-var VERSION = new core.Version('6.1.0-beta.2+39.sha-fe8fcc8');
+var VERSION = new core.Version('6.1.0-beta.2+40.sha-50fb13f');
 
 /**
  * @license
