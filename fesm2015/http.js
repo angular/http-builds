fefeee1dd8d7165af57a6c37c916c2209d3faa04
Jason Aden <jasonaden@google.com>
2018-06-27 22:08:28 +0000

    6d68f3e docs(changelog): remove repeating blocks (#24654) PR Close #24654

--- fesm2015/http.js
+++ fesm2015/http.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.0-beta.2+40.sha-50fb13f
+ * @license Angular v6.1.0-beta.2+41.sha-6d68f3e
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1765,7 +1765,7 @@ JsonpModule = __decorate([
 /**
  * @deprecated see https://angular.io/guide/http
  */
-const VERSION = new Version('6.1.0-beta.2+40.sha-50fb13f');
+const VERSION = new Version('6.1.0-beta.2+41.sha-6d68f3e');
 
 /**
  * @license
