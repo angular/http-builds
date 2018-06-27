57babd8c960ac2dd3cc032087939c8d93a90a87a
Jason Aden <jasonaden@google.com>
2018-06-27 23:09:14 +0000

    39c8bae fix(common): use correct ICU plural for locale mk (#24659) PR Close #24659

--- fesm2015/http.js
+++ fesm2015/http.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.0-beta.2+32.sha-855e8ad
+ * @license Angular v6.1.0-beta.2+54.sha-39c8bae
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2145,7 +2145,7 @@ JsonpModule.decorators = [
 /**
  * @deprecated see https://angular.io/guide/http
  */
-const VERSION = new Version('6.1.0-beta.2+32.sha-855e8ad');
+const VERSION = new Version('6.1.0-beta.2+54.sha-39c8bae');
 
 /**
  * @fileoverview added by tsickle
