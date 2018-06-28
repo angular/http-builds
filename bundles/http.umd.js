9deb9959841b3df93faf38b9db61e5ab25fc468d
Miško Hevery <misko@hevery.com>
2018-06-28 22:44:23 +0000

    3db9d57 fix(docs-infra): use clean package.json template when generating zips (#24691)

--- bundles/http.umd.js
+++ bundles/http.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.0-beta.3+11.sha-0ede987
+ * @license Angular v6.1.0-beta.3+13.sha-3db9d57
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1915,7 +1915,7 @@ var JsonpModule = /** @class */ (function () {
 /**
  * @deprecated see https://angular.io/guide/http
  */
-var VERSION = new core.Version('6.1.0-beta.3+11.sha-0ede987');
+var VERSION = new core.Version('6.1.0-beta.3+13.sha-3db9d57');
 
 /**
  * @license
