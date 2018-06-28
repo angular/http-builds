c353595f10c3752cec09afefa2f245760005e47d
Miško Hevery <misko@hevery.com>
2018-06-28 22:41:13 +0000

    d89f57f build(docs-infra): render short description of parameters in API docs (#24537)

--- fesm2015/http.js
+++ fesm2015/http.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.7+3.sha-ac5b69f
+ * @license Angular v6.0.7+4.sha-d89f57f
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2145,7 +2145,7 @@ JsonpModule.decorators = [
 /**
  * @deprecated see https://angular.io/guide/http
  */
-const VERSION = new Version('6.0.7+3.sha-ac5b69f');
+const VERSION = new Version('6.0.7+4.sha-d89f57f');
 
 /**
  * @fileoverview added by tsickle
