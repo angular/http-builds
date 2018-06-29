b0b4272042f319f64fffe15d70d0a09418a27894
Misko Hevery <misko@hevery.com>
2018-06-29 14:28:49 +0000

    ff84c5c fix(common): properly update collection reference in NgForOf (#24684)

--- bundles/http.umd.js
+++ bundles/http.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.0-beta.3+18.sha-87ddbdf
+ * @license Angular v6.1.0-beta.3+19.sha-ff84c5c
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1915,7 +1915,7 @@ var JsonpModule = /** @class */ (function () {
 /**
  * @deprecated see https://angular.io/guide/http
  */
-var VERSION = new core.Version('6.1.0-beta.3+18.sha-87ddbdf');
+var VERSION = new core.Version('6.1.0-beta.3+19.sha-ff84c5c');
 
 /**
  * @license
