44adc08d1130cae8b1f907d44c9e475aa5cda5f6
Misko Hevery <misko@hevery.com>
2018-06-29 14:35:16 +0000

    d243baf refactor(ivy): remove pChild from LNode (#24705)

--- fesm2015/http.js
+++ fesm2015/http.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.0-beta.3+19.sha-ff84c5c
+ * @license Angular v6.1.0-beta.3+20.sha-d243baf
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2145,7 +2145,7 @@ JsonpModule.decorators = [
 /**
  * @deprecated see https://angular.io/guide/http
  */
-const VERSION = new Version('6.1.0-beta.3+19.sha-ff84c5c');
+const VERSION = new Version('6.1.0-beta.3+20.sha-d243baf');
 
 /**
  * @fileoverview added by tsickle
