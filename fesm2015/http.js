eb42e9caee3597e53fb337c8f421b7b6b8463e70
Misko Hevery <misko@hevery.com>
2018-06-29 14:24:22 +0000

    9803cb0 feat(ivy): Add InheritanceDefinitionFeature to support directive inheritance (#24570)

--- fesm2015/http.js
+++ fesm2015/http.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.0-beta.3+16.sha-13d60ea
+ * @license Angular v6.1.0-beta.3+17.sha-9803cb0
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1765,7 +1765,7 @@ JsonpModule = __decorate([
 /**
  * @deprecated see https://angular.io/guide/http
  */
-const VERSION = new Version('6.1.0-beta.3+16.sha-13d60ea');
+const VERSION = new Version('6.1.0-beta.3+17.sha-9803cb0');
 
 /**
  * @license
