29119896cbc7956091abb859e07127cb504dfe1c
Miško Hevery <misko@hevery.com>
2018-07-02 22:44:01 +0000

    a294e0d fix(ivy): correct position for re-projected containers (#24721)

--- fesm5/http.js
+++ fesm5/http.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.0-beta.3+20.sha-d243baf
+ * @license Angular v6.1.0-beta.3+25.sha-a294e0d
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1840,7 +1840,7 @@ var JsonpModule = /** @class */ (function () {
 /**
  * @deprecated see https://angular.io/guide/http
  */
-var VERSION = new Version('6.1.0-beta.3+20.sha-d243baf');
+var VERSION = new Version('6.1.0-beta.3+25.sha-a294e0d');
 
 /**
  * @license
