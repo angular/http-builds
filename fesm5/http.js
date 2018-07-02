3f578212d6e027f38d534fea00aeb31b8518e535
Miško Hevery <misko@hevery.com>
2018-07-02 23:16:13 +0000

    948e223 docs(aio): update contributors entry (#23786)

--- fesm5/http.js
+++ fesm5/http.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.0-beta.3+24.sha-3553977
+ * @license Angular v6.1.0-beta.3+26.sha-948e223
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1840,7 +1840,7 @@ var JsonpModule = /** @class */ (function () {
 /**
  * @deprecated see https://angular.io/guide/http
  */
-var VERSION = new Version('6.1.0-beta.3+24.sha-3553977');
+var VERSION = new Version('6.1.0-beta.3+26.sha-948e223');
 
 /**
  * @license
