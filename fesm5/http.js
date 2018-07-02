db3b7dd49ae768eb209aab38d234af7c5814b9c4
Miško Hevery <misko@hevery.com>
2018-07-02 22:08:49 +0000

    4e7a44c docs: fix typo in pipes guide (#24452)

--- fesm5/http.js
+++ fesm5/http.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.0-beta.3+21.sha-d1805d0
+ * @license Angular v6.1.0-beta.3+22.sha-4e7a44c
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1840,7 +1840,7 @@ var JsonpModule = /** @class */ (function () {
 /**
  * @deprecated see https://angular.io/guide/http
  */
-var VERSION = new Version('6.1.0-beta.3+21.sha-d1805d0');
+var VERSION = new Version('6.1.0-beta.3+22.sha-4e7a44c');
 
 /**
  * @license
