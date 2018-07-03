18db971e109b3546f064e7c264f0abcc0702ecd7
Miško Hevery <misko@hevery.com>
2018-07-03 16:05:43 +0000

    c94a2c9 build(bazel): update to latest rules_typescript (#24737)

--- bundles/http.umd.js
+++ bundles/http.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.0-beta.3+26.sha-948e223
+ * @license Angular v6.1.0-beta.3+27.sha-c94a2c9
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1927,7 +1927,7 @@ var JsonpModule = /** @class */ (function () {
 /**
  * @deprecated see https://angular.io/guide/http
  */
-var VERSION = new core.Version('6.1.0-beta.3+26.sha-948e223');
+var VERSION = new core.Version('6.1.0-beta.3+27.sha-c94a2c9');
 
 /**
  * @license
