1b38e836b9df08779b650267df599b994148ef4a
Miško Hevery <misko@hevery.com>
2018-07-03 23:04:31 +0000

    b6af870 feat(ivy): AOT support for compilation of @Pipes (#24703)

--- bundles/http.umd.js
+++ bundles/http.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.0-beta.3+29.sha-0c3738a
+ * @license Angular v6.1.0-beta.3+35.sha-b6af870
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1915,7 +1915,7 @@ var JsonpModule = /** @class */ (function () {
 /**
  * @deprecated see https://angular.io/guide/http
  */
-var VERSION = new core.Version('6.1.0-beta.3+29.sha-0c3738a');
+var VERSION = new core.Version('6.1.0-beta.3+35.sha-b6af870');
 
 /**
  * @license
--- src/backends/xhr_backend.d.ts
+++ src/backends/xhr_backend.d.ts
@@ -1,3 +1,10 @@
+/**
+ * @license
+ * Copyright Google Inc. All Rights Reserved.
+ *
+ * Use of this source code is governed by an MIT-style license that can be
+ * found in the LICENSE file at https://angular.io/license
+ */
 import { Observable } from 'rxjs';
 import { ResponseOptions } from '../base_response_options';
 import { ReadyState } from '../enums';
@@ -24,7 +31,7 @@ export declare class XHRConnection implements Connection {
     response: Observable<Response>;
     readyState: ReadyState;
     constructor(req: Request, browserXHR: BrowserXhr, baseResponseOptions?: ResponseOptions);
-    setDetectedContentType(req: any, _xhr: any): void;
+    setDetectedContentType(req: any /** TODO Request */, _xhr: any /** XMLHttpRequest */): void;
 }
 /**
  * `XSRFConfiguration` sets up Cross Site Request Forgery (XSRF) protection for the application
