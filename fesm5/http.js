7a91ea17aa43fb5eabe7456011e7f73189669763
Miško Hevery <misko@hevery.com>
2018-07-03 21:02:20 +0000

    e3064d5 feat: typescript 2.9 support (#24652)

--- fesm5/http.js
+++ fesm5/http.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.0-beta.3+28.sha-0922228
+ * @license Angular v6.1.0-beta.3+30.sha-e3064d5
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1840,7 +1840,7 @@ var JsonpModule = /** @class */ (function () {
 /**
  * @deprecated see https://angular.io/guide/http
  */
-var VERSION = new Version('6.1.0-beta.3+28.sha-0922228');
+var VERSION = new Version('6.1.0-beta.3+30.sha-e3064d5');
 
 /**
  * @license
