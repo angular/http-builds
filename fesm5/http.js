7b9ac71a1e23eba3ef36c4497bb30619e40c1a26
Miško Hevery <misko@hevery.com>
2018-07-03 23:06:20 +0000

    7f3242a docs: fix documention for attributes directive (#24367)

--- fesm5/http.js
+++ fesm5/http.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.0-beta.3+35.sha-b6af870
+ * @license Angular v6.1.0-beta.3+31.sha-7f3242a
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1840,7 +1840,7 @@ var JsonpModule = /** @class */ (function () {
 /**
  * @deprecated see https://angular.io/guide/http
  */
-var VERSION = new Version('6.1.0-beta.3+35.sha-b6af870');
+var VERSION = new Version('6.1.0-beta.3+31.sha-7f3242a');
 
 /**
  * @license
