4256edd9af57003fec00e5a22d62f0d31c0491bf
Miško Hevery <misko@hevery.com>
2018-07-04 00:12:29 +0000

    b6af870 feat(ivy): AOT support for compilation of @Pipes (#24703)

--- src/backends/browser_xhr.d.ts
+++ src/backends/browser_xhr.d.ts
@@ -1,4 +1,11 @@
 /**
+ * @license
+ * Copyright Google Inc. All Rights Reserved.
+ *
+ * Use of this source code is governed by an MIT-style license that can be
+ * found in the LICENSE file at https://angular.io/license
+ */
+/**
  * A backend for http that uses the `XMLHttpRequest` browser API.
  *
  * Take care not to evaluate this in non-browser contexts.
