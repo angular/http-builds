e6ad7a83004865f66fd84c07165ce6726a5fa921
Miško Hevery <misko@hevery.com>
2018-07-06 18:15:48 +0000

    b51ce62 docs(aio): added a link to Angular Zero online course (Traditional Chinese) (#24228)

--- fesm2015/http.js
+++ fesm2015/http.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.7+17.sha-69c8226
+ * @license Angular v6.0.7+19.sha-b51ce62
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2145,7 +2145,7 @@ JsonpModule.decorators = [
 /**
  * @deprecated see https://angular.io/guide/http
  */
-const VERSION = new Version('6.0.7+17.sha-69c8226');
+const VERSION = new Version('6.0.7+19.sha-b51ce62');
 
 /**
  * @fileoverview added by tsickle
