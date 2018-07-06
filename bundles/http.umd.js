71412e24b00293d9b37cb9cb9f540e9180ccc163
Miško Hevery <misko@hevery.com>
2018-07-06 18:27:11 +0000

    fddd2af test: integration test for TS 2.9.x (#24749)

--- bundles/http.umd.js
+++ bundles/http.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.0-beta.3+44.sha-d5a9396
+ * @license Angular v6.1.0-beta.3+45.sha-fddd2af
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1915,7 +1915,7 @@ var JsonpModule = /** @class */ (function () {
 /**
  * @deprecated see https://angular.io/guide/http
  */
-var VERSION = new core.Version('6.1.0-beta.3+44.sha-d5a9396');
+var VERSION = new core.Version('6.1.0-beta.3+45.sha-fddd2af');
 
 /**
  * @license
