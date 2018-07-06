13f2c0e6a4c522260d14b6c1f210f3bb904db950
Miško Hevery <misko@hevery.com>
2018-07-06 18:25:35 +0000

    d5a9396 docs(changelog): correct inaccuracies (#24713) PR Close #24713

--- fesm5/http.js
+++ fesm5/http.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.0-beta.3+43.sha-3e6a722
+ * @license Angular v6.1.0-beta.3+44.sha-d5a9396
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1840,7 +1840,7 @@ var JsonpModule = /** @class */ (function () {
 /**
  * @deprecated see https://angular.io/guide/http
  */
-var VERSION = new Version('6.1.0-beta.3+43.sha-3e6a722');
+var VERSION = new Version('6.1.0-beta.3+44.sha-d5a9396');
 
 /**
  * @license
