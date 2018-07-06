e808bfa29914395fca205f352cc0f20e1f28a674
Miško Hevery <misko@hevery.com>
2018-07-06 21:47:35 +0000

    3980640 feat(ivy): properly apply style="", [style], [style.foo] and [attr.style] bindings (#24602)

--- bundles/http.umd.js
+++ bundles/http.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.0-beta.3+60.sha-52d43a9
+ * @license Angular v6.1.0-beta.3+61.sha-3980640
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1866,7 +1866,7 @@
     /**
      * @deprecated see https://angular.io/guide/http
      */
-    var VERSION = new core.Version('6.1.0-beta.3+60.sha-52d43a9');
+    var VERSION = new core.Version('6.1.0-beta.3+61.sha-3980640');
 
     /**
      * @license
