baf4ea35306d9c3fb3bf2c97ce5cf942b1828c9d
Miško Hevery <misko@hevery.com>
2018-07-06 18:20:52 +0000

    0437598 docs(common): fix in the documentation of PUT (#24528) PR Close #24528

--- fesm2015/http.js
+++ fesm2015/http.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.7+22.sha-2d1102f
+ * @license Angular v6.0.7+21.sha-0437598
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2145,7 +2145,7 @@ JsonpModule.decorators = [
 /**
  * @deprecated see https://angular.io/guide/http
  */
-const VERSION = new Version('6.0.7+22.sha-2d1102f');
+const VERSION = new Version('6.0.7+21.sha-0437598');
 
 /**
  * @fileoverview added by tsickle
