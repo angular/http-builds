088b8f74e734c43e31bd7b47c74d0a58da684c6f
Miško Hevery <misko@hevery.com>
2018-07-06 18:18:24 +0000

    b719905 docs: clarify faqs about services (#24079)

--- fesm2015/http.js
+++ fesm2015/http.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.0-beta.3+38.sha-56a8533
+ * @license Angular v6.1.0-beta.3+39.sha-b719905
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2211,7 +2211,7 @@ JsonpModule.decorators = [
 /** *
  * @deprecated see https://angular.io/guide/http
   @type {?} */
-const VERSION = new Version('6.1.0-beta.3+38.sha-56a8533');
+const VERSION = new Version('6.1.0-beta.3+39.sha-b719905');
 
 /**
  * @fileoverview added by tsickle
