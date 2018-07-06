aca643bfc886885038c1f724ddc2d836c6662868
Miško Hevery <misko@hevery.com>
2018-07-06 18:19:33 +0000

    2d1102f docs: add workspace and related cli terms (#24633)

--- fesm2015/http.js
+++ fesm2015/http.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.7+20.sha-bc2bf18
+ * @license Angular v6.0.7+22.sha-2d1102f
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2145,7 +2145,7 @@ JsonpModule.decorators = [
 /**
  * @deprecated see https://angular.io/guide/http
  */
-const VERSION = new Version('6.0.7+20.sha-bc2bf18');
+const VERSION = new Version('6.0.7+22.sha-2d1102f');
 
 /**
  * @fileoverview added by tsickle
