79ba82c54d963ad95efdf88b7a6144d7edbf70ca
Miško Hevery <misko@hevery.com>
2018-07-09 22:55:50 +0000

    3d20c50 fix(ivy): correctly resolve Array  property access (#24664)

--- fesm2015/http.js
+++ fesm2015/http.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.0-beta.3+63.sha-dcabb05
+ * @license Angular v6.1.0-beta.3+64.sha-3d20c50
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2095,7 +2095,7 @@ JsonpModule.decorators = [
 /** *
  * @deprecated see https://angular.io/guide/http
   @type {?} */
-const VERSION = new Version('6.1.0-beta.3+63.sha-dcabb05');
+const VERSION = new Version('6.1.0-beta.3+64.sha-3d20c50');
 
 /**
  * @fileoverview added by tsickle
