b8adc3a8283ec8ac351a1a67076e0324d7fe3fd9
Miško Hevery <misko@hevery.com>
2018-07-10 00:08:50 +0000

    3d20c50 fix(ivy): correctly resolve Array  property access (#24664)

--- bundles/http.umd.js
+++ bundles/http.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.0-beta.3+61.sha-3980640
+ * @license Angular v6.1.0-beta.3+64.sha-3d20c50
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1874,7 +1874,7 @@
     /**
      * @deprecated see https://angular.io/guide/http
      */
-    var VERSION = new core.Version('6.1.0-beta.3+61.sha-3980640');
+    var VERSION = new core.Version('6.1.0-beta.3+64.sha-3d20c50');
 
     /**
      * @license
