94ce708537654794deb9341e8f319afe08c0d41e
Miško Hevery <misko@hevery.com>
2018-07-10 19:30:28 +0000

    a167bca docs: unified console.log single string style (#22737)

--- fesm5/http.js
+++ fesm5/http.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.0-beta.3+68.sha-197387d
+ * @license Angular v6.1.0-beta.3+70.sha-a167bca
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1828,7 +1828,7 @@ var JsonpModule = /** @class */ (function () {
 /**
  * @deprecated see https://angular.io/guide/http
  */
-var VERSION = new Version('6.1.0-beta.3+68.sha-197387d');
+var VERSION = new Version('6.1.0-beta.3+70.sha-a167bca');
 
 /**
  * @license
