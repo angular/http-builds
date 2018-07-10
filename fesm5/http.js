76c526832ee32deb51e8b150a0c8586498fd2339
Miško Hevery <misko@hevery.com>
2018-07-10 20:58:35 +0000

    3a19f70 refactor(ivy): replace pNextOrParent with TNode props (#24752)

--- fesm5/http.js
+++ fesm5/http.js
@@ -1,10 +1,10 @@
 /**
- * @license Angular v6.1.0-beta.3+64.sha-3d20c50
+ * @license Angular v6.1.0-beta.3+75.sha-3a19f70
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
 
-import { __decorate, __metadata, __extends, __read, __spread } from 'tslib';
+import { __extends, __decorate, __metadata, __read, __spread } from 'tslib';
 import { Injectable, NgModule, Version } from '@angular/core';
 import { Observable } from 'rxjs';
 import { ɵgetDOM } from '@angular/platform-browser';
@@ -1828,7 +1828,7 @@ var JsonpModule = /** @class */ (function () {
 /**
  * @deprecated see https://angular.io/guide/http
  */
-var VERSION = new Version('6.1.0-beta.3+64.sha-3d20c50');
+var VERSION = new Version('6.1.0-beta.3+75.sha-3a19f70');
 
 /**
  * @license
