f7635d0b21646c62cb4f9102457f469dedeefd12
Pete Bacon Darwin <pete@bacondarwin.com>
2018-07-11 22:23:37 +0000

    6b3f5dd feat(ivy): implement ngcc ESM5 bits

--- fesm2015/http.js
+++ fesm2015/http.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.0-beta.3+84.sha-da016d0
+ * @license Angular v6.1.0-beta.3+86.sha-6b3f5dd
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1753,7 +1753,7 @@ JsonpModule = __decorate([
 /**
  * @deprecated see https://angular.io/guide/http
  */
-const VERSION = new Version('6.1.0-beta.3+84.sha-da016d0');
+const VERSION = new Version('6.1.0-beta.3+86.sha-6b3f5dd');
 
 /**
  * @license
