ebef04d5a2012cd41daf01942623b9c0cd41affb
Matias Niemelä <matias@yearofmoo.com>
2018-07-12 21:43:11 +0000

    ee50ee4 build(bazel): try removing gazelle (#24787)

--- fesm5/http.js
+++ fesm5/http.js
@@ -1,11 +1,11 @@
 /**
- * @license Angular v6.1.0-beta.3+95.sha-0399c69
+ * @license Angular v6.1.0-beta.3+98.sha-ee50ee4
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
 
 import { Injectable, NgModule, Version } from '@angular/core';
-import { __read, __extends, __spread } from 'tslib';
+import { __extends, __read, __spread } from 'tslib';
 import { Observable } from 'rxjs';
 import { ɵgetDOM } from '@angular/platform-browser';
 
@@ -1847,7 +1847,7 @@ var JsonpModule = /** @class */ (function () {
 /**
  * @deprecated see https://angular.io/guide/http
  */
-var VERSION = new Version('6.1.0-beta.3+95.sha-0399c69');
+var VERSION = new Version('6.1.0-beta.3+98.sha-ee50ee4');
 
 /**
  * @license
