e1a1b91fa91461efc5ffb413ca9e3f27e1719c4e
Matias Niemelä <matias@yearofmoo.com>
2018-07-12 21:50:41 +0000

    9a6f27c fix(ivy): support zero-argument @NgModule() invocations (#24738)

--- fesm5/http.js
+++ fesm5/http.js
@@ -1,10 +1,10 @@
 /**
- * @license Angular v6.1.0-beta.3+108.sha-80a74b4
+ * @license Angular v6.1.0-beta.3+107.sha-9a6f27c
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
 
-import { __extends, __decorate, __metadata, __spread, __read } from 'tslib';
+import { __decorate, __extends, __metadata, __spread, __read } from 'tslib';
 import { Injectable, NgModule, Version } from '@angular/core';
 import { Observable } from 'rxjs';
 import { ɵgetDOM } from '@angular/platform-browser';
@@ -1828,7 +1828,7 @@ var JsonpModule = /** @class */ (function () {
 /**
  * @deprecated see https://angular.io/guide/http
  */
-var VERSION = new Version('6.1.0-beta.3+108.sha-80a74b4');
+var VERSION = new Version('6.1.0-beta.3+107.sha-9a6f27c');
 
 /**
  * @license
