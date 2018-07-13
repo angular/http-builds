a30a01d809976b99aadbcb002b6a87d6844ff0aa
Matias Niemelä <matias@yearofmoo.com>
2018-07-13 18:59:22 +0000

    1821b75 test(ivy): run render3 tests with test.sh (#24866) PR Close #24866

--- bundles/http.umd.js
+++ bundles/http.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.0-beta.3+111.sha-82004c7
+ * @license Angular v6.1.0-beta.3+112.sha-1821b75
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1874,7 +1874,7 @@
     /**
      * @deprecated see https://angular.io/guide/http
      */
-    var VERSION = new core.Version('6.1.0-beta.3+111.sha-82004c7');
+    var VERSION = new core.Version('6.1.0-beta.3+112.sha-1821b75');
 
     /**
      * @license
