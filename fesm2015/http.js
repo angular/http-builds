6814a79952c7117f5c68b4ab8171d846c242b003
Matias Niemelä <matias@yearofmoo.com>
2018-07-13 19:40:15 +0000

    97277bc build: update to Bazel 0.15 (#24841)

--- fesm2015/http.js
+++ fesm2015/http.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.0-beta.3+112.sha-1821b75
+ * @license Angular v6.1.0-beta.3+113.sha-97277bc
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2095,7 +2095,7 @@ JsonpModule.decorators = [
 /** *
  * @deprecated see https://angular.io/guide/http
   @type {?} */
-const VERSION = new Version('6.1.0-beta.3+112.sha-1821b75');
+const VERSION = new Version('6.1.0-beta.3+113.sha-97277bc');
 
 /**
  * @fileoverview added by tsickle
