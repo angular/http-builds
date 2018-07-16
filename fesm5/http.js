f65e6e7671113ae4da8c17697e2ae357d235f779
Pete Bacon Darwin <pete@bacondarwin.com>
2018-07-16 10:07:18 +0000

    31b4043 fixup! refactor(ivy): allow `ImportManager` to have configurable prefix

--- fesm5/http.js
+++ fesm5/http.js
@@ -1,11 +1,11 @@
 /**
- * @license Angular v6.1.0-beta.3+129.sha-acdb672
+ * @license Angular v6.1.0-beta.3+130.sha-31b4043
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
 
 import { Injectable, NgModule, Version } from '@angular/core';
-import { __extends, __read, __spread } from 'tslib';
+import { __read, __extends, __spread } from 'tslib';
 import { Observable } from 'rxjs';
 import { ɵgetDOM } from '@angular/platform-browser';
 
@@ -1847,7 +1847,7 @@ var JsonpModule = /** @class */ (function () {
 /**
  * @deprecated see https://angular.io/guide/http
  */
-var VERSION = new Version('6.1.0-beta.3+129.sha-acdb672');
+var VERSION = new Version('6.1.0-beta.3+130.sha-31b4043');
 
 /**
  * @license
