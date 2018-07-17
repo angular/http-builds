a6568229fb04078c62a60af670fa7823c5f31808
Matias Niemelä <matias@yearofmoo.com>
2018-07-17 21:16:57 +0000

    06a3398 build: rename angular_devkit dependency to angular_cli (#24842)

--- fesm2015/http.js
+++ fesm2015/http.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.0-beta.3+118.sha-ba3eb8b
+ * @license Angular v6.1.0-beta.3+119.sha-06a3398
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2095,7 +2095,7 @@ JsonpModule.decorators = [
 /** *
  * @deprecated see https://angular.io/guide/http
   @type {?} */
-const VERSION = new Version('6.1.0-beta.3+118.sha-ba3eb8b');
+const VERSION = new Version('6.1.0-beta.3+119.sha-06a3398');
 
 /**
  * @fileoverview added by tsickle
--- fesm5/http.js
+++ fesm5/http.js
@@ -1,11 +1,11 @@
 /**
- * @license Angular v6.1.0-beta.3+118.sha-ba3eb8b
+ * @license Angular v6.1.0-beta.3+119.sha-06a3398
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
 
 import { Injectable, NgModule, Version } from '@angular/core';
-import { __extends, __read, __spread } from 'tslib';
+import { __read, __extends, __spread } from 'tslib';
 import { Observable } from 'rxjs';
 import { ɵgetDOM } from '@angular/platform-browser';
 
@@ -1847,7 +1847,7 @@ var JsonpModule = /** @class */ (function () {
 /**
  * @deprecated see https://angular.io/guide/http
  */
-var VERSION = new Version('6.1.0-beta.3+118.sha-ba3eb8b');
+var VERSION = new Version('6.1.0-beta.3+119.sha-06a3398');
 
 /**
  * @license
