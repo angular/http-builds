70943e921273a8278e882da2837134aa49b8c8e0
Matias Niemelä <matias@yearofmoo.com>
2018-07-18 18:32:24 +0000

    6731c3d docs: fix typo in user input guide (#22630)

--- bundles/http.umd.js
+++ bundles/http.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.9+12.sha-0cbef9a
+ * @license Angular v6.0.9+13.sha-6731c3d
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1874,7 +1874,7 @@
     /**
      * @deprecated see https://angular.io/guide/http
      */
-    var VERSION = new core.Version('6.0.9+12.sha-0cbef9a');
+    var VERSION = new core.Version('6.0.9+13.sha-6731c3d');
 
     /**
      * @license
--- fesm5/http.js
+++ fesm5/http.js
@@ -1,11 +1,11 @@
 /**
- * @license Angular v6.0.9+12.sha-0cbef9a
+ * @license Angular v6.0.9+13.sha-6731c3d
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
 
 import { Injectable, NgModule, Version } from '@angular/core';
-import { __extends, __read, __spread } from 'tslib';
+import { __read, __extends, __spread } from 'tslib';
 import { Observable } from 'rxjs';
 import { ɵgetDOM } from '@angular/platform-browser';
 
@@ -1847,7 +1847,7 @@ var JsonpModule = /** @class */ (function () {
 /**
  * @deprecated see https://angular.io/guide/http
  */
-var VERSION = new Version('6.0.9+12.sha-0cbef9a');
+var VERSION = new Version('6.0.9+13.sha-6731c3d');
 
 /**
  * @license
