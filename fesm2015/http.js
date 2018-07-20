8cb7663d9f740ba55214f6e82fe5a5609ed92308
Victor Berchet <victor@suumit.com>
2018-07-20 19:57:28 +0000

    bb58138 docs: fix bad link (#24825)

--- fesm2015/http.js
+++ fesm2015/http.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.0-rc.3+16.sha-b8f740b
+ * @license Angular v6.1.0-rc.3+17.sha-bb58138
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2095,7 +2095,7 @@ JsonpModule.decorators = [
 /** *
  * @deprecated see https://angular.io/guide/http
   @type {?} */
-const VERSION = new Version('6.1.0-rc.3+16.sha-b8f740b');
+const VERSION = new Version('6.1.0-rc.3+17.sha-bb58138');
 
 /**
  * @fileoverview added by tsickle
