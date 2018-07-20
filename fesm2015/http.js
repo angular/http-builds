fadafacda9149eff006f3a5411271eea075bbc17
Victor Berchet <victor@suumit.com>
2018-07-20 19:02:18 +0000

    270176b docs: more info on currency digits (#24661)

--- fesm2015/http.js
+++ fesm2015/http.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.0-rc.3+1.sha-2aab1c9
+ * @license Angular v6.1.0-rc.3+3.sha-270176b
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2095,7 +2095,7 @@ JsonpModule.decorators = [
 /** *
  * @deprecated see https://angular.io/guide/http
   @type {?} */
-const VERSION = new Version('6.1.0-rc.3+1.sha-2aab1c9');
+const VERSION = new Version('6.1.0-rc.3+3.sha-270176b');
 
 /**
  * @fileoverview added by tsickle
