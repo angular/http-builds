f20a3085586d087271c9954d279b649119104fa8
Victor Berchet <victor@suumit.com>
2018-07-20 19:29:25 +0000

    5a1ca35 docs: remove closing parenthesis from provides guide (#24935)

--- fesm2015/http.js
+++ fesm2015/http.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.9+26.sha-2a5a05b
+ * @license Angular v6.0.9+27.sha-5a1ca35
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2029,7 +2029,7 @@ JsonpModule.decorators = [
 /**
  * @deprecated see https://angular.io/guide/http
  */
-const /** @type {?} */ VERSION = new Version('6.0.9+26.sha-2a5a05b');
+const /** @type {?} */ VERSION = new Version('6.0.9+27.sha-5a1ca35');
 
 /**
  * @fileoverview added by tsickle
