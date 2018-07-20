79e5c8abd9ce69f8567b8440f4b64ac940939e2b
Victor Berchet <victor@suumit.com>
2018-07-20 19:26:12 +0000

    c438b5e build(bazel): turn on preserve-symlinks (#24881)

--- fesm5/http.js
+++ fesm5/http.js
@@ -1,10 +1,10 @@
 /**
- * @license Angular v6.1.0-rc.3+8.sha-1c533c9
+ * @license Angular v6.1.0-rc.3+11.sha-c438b5e
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
 
-import { __extends, __decorate, __metadata, __read, __spread } from 'tslib';
+import { __decorate, __metadata, __extends, __spread, __read } from 'tslib';
 import { Injectable, NgModule, Version } from '@angular/core';
 import { Observable } from 'rxjs';
 import { ɵgetDOM } from '@angular/platform-browser';
@@ -1828,7 +1828,7 @@ var JsonpModule = /** @class */ (function () {
 /**
  * @deprecated see https://angular.io/guide/http
  */
-var VERSION = new Version('6.1.0-rc.3+8.sha-1c533c9');
+var VERSION = new Version('6.1.0-rc.3+11.sha-c438b5e');
 
 /**
  * @license
