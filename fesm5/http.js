862aa205aa6da3fb9c9ac34c33e9d6afdc01cbf1
Victor Berchet <victor@suumit.com>
2018-07-20 19:28:22 +0000

    2d38fa1 test(platform-webworker): avoid flakes due to existing `PlatformRef` (#24916)

--- fesm5/http.js
+++ fesm5/http.js
@@ -1,10 +1,10 @@
 /**
- * @license Angular v6.1.0-rc.3+11.sha-c438b5e
+ * @license Angular v6.1.0-rc.3+13.sha-2d38fa1
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
 
-import { __decorate, __metadata, __extends, __spread, __read } from 'tslib';
+import { __extends, __decorate, __metadata, __spread, __read } from 'tslib';
 import { Injectable, NgModule, Version } from '@angular/core';
 import { Observable } from 'rxjs';
 import { ɵgetDOM } from '@angular/platform-browser';
@@ -1828,7 +1828,7 @@ var JsonpModule = /** @class */ (function () {
 /**
  * @deprecated see https://angular.io/guide/http
  */
-var VERSION = new Version('6.1.0-rc.3+11.sha-c438b5e');
+var VERSION = new Version('6.1.0-rc.3+13.sha-2d38fa1');
 
 /**
  * @license
