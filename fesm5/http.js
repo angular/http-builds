286f9a0d0526d6d3c48a1a58c03420c52552e31c
Victor Berchet <victor@suumit.com>
2018-07-20 19:29:45 +0000

    23766b8 build: Fix windows tests (#24927)

--- fesm5/http.js
+++ fesm5/http.js
@@ -1,11 +1,11 @@
 /**
- * @license Angular v6.1.0-rc.3+13.sha-2d38fa1
+ * @license Angular v6.1.0-rc.3+15.sha-23766b8
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
 
 import { Injectable, NgModule, Version } from '@angular/core';
-import { __read, __extends, __spread } from 'tslib';
+import { __extends, __read, __spread } from 'tslib';
 import { Observable } from 'rxjs';
 import { ɵgetDOM } from '@angular/platform-browser';
 
@@ -1847,7 +1847,7 @@ var JsonpModule = /** @class */ (function () {
 /**
  * @deprecated see https://angular.io/guide/http
  */
-var VERSION = new Version('6.1.0-rc.3+13.sha-2d38fa1');
+var VERSION = new Version('6.1.0-rc.3+15.sha-23766b8');
 
 /**
  * @license
