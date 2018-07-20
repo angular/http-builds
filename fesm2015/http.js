4d25f5a0fefecabe570f4944f207a6826c1f30fe
Victor Berchet <victor@suumit.com>
2018-07-20 19:59:47 +0000

    8620373 fix(service-worker): don't include sourceMappingURL in ngsw-worker (#24877)

--- fesm2015/http.js
+++ fesm2015/http.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.0-rc.3+17.sha-bb58138
+ * @license Angular v6.1.0-rc.3+41.sha-8620373
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2095,7 +2095,7 @@ JsonpModule.decorators = [
 /** *
  * @deprecated see https://angular.io/guide/http
   @type {?} */
-const VERSION = new Version('6.1.0-rc.3+17.sha-bb58138');
+const VERSION = new Version('6.1.0-rc.3+41.sha-8620373');
 
 /**
  * @fileoverview added by tsickle
