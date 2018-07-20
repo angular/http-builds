33e4274ec85d3a6b8d80f531c5279537201b67ca
Victor Berchet <victor@suumit.com>
2018-07-20 19:41:11 +0000

    b8f740b docs: remove closing parenthesis from provides guide (#24935)

--- fesm5/http.js
+++ fesm5/http.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.0-rc.3+15.sha-23766b8
+ * @license Angular v6.1.0-rc.3+16.sha-b8f740b
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1828,7 +1828,7 @@ var JsonpModule = /** @class */ (function () {
 /**
  * @deprecated see https://angular.io/guide/http
  */
-var VERSION = new Version('6.1.0-rc.3+15.sha-23766b8');
+var VERSION = new Version('6.1.0-rc.3+16.sha-b8f740b');
 
 /**
  * @license
