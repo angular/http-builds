990adc051f023ca66aa90323db6e875071a20710
Victor Berchet <victor@suumit.com>
2018-07-20 18:40:19 +0000

    2aab1c9 ci: remove Tina from pullaprove (#25006)

--- fesm5/http.js
+++ fesm5/http.js
@@ -1,10 +1,10 @@
 /**
- * @license Angular v6.1.0-rc.3
+ * @license Angular v6.1.0-rc.3+1.sha-2aab1c9
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
 
-import { __extends, __decorate, __metadata, __spread, __read } from 'tslib';
+import { __extends, __decorate, __metadata, __read, __spread } from 'tslib';
 import { Injectable, NgModule, Version } from '@angular/core';
 import { Observable } from 'rxjs';
 import { ɵgetDOM } from '@angular/platform-browser';
@@ -1828,7 +1828,7 @@ var JsonpModule = /** @class */ (function () {
 /**
  * @deprecated see https://angular.io/guide/http
  */
-var VERSION = new Version('6.1.0-rc.3');
+var VERSION = new Version('6.1.0-rc.3+1.sha-2aab1c9');
 
 /**
  * @license
