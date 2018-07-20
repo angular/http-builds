5861fa640d6e4134cc53797d410384787c6a304f
Victor Berchet <victor@suumit.com>
2018-07-20 19:56:30 +0000

    f2aeb70 docs: fix bad link (#24825)

--- bundles/http.umd.js
+++ bundles/http.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.9+27.sha-5a1ca35
+ * @license Angular v6.0.9+28.sha-f2aeb70
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1874,7 +1874,7 @@
     /**
      * @deprecated see https://angular.io/guide/http
      */
-    var VERSION = new core.Version('6.0.9+27.sha-5a1ca35');
+    var VERSION = new core.Version('6.0.9+28.sha-f2aeb70');
 
     /**
      * @license
