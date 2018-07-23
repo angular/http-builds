106feacdf7e07e9bbee288e01ba2514bdbed45ca
Victor Berchet <victor@suumit.com>
2018-07-23 17:19:41 +0000

    7f3a5ec docs: square odds example in rxjs guide (#24947)

--- fesm2015/http.js
+++ fesm2015/http.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.9+31.sha-68df0d2
+ * @license Angular v6.0.9+35.sha-7f3a5ec
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2029,7 +2029,7 @@ JsonpModule.decorators = [
 /**
  * @deprecated see https://angular.io/guide/http
  */
-const /** @type {?} */ VERSION = new Version('6.0.9+31.sha-68df0d2');
+const /** @type {?} */ VERSION = new Version('6.0.9+35.sha-7f3a5ec');
 
 /**
  * @fileoverview added by tsickle
