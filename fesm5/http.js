4b61b0c7d289710bc33f0f5c5be01ab0689585d7
Victor Berchet <victor@suumit.com>
2018-07-23 17:49:59 +0000

    169e9dd feat(ivy): bridge compile instructions to include sanitization helpers (#24938)

--- fesm5/http.js
+++ fesm5/http.js
@@ -1,11 +1,11 @@
 /**
- * @license Angular v6.1.0-rc.3+51.sha-edef58f
+ * @license Angular v6.1.0-rc.3+53.sha-169e9dd
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
 
 import { Injectable, NgModule, Version } from '@angular/core';
-import { __extends, __read, __spread } from 'tslib';
+import { __read, __extends, __spread } from 'tslib';
 import { Observable } from 'rxjs';
 import { ɵgetDOM } from '@angular/platform-browser';
 
@@ -1847,7 +1847,7 @@ var JsonpModule = /** @class */ (function () {
 /**
  * @deprecated see https://angular.io/guide/http
  */
-var VERSION = new Version('6.1.0-rc.3+51.sha-edef58f');
+var VERSION = new Version('6.1.0-rc.3+53.sha-169e9dd');
 
 /**
  * @license
