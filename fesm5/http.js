ef3802ac8b0eee2672896fedb23626ca4378eb4b
Victor Berchet <victor@suumit.com>
2018-07-23 17:19:15 +0000

    7729bb2 docs: fix instructions for switching directories (#24439)

--- fesm5/http.js
+++ fesm5/http.js
@@ -1,11 +1,11 @@
 /**
- * @license Angular v6.1.0-rc.3+46.sha-4263245
+ * @license Angular v6.1.0-rc.3+47.sha-7729bb2
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
 
 import { Injectable, NgModule, Version } from '@angular/core';
-import { __extends, __read, __spread } from 'tslib';
+import { __read, __extends, __spread } from 'tslib';
 import { Observable } from 'rxjs';
 import { ɵgetDOM } from '@angular/platform-browser';
 
@@ -1847,7 +1847,7 @@ var JsonpModule = /** @class */ (function () {
 /**
  * @deprecated see https://angular.io/guide/http
  */
-var VERSION = new Version('6.1.0-rc.3+46.sha-4263245');
+var VERSION = new Version('6.1.0-rc.3+47.sha-7729bb2');
 
 /**
  * @license
