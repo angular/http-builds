022e4e797044da0b95957e7a9686cfbe5da788db
Victor Berchet <victor@suumit.com>
2018-07-25 04:56:32 +0000

    c205516 docs: refactor ngmodules example (#25072)

--- fesm2015/http.js
+++ fesm2015/http.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.0-rc.3+68.sha-777bd41
+ * @license Angular v6.1.0-rc.3+69.sha-c205516
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2095,7 +2095,7 @@ JsonpModule.decorators = [
 /** *
  * @deprecated see https://angular.io/guide/http
   @type {?} */
-const VERSION = new Version('6.1.0-rc.3+68.sha-777bd41');
+const VERSION = new Version('6.1.0-rc.3+69.sha-c205516');
 
 /**
  * @fileoverview added by tsickle
