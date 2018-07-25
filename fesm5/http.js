fd691fecdf7ac4fb04f646e4d051f5309484f10a
Victor Berchet <victor@suumit.com>
2018-07-25 04:57:49 +0000

    c205516 docs: refactor ngmodules example (#25072)

--- fesm5/http.js
+++ fesm5/http.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.0-rc.3+68.sha-777bd41
+ * @license Angular v6.1.0-rc.3+69.sha-c205516
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1828,7 +1828,7 @@ var JsonpModule = /** @class */ (function () {
 /**
  * @deprecated see https://angular.io/guide/http
  */
-var VERSION = new Version('6.1.0-rc.3+68.sha-777bd41');
+var VERSION = new Version('6.1.0-rc.3+69.sha-c205516');
 
 /**
  * @license
