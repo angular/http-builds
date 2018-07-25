5334609f974d56ade7d55e211be9098d870ddea3
Victor Berchet <victor@suumit.com>
2018-07-25 04:43:34 +0000

    1e79014 docs: replace angular/http with HttpClient (#25066)

--- bundles/http.umd.js
+++ bundles/http.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.0-rc.3+65.sha-65e18dc
+ * @license Angular v6.1.0-rc.3+67.sha-1e79014
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1874,7 +1874,7 @@
     /**
      * @deprecated see https://angular.io/guide/http
      */
-    var VERSION = new core.Version('6.1.0-rc.3+65.sha-65e18dc');
+    var VERSION = new core.Version('6.1.0-rc.3+67.sha-1e79014');
 
     /**
      * @license
--- fesm5/http.js
+++ fesm5/http.js
@@ -1,11 +1,11 @@
 /**
- * @license Angular v6.1.0-rc.3+65.sha-65e18dc
+ * @license Angular v6.1.0-rc.3+67.sha-1e79014
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
 
 import { Injectable, NgModule, Version } from '@angular/core';
-import { __read, __extends, __spread } from 'tslib';
+import { __extends, __read, __spread } from 'tslib';
 import { Observable } from 'rxjs';
 import { ɵgetDOM } from '@angular/platform-browser';
 
@@ -1847,7 +1847,7 @@ var JsonpModule = /** @class */ (function () {
 /**
  * @deprecated see https://angular.io/guide/http
  */
-var VERSION = new Version('6.1.0-rc.3+65.sha-65e18dc');
+var VERSION = new Version('6.1.0-rc.3+67.sha-1e79014');
 
 /**
  * @license
