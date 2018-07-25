bbe1f1be7aea5d982325eee5fd08c7172a3d8c8a
Victor Berchet <victor@suumit.com>
2018-07-25 04:46:55 +0000

    777bd41 docs: replace angular/http with HttpClient (#25068)

--- fesm5/http.js
+++ fesm5/http.js
@@ -1,10 +1,10 @@
 /**
- * @license Angular v6.1.0-rc.3+67.sha-1e79014
+ * @license Angular v6.1.0-rc.3+68.sha-777bd41
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
 
-import { __extends, __decorate, __metadata, __spread, __read } from 'tslib';
+import { __decorate, __metadata, __extends, __spread, __read } from 'tslib';
 import { Injectable, NgModule, Version } from '@angular/core';
 import { Observable } from 'rxjs';
 import { ɵgetDOM } from '@angular/platform-browser';
@@ -1828,7 +1828,7 @@ var JsonpModule = /** @class */ (function () {
 /**
  * @deprecated see https://angular.io/guide/http
  */
-var VERSION = new Version('6.1.0-rc.3+67.sha-1e79014');
+var VERSION = new Version('6.1.0-rc.3+68.sha-777bd41');
 
 /**
  * @license
