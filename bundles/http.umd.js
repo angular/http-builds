2a9aeafae434f2924c449f1222a1938b7c50729c
Igor Minar <igor@angular.io>
2018-07-27 19:19:41 +0000

    39abe7b test: fix typings for `DoneFn` (#25163)

--- bundles/http.umd.js
+++ bundles/http.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.0+9.sha-36a7705
+ * @license Angular v6.1.0+10.sha-39abe7b
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1869,7 +1869,7 @@
     /**
      * @deprecated see https://angular.io/guide/http
      */
-    var VERSION = new core.Version('6.1.0+9.sha-36a7705');
+    var VERSION = new core.Version('6.1.0+10.sha-39abe7b');
 
     /**
      * @license
--- fesm2015/http.js
+++ fesm2015/http.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.0+9.sha-36a7705
+ * @license Angular v6.1.0+10.sha-39abe7b
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1753,7 +1753,7 @@ JsonpModule = __decorate([
 /**
  * @deprecated see https://angular.io/guide/http
  */
-const VERSION = new Version('6.1.0+9.sha-36a7705');
+const VERSION = new Version('6.1.0+10.sha-39abe7b');
 
 /**
  * @license
