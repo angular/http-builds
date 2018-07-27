3e05b63114094b8cd306f4d3db71cb17fd6f6d2b
Igor Minar <igor@angular.io>
2018-07-27 00:50:43 +0000

    601064e build(bazel): add comment about angular bazel rules API re-export from /index.bzl (#24663)

--- fesm2015/http.js
+++ fesm2015/http.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.0
+ * @license Angular v6.1.0+13.sha-601064e
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1753,7 +1753,7 @@ JsonpModule = __decorate([
 /**
  * @deprecated see https://angular.io/guide/http
  */
-const VERSION = new Version('6.1.0');
+const VERSION = new Version('6.1.0+13.sha-601064e');
 
 /**
  * @license
