42da46b15bae0bc7b3f4a60a9757a436b68133b9
Igor Minar <igor@angular.io>
2018-07-27 20:27:26 +0000

    3d81e06 refactor(ivy): do not deep import from ngtsc into ngcc

--- fesm2015/http.js
+++ fesm2015/http.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.0+28.sha-dad1d3d
+ * @license Angular v6.1.0+34.sha-3d81e06
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2095,7 +2095,7 @@ JsonpModule.decorators = [
 /** *
  * @deprecated see https://angular.io/guide/http
   @type {?} */
-const VERSION = new Version('6.1.0+28.sha-dad1d3d');
+const VERSION = new Version('6.1.0+34.sha-3d81e06');
 
 /**
  * @fileoverview added by tsickle
--- fesm5/http.js
+++ fesm5/http.js
@@ -1,11 +1,11 @@
 /**
- * @license Angular v6.1.0+28.sha-dad1d3d
+ * @license Angular v6.1.0+34.sha-3d81e06
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
 
 import { Injectable, NgModule, Version } from '@angular/core';
-import { __read, __extends, __spread } from 'tslib';
+import { __extends, __read, __spread } from 'tslib';
 import { Observable } from 'rxjs';
 import { ɵgetDOM } from '@angular/platform-browser';
 
@@ -1847,7 +1847,7 @@ var JsonpModule = /** @class */ (function () {
 /**
  * @deprecated see https://angular.io/guide/http
  */
-var VERSION = new Version('6.1.0+28.sha-dad1d3d');
+var VERSION = new Version('6.1.0+34.sha-3d81e06');
 
 /**
  * @license
