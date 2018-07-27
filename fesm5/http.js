79bf492d65cb0c280728b9f785994219cf2cbaf9
Igor Minar <igor@angular.io>
2018-07-27 17:39:30 +0000

    738f296 docs: Change unnecessary step in ToH-Tutorial (#25059)

--- fesm5/http.js
+++ fesm5/http.js
@@ -1,11 +1,11 @@
 /**
- * @license Angular v6.1.0+1.sha-514d03f
+ * @license Angular v6.1.0+6.sha-738f296
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
 
 import { Injectable, NgModule, Version } from '@angular/core';
-import { __read, __extends, __spread } from 'tslib';
+import { __extends, __read, __spread } from 'tslib';
 import { Observable } from 'rxjs';
 import { ɵgetDOM } from '@angular/platform-browser';
 
@@ -1847,7 +1847,7 @@ var JsonpModule = /** @class */ (function () {
 /**
  * @deprecated see https://angular.io/guide/http
  */
-var VERSION = new Version('6.1.0+1.sha-514d03f');
+var VERSION = new Version('6.1.0+6.sha-738f296');
 
 /**
  * @license
