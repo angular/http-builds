cf02ff5e8bd0b07128678b92b543329e6f3e9fb0
Igor Minar <igor@angular.io>
2018-07-28 00:56:33 +0000

    a87d44c refactor(ivy): do not deep import from ngtsc into ngcc (#24897)

--- fesm5/http.js
+++ fesm5/http.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.0+25.sha-8e65891
+ * @license Angular v6.1.0+40.sha-a87d44c
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1828,7 +1828,7 @@ var JsonpModule = /** @class */ (function () {
 /**
  * @deprecated see https://angular.io/guide/http
  */
-var VERSION = new Version('6.1.0+25.sha-8e65891');
+var VERSION = new Version('6.1.0+40.sha-a87d44c');
 
 /**
  * @license
