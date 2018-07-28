d3de421a363417dcb8c303e7023921faaf103c75
Igor Minar <igor@angular.io>
2018-07-28 01:04:46 +0000

    6a797d5 refactor(ivy): `element` and `ElementStart` retuns `void` (#25173)

--- fesm2015/http.js
+++ fesm2015/http.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.0+41.sha-f82b6b2
+ * @license Angular v6.1.0+43.sha-6a797d5
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2095,7 +2095,7 @@ JsonpModule.decorators = [
 /** *
  * @deprecated see https://angular.io/guide/http
   @type {?} */
-const VERSION = new Version('6.1.0+41.sha-f82b6b2');
+const VERSION = new Version('6.1.0+43.sha-6a797d5');
 
 /**
  * @fileoverview added by tsickle
