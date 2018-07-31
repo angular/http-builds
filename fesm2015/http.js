8840055b4b3160df910c0682e8d19781a378a5fc
Igor Minar <igor@angular.io>
2018-07-31 05:32:54 +0000

    e3834b7 feat(ivy): support change detection on the root view (#25085)

--- fesm2015/http.js
+++ fesm2015/http.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.0+56.sha-3f20a2f
+ * @license Angular v6.1.0+55.sha-e3834b7
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2095,7 +2095,7 @@ JsonpModule.decorators = [
 /** *
  * @deprecated see https://angular.io/guide/http
   @type {?} */
-const VERSION = new Version('6.1.0+56.sha-3f20a2f');
+const VERSION = new Version('6.1.0+55.sha-e3834b7');
 
 /**
  * @fileoverview added by tsickle
