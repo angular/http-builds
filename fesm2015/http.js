ad856fbd250f5b310cf6c79ccaa674147e889aa3
Igor Minar <igor@angular.io>
2018-07-31 21:24:15 +0000

    4cb1074 docs(aio): add short description for entryComponents (#21360)

--- fesm2015/http.js
+++ fesm2015/http.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.0+58.sha-5254d34
+ * @license Angular v6.1.0+61.sha-4cb1074
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2095,7 +2095,7 @@ JsonpModule.decorators = [
 /** *
  * @deprecated see https://angular.io/guide/http
   @type {?} */
-const VERSION = new Version('6.1.0+58.sha-5254d34');
+const VERSION = new Version('6.1.0+61.sha-4cb1074');
 
 /**
  * @fileoverview added by tsickle
