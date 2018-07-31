400f4c57d31adeb0bfabacb93596818f57d152a3
Igor Minar <igor@angular.io>
2018-07-31 21:25:37 +0000

    4cb1074 docs(aio): add short description for entryComponents (#21360)

--- bundles/http.umd.js
+++ bundles/http.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.0+58.sha-5254d34
+ * @license Angular v6.1.0+61.sha-4cb1074
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1869,7 +1869,7 @@
     /**
      * @deprecated see https://angular.io/guide/http
      */
-    var VERSION = new core.Version('6.1.0+58.sha-5254d34');
+    var VERSION = new core.Version('6.1.0+61.sha-4cb1074');
 
     /**
      * @license
