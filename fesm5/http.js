158a7c97eab75081bd3e697ff1988f2e0b689267
Igor Minar <igor@angular.io>
2018-07-31 19:50:15 +0000

    875efa8 docs(docs-infra): fix topnav layout for smaller screens (#25181)

--- fesm5/http.js
+++ fesm5/http.js
@@ -1,11 +1,11 @@
 /**
- * @license Angular v6.1.0+17.sha-74964bd
+ * @license Angular v6.1.0+18.sha-875efa8
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
 
 import { Injectable, NgModule, Version } from '@angular/core';
-import { __extends, __read, __spread } from 'tslib';
+import { __read, __extends, __spread } from 'tslib';
 import { Observable } from 'rxjs';
 import { ɵgetDOM } from '@angular/platform-browser';
 
@@ -1847,7 +1847,7 @@ var JsonpModule = /** @class */ (function () {
 /**
  * @deprecated see https://angular.io/guide/http
  */
-var VERSION = new Version('6.1.0+17.sha-74964bd');
+var VERSION = new Version('6.1.0+18.sha-875efa8');
 
 /**
  * @license
