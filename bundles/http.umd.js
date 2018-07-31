a1c9a504ab03e19a22ca580427f2ba67e8522a05
Igor Minar <igor@angular.io>
2018-07-31 00:51:29 +0000

    3664829 refactor(ivy): misc (#25174)

--- bundles/http.umd.js
+++ bundles/http.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.0+52.sha-e99d860
+ * @license Angular v6.1.0+54.sha-3664829
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1877,7 +1877,7 @@
     /**
      * @deprecated see https://angular.io/guide/http
      */
-    var VERSION = new core.Version('6.1.0+52.sha-e99d860');
+    var VERSION = new core.Version('6.1.0+54.sha-3664829');
 
     /**
      * @license
