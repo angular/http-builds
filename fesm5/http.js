07e3bdaad112ac937f502811abc3b1939176446f
Igor Minar <igor@angular.io>
2018-08-01 20:16:16 +0000

    1e53278 docs(core): replace ReflectiveInjector example with Static Injector example (#25162)

--- fesm5/http.js
+++ fesm5/http.js
@@ -1,11 +1,11 @@
 /**
- * @license Angular v6.1.0+67.sha-4976a58
+ * @license Angular v6.1.0+72.sha-1e53278
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
 
 import { Injectable, NgModule, Version } from '@angular/core';
-import { __read, __extends, __spread } from 'tslib';
+import { __extends, __read, __spread } from 'tslib';
 import { Observable } from 'rxjs';
 import { ɵgetDOM } from '@angular/platform-browser';
 
@@ -1847,7 +1847,7 @@ var JsonpModule = /** @class */ (function () {
 /**
  * @deprecated see https://angular.io/guide/http
  */
-var VERSION = new Version('6.1.0+67.sha-4976a58');
+var VERSION = new Version('6.1.0+72.sha-1e53278');
 
 /**
  * @license
