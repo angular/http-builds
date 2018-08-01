addc11a1e447b4d85594679c5671a701d940d4fd
Igor Minar <igor@angular.io>
2018-08-01 20:48:34 +0000

    5342aea docs(aio): update Kendo UI description in resource.json (#24845)

--- fesm2015/http.js
+++ fesm2015/http.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.0+80.sha-1dd2eaa
+ * @license Angular v6.1.0+81.sha-5342aea
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1753,7 +1753,7 @@ JsonpModule = __decorate([
 /**
  * @deprecated see https://angular.io/guide/http
  */
-const VERSION = new Version('6.1.0+80.sha-1dd2eaa');
+const VERSION = new Version('6.1.0+81.sha-5342aea');
 
 /**
  * @license
