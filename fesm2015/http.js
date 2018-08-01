347d3e2fdc6e18cd36714653da9b9ba158489930
Igor Minar <igor@angular.io>
2018-08-01 20:26:58 +0000

    7a4fb44 docs(aio): add Kevin Yang to GDE resources (#24791)

--- fesm2015/http.js
+++ fesm2015/http.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.0+74.sha-a9ecf4b
+ * @license Angular v6.1.0+77.sha-7a4fb44
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2095,7 +2095,7 @@ JsonpModule.decorators = [
 /** *
  * @deprecated see https://angular.io/guide/http
   @type {?} */
-const VERSION = new Version('6.1.0+74.sha-a9ecf4b');
+const VERSION = new Version('6.1.0+77.sha-7a4fb44');
 
 /**
  * @fileoverview added by tsickle
