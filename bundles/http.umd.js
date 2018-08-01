448c5f3e760b210c9d1f9242e68399503856429f
Igor Minar <igor@angular.io>
2018-08-01 20:17:34 +0000

    1e53278 docs(core): replace ReflectiveInjector example with Static Injector example (#25162)

--- bundles/http.umd.js
+++ bundles/http.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.0+67.sha-4976a58
+ * @license Angular v6.1.0+72.sha-1e53278
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1869,7 +1869,7 @@
     /**
      * @deprecated see https://angular.io/guide/http
      */
-    var VERSION = new core.Version('6.1.0+67.sha-4976a58');
+    var VERSION = new core.Version('6.1.0+72.sha-1e53278');
 
     /**
      * @license
