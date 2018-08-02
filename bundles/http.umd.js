6246ae4c51b989afcf9e2a76f54d6989238cec49
Igor Minar <igor@angular.io>
2018-08-02 22:05:06 +0000

    6bacd32 release: cut the v7.0.0-beta.0 release

--- bundles/http.umd.js
+++ bundles/http.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.0+100.sha-183757d
+ * @license Angular v7.0.0-beta.0
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1869,7 +1869,7 @@
     /**
      * @deprecated see https://angular.io/guide/http
      */
-    var VERSION = new core.Version('6.1.0+100.sha-183757d');
+    var VERSION = new core.Version('7.0.0-beta.0');
 
     /**
      * @license
