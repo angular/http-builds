05fec87c0bedc78acd7bb2f8e876b2cbc00921ef
Igor Minar <igor@angular.io>
2018-08-02 00:06:25 +0000

    5ca35b3 docs: Update the link to the Jasmine docs (#25175)

--- bundles/http.umd.js
+++ bundles/http.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.0+42.sha-0a6a3f3
+ * @license Angular v6.1.0+43.sha-5ca35b3
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1869,7 +1869,7 @@
     /**
      * @deprecated see https://angular.io/guide/http
      */
-    var VERSION = new core.Version('6.1.0+42.sha-0a6a3f3');
+    var VERSION = new core.Version('6.1.0+43.sha-5ca35b3');
 
     /**
      * @license
--- fesm2015/http.js
+++ fesm2015/http.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.0+42.sha-0a6a3f3
+ * @license Angular v6.1.0+43.sha-5ca35b3
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1753,7 +1753,7 @@ JsonpModule = __decorate([
 /**
  * @deprecated see https://angular.io/guide/http
  */
-const VERSION = new Version('6.1.0+42.sha-0a6a3f3');
+const VERSION = new Version('6.1.0+43.sha-5ca35b3');
 
 /**
  * @license
