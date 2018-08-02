1ab31ec8ea4b5dc6d4f2b1cba7dd164b39062a1f
Igor Minar <igor@angular.io>
2018-08-02 16:12:33 +0000

    9ddf269 docs(elements): add section about custom element typings in `elements` guide (#25219)

--- fesm2015/http.js
+++ fesm2015/http.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.0+45.sha-1ef2eae
+ * @license Angular v6.1.0+48.sha-9ddf269
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2095,7 +2095,7 @@ JsonpModule.decorators = [
 /** *
  * @deprecated see https://angular.io/guide/http
   @type {?} */
-const VERSION = new Version('6.1.0+45.sha-1ef2eae');
+const VERSION = new Version('6.1.0+48.sha-9ddf269');
 
 /**
  * @fileoverview added by tsickle
