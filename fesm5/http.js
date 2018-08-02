4881806e835bc3ff4a2a56c6c26e59240665450d
Igor Minar <igor@angular.io>
2018-08-02 00:17:29 +0000

    d5d034a docs: update reactiveconf 2018 in events (#24739)

--- fesm5/http.js
+++ fesm5/http.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.0+43.sha-5ca35b3
+ * @license Angular v6.1.0+44.sha-d5d034a
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1828,7 +1828,7 @@ var JsonpModule = /** @class */ (function () {
 /**
  * @deprecated see https://angular.io/guide/http
  */
-var VERSION = new Version('6.1.0+43.sha-5ca35b3');
+var VERSION = new Version('6.1.0+44.sha-d5d034a');
 
 /**
  * @license
