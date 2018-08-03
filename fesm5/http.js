e6994c58d982af7c8b7717ae27908620b0a9cb5d
Kara Erickson <karakara@google.com>
2018-08-03 07:07:53 +0000

    b38931b fix(ivy): use devModeEqual in no change mode (#25252)

--- fesm5/http.js
+++ fesm5/http.js
@@ -1,11 +1,11 @@
 /**
- * @license Angular v7.0.0-beta.0+8.sha-728d98d
+ * @license Angular v7.0.0-beta.0+5.sha-b38931b
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
 
 import { Injectable, NgModule, Version } from '@angular/core';
-import { __read, __extends, __spread } from 'tslib';
+import { __extends, __read, __spread } from 'tslib';
 import { Observable } from 'rxjs';
 import { ɵgetDOM } from '@angular/platform-browser';
 
@@ -1847,7 +1847,7 @@ var JsonpModule = /** @class */ (function () {
 /**
  * @deprecated see https://angular.io/guide/http
  */
-var VERSION = new Version('7.0.0-beta.0+8.sha-728d98d');
+var VERSION = new Version('7.0.0-beta.0+5.sha-b38931b');
 
 /**
  * @license
