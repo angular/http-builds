31161f4cf335686aa48ada5cea335457978447aa
Kara Erickson <karakara@google.com>
2018-08-03 21:51:10 +0000

    3f43dbb style(common): fix short param names (#23667)

--- fesm2015/http.js
+++ fesm2015/http.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.1
+ * @license Angular v6.1.1+2.sha-3f43dbb
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2095,7 +2095,7 @@ JsonpModule.decorators = [
 /** *
  * @deprecated see https://angular.io/guide/http
   @type {?} */
-const VERSION = new Version('6.1.1');
+const VERSION = new Version('6.1.1+2.sha-3f43dbb');
 
 /**
  * @fileoverview added by tsickle
