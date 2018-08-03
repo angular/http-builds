17d30220039ee0856282fd958e5d0acf34cc5be6
Igor Minar <igor@angular.io>
2018-08-03 00:34:24 +0000

    183757d fix(core): fix tests for uninitialized @Output error (#19116)

--- fesm2015/http.js
+++ fesm2015/http.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.0+8.sha-3169edd
+ * @license Angular v6.1.0+100.sha-183757d
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1753,7 +1753,7 @@ JsonpModule = __decorate([
 /**
  * @deprecated see https://angular.io/guide/http
  */
-const VERSION = new Version('6.1.0+8.sha-3169edd');
+const VERSION = new Version('6.1.0+100.sha-183757d');
 
 /**
  * @license
