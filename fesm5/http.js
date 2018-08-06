450a4d11adfc2a7e1ab082695de0fb715bd7c424
Kara Erickson <karakara@google.com>
2018-08-06 22:26:12 +0000

    0c028a0 docs: remove code in universal hero detail component (#25215)

--- fesm5/http.js
+++ fesm5/http.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.1+12.sha-40904ce
+ * @license Angular v6.1.1+14.sha-0c028a0
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1828,7 +1828,7 @@ var JsonpModule = /** @class */ (function () {
 /**
  * @deprecated see https://angular.io/guide/http
  */
-var VERSION = new Version('6.1.1+12.sha-40904ce');
+var VERSION = new Version('6.1.1+14.sha-0c028a0');
 
 /**
  * @license
