1bb104aafab93f9d06973de8f5347818cba51fcf
Kara Erickson <karakara@google.com>
2018-08-06 22:38:18 +0000

    8d0f8bd docs: fix table in comparing observables guide (#22485)

--- fesm5/http.js
+++ fesm5/http.js
@@ -1,10 +1,10 @@
 /**
- * @license Angular v6.1.1+19.sha-8cbb836
+ * @license Angular v6.1.1+18.sha-8d0f8bd
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
 
-import { __extends, __decorate, __metadata, __spread, __read } from 'tslib';
+import { __decorate, __metadata, __extends, __read, __spread } from 'tslib';
 import { Injectable, NgModule, Version } from '@angular/core';
 import { Observable } from 'rxjs';
 import { ɵgetDOM } from '@angular/platform-browser';
@@ -1828,7 +1828,7 @@ var JsonpModule = /** @class */ (function () {
 /**
  * @deprecated see https://angular.io/guide/http
  */
-var VERSION = new Version('6.1.1+19.sha-8cbb836');
+var VERSION = new Version('6.1.1+18.sha-8d0f8bd');
 
 /**
  * @license
