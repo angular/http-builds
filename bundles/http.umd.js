c3eda3c7e4b1b80a07dba27d59fe3879de702b71
Kara Erickson <karakara@google.com>
2018-08-06 17:35:51 +0000

    478eca3 docs(aio): add Made with Angular (#21297)

--- bundles/http.umd.js
+++ bundles/http.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.1+5.sha-2e16039
+ * @license Angular v6.1.1+6.sha-478eca3
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1869,7 +1869,7 @@
     /**
      * @deprecated see https://angular.io/guide/http
      */
-    var VERSION = new core.Version('6.1.1+5.sha-2e16039');
+    var VERSION = new core.Version('6.1.1+6.sha-478eca3');
 
     /**
      * @license
