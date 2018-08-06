83cf29d3602edf226734ef79a805223e2aedb3d1
Kara Erickson <karakara@google.com>
2018-08-06 23:16:12 +0000

    3355502 fix(ivy): support ng-container at the root of a view with delayed insertion (#25329)

--- fesm5/http.js
+++ fesm5/http.js
@@ -1,11 +1,11 @@
 /**
- * @license Angular v7.0.0-beta.0+38.sha-16c03c0
+ * @license Angular v7.0.0-beta.0+37.sha-3355502
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
 
 import { Injectable, NgModule, Version } from '@angular/core';
-import { __read, __extends, __spread } from 'tslib';
+import { __extends, __read, __spread } from 'tslib';
 import { Observable } from 'rxjs';
 import { ɵgetDOM } from '@angular/platform-browser';
 
@@ -1847,7 +1847,7 @@ var JsonpModule = /** @class */ (function () {
 /**
  * @deprecated see https://angular.io/guide/http
  */
-var VERSION = new Version('7.0.0-beta.0+38.sha-16c03c0');
+var VERSION = new Version('7.0.0-beta.0+37.sha-3355502');
 
 /**
  * @license
