0f4506d60d01b8c98e9626fc15f4088a02ce6e76
Kara Erickson <karakara@google.com>
2018-08-06 22:28:12 +0000

    2b906f6 docs: fix typo (#25331) PR Close #25331

--- fesm5/http.js
+++ fesm5/http.js
@@ -1,11 +1,11 @@
 /**
- * @license Angular v7.0.0-beta.0+29.sha-a9881bb
+ * @license Angular v7.0.0-beta.0+27.sha-2b906f6
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
 
 import { Injectable, NgModule, Version } from '@angular/core';
-import { __read, __extends, __spread } from 'tslib';
+import { __extends, __read, __spread } from 'tslib';
 import { Observable } from 'rxjs';
 import { ɵgetDOM } from '@angular/platform-browser';
 
@@ -1847,7 +1847,7 @@ var JsonpModule = /** @class */ (function () {
 /**
  * @deprecated see https://angular.io/guide/http
  */
-var VERSION = new Version('7.0.0-beta.0+29.sha-a9881bb');
+var VERSION = new Version('7.0.0-beta.0+27.sha-2b906f6');
 
 /**
  * @license
