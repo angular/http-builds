fc78ff0d4197c4a84f46c1bc01e7621c29bb3001
Kara Erickson <karakara@google.com>
2018-08-06 22:29:24 +0000

    6e7d5f0 docs(core): fix tree-shakable spelling (#24057)

--- fesm2015/http.js
+++ fesm2015/http.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.1+17.sha-66547d8
+ * @license Angular v6.1.1+16.sha-6e7d5f0
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2095,7 +2095,7 @@ JsonpModule.decorators = [
 /** *
  * @deprecated see https://angular.io/guide/http
   @type {?} */
-const VERSION = new Version('6.1.1+17.sha-66547d8');
+const VERSION = new Version('6.1.1+16.sha-6e7d5f0');
 
 /**
  * @fileoverview added by tsickle
--- fesm5/http.js
+++ fesm5/http.js
@@ -1,11 +1,11 @@
 /**
- * @license Angular v6.1.1+17.sha-66547d8
+ * @license Angular v6.1.1+16.sha-6e7d5f0
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
 
 import { Injectable, NgModule, Version } from '@angular/core';
-import { __read, __extends, __spread } from 'tslib';
+import { __extends, __read, __spread } from 'tslib';
 import { Observable } from 'rxjs';
 import { ɵgetDOM } from '@angular/platform-browser';
 
@@ -1847,7 +1847,7 @@ var JsonpModule = /** @class */ (function () {
 /**
  * @deprecated see https://angular.io/guide/http
  */
-var VERSION = new Version('6.1.1+17.sha-66547d8');
+var VERSION = new Version('6.1.1+16.sha-6e7d5f0');
 
 /**
  * @license
