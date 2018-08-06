b8a08fabdfd1fed52d9f9004918160436b960914
Kara Erickson <karakara@google.com>
2018-08-06 22:25:51 +0000

    40904ce docs: add missing word in outputs description. (#25330)

--- bundles/http.umd.js
+++ bundles/http.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.1+13.sha-a54c049
+ * @license Angular v6.1.1+12.sha-40904ce
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1869,7 +1869,7 @@
     /**
      * @deprecated see https://angular.io/guide/http
      */
-    var VERSION = new core.Version('6.1.1+13.sha-a54c049');
+    var VERSION = new core.Version('6.1.1+12.sha-40904ce');
 
     /**
      * @license
--- fesm5/http.js
+++ fesm5/http.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.1+13.sha-a54c049
+ * @license Angular v6.1.1+12.sha-40904ce
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1828,7 +1828,7 @@ var JsonpModule = /** @class */ (function () {
 /**
  * @deprecated see https://angular.io/guide/http
  */
-var VERSION = new Version('6.1.1+13.sha-a54c049');
+var VERSION = new Version('6.1.1+12.sha-40904ce');
 
 /**
  * @license
