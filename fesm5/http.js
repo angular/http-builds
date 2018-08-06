fce566ef67fafa2ba49fcf16e5b4055d8eaaecae
Kara Erickson <karakara@google.com>
2018-08-06 22:56:04 +0000

    16c03c0 fix(core): In Testability.whenStable update callback, pass more complete (#25010) data about tasks.

--- fesm5/http.js
+++ fesm5/http.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.0.0-beta.0+36.sha-02c15a2
+ * @license Angular v7.0.0-beta.0+38.sha-16c03c0
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1847,7 +1847,7 @@ var JsonpModule = /** @class */ (function () {
 /**
  * @deprecated see https://angular.io/guide/http
  */
-var VERSION = new Version('7.0.0-beta.0+36.sha-02c15a2');
+var VERSION = new Version('7.0.0-beta.0+38.sha-16c03c0');
 
 /**
  * @license
