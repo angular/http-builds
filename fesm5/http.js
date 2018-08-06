71db2612c4f55d3ea225cb84720943f372963071
Kara Erickson <karakara@google.com>
2018-08-06 23:29:36 +0000

    2505c07 test(upgrade): reduce flaky-ness by increasing timeout (#24937)

--- fesm5/http.js
+++ fesm5/http.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.0.0-beta.0+37.sha-3355502
+ * @license Angular v7.0.0-beta.0+41.sha-2505c07
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1828,7 +1828,7 @@ var JsonpModule = /** @class */ (function () {
 /**
  * @deprecated see https://angular.io/guide/http
  */
-var VERSION = new Version('7.0.0-beta.0+37.sha-3355502');
+var VERSION = new Version('7.0.0-beta.0+41.sha-2505c07');
 
 /**
  * @license
