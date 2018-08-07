00ed8e560bb04679fc5f09c31adc6bf71d01a5d7
Kara Erickson <karakara@google.com>
2018-08-07 18:50:11 +0000

    155d938 docs: refining code of tutorial 7 routing (#22151)

--- fesm2015/http.js
+++ fesm2015/http.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.1+23.sha-b75a985
+ * @license Angular v6.1.1+25.sha-155d938
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1753,7 +1753,7 @@ JsonpModule = __decorate([
 /**
  * @deprecated see https://angular.io/guide/http
  */
-const VERSION = new Version('6.1.1+23.sha-b75a985');
+const VERSION = new Version('6.1.1+25.sha-155d938');
 
 /**
  * @license
--- fesm5/http.js
+++ fesm5/http.js
@@ -1,10 +1,10 @@
 /**
- * @license Angular v6.1.1+23.sha-b75a985
+ * @license Angular v6.1.1+25.sha-155d938
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
 
-import { __decorate, __metadata, __extends, __spread, __read } from 'tslib';
+import { __extends, __decorate, __metadata, __spread, __read } from 'tslib';
 import { Injectable, NgModule, Version } from '@angular/core';
 import { Observable } from 'rxjs';
 import { ɵgetDOM } from '@angular/platform-browser';
@@ -1828,7 +1828,7 @@ var JsonpModule = /** @class */ (function () {
 /**
  * @deprecated see https://angular.io/guide/http
  */
-var VERSION = new Version('6.1.1+23.sha-b75a985');
+var VERSION = new Version('6.1.1+25.sha-155d938');
 
 /**
  * @license
