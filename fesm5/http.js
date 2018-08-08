d96336475165f2ef52f2c164c506841a3c901781
Kara Erickson <karakara@google.com>
2018-08-08 23:26:01 +0000

    b64fed1 fix(ivy): prevent ngDevMode from getting removed too early (#25371)

--- fesm5/http.js
+++ fesm5/http.js
@@ -1,11 +1,11 @@
 /**
- * @license Angular v7.0.0-beta.0+50.sha-732026c
+ * @license Angular v7.0.0-beta.1+10.sha-b64fed1
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
 
 import { Injectable, NgModule, Version } from '@angular/core';
-import { __extends, __read, __spread } from 'tslib';
+import { __read, __extends, __spread } from 'tslib';
 import { Observable } from 'rxjs';
 import { ɵgetDOM } from '@angular/platform-browser';
 
@@ -1847,7 +1847,7 @@ var JsonpModule = /** @class */ (function () {
 /**
  * @deprecated see https://angular.io/guide/http
  */
-var VERSION = new Version('7.0.0-beta.0+50.sha-732026c');
+var VERSION = new Version('7.0.0-beta.1+10.sha-b64fed1');
 
 /**
  * @license
