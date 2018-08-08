4659d2f94f7e1993c307ce9c58eaa20ffa5af728
Kara Erickson <karakara@google.com>
2018-08-08 22:04:53 +0000

    8bbce3f docs: copy edit architecture guide (#25328)

--- fesm5/http.js
+++ fesm5/http.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.0.0-beta.1+10.sha-b64fed1
+ * @license Angular v7.0.0-beta.1+9.sha-8bbce3f
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1847,7 +1847,7 @@ var JsonpModule = /** @class */ (function () {
 /**
  * @deprecated see https://angular.io/guide/http
  */
-var VERSION = new Version('7.0.0-beta.1+10.sha-b64fed1');
+var VERSION = new Version('7.0.0-beta.1+9.sha-8bbce3f');
 
 /**
  * @license
