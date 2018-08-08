7cca4f8a2e9d8b9d38e5dd919778d338d4b96ab1
Kara Erickson <karakara@google.com>
2018-08-08 22:20:16 +0000

    3449f1e docs: copy edit architecture guide (#25328)

--- bundles/http.umd.js
+++ bundles/http.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.2+7.sha-2951e72
+ * @license Angular v6.1.2+6.sha-3449f1e
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1869,7 +1869,7 @@
     /**
      * @deprecated see https://angular.io/guide/http
      */
-    var VERSION = new core.Version('6.1.2+7.sha-2951e72');
+    var VERSION = new core.Version('6.1.2+6.sha-3449f1e');
 
     /**
      * @license
