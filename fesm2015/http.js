b66147c41d83242dd000d476e40fa27c88a2554f
Ben Lesh <ben@benlesh.com>
2018-08-09 18:15:33 +0000

    7058072 feat(ivy): enable .ngfactory.js generation in g3 only (#25392)

--- fesm2015/http.js
+++ fesm2015/http.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.0.0-beta.1+12.sha-9c92a6f
+ * @license Angular v7.0.0-beta.1+18.sha-7058072
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2095,7 +2095,7 @@ JsonpModule.decorators = [
 /** *
  * @deprecated see https://angular.io/guide/http
   @type {?} */
-const VERSION = new Version('7.0.0-beta.1+12.sha-9c92a6f');
+const VERSION = new Version('7.0.0-beta.1+18.sha-7058072');
 
 /**
  * @fileoverview added by tsickle
