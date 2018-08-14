78ec16ae5dfa38324c761b6ebb7a793bde333fc3
Ben Lesh <ben@benlesh.com>
2018-08-14 22:32:11 +0000

    26066f2 fix(ivy): consider exported modules from other compilation scopes (#25425)

--- fesm5/http.js
+++ fesm5/http.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.0.0-beta.1+43.sha-c13901f
+ * @license Angular v7.0.0-beta.1+46.sha-26066f2
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1828,7 +1828,7 @@ var JsonpModule = /** @class */ (function () {
 /**
  * @deprecated see https://angular.io/guide/http
  */
-var VERSION = new Version('7.0.0-beta.1+43.sha-c13901f');
+var VERSION = new Version('7.0.0-beta.1+46.sha-26066f2');
 
 /**
  * @license
