36f65d34509121958382af3f0d5bfb42489cabae
Ben Lesh <ben@benlesh.com>
2018-08-14 20:15:34 +0000

    ecb5dc0 docs: add Accelebrate to resources (#23204)

--- fesm5/http.js
+++ fesm5/http.js
@@ -1,10 +1,10 @@
 /**
- * @license Angular v7.0.0-beta.1+38.sha-9117fa1
+ * @license Angular v7.0.0-beta.1+34.sha-ecb5dc0
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
 
-import { __extends, __decorate, __metadata, __spread, __read } from 'tslib';
+import { __decorate, __metadata, __extends, __spread, __read } from 'tslib';
 import { Injectable, NgModule, Version } from '@angular/core';
 import { Observable } from 'rxjs';
 import { ɵgetDOM } from '@angular/platform-browser';
@@ -1828,7 +1828,7 @@ var JsonpModule = /** @class */ (function () {
 /**
  * @deprecated see https://angular.io/guide/http
  */
-var VERSION = new Version('7.0.0-beta.1+38.sha-9117fa1');
+var VERSION = new Version('7.0.0-beta.1+34.sha-ecb5dc0');
 
 /**
  * @license
