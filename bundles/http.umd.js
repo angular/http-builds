4790e840c09f329f77a1fa5c2bf3cbda53687b0c
Ben Lesh <ben@benlesh.com>
2018-08-14 20:14:13 +0000

    ecb5dc0 docs: add Accelebrate to resources (#23204)

--- bundles/http.umd.js
+++ bundles/http.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.0.0-beta.1+38.sha-9117fa1
+ * @license Angular v7.0.0-beta.1+34.sha-ecb5dc0
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1877,7 +1877,7 @@
     /**
      * @deprecated see https://angular.io/guide/http
      */
-    var VERSION = new core.Version('7.0.0-beta.1+38.sha-9117fa1');
+    var VERSION = new core.Version('7.0.0-beta.1+34.sha-ecb5dc0');
 
     /**
      * @license
