077d9917deb2bc231ee9e96d59af50e3fc6c2b99
Ben Lesh <ben@benlesh.com>
2018-08-14 05:01:57 +0000

    1bb3014 fix(ivy): add typeof guard around ngDevMode for instances where we cannot set it in Node (#25475)

--- fesm5/http.js
+++ fesm5/http.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.0.0-beta.1+27.sha-fb2c524
+ * @license Angular v7.0.0-beta.1+28.sha-1bb3014
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1828,7 +1828,7 @@ var JsonpModule = /** @class */ (function () {
 /**
  * @deprecated see https://angular.io/guide/http
  */
-var VERSION = new Version('7.0.0-beta.1+27.sha-fb2c524');
+var VERSION = new Version('7.0.0-beta.1+28.sha-1bb3014');
 
 /**
  * @license
