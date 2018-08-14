7cdd7f2d78631747a5af0378f56909acd624cb86
Ben Lesh <ben@benlesh.com>
2018-08-14 20:16:51 +0000

    0c4209f refactor(ivy): harmonize container and element / elementContainer signatures (#25458)

--- fesm5/http.js
+++ fesm5/http.js
@@ -1,11 +1,11 @@
 /**
- * @license Angular v7.0.0-beta.1+34.sha-ecb5dc0
+ * @license Angular v7.0.0-beta.1+37.sha-0c4209f
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
 
 import { Injectable, NgModule, Version } from '@angular/core';
-import { __read, __extends, __spread } from 'tslib';
+import { __extends, __read, __spread } from 'tslib';
 import { Observable } from 'rxjs';
 import { ɵgetDOM } from '@angular/platform-browser';
 
@@ -1847,7 +1847,7 @@ var JsonpModule = /** @class */ (function () {
 /**
  * @deprecated see https://angular.io/guide/http
  */
-var VERSION = new Version('7.0.0-beta.1+34.sha-ecb5dc0');
+var VERSION = new Version('7.0.0-beta.1+37.sha-0c4209f');
 
 /**
  * @license
