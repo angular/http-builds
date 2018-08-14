99af0df87e0c75b9d0c6861cae790af954c29c01
Ben Lesh <ben@benlesh.com>
2018-08-14 05:48:16 +0000

    5f08bdf ci: github robot should enforce that all requested reviews are submitted (#25336)

--- fesm5/http.js
+++ fesm5/http.js
@@ -1,11 +1,11 @@
 /**
- * @license Angular v6.1.2+11.sha-151e4b9
+ * @license Angular v6.1.2+13.sha-5f08bdf
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
 
 import { Injectable, NgModule, Version } from '@angular/core';
-import { __read, __extends, __spread } from 'tslib';
+import { __extends, __read, __spread } from 'tslib';
 import { Observable } from 'rxjs';
 import { ɵgetDOM } from '@angular/platform-browser';
 
@@ -1847,7 +1847,7 @@ var JsonpModule = /** @class */ (function () {
 /**
  * @deprecated see https://angular.io/guide/http
  */
-var VERSION = new Version('6.1.2+11.sha-151e4b9');
+var VERSION = new Version('6.1.2+13.sha-5f08bdf');
 
 /**
  * @license
