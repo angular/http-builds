a2769b7bdfb7006ee9bc01a0ce1de8b5cdf91eb0
Ben Lesh <ben@benlesh.com>
2018-08-15 00:50:21 +0000

    ed2a47f build(bazel): update to rules_typescript 0.16.0 & update to tagged rules_webtesting 0.2.1 (#25486)

--- fesm2015/http.js
+++ fesm2015/http.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.2+19.sha-cdee9ad
+ * @license Angular v6.1.2+20.sha-ed2a47f
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2095,7 +2095,7 @@ JsonpModule.decorators = [
 /** *
  * @deprecated see https://angular.io/guide/http
   @type {?} */
-const VERSION = new Version('6.1.2+19.sha-cdee9ad');
+const VERSION = new Version('6.1.2+20.sha-ed2a47f');
 
 /**
  * @fileoverview added by tsickle
