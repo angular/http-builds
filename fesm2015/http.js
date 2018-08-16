c218573b688aeaf2c82d06f976497ea377502843
Jason Aden <jasonaden@google.com>
2018-08-16 22:12:05 +0000

    36fb4f4 docs: reactive forms guide copy edits (#25417)

--- fesm2015/http.js
+++ fesm2015/http.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.3
+ * @license Angular v6.1.3+1.sha-36fb4f4
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2095,7 +2095,7 @@ JsonpModule.decorators = [
 /** *
  * @deprecated see https://angular.io/guide/http
   @type {?} */
-const VERSION = new Version('6.1.3');
+const VERSION = new Version('6.1.3+1.sha-36fb4f4');
 
 /**
  * @fileoverview added by tsickle
