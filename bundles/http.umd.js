df37ed8aa11aef11ff65275c956c8a98f0e81f8d
Pete Bacon Darwin <pete@bacondarwin.com>
2018-08-16 17:46:13 +0000

    73146c1 fix(ivy): ensure factory statements are emitted correctly

--- bundles/http.umd.js
+++ bundles/http.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.0.0-beta.1+43.sha-82c8052
+ * @license Angular v7.0.0-beta.2+33.sha-73146c1
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1877,7 +1877,7 @@
     /**
      * @deprecated see https://angular.io/guide/http
      */
-    var VERSION = new core.Version('7.0.0-beta.1+43.sha-82c8052');
+    var VERSION = new core.Version('7.0.0-beta.2+33.sha-73146c1');
 
     /**
      * @license
