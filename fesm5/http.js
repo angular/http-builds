719a7941b5078ddf9046e838db8cbd42c6218532
Jason Aden <jasonaden@google.com>
2018-08-17 18:16:09 +0000

    31f0f5b feat(ivy): add support for local refs on ng-template (#25482)

--- fesm5/http.js
+++ fesm5/http.js
@@ -1,10 +1,10 @@
 /**
- * @license Angular v7.0.0-beta.2+22.sha-f053a3f
+ * @license Angular v7.0.0-beta.2+21.sha-31f0f5b
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
 
-import { __extends, __decorate, __metadata, __spread, __read } from 'tslib';
+import { __decorate, __metadata, __extends, __spread, __read } from 'tslib';
 import { Injectable, NgModule, Version } from '@angular/core';
 import { Observable } from 'rxjs';
 import { ɵgetDOM } from '@angular/platform-browser';
@@ -1828,7 +1828,7 @@ var JsonpModule = /** @class */ (function () {
 /**
  * @deprecated see https://angular.io/guide/http
  */
-var VERSION = new Version('7.0.0-beta.2+22.sha-f053a3f');
+var VERSION = new Version('7.0.0-beta.2+21.sha-31f0f5b');
 
 /**
  * @license
