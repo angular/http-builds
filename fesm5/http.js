ea670b930a8293760423510b2a7cbd829fc8ddf1
Jason Aden <jasonaden@google.com>
2018-08-17 21:55:49 +0000

    2b810a4 docs(docs-infra): the `build.sh` script was renamed to `create-image.sh` (#25554)

--- fesm5/http.js
+++ fesm5/http.js
@@ -1,11 +1,11 @@
 /**
- * @license Angular v6.1.3+14.sha-a5cc9db
+ * @license Angular v6.1.3+13.sha-2b810a4
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
 
 import { Injectable, NgModule, Version } from '@angular/core';
-import { __read, __extends, __spread } from 'tslib';
+import { __extends, __read, __spread } from 'tslib';
 import { Observable } from 'rxjs';
 import { ɵgetDOM } from '@angular/platform-browser';
 
@@ -1847,7 +1847,7 @@ var JsonpModule = /** @class */ (function () {
 /**
  * @deprecated see https://angular.io/guide/http
  */
-var VERSION = new Version('6.1.3+14.sha-a5cc9db');
+var VERSION = new Version('6.1.3+13.sha-2b810a4');
 
 /**
  * @license
