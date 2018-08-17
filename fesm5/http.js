80fe76d055c290490d0a6368c6282da5056fb918
Jason Aden <jasonaden@google.com>
2018-08-17 23:12:48 +0000

    f2aa9c6 refactor(ivy): use generated consts value to set binding index (#25533)

--- fesm5/http.js
+++ fesm5/http.js
@@ -1,11 +1,11 @@
 /**
- * @license Angular v7.0.0-beta.2+22.sha-f053a3f
+ * @license Angular v7.0.0-beta.2+26.sha-f2aa9c6
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
 
 import { Injectable, NgModule, Version } from '@angular/core';
-import { __read, __extends, __spread } from 'tslib';
+import { __extends, __read, __spread } from 'tslib';
 import { Observable } from 'rxjs';
 import { ɵgetDOM } from '@angular/platform-browser';
 
@@ -1847,7 +1847,7 @@ var JsonpModule = /** @class */ (function () {
 /**
  * @deprecated see https://angular.io/guide/http
  */
-var VERSION = new Version('7.0.0-beta.2+22.sha-f053a3f');
+var VERSION = new Version('7.0.0-beta.2+26.sha-f2aa9c6');
 
 /**
  * @license
