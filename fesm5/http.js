5053db0f1a993ca55756c3929db9ffad9e6aa2ff
Jason Aden <jasonaden@google.com>
2018-08-20 19:34:10 +0000

    78750a7 docs: fix typo in service worker getting started guide (#25512)

--- fesm5/http.js
+++ fesm5/http.js
@@ -1,10 +1,10 @@
 /**
- * @license Angular v6.1.3+16.sha-292b435
+ * @license Angular v6.1.3+19.sha-78750a7
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
 
-import { __extends, __decorate, __metadata, __spread, __read } from 'tslib';
+import { __decorate, __metadata, __extends, __spread, __read } from 'tslib';
 import { Injectable, NgModule, Version } from '@angular/core';
 import { Observable } from 'rxjs';
 import { ɵgetDOM } from '@angular/platform-browser';
@@ -1828,7 +1828,7 @@ var JsonpModule = /** @class */ (function () {
 /**
  * @deprecated see https://angular.io/guide/http
  */
-var VERSION = new Version('6.1.3+16.sha-292b435');
+var VERSION = new Version('6.1.3+19.sha-78750a7');
 
 /**
  * @license
