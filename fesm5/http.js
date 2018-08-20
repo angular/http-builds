67e14803d3cfde9f2ae88cde7ed54dbbb63659ce
Jason Aden <jasonaden@google.com>
2018-08-20 19:22:17 +0000

    21a1440 refactor(ivy): generate vars in component defs (#25562)

--- fesm5/http.js
+++ fesm5/http.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.0.0-beta.2+26.sha-f2aa9c6
+ * @license Angular v7.0.0-beta.2+28.sha-21a1440
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1847,7 +1847,7 @@ var JsonpModule = /** @class */ (function () {
 /**
  * @deprecated see https://angular.io/guide/http
  */
-var VERSION = new Version('7.0.0-beta.2+26.sha-f2aa9c6');
+var VERSION = new Version('7.0.0-beta.2+28.sha-21a1440');
 
 /**
  * @license
