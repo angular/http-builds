f84249031320c836b60dd59e7bb9744d36cfb933
Jason Aden <jasonaden@google.com>
2018-08-21 19:35:04 +0000

    1fa9790 docs: Improve docs for downgrading a service (#19371)

--- fesm2015/http.js
+++ fesm2015/http.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.3+21.sha-7e61645
+ * @license Angular v6.1.3+22.sha-1fa9790
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2095,7 +2095,7 @@ JsonpModule.decorators = [
 /** *
  * @deprecated see https://angular.io/guide/http
   @type {?} */
-const VERSION = new Version('6.1.3+21.sha-7e61645');
+const VERSION = new Version('6.1.3+22.sha-1fa9790');
 
 /**
  * @fileoverview added by tsickle
