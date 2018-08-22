1e4b1b6911d67e76db84c1b4107ff5d6b09dffe5
Jason Aden <jasonaden@google.com>
2018-08-22 23:54:57 +0000

    52605aa docs: release notes for the v6.1.4 release

--- fesm5/http.js
+++ fesm5/http.js
@@ -1,11 +1,11 @@
 /**
- * @license Angular v7.0.0-beta.2+39.sha-f33dbf4
+ * @license Angular v7.0.0-beta.2+42.sha-52605aa
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
 
 import { Injectable, NgModule, Version } from '@angular/core';
-import { __read, __extends, __spread } from 'tslib';
+import { __extends, __read, __spread } from 'tslib';
 import { Observable } from 'rxjs';
 import { ɵgetDOM } from '@angular/platform-browser';
 
@@ -1847,7 +1847,7 @@ var JsonpModule = /** @class */ (function () {
 /**
  * @deprecated see https://angular.io/guide/http
  */
-var VERSION = new Version('7.0.0-beta.2+39.sha-f33dbf4');
+var VERSION = new Version('7.0.0-beta.2+42.sha-52605aa');
 
 /**
  * @license
