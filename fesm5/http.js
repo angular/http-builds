2c0a045d1e001f7c9d48d1e0a6dc84d12433cbcd
Matias Niemelä <matias@yearofmoo.com>
2018-08-23 02:50:20 +0000

    668bfce fix(docs-infra): fix closure warning issue for improper internal flag (#25628)

--- fesm5/http.js
+++ fesm5/http.js
@@ -1,11 +1,11 @@
 /**
- * @license Angular v7.0.0-beta.3+28.sha-eb1fe19
+ * @license Angular v7.0.0-beta.3+29.sha-668bfce
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
 
 import { Injectable, NgModule, Version } from '@angular/core';
-import { __extends, __read, __spread } from 'tslib';
+import { __read, __extends, __spread } from 'tslib';
 import { Observable } from 'rxjs';
 import { ɵgetDOM } from '@angular/platform-browser';
 
@@ -1847,7 +1847,7 @@ var JsonpModule = /** @class */ (function () {
 /**
  * @deprecated see https://angular.io/guide/http
  */
-var VERSION = new Version('7.0.0-beta.3+28.sha-eb1fe19');
+var VERSION = new Version('7.0.0-beta.3+29.sha-668bfce');
 
 /**
  * @license
