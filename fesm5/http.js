c51d259e623cd0c893fdcee8d9f8f5775f019c97
Matias Niemelä <matias@yearofmoo.com>
2018-08-23 22:38:09 +0000

    4e82a76 docs(elements): fix typo (tranformation --> transformation) (#25600)

--- fesm5/http.js
+++ fesm5/http.js
@@ -1,11 +1,11 @@
 /**
- * @license Angular v6.1.4+3.sha-51d5b43
+ * @license Angular v6.1.4+4.sha-4e82a76
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
 
 import { Injectable, NgModule, Version } from '@angular/core';
-import { __extends, __read, __spread } from 'tslib';
+import { __read, __extends, __spread } from 'tslib';
 import { Observable } from 'rxjs';
 import { ɵgetDOM } from '@angular/platform-browser';
 
@@ -1847,7 +1847,7 @@ var JsonpModule = /** @class */ (function () {
 /**
  * @deprecated see https://angular.io/guide/http
  */
-var VERSION = new Version('6.1.4+3.sha-51d5b43');
+var VERSION = new Version('6.1.4+4.sha-4e82a76');
 
 /**
  * @license
