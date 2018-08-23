b54ca88c87f10f7614e99138210ac6fbbb651071
Jason Aden <jasonaden@google.com>
2018-08-23 00:40:51 +0000

    8de57c9 release: cut the v6.1.4 release

--- fesm2015/http.js
+++ fesm2015/http.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.3+23.sha-ace4e4f
+ * @license Angular v6.1.4
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1753,7 +1753,7 @@ JsonpModule = __decorate([
 /**
  * @deprecated see https://angular.io/guide/http
  */
-const VERSION = new Version('6.1.3+23.sha-ace4e4f');
+const VERSION = new Version('6.1.4');
 
 /**
  * @license
