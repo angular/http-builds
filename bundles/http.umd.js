30b73b22cfaf0901648b09ac5772142bac8b3fdb
Matias Niemelä <matias@yearofmoo.com>
2018-08-23 00:32:19 +0000

    61218f5 fix(ivy): ensure factory statements are emitted correctly (#25406)

--- bundles/http.umd.js
+++ bundles/http.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.0.0-beta.3
+ * @license Angular v7.0.0-beta.3+24.sha-61218f5
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1869,7 +1869,7 @@
     /**
      * @deprecated see https://angular.io/guide/http
      */
-    var VERSION = new core.Version('7.0.0-beta.3');
+    var VERSION = new core.Version('7.0.0-beta.3+24.sha-61218f5');
 
     /**
      * @license
