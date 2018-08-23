afc970204913e2265226f082f252a6c2e4d16766
Matias Niemelä <matias@yearofmoo.com>
2018-08-23 20:23:54 +0000

    3d41739 fix(aio): show aio-themed 404 page for unknown resources (#23188)

--- fesm2015/http.js
+++ fesm2015/http.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.0.0-beta.3+29.sha-668bfce
+ * @license Angular v7.0.0-beta.3+30.sha-3d41739
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2095,7 +2095,7 @@ JsonpModule.decorators = [
 /** *
  * @deprecated see https://angular.io/guide/http
   @type {?} */
-const VERSION = new Version('7.0.0-beta.3+29.sha-668bfce');
+const VERSION = new Version('7.0.0-beta.3+30.sha-3d41739');
 
 /**
  * @fileoverview added by tsickle
