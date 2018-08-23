e7355ffc372d793692bd71ff5cd5fa9d42ceea33
Matias Niemelä <matias@yearofmoo.com>
2018-08-23 02:33:04 +0000

    82f26fe build(bazel): remove workaround no longer needed for module names for ngfactory & ngsummary files (#25604)

--- fesm2015/http.js
+++ fesm2015/http.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.4
+ * @license Angular v6.1.4+1.sha-82f26fe
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1753,7 +1753,7 @@ JsonpModule = __decorate([
 /**
  * @deprecated see https://angular.io/guide/http
  */
-const VERSION = new Version('6.1.4');
+const VERSION = new Version('6.1.4+1.sha-82f26fe');
 
 /**
  * @license
