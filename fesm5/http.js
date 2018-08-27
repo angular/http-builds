ade4610c03b40906b824db31bcd85b2e1e718a49
Matias Niemelä <matias@yearofmoo.com>
2018-08-27 23:24:25 +0000

    b1902db build(docs-infra): render all overloads if they are abstract (#25670)

--- fesm5/http.js
+++ fesm5/http.js
@@ -1,10 +1,10 @@
 /**
- * @license Angular v7.0.0-beta.3+58.sha-6a0f78f
+ * @license Angular v7.0.0-beta.3+61.sha-b1902db
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
 
-import { __extends, __decorate, __metadata, __spread, __read } from 'tslib';
+import { __decorate, __metadata, __extends, __spread, __read } from 'tslib';
 import { Injectable, NgModule, Version } from '@angular/core';
 import { Observable } from 'rxjs';
 import { ɵgetDOM } from '@angular/platform-browser';
@@ -1828,7 +1828,7 @@ var JsonpModule = /** @class */ (function () {
 /**
  * @deprecated see https://angular.io/guide/http
  */
-var VERSION = new Version('7.0.0-beta.3+58.sha-6a0f78f');
+var VERSION = new Version('7.0.0-beta.3+61.sha-b1902db');
 
 /**
  * @license
