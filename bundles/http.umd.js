a38636d020b6f86bc4c1b40c4d3fcc9c0f85a20f
Matias Niemelä <matias@yearofmoo.com>
2018-08-27 23:21:05 +0000

    6a0f78f fix(ivy): match directives on bindings and element outputs (#25614)

--- bundles/http.umd.js
+++ bundles/http.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.0.0-beta.3+57.sha-3634575
+ * @license Angular v7.0.0-beta.3+58.sha-6a0f78f
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1869,7 +1869,7 @@
     /**
      * @deprecated see https://angular.io/guide/http
      */
-    var VERSION = new core.Version('7.0.0-beta.3+57.sha-3634575');
+    var VERSION = new core.Version('7.0.0-beta.3+58.sha-6a0f78f');
 
     /**
      * @license
