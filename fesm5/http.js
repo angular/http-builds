1fa2a0816362e291834e33ef84fb4c2486987fc3
Matias Niemelä <matias@yearofmoo.com>
2018-08-30 01:37:27 +0000

    dd2a650 release: cut the v7.0.0-beta.4 release

--- fesm5/http.js
+++ fesm5/http.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.0.0-beta.3+77.sha-72b3b27
+ * @license Angular v7.0.0-beta.4
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1828,7 +1828,7 @@ var JsonpModule = /** @class */ (function () {
 /**
  * @deprecated see https://angular.io/guide/http
  */
-var VERSION = new Version('7.0.0-beta.3+77.sha-72b3b27');
+var VERSION = new Version('7.0.0-beta.4');
 
 /**
  * @license
