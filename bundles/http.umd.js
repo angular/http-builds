62d4552f9d619775f47d0ef2366c4c8197d89c57
Matias Niemelä <matias@yearofmoo.com>
2018-08-30 00:18:08 +0000

    0a3dd87 build: fix bad merge onto patch branch (#25729)

--- bundles/http.umd.js
+++ bundles/http.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.4+8.sha-e40519c
+ * @license Angular v6.1.5+2.sha-0a3dd87
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1869,7 +1869,7 @@
     /**
      * @deprecated see https://angular.io/guide/http
      */
-    var VERSION = new core.Version('6.1.4+8.sha-e40519c');
+    var VERSION = new core.Version('6.1.5+2.sha-0a3dd87');
 
     /**
      * @license
