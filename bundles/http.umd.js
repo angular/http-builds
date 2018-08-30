d39e573172a35d6b551c0919bf93b0ab5d607e99
Matias Niemelä <matias@yearofmoo.com>
2018-08-30 01:36:03 +0000

    dd2a650 release: cut the v7.0.0-beta.4 release

--- bundles/http.umd.js
+++ bundles/http.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.0.0-beta.3+77.sha-72b3b27
+ * @license Angular v7.0.0-beta.4
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1869,7 +1869,7 @@
     /**
      * @deprecated see https://angular.io/guide/http
      */
-    var VERSION = new core.Version('7.0.0-beta.3+77.sha-72b3b27');
+    var VERSION = new core.Version('7.0.0-beta.4');
 
     /**
      * @license
