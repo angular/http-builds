8a6bb2d356b5fefc332ec1979f6c1c58908ca05b
Matias Niemelä <matias@yearofmoo.com>
2018-08-30 00:20:58 +0000

    72b3b27 docs: release notes for the v6.1.6 release

--- bundles/http.umd.js
+++ bundles/http.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.0.0-beta.3+76.sha-f394ba0
+ * @license Angular v7.0.0-beta.3+77.sha-72b3b27
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1869,7 +1869,7 @@
     /**
      * @deprecated see https://angular.io/guide/http
      */
-    var VERSION = new core.Version('7.0.0-beta.3+76.sha-f394ba0');
+    var VERSION = new core.Version('7.0.0-beta.3+77.sha-72b3b27');
 
     /**
      * @license
