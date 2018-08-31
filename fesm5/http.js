7c698bf2ecb284703d1ca5a52a181f876e7bf1d2
Misko Hevery <misko@hevery.com>
2018-08-31 21:34:25 +0000

    abd29f5 docs(forms): update API reference for reactive and template-driven forms modules (#25687)

--- fesm5/http.js
+++ fesm5/http.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.0.0-beta.4+31.sha-31349fd
+ * @license Angular v7.0.0-beta.4+35.sha-abd29f5
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1828,7 +1828,7 @@ var JsonpModule = /** @class */ (function () {
 /**
  * @deprecated see https://angular.io/guide/http
  */
-var VERSION = new Version('7.0.0-beta.4+31.sha-31349fd');
+var VERSION = new Version('7.0.0-beta.4+35.sha-abd29f5');
 
 /**
  * @license
