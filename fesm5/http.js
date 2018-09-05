978d692ea15fb2c6de706373a9132c10dd1d2cf3
Misko Hevery <misko@hevery.com>
2018-09-05 19:28:06 +0000

    1e7a873 build(bazel): remove unused angular.tsconfig.json integration/bazel test (#25778)

--- fesm5/http.js
+++ fesm5/http.js
@@ -1,10 +1,10 @@
 /**
- * @license Angular v7.0.0-beta.4+50.sha-13b8399
+ * @license Angular v7.0.0-beta.4+51.sha-1e7a873
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
 
-import { __decorate, __read, __metadata, __extends, __spread } from 'tslib';
+import { __decorate, __metadata, __extends, __read, __spread } from 'tslib';
 import { Injectable, NgModule, Version } from '@angular/core';
 import { Observable } from 'rxjs';
 import { ɵgetDOM } from '@angular/platform-browser';
@@ -1828,7 +1828,7 @@ var JsonpModule = /** @class */ (function () {
 /**
  * @deprecated see https://angular.io/guide/http
  */
-var VERSION = new Version('7.0.0-beta.4+50.sha-13b8399');
+var VERSION = new Version('7.0.0-beta.4+51.sha-1e7a873');
 
 /**
  * @license
