f2a95f8c56a3a5c1935fdb94dacc6920c2488a8d
Misko Hevery <misko@hevery.com>
2018-09-05 19:25:33 +0000

    23a96dc feat(router): warn if navigation triggered outside Angular zone (#24959)

--- fesm2015/http.js
+++ fesm2015/http.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.6+13.sha-d752a89
+ * @license Angular v6.1.6+19.sha-23a96dc
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1753,7 +1753,7 @@ JsonpModule = __decorate([
 /**
  * @deprecated see https://angular.io/guide/http
  */
-const VERSION = new Version('6.1.6+13.sha-d752a89');
+const VERSION = new Version('6.1.6+19.sha-23a96dc');
 
 /**
  * @license
