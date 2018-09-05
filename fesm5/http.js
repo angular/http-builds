18c88af910625f2dd5efacb5483949b063c46141
Misko Hevery <misko@hevery.com>
2018-09-05 19:19:08 +0000

    a417b2b fix(ivy): detect frozen flyweight objects in definitions and unfreeze (#25755)

--- fesm5/http.js
+++ fesm5/http.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.0.0-beta.4+44.sha-25c1f33
+ * @license Angular v7.0.0-beta.4+46.sha-a417b2b
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1828,7 +1828,7 @@ var JsonpModule = /** @class */ (function () {
 /**
  * @deprecated see https://angular.io/guide/http
  */
-var VERSION = new Version('7.0.0-beta.4+44.sha-25c1f33');
+var VERSION = new Version('7.0.0-beta.4+46.sha-a417b2b');
 
 /**
  * @license
