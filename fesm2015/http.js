6d3d1dc16e447415e82bc32f7c2b8bd69e4a663a
Misko Hevery <misko@hevery.com>
2018-09-05 20:00:58 +0000

    8f8c390 test: remove deprecated Buffer usage in sourcemap test (#25805)

--- fesm2015/http.js
+++ fesm2015/http.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.6+16.sha-27f0817
+ * @license Angular v6.1.6+20.sha-8f8c390
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2095,7 +2095,7 @@ JsonpModule.decorators = [
 /** *
  * @deprecated see https://angular.io/guide/http
   @type {?} */
-const VERSION = new Version('6.1.6+16.sha-27f0817');
+const VERSION = new Version('6.1.6+20.sha-8f8c390');
 
 /**
  * @fileoverview added by tsickle
