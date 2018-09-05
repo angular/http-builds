066024e19b1e211aa29655950c4ee4dfb2ecbdca
Misko Hevery <misko@hevery.com>
2018-09-05 19:23:10 +0000

    234661b feat(docs-infra): disable "status" selector in API list when displaying only packages (#25718)

--- fesm5/http.js
+++ fesm5/http.js
@@ -1,10 +1,10 @@
 /**
- * @license Angular v7.0.0-beta.4+46.sha-a417b2b
+ * @license Angular v7.0.0-beta.4+48.sha-234661b
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
 
-import { __decorate, __read, __metadata, __extends, __spread } from 'tslib';
+import { __decorate, __metadata, __extends, __read, __spread } from 'tslib';
 import { Injectable, NgModule, Version } from '@angular/core';
 import { Observable } from 'rxjs';
 import { ɵgetDOM } from '@angular/platform-browser';
@@ -1828,7 +1828,7 @@ var JsonpModule = /** @class */ (function () {
 /**
  * @deprecated see https://angular.io/guide/http
  */
-var VERSION = new Version('7.0.0-beta.4+46.sha-a417b2b');
+var VERSION = new Version('7.0.0-beta.4+48.sha-234661b');
 
 /**
  * @license
