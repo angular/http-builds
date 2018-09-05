e04f925a0ccdce1bfb631ac43c2c1c8ba00c4b46
Misko Hevery <misko@hevery.com>
2018-09-05 20:31:34 +0000

    82e0676 fix(upgrade): trigger `$destroy` event on upgraded component element (#25357)

--- fesm5/http.js
+++ fesm5/http.js
@@ -1,10 +1,10 @@
 /**
- * @license Angular v6.1.6+27.sha-3a0b735
+ * @license Angular v6.1.6+22.sha-82e0676
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
 
-import { __decorate, __metadata, __extends, __read, __spread } from 'tslib';
+import { __extends, __decorate, __metadata, __spread, __read } from 'tslib';
 import { Injectable, NgModule, Version } from '@angular/core';
 import { Observable } from 'rxjs';
 import { ɵgetDOM } from '@angular/platform-browser';
@@ -1828,7 +1828,7 @@ var JsonpModule = /** @class */ (function () {
 /**
  * @deprecated see https://angular.io/guide/http
  */
-var VERSION = new Version('6.1.6+27.sha-3a0b735');
+var VERSION = new Version('6.1.6+22.sha-82e0676');
 
 /**
  * @license
