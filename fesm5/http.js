8db1bb1e2ea0b69103c5a8210caaea645170b486
Misko Hevery <misko@hevery.com>
2018-09-05 19:20:12 +0000

    4596fc0 refactor(docs-infra): bump polyfills payload limit (#25806)

--- fesm5/http.js
+++ fesm5/http.js
@@ -1,11 +1,11 @@
 /**
- * @license Angular v6.1.6+14.sha-46de203
+ * @license Angular v6.1.6+15.sha-4596fc0
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
 
 import { Injectable, NgModule, Version } from '@angular/core';
-import { __extends, __read, __spread } from 'tslib';
+import { __read, __extends, __spread } from 'tslib';
 import { Observable } from 'rxjs';
 import { ɵgetDOM } from '@angular/platform-browser';
 
@@ -1847,7 +1847,7 @@ var JsonpModule = /** @class */ (function () {
 /**
  * @deprecated see https://angular.io/guide/http
  */
-var VERSION = new Version('6.1.6+14.sha-46de203');
+var VERSION = new Version('6.1.6+15.sha-4596fc0');
 
 /**
  * @license
