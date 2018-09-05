07ea10b5e311f5edd9be668eb06faea4f7d74b4c
Misko Hevery <misko@hevery.com>
2018-09-05 19:27:01 +0000

    13b8399 test(docs-infra): fix double-slash in URL of `aio_monitoring` test (#25641)

--- fesm5/http.js
+++ fesm5/http.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.0.0-beta.4+52.sha-51c26b8
+ * @license Angular v7.0.0-beta.4+50.sha-13b8399
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1828,7 +1828,7 @@ var JsonpModule = /** @class */ (function () {
 /**
  * @deprecated see https://angular.io/guide/http
  */
-var VERSION = new Version('7.0.0-beta.4+52.sha-51c26b8');
+var VERSION = new Version('7.0.0-beta.4+50.sha-13b8399');
 
 /**
  * @license
