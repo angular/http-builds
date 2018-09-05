4c645b040bef59a92cb0e2e66d2a0071afa92b20
Misko Hevery <misko@hevery.com>
2018-09-05 20:14:36 +0000

    dcad054 build: update ngcontainer to node 10.9.0 (#25812)

--- fesm5/http.js
+++ fesm5/http.js
@@ -1,10 +1,10 @@
 /**
- * @license Angular v7.0.0-beta.4+57.sha-397b047
+ * @license Angular v7.0.0-beta.4+59.sha-dcad054
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
 
-import { __decorate, __read, __metadata, __extends, __spread } from 'tslib';
+import { __decorate, __read, __extends, __metadata, __spread } from 'tslib';
 import { Injectable, NgModule, Version } from '@angular/core';
 import { Observable } from 'rxjs';
 import { ɵgetDOM } from '@angular/platform-browser';
@@ -1828,7 +1828,7 @@ var JsonpModule = /** @class */ (function () {
 /**
  * @deprecated see https://angular.io/guide/http
  */
-var VERSION = new Version('7.0.0-beta.4+57.sha-397b047');
+var VERSION = new Version('7.0.0-beta.4+59.sha-dcad054');
 
 /**
  * @license
