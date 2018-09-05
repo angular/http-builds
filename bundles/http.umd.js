6974ea381b7b1c34d069b75f382f6fdd11166e58
Misko Hevery <misko@hevery.com>
2018-09-05 19:20:29 +0000

    a417b2b fix(ivy): detect frozen flyweight objects in definitions and unfreeze (#25755)

--- bundles/http.umd.js
+++ bundles/http.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.0.0-beta.4+44.sha-25c1f33
+ * @license Angular v7.0.0-beta.4+46.sha-a417b2b
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1869,7 +1869,7 @@
     /**
      * @deprecated see https://angular.io/guide/http
      */
-    var VERSION = new core.Version('7.0.0-beta.4+44.sha-25c1f33');
+    var VERSION = new core.Version('7.0.0-beta.4+46.sha-a417b2b');
 
     /**
      * @license
