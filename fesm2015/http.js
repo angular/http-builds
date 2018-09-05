84dbd4003b34a66e5d642184ef36f286ccc7994d
Misko Hevery <misko@hevery.com>
2018-09-05 20:29:59 +0000

    82e0676 fix(upgrade): trigger `$destroy` event on upgraded component element (#25357)

--- fesm2015/http.js
+++ fesm2015/http.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.6+27.sha-3a0b735
+ * @license Angular v6.1.6+22.sha-82e0676
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2095,7 +2095,7 @@ JsonpModule.decorators = [
 /** *
  * @deprecated see https://angular.io/guide/http
   @type {?} */
-const VERSION = new Version('6.1.6+27.sha-3a0b735');
+const VERSION = new Version('6.1.6+22.sha-82e0676');
 
 /**
  * @fileoverview added by tsickle
