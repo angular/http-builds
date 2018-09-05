4f41288a04c80d352a2dd79d3fbe39e42ff5f462
Misko Hevery <misko@hevery.com>
2018-09-05 20:03:05 +0000

    8f8c390 test: remove deprecated Buffer usage in sourcemap test (#25805)

--- bundles/http.umd.js
+++ bundles/http.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.6+16.sha-27f0817
+ * @license Angular v6.1.6+20.sha-8f8c390
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1869,7 +1869,7 @@
     /**
      * @deprecated see https://angular.io/guide/http
      */
-    var VERSION = new core.Version('6.1.6+16.sha-27f0817');
+    var VERSION = new core.Version('6.1.6+20.sha-8f8c390');
 
     /**
      * @license
