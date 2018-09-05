837e842130e058d39f8c2c730ebb8596ae56e51d
Misko Hevery <misko@hevery.com>
2018-09-05 18:34:44 +0000

    c1ae3c1 build(docs-infra): ensure root node_modules exists (#25811)

--- fesm5/http.js
+++ fesm5/http.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.0.0-beta.4+36.sha-e84da19
+ * @license Angular v7.0.0-beta.4+42.sha-c1ae3c1
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1828,7 +1828,7 @@ var JsonpModule = /** @class */ (function () {
 /**
  * @deprecated see https://angular.io/guide/http
  */
-var VERSION = new Version('7.0.0-beta.4+36.sha-e84da19');
+var VERSION = new Version('7.0.0-beta.4+42.sha-c1ae3c1');
 
 /**
  * @license
