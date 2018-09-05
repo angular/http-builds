49d89d6f867620d6741ef2a200625025e5fc1aee
Misko Hevery <misko@hevery.com>
2018-09-05 18:48:07 +0000

    25c1f33 refactor(docs-infra): bump polyfills payload limit (#25806)

--- bundles/http.umd.js
+++ bundles/http.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.0.0-beta.4+42.sha-c1ae3c1
+ * @license Angular v7.0.0-beta.4+44.sha-25c1f33
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1869,7 +1869,7 @@
     /**
      * @deprecated see https://angular.io/guide/http
      */
-    var VERSION = new core.Version('7.0.0-beta.4+42.sha-c1ae3c1');
+    var VERSION = new core.Version('7.0.0-beta.4+44.sha-25c1f33');
 
     /**
      * @license
