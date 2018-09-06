39deff8583c429dd6c3d5f9708a29bebb70eaa96
Igor Minar <igor@angular.io>
2018-09-06 23:39:46 +0000

    f707f54 build: update to Node 10 (#25822)

--- bundles/http.umd.js
+++ bundles/http.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.7+8.sha-b9a5ce1
+ * @license Angular v6.1.7+6.sha-f707f54
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1869,7 +1869,7 @@
     /**
      * @deprecated see https://angular.io/guide/http
      */
-    var VERSION = new core.Version('6.1.7+8.sha-b9a5ce1');
+    var VERSION = new core.Version('6.1.7+6.sha-f707f54');
 
     /**
      * @license
--- fesm5/http.js
+++ fesm5/http.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.7+8.sha-b9a5ce1
+ * @license Angular v6.1.7+6.sha-f707f54
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1828,7 +1828,7 @@ var JsonpModule = /** @class */ (function () {
 /**
  * @deprecated see https://angular.io/guide/http
  */
-var VERSION = new Version('6.1.7+8.sha-b9a5ce1');
+var VERSION = new Version('6.1.7+6.sha-f707f54');
 
 /**
  * @license
