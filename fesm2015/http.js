12434a9c872743120e26ed8256e1a0813be93691
Igor Minar <igor@angular.io>
2018-09-10 17:41:28 +0000

    91d7993 refactor(ivy): traverse tNode tree directly (#25872)

--- fesm2015/http.js
+++ fesm2015/http.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.0.0-beta.5+10.sha-96eb79b
+ * @license Angular v7.0.0-beta.5+11.sha-91d7993
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2095,7 +2095,7 @@ JsonpModule.decorators = [
 /** *
  * @deprecated see https://angular.io/guide/http
   @type {?} */
-const VERSION = new Version('7.0.0-beta.5+10.sha-96eb79b');
+const VERSION = new Version('7.0.0-beta.5+11.sha-91d7993');
 
 /**
  * @fileoverview added by tsickle
