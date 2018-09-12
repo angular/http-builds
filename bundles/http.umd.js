7a62fa73c82614f20110cec995223dda82d02882
Matias Niemelä <matias@yearofmoo.com>
2018-09-12 18:04:55 +0000

    10a656f refactor(ivy): ensure hello world doesn't pull in context discovery creation code (#25895)

--- bundles/http.umd.js
+++ bundles/http.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.0.0-beta.5+26.sha-21009b0
+ * @license Angular v7.0.0-beta.5+30.sha-10a656f
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1869,7 +1869,7 @@
     /**
      * @deprecated see https://angular.io/guide/http
      */
-    var VERSION = new core.Version('7.0.0-beta.5+26.sha-21009b0');
+    var VERSION = new core.Version('7.0.0-beta.5+30.sha-10a656f');
 
     /**
      * @license
