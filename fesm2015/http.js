35304ecb05bc82b22a981be7125cc46899156c14
Igor Minar <igor@angular.io>
2018-09-12 00:41:48 +0000

    a31cfc5 fix(compiler): Fix look up of entryComponents in AOT Summaries (#24892)

--- fesm2015/http.js
+++ fesm2015/http.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.7+13.sha-8d7fbb6
+ * @license Angular v6.1.7+12.sha-a31cfc5
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2095,7 +2095,7 @@ JsonpModule.decorators = [
 /** *
  * @deprecated see https://angular.io/guide/http
   @type {?} */
-const VERSION = new Version('6.1.7+13.sha-8d7fbb6');
+const VERSION = new Version('6.1.7+12.sha-a31cfc5');
 
 /**
  * @fileoverview added by tsickle
