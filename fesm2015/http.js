0f79ff604a45c23e6152a372127c958021f7b51c
Ben Lesh <ben@benlesh.com>
2018-09-18 21:01:37 +0000

    9b1bb37 fix(ivy): ngcc should compile entry-points in the correct order (#25862)

--- fesm2015/http.js
+++ fesm2015/http.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.0.0-beta.5+37.sha-21e566d
+ * @license Angular v7.0.0-beta.5+44.sha-9b1bb37
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2095,7 +2095,7 @@ JsonpModule.decorators = [
 /** *
  * @deprecated see https://angular.io/guide/http
   @type {?} */
-const VERSION = new Version('7.0.0-beta.5+37.sha-21e566d');
+const VERSION = new Version('7.0.0-beta.5+44.sha-9b1bb37');
 
 /**
  * @fileoverview added by tsickle
