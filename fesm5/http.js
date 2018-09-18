b38f0cb60d3aba5ce02eda6f7bf7d68cbf0f26a9
Ben Lesh <ben@benlesh.com>
2018-09-18 21:14:28 +0000

    5241ea0 test(bazel): Run Angular test on RBE (#25370)

--- fesm5/http.js
+++ fesm5/http.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.0.0-beta.5+48.sha-a53a559
+ * @license Angular v7.0.0-beta.5+52.sha-5241ea0
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1828,7 +1828,7 @@ var JsonpModule = /** @class */ (function () {
 /**
  * @deprecated see https://angular.io/guide/http
  */
-var VERSION = new Version('7.0.0-beta.5+48.sha-a53a559');
+var VERSION = new Version('7.0.0-beta.5+52.sha-5241ea0');
 
 /**
  * @license
