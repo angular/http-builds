90d9fed54c9648f288edac4224d1c817690455ac
Ben Lesh <ben@benlesh.com>
2018-09-18 21:06:58 +0000

    f47f262 refactor(ivy): remove LNode.view (#25988)

--- fesm5/http.js
+++ fesm5/http.js
@@ -1,10 +1,10 @@
 /**
- * @license Angular v7.0.0-beta.5+44.sha-9b1bb37
+ * @license Angular v7.0.0-beta.5+39.sha-f47f262
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
 
-import { __decorate, __metadata, __extends, __read, __spread } from 'tslib';
+import { __decorate, __metadata, __read, __extends, __spread } from 'tslib';
 import { Injectable, NgModule, Version } from '@angular/core';
 import { Observable } from 'rxjs';
 import { ɵgetDOM } from '@angular/platform-browser';
@@ -1828,7 +1828,7 @@ var JsonpModule = /** @class */ (function () {
 /**
  * @deprecated see https://angular.io/guide/http
  */
-var VERSION = new Version('7.0.0-beta.5+44.sha-9b1bb37');
+var VERSION = new Version('7.0.0-beta.5+39.sha-f47f262');
 
 /**
  * @license
