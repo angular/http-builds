a11610cfa31d49f89b27114d2dbba9c76f00bb24
Ben Lesh <ben@benlesh.com>
2018-09-19 22:37:03 +0000

    3988ebf release: cut the v6.1.8 release

--- fesm5/http.js
+++ fesm5/http.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.7+39.sha-a41f331
+ * @license Angular v6.1.8
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1828,7 +1828,7 @@ var JsonpModule = /** @class */ (function () {
 /**
  * @deprecated see https://angular.io/guide/http
  */
-var VERSION = new Version('6.1.7+39.sha-a41f331');
+var VERSION = new Version('6.1.8');
 
 /**
  * @license
