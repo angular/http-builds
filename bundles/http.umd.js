bf7e46af5ad07a7431107d33a3a1517acc0308fa
Ben Lesh <ben@benlesh.com>
2018-09-19 19:22:56 +0000

    a41f331 docs: add ngmodule api doc (#25618)

--- bundles/http.umd.js
+++ bundles/http.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.7+38.sha-71628f1
+ * @license Angular v6.1.7+39.sha-a41f331
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1869,7 +1869,7 @@
     /**
      * @deprecated see https://angular.io/guide/http
      */
-    var VERSION = new core.Version('6.1.7+38.sha-71628f1');
+    var VERSION = new core.Version('6.1.7+39.sha-a41f331');
 
     /**
      * @license
