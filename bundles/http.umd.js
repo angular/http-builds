16815083c3935b6977ed035b0badb8c165ed876f
Ben Lesh <ben@benlesh.com>
2018-09-19 17:23:30 +0000

    e8bab13 docs: delete extra sentence (#25984)

--- bundles/http.umd.js
+++ bundles/http.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.0.0-beta.5+51.sha-cbbad1b
+ * @license Angular v7.0.0-beta.5+54.sha-e8bab13
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1869,7 +1869,7 @@
     /**
      * @deprecated see https://angular.io/guide/http
      */
-    var VERSION = new core.Version('7.0.0-beta.5+51.sha-cbbad1b');
+    var VERSION = new core.Version('7.0.0-beta.5+54.sha-e8bab13');
 
     /**
      * @license
