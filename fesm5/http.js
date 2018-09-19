377e630af26021c3fb0b48c13050b24e7e6710fa
Ben Lesh <ben@benlesh.com>
2018-09-19 18:18:18 +0000

    f91b045 docs(animations): updated animation docs (#24206)

--- fesm5/http.js
+++ fesm5/http.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.0.0-beta.5+54.sha-e8bab13
+ * @license Angular v7.0.0-beta.5+55.sha-f91b045
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1828,7 +1828,7 @@ var JsonpModule = /** @class */ (function () {
 /**
  * @deprecated see https://angular.io/guide/http
  */
-var VERSION = new Version('7.0.0-beta.5+54.sha-e8bab13');
+var VERSION = new Version('7.0.0-beta.5+55.sha-f91b045');
 
 /**
  * @license
