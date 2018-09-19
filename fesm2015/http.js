777a24f3d9d8cc7aaacbd2e248bb362a4d3c0023
Ben Lesh <ben@benlesh.com>
2018-09-19 19:21:33 +0000

    a41f331 docs: add ngmodule api doc (#25618)

--- fesm2015/http.js
+++ fesm2015/http.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.7+38.sha-71628f1
+ * @license Angular v6.1.7+39.sha-a41f331
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2095,7 +2095,7 @@ JsonpModule.decorators = [
 /** *
  * @deprecated see https://angular.io/guide/http
   @type {?} */
-const VERSION = new Version('6.1.7+38.sha-71628f1');
+const VERSION = new Version('6.1.7+39.sha-a41f331');
 
 /**
  * @fileoverview added by tsickle
