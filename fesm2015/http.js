db9e7f1265db50dedc704ece77046b63c15be1e5
Kara Erickson <karakara@google.com>
2018-09-20 00:42:20 +0000

    c7a6adc docs(forms): update form apis based on review feedback (#25724)

--- fesm2015/http.js
+++ fesm2015/http.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.8
+ * @license Angular v6.1.8+3.sha-c7a6adc
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2095,7 +2095,7 @@ JsonpModule.decorators = [
 /** *
  * @deprecated see https://angular.io/guide/http
   @type {?} */
-const VERSION = new Version('6.1.8');
+const VERSION = new Version('6.1.8+3.sha-c7a6adc');
 
 /**
  * @fileoverview added by tsickle
