91c7d0553c2aa272eb0c1da122f1d4c484414f87
Kara Erickson <karakara@google.com>
2018-09-20 18:23:40 +0000

    f1cb460 ci: only run `aio_preview` job on PR builds (#26030)

--- fesm5/http.js
+++ fesm5/http.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.8+10.sha-0ec925b
+ * @license Angular v6.1.8+11.sha-f1cb460
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1828,7 +1828,7 @@ var JsonpModule = /** @class */ (function () {
 /**
  * @deprecated see https://angular.io/guide/http
  */
-var VERSION = new Version('6.1.8+10.sha-0ec925b');
+var VERSION = new Version('6.1.8+11.sha-f1cb460');
 
 /**
  * @license
