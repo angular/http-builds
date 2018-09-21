39c58447811b0b3ff53a94a559f968955c0ebd76
Kara Erickson <karakara@google.com>
2018-09-21 18:51:16 +0000

    61b4c26 ci: correct github robot size check configuration (#26057)

--- fesm2015/http.js
+++ fesm2015/http.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.8+13.sha-a135f48
+ * @license Angular v6.1.8+12.sha-61b4c26
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2095,7 +2095,7 @@ JsonpModule.decorators = [
 /** *
  * @deprecated see https://angular.io/guide/http
   @type {?} */
-const VERSION = new Version('6.1.8+13.sha-a135f48');
+const VERSION = new Version('6.1.8+12.sha-61b4c26');
 
 /**
  * @fileoverview added by tsickle
