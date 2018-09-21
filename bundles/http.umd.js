a5fa572afa8794cfde557f4a6e85b90cf537015d
Kara Erickson <karakara@google.com>
2018-09-21 19:01:32 +0000

    ee9f0b5 build(docs-infra): do not include license comment in first API doc (#26050)

--- bundles/http.umd.js
+++ bundles/http.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.8+18.sha-05d1b84
+ * @license Angular v6.1.8+14.sha-ee9f0b5
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1869,7 +1869,7 @@
     /**
      * @deprecated see https://angular.io/guide/http
      */
-    var VERSION = new core.Version('6.1.8+18.sha-05d1b84');
+    var VERSION = new core.Version('6.1.8+14.sha-ee9f0b5');
 
     /**
      * @license
