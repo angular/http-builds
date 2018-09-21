2885c3d4d884854e037ee3ab4b704bdffc1fe21b
Kara Erickson <karakara@google.com>
2018-09-21 18:07:06 +0000

    9931bd7 build(docs-infra): do not include license comment in first API doc (#26050)

--- fesm5/http.js
+++ fesm5/http.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.0.0-beta.6+33.sha-026b60c
+ * @license Angular v7.0.0-beta.6+30.sha-9931bd7
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1828,7 +1828,7 @@ var JsonpModule = /** @class */ (function () {
 /**
  * @deprecated see https://angular.io/guide/http
  */
-var VERSION = new Version('7.0.0-beta.6+33.sha-026b60c');
+var VERSION = new Version('7.0.0-beta.6+30.sha-9931bd7');
 
 /**
  * @license
