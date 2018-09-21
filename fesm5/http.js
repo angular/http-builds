cec5f35782e1cc8130eb8768714ec4be83267b5a
Kara Erickson <karakara@google.com>
2018-09-21 18:06:14 +0000

    4809483 docs: add Sajee to contributors (#26028)

--- fesm5/http.js
+++ fesm5/http.js
@@ -1,10 +1,10 @@
 /**
- * @license Angular v7.0.0-beta.6+30.sha-9931bd7
+ * @license Angular v7.0.0-beta.6+29.sha-4809483
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
 
-import { __decorate, __metadata, __extends, __read, __spread } from 'tslib';
+import { __decorate, __metadata, __read, __extends, __spread } from 'tslib';
 import { Injectable, NgModule, Version } from '@angular/core';
 import { Observable } from 'rxjs';
 import { ɵgetDOM } from '@angular/platform-browser';
@@ -1828,7 +1828,7 @@ var JsonpModule = /** @class */ (function () {
 /**
  * @deprecated see https://angular.io/guide/http
  */
-var VERSION = new Version('7.0.0-beta.6+30.sha-9931bd7');
+var VERSION = new Version('7.0.0-beta.6+29.sha-4809483');
 
 /**
  * @license
