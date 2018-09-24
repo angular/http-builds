443042e2e1332d33cfcafe53826fe4ae8bc800c3
Kara Erickson <karakara@google.com>
2018-09-24 16:56:14 +0000

    47eb212 docs: fix Sajee info in contributors (#26063)

--- bundles/http.umd.js
+++ bundles/http.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.0.0-beta.6+51.sha-b8422b4
+ * @license Angular v7.0.0-beta.6+52.sha-47eb212
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1876,7 +1876,7 @@
     /**
      * @deprecated see https://angular.io/guide/http
      */
-    var VERSION = new core.Version('7.0.0-beta.6+51.sha-b8422b4');
+    var VERSION = new core.Version('7.0.0-beta.6+52.sha-47eb212');
 
     /**
      * @license
