f6674a35049a6eaca8a5a969a2c59632be2a5602
Kara Erickson <karakara@google.com>
2018-09-24 21:27:58 +0000

    cf095d9 docs: fix a typo (#26074) PR Close #26074

--- fesm5/http.js
+++ fesm5/http.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.0.0-beta.6+56.sha-23ec88e
+ * @license Angular v7.0.0-beta.6+57.sha-cf095d9
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1835,7 +1835,7 @@ var JsonpModule = /** @class */ (function () {
 /**
  * @deprecated see https://angular.io/guide/http
  */
-var VERSION = new Version('7.0.0-beta.6+56.sha-23ec88e');
+var VERSION = new Version('7.0.0-beta.6+57.sha-cf095d9');
 
 /**
  * @license
