4f1262419cf055c8db1b144f2a2b01f4282f0a1e
Kara Erickson <karakara@google.com>
2018-09-24 18:14:45 +0000

    a169743 fix(service-worker): do not blow up when caches are unwritable (#26042)

--- fesm5/http.js
+++ fesm5/http.js
@@ -1,10 +1,10 @@
 /**
- * @license Angular v6.1.8+35.sha-77d2cbd
+ * @license Angular v6.1.8+38.sha-a169743
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
 
-import { __decorate, __metadata, __extends, __read, __spread } from 'tslib';
+import { __decorate, __metadata, __read, __extends, __spread } from 'tslib';
 import { Injectable, NgModule, Version } from '@angular/core';
 import { Observable } from 'rxjs';
 import { ɵgetDOM } from '@angular/platform-browser';
@@ -1835,7 +1835,7 @@ var JsonpModule = /** @class */ (function () {
 /**
  * @deprecated see https://angular.io/guide/http
  */
-var VERSION = new Version('6.1.8+35.sha-77d2cbd');
+var VERSION = new Version('6.1.8+38.sha-a169743');
 
 /**
  * @license
