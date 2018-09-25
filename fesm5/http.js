cf827d19438660a401b075f78f95b85299252bcd
Kara Erickson <karakara@google.com>
2018-09-25 18:50:49 +0000

    482e12c build: remove obsolete comment in `env.sh` (#25819)

--- fesm5/http.js
+++ fesm5/http.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.0.0-beta.6+58.sha-0c34471
+ * @license Angular v7.0.0-beta.6+59.sha-482e12c
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1835,7 +1835,7 @@ var JsonpModule = /** @class */ (function () {
 /**
  * @deprecated see https://angular.io/guide/http
  */
-var VERSION = new Version('7.0.0-beta.6+58.sha-0c34471');
+var VERSION = new Version('7.0.0-beta.6+59.sha-482e12c');
 
 /**
  * @license
