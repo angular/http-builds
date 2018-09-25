cb7b094dc187f603153e9ce763ff62b2a8b1dab5
Kara Erickson <karakara@google.com>
2018-09-25 17:32:51 +0000

    0c34471 feat(ivy): expose a series of helpful application inspection tools (#25919)

--- fesm5/http.js
+++ fesm5/http.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.0.0-beta.6+57.sha-cf095d9
+ * @license Angular v7.0.0-beta.6+58.sha-0c34471
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1835,7 +1835,7 @@ var JsonpModule = /** @class */ (function () {
 /**
  * @deprecated see https://angular.io/guide/http
  */
-var VERSION = new Version('7.0.0-beta.6+57.sha-cf095d9');
+var VERSION = new Version('7.0.0-beta.6+58.sha-0c34471');
 
 /**
  * @license
