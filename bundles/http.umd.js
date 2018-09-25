88d7ef820aa3368ff059c1f3a8a0ca19eff4ac4f
Kara Erickson <karakara@google.com>
2018-09-25 19:13:14 +0000

    42e2e7c build: remove obsolete comment in `env.sh` (#25819)

--- bundles/http.umd.js
+++ bundles/http.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.8+39.sha-adad170
+ * @license Angular v6.1.8+40.sha-42e2e7c
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1876,7 +1876,7 @@
     /**
      * @deprecated see https://angular.io/guide/http
      */
-    var VERSION = new core.Version('6.1.8+39.sha-adad170');
+    var VERSION = new core.Version('6.1.8+40.sha-42e2e7c');
 
     /**
      * @license
--- fesm5/http.js
+++ fesm5/http.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.8+39.sha-adad170
+ * @license Angular v6.1.8+40.sha-42e2e7c
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1835,7 +1835,7 @@ var JsonpModule = /** @class */ (function () {
 /**
  * @deprecated see https://angular.io/guide/http
  */
-var VERSION = new Version('6.1.8+39.sha-adad170');
+var VERSION = new Version('6.1.8+40.sha-42e2e7c');
 
 /**
  * @license
