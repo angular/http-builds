e22b90f36d430b9d62e2d6ae8190fd0d64bcd3b7
Kara Erickson <karakara@google.com>
2018-09-26 02:23:50 +0000

    c7e2930 docs: fix issues related to tutorial. (#24445)

--- bundles/http.umd.js
+++ bundles/http.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.0.0-beta.6+60.sha-6a62ed2
+ * @license Angular v7.0.0-beta.6+61.sha-c7e2930
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1876,7 +1876,7 @@
     /**
      * @deprecated see https://angular.io/guide/http
      */
-    var VERSION = new core.Version('7.0.0-beta.6+60.sha-6a62ed2');
+    var VERSION = new core.Version('7.0.0-beta.6+61.sha-c7e2930');
 
     /**
      * @license
