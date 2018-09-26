732c698b8cbb1ba54d4149c40d6535b5d4f4c323
Kara Erickson <karakara@google.com>
2018-09-26 23:59:18 +0000

    c12e553 fix(docs-infra): use correct parameters for paginated requests to GitHub (#25671)

--- fesm5/http.js
+++ fesm5/http.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.9+1.sha-5881f34
+ * @license Angular v6.1.9+18.sha-c12e553
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1835,7 +1835,7 @@ var JsonpModule = /** @class */ (function () {
 /**
  * @deprecated see https://angular.io/guide/http
  */
-var VERSION = new Version('6.1.9+1.sha-5881f34');
+var VERSION = new Version('6.1.9+18.sha-c12e553');
 
 /**
  * @license
