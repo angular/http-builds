7cf45383c4c7de208f7951d6ccdac53d2d097b35
Kara Erickson <karakara@google.com>
2018-09-26 18:52:00 +0000

    159e8b4 docs: update routing integration section based on feedback (#20023)

--- fesm5/http.js
+++ fesm5/http.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.8+41.sha-559c647
+ * @license Angular v6.1.8+65.sha-159e8b4
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1835,7 +1835,7 @@ var JsonpModule = /** @class */ (function () {
 /**
  * @deprecated see https://angular.io/guide/http
  */
-var VERSION = new Version('6.1.8+41.sha-559c647');
+var VERSION = new Version('6.1.8+65.sha-159e8b4');
 
 /**
  * @license
