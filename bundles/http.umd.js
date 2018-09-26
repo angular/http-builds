9c88bdca36b77f76ce111e0517b9e1bea88d5db4
Kara Erickson <karakara@google.com>
2018-09-26 02:31:04 +0000

    d191085 docs: cleanup minor changes for forms overview (#25663)

--- bundles/http.umd.js
+++ bundles/http.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.0.0-beta.6+61.sha-c7e2930
+ * @license Angular v7.0.0-beta.6+79.sha-d191085
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1876,7 +1876,7 @@
     /**
      * @deprecated see https://angular.io/guide/http
      */
-    var VERSION = new core.Version('7.0.0-beta.6+61.sha-c7e2930');
+    var VERSION = new core.Version('7.0.0-beta.6+79.sha-d191085');
 
     /**
      * @license
