b6b6906d37305727a0e637cf8debe87a2fdb4978
Alex Rickabaugh <alx+alxhub@alxandria.net>
2018-09-27 19:54:09 +0000

    63b795a refactor(ivy): make sure that test bed symbols are imported from ivy_switch (#26121)

--- bundles/http.umd.js
+++ bundles/http.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.0.0-beta.7+29.sha-5f6900e
+ * @license Angular v7.0.0-beta.7+30.sha-63b795a
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1876,7 +1876,7 @@
     /**
      * @deprecated see https://angular.io/guide/http
      */
-    var VERSION = new core.Version('7.0.0-beta.7+29.sha-5f6900e');
+    var VERSION = new core.Version('7.0.0-beta.7+30.sha-63b795a');
 
     /**
      * @license
