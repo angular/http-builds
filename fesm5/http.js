d1097d86699590387d8194ef9a918772026579da
Alex Rickabaugh <alx+alxhub@alxandria.net>
2018-09-27 19:34:49 +0000

    325e801 fixup! feat(ivy): adding support for ngNonBindable attribute

--- fesm5/http.js
+++ fesm5/http.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.0.0-beta.7+20.sha-eeebe28
+ * @license Angular v7.0.0-beta.7+28.sha-325e801
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1835,7 +1835,7 @@ var JsonpModule = /** @class */ (function () {
 /**
  * @deprecated see https://angular.io/guide/http
  */
-var VERSION = new Version('7.0.0-beta.7+20.sha-eeebe28');
+var VERSION = new Version('7.0.0-beta.7+28.sha-325e801');
 
 /**
  * @license
