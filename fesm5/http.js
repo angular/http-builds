a9faacd3781c005c067bfc5072e3b457e1ea55cd
Alex Rickabaugh <alx+alxhub@alxandria.net>
2018-09-28 17:17:38 +0000

    86e6a20 test: remove typescript 2.9 and 3.0 typings tests (#26151)

--- fesm5/http.js
+++ fesm5/http.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.0.0-beta.7+52.sha-794c359
+ * @license Angular v7.0.0-beta.7+50.sha-86e6a20
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1835,7 +1835,7 @@ var JsonpModule = /** @class */ (function () {
 /**
  * @deprecated see https://angular.io/guide/http
  */
-var VERSION = new Version('7.0.0-beta.7+52.sha-794c359');
+var VERSION = new Version('7.0.0-beta.7+50.sha-86e6a20');
 
 /**
  * @license
