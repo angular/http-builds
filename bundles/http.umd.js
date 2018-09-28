b634a803751f3d0f9d13d4cedce6ff056d968d56
Alex Rickabaugh <alx+alxhub@alxandria.net>
2018-09-28 23:18:39 +0000

    2ad1bb4 release: cut the v7.0.0-rc.0 release

--- bundles/http.umd.js
+++ bundles/http.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.0.0-beta.7+51.sha-b807106
+ * @license Angular v7.0.0-rc.0
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1876,7 +1876,7 @@
     /**
      * @deprecated see https://angular.io/guide/http
      */
-    var VERSION = new core.Version('7.0.0-beta.7+51.sha-b807106');
+    var VERSION = new core.Version('7.0.0-rc.0');
 
     /**
      * @license
