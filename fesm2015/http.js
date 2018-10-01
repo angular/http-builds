8acf7e615e04163fb534c234c3125aa7ce51d54e
Alex Rickabaugh <alx+alxhub@alxandria.net>
2018-10-01 21:23:14 +0000

    66c2d08 ci(docs-infra): run the  script in the correct folder

--- fesm2015/http.js
+++ fesm2015/http.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.9+36.sha-c1bf82a
+ * @license Angular v6.1.9+37.sha-66c2d08
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1760,7 +1760,7 @@ JsonpModule = __decorate([
 /**
  * @deprecated see https://angular.io/guide/http
  */
-const VERSION = new Version('6.1.9+36.sha-c1bf82a');
+const VERSION = new Version('6.1.9+37.sha-66c2d08');
 
 /**
  * @license
