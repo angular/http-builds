27a6905d03ae56e5c3feff14a01c26263779ddd7
Alex Rickabaugh <alx+alxhub@alxandria.net>
2018-10-03 20:08:54 +0000

    cb59d87 feat(ivy): compiler support to generate QUERY_READ_FROM_NODE calls (#26171)

--- fesm5/http.js
+++ fesm5/http.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.0.0-rc.0+24.sha-d216a46
+ * @license Angular v7.0.0-rc.0+25.sha-cb59d87
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1835,7 +1835,7 @@ var JsonpModule = /** @class */ (function () {
 /**
  * @deprecated see https://angular.io/guide/http
  */
-var VERSION = new Version('7.0.0-rc.0+24.sha-d216a46');
+var VERSION = new Version('7.0.0-rc.0+25.sha-cb59d87');
 
 /**
  * @license
