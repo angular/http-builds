3419f2a1da9cc156fa971a5f6815f4ec85a80b53
Jason Aden <jasonaden@google.com>
2018-10-04 18:10:35 +0000

    1c1fd98 docs: add Suguru Inatomi to GDE resources (#26219)

--- fesm5/http.js
+++ fesm5/http.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.9+56.sha-d815e41
+ * @license Angular v6.1.9+57.sha-1c1fd98
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1835,7 +1835,7 @@ var JsonpModule = /** @class */ (function () {
 /**
  * @deprecated see https://angular.io/guide/http
  */
-var VERSION = new Version('6.1.9+56.sha-d815e41');
+var VERSION = new Version('6.1.9+57.sha-1c1fd98');
 
 /**
  * @license
