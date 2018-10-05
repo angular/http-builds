ada752c7e161a074090efdaed7d1c012f0094aa4
Jason Aden <jasonaden@google.com>
2018-10-05 23:11:24 +0000

    b35ab4f docs: clarify CurrencyPipe display property (#25852)

--- fesm2015/http.js
+++ fesm2015/http.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.9+65.sha-ff98003
+ * @license Angular v6.1.9+67.sha-b35ab4f
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2102,7 +2102,7 @@ JsonpModule.decorators = [
 /** *
  * @deprecated see https://angular.io/guide/http
   @type {?} */
-const VERSION = new Version('6.1.9+65.sha-ff98003');
+const VERSION = new Version('6.1.9+67.sha-b35ab4f');
 
 /**
  * @fileoverview added by tsickle
