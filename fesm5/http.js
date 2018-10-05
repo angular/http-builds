ee7805457898ef261ca984e27a618688c2482469
Jason Aden <jasonaden@google.com>
2018-10-05 21:14:24 +0000

    bfeed84 docs(service-worker): improve API docs for `SwPush`/`SwUpdate` (#23138)

--- fesm5/http.js
+++ fesm5/http.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.0.0-rc.0+49.sha-3cce4af
+ * @license Angular v7.0.0-rc.0+53.sha-bfeed84
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1835,7 +1835,7 @@ var JsonpModule = /** @class */ (function () {
 /**
  * @deprecated see https://angular.io/guide/http
  */
-var VERSION = new Version('7.0.0-rc.0+49.sha-3cce4af');
+var VERSION = new Version('7.0.0-rc.0+53.sha-bfeed84');
 
 /**
  * @license
