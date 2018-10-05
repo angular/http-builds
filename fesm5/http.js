738aa72e1271d09da3c4890ad0157a9afe59b700
Jason Aden <jasonaden@google.com>
2018-10-05 23:20:47 +0000

    3886bfa docs: Rename 'QuickStart' into 'Getting Started' (#25762)

--- fesm5/http.js
+++ fesm5/http.js
@@ -1,10 +1,10 @@
 /**
- * @license Angular v6.1.9+64.sha-2fe401d
+ * @license Angular v6.1.9+68.sha-3886bfa
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
 
-import { __decorate, __metadata, __read, __extends, __spread } from 'tslib';
+import { __decorate, __metadata, __extends, __read, __spread } from 'tslib';
 import { Injectable, NgModule, Version } from '@angular/core';
 import { Observable } from 'rxjs';
 import { ɵgetDOM } from '@angular/platform-browser';
@@ -1835,7 +1835,7 @@ var JsonpModule = /** @class */ (function () {
 /**
  * @deprecated see https://angular.io/guide/http
  */
-var VERSION = new Version('6.1.9+64.sha-2fe401d');
+var VERSION = new Version('6.1.9+68.sha-3886bfa');
 
 /**
  * @license
