7320f3cbe3989a75bd87537ff1ddb32fbfd8dee3
Jason Aden <jasonaden@google.com>
2018-10-05 23:15:38 +0000

    f543d71 docs: add package doc files (#26047)

--- fesm2015/http.js
+++ fesm2015/http.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.0.0-rc.0+57.sha-ab6f055
+ * @license Angular v7.0.0-rc.0+59.sha-f543d71
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2102,7 +2102,7 @@ JsonpModule.decorators = [
 /** *
  * @deprecated see https://angular.io/guide/http
   @type {?} */
-const VERSION = new Version('7.0.0-rc.0+57.sha-ab6f055');
+const VERSION = new Version('7.0.0-rc.0+59.sha-f543d71');
 
 /**
  * @fileoverview added by tsickle
