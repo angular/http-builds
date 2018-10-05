f26c8423acee1c8114dad0f2d16db835ad9a7b52
Jason Aden <jasonaden@google.com>
2018-10-05 21:27:40 +0000

    8f25321 refactor(ivy): update context discovery to prep for dir merge (#26262)

--- fesm5/http.js
+++ fesm5/http.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.0.0-rc.0+54.sha-245b85f
+ * @license Angular v7.0.0-rc.0+48.sha-8f25321
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1835,7 +1835,7 @@ var JsonpModule = /** @class */ (function () {
 /**
  * @deprecated see https://angular.io/guide/http
  */
-var VERSION = new Version('7.0.0-rc.0+54.sha-245b85f');
+var VERSION = new Version('7.0.0-rc.0+48.sha-8f25321');
 
 /**
  * @license
