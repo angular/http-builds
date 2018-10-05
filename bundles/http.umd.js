edc6182caad944efec977539f66054cfc28e37f7
Jason Aden <jasonaden@google.com>
2018-10-05 23:16:00 +0000

    ab6f055 ci(docs-infra): show custom 404 page on preview server (for consistency) (#26199)

--- bundles/http.umd.js
+++ bundles/http.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.0.0-rc.0+58.sha-3a5cb1c
+ * @license Angular v7.0.0-rc.0+57.sha-ab6f055
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1876,7 +1876,7 @@
     /**
      * @deprecated see https://angular.io/guide/http
      */
-    var VERSION = new core.Version('7.0.0-rc.0+58.sha-3a5cb1c');
+    var VERSION = new core.Version('7.0.0-rc.0+57.sha-ab6f055');
 
     /**
      * @license
