6841eed6c39821f4217ff58fefbbcadf7a2941cd
Jason Aden <jasonaden@google.com>
2018-10-08 19:39:12 +0000

    623adbb fix(upgrade): properly destroy upgraded component elements and descendants (#26209)

--- fesm2015/http.js
+++ fesm2015/http.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.9+71.sha-3660ff8
+ * @license Angular v6.1.9+72.sha-623adbb
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1760,7 +1760,7 @@ JsonpModule = __decorate([
 /**
  * @deprecated see https://angular.io/guide/http
  */
-const VERSION = new Version('6.1.9+71.sha-3660ff8');
+const VERSION = new Version('6.1.9+72.sha-623adbb');
 
 /**
  * @license
