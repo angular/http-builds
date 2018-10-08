2a276631bc27a46d6fe989e86e5fd370030eb98d
Jason Aden <jasonaden@google.com>
2018-10-08 21:12:52 +0000

    ed04e99 docs: update docs to reflect the changes in RxJS 6 (#26238)

--- fesm2015/http.js
+++ fesm2015/http.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.9+72.sha-623adbb
+ * @license Angular v6.1.9+73.sha-ed04e99
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1760,7 +1760,7 @@ JsonpModule = __decorate([
 /**
  * @deprecated see https://angular.io/guide/http
  */
-const VERSION = new Version('6.1.9+72.sha-623adbb');
+const VERSION = new Version('6.1.9+73.sha-ed04e99');
 
 /**
  * @license
--- fesm5/http.js
+++ fesm5/http.js
@@ -1,10 +1,10 @@
 /**
- * @license Angular v6.1.9+72.sha-623adbb
+ * @license Angular v6.1.9+73.sha-ed04e99
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
 
-import { __decorate, __metadata, __extends, __read, __spread } from 'tslib';
+import { __decorate, __metadata, __extends, __spread, __read } from 'tslib';
 import { Injectable, NgModule, Version } from '@angular/core';
 import { Observable } from 'rxjs';
 import { ɵgetDOM } from '@angular/platform-browser';
@@ -1835,7 +1835,7 @@ var JsonpModule = /** @class */ (function () {
 /**
  * @deprecated see https://angular.io/guide/http
  */
-var VERSION = new Version('6.1.9+72.sha-623adbb');
+var VERSION = new Version('6.1.9+73.sha-ed04e99');
 
 /**
  * @license
