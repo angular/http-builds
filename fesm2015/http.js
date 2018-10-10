3e685180d45180adf09e5f2c61d6faad89b3b036
Jason Aden <jasonaden@google.com>
2018-10-10 22:19:24 +0000

    65555fe Revert "fix(upgrade): properly destroy upgraded component elements and descendants (#26209)"

--- fesm2015/http.js
+++ fesm2015/http.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.10
+ * @license Angular v6.1.10+2.sha-65555fe
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1760,7 +1760,7 @@ JsonpModule = __decorate([
 /**
  * @deprecated see https://angular.io/guide/http
  */
-const VERSION = new Version('6.1.10');
+const VERSION = new Version('6.1.10+2.sha-65555fe');
 
 /**
  * @license
