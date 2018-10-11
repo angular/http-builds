4a9d78ec26f8361dd45197a0e4a33d9a075fef7d
Miško Hevery <misko@hevery.com>
2018-10-11 20:27:59 +0000

    39f42ba feat(ivy): i18n compiler support for element attributes (#26280)

--- fesm2015/http.js
+++ fesm2015/http.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.0.0-rc.0+105.sha-7acdad6
+ * @license Angular v7.0.0-rc.1+4.sha-39f42ba
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2102,7 +2102,7 @@ JsonpModule.decorators = [
 /** *
  * @deprecated see https://angular.io/guide/http
   @type {?} */
-const VERSION = new Version('7.0.0-rc.0+105.sha-7acdad6');
+const VERSION = new Version('7.0.0-rc.1+4.sha-39f42ba');
 
 /**
  * @fileoverview added by tsickle
