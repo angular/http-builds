44c82dda171cd595ef5e5e106ce921ea79c2b36b
Miško Hevery <misko@hevery.com>
2018-10-12 16:39:15 +0000

    ea20ae6 build(docs-infra): only show name in 'inherited from' section (#26387)

--- bundles/http.umd.js
+++ bundles/http.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.0.0-rc.1+32.sha-7777a99
+ * @license Angular v7.0.0-rc.1+33.sha-ea20ae6
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1876,7 +1876,7 @@
     /**
      * @deprecated see https://angular.io/guide/http
      */
-    var VERSION = new core.Version('7.0.0-rc.1+32.sha-7777a99');
+    var VERSION = new core.Version('7.0.0-rc.1+33.sha-ea20ae6');
 
     /**
      * @license
