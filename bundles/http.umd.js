2bb03ef09685d5e1d99ec745c968c02dcb0f3928
Miško Hevery <misko@hevery.com>
2018-10-12 21:40:55 +0000

    a19b690 docs: move cli commands above api in nav (#26405)

--- bundles/http.umd.js
+++ bundles/http.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.0.0-rc.1+38.sha-9e32dc7
+ * @license Angular v7.0.0-rc.1+37.sha-a19b690
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1876,7 +1876,7 @@
     /**
      * @deprecated see https://angular.io/guide/http
      */
-    var VERSION = new core.Version('7.0.0-rc.1+38.sha-9e32dc7');
+    var VERSION = new core.Version('7.0.0-rc.1+37.sha-a19b690');
 
     /**
      * @license
