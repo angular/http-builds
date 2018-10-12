2f6d1fcc651f143f1e34d378eae2fcd6c1581ded
Miško Hevery <misko@hevery.com>
2018-10-12 21:51:13 +0000

    6790709 fix(docs-infra): prevent unnecessary SideNav scrollbar (#26416)

--- fesm5/http.js
+++ fesm5/http.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.10+8.sha-db49bea
+ * @license Angular v6.1.10+10.sha-6790709
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1835,7 +1835,7 @@ var JsonpModule = /** @class */ (function () {
 /**
  * @deprecated see https://angular.io/guide/http
  */
-var VERSION = new Version('6.1.10+8.sha-db49bea');
+var VERSION = new Version('6.1.10+10.sha-6790709');
 
 /**
  * @license
