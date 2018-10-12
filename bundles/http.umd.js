dc7ef9f4035809c06964de3a9270424832f9d9dd
Miško Hevery <misko@hevery.com>
2018-10-12 16:37:17 +0000

    db49bea build(docs-infra): upgrade @angular/material to 7.0.0-rc.1 (#26394)

--- bundles/http.umd.js
+++ bundles/http.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.10+5.sha-4f09f7d
+ * @license Angular v6.1.10+8.sha-db49bea
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1876,7 +1876,7 @@
     /**
      * @deprecated see https://angular.io/guide/http
      */
-    var VERSION = new core.Version('6.1.10+5.sha-4f09f7d');
+    var VERSION = new core.Version('6.1.10+8.sha-db49bea');
 
     /**
      * @license
--- fesm5/http.js
+++ fesm5/http.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.10+5.sha-4f09f7d
+ * @license Angular v6.1.10+8.sha-db49bea
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1835,7 +1835,7 @@ var JsonpModule = /** @class */ (function () {
 /**
  * @deprecated see https://angular.io/guide/http
  */
-var VERSION = new Version('6.1.10+5.sha-4f09f7d');
+var VERSION = new Version('6.1.10+8.sha-db49bea');
 
 /**
  * @license
