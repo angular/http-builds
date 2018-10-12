56f5d6846f29fb9edcd17b4a347ae2702191c010
Miško Hevery <misko@hevery.com>
2018-10-12 16:22:20 +0000

    7fbeb04 build(docs-infra): upgrade @angular/material to 7.0.0-rc.1 (#26394)

--- fesm2015/http.js
+++ fesm2015/http.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.0.0-rc.1+26.sha-735dfd3
+ * @license Angular v7.0.0-rc.1+30.sha-7fbeb04
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2102,7 +2102,7 @@ JsonpModule.decorators = [
 /** *
  * @deprecated see https://angular.io/guide/http
   @type {?} */
-const VERSION = new Version('7.0.0-rc.1+26.sha-735dfd3');
+const VERSION = new Version('7.0.0-rc.1+30.sha-7fbeb04');
 
 /**
  * @fileoverview added by tsickle
