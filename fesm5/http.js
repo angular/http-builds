245f64c3294dc53a89db404dbebc13b7c0b7bbdf
Miško Hevery <misko@hevery.com>
2018-10-15 17:58:22 +0000

    81724f5 ci(docs-infra): allow `aio_local` builds to fail on Travis

--- fesm5/http.js
+++ fesm5/http.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.10+11.sha-1f06b6c
+ * @license Angular v6.1.10+12.sha-81724f5
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1835,7 +1835,7 @@ var JsonpModule = /** @class */ (function () {
 /**
  * @deprecated see https://angular.io/guide/http
  */
-var VERSION = new Version('6.1.10+11.sha-1f06b6c');
+var VERSION = new Version('6.1.10+12.sha-81724f5');
 
 /**
  * @license
