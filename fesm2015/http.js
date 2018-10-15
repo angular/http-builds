1317ea153b4df023c6522a9fbb159af9a3f1eddd
Miško Hevery <misko@hevery.com>
2018-10-15 19:06:01 +0000

    a251374 docs: update process for cli tool and restructure doc (#25752)

--- fesm2015/http.js
+++ fesm2015/http.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.10+13.sha-2b00c17
+ * @license Angular v6.1.10+14.sha-a251374
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1760,7 +1760,7 @@ JsonpModule = __decorate([
 /**
  * @deprecated see https://angular.io/guide/http
  */
-const VERSION = new Version('6.1.10+13.sha-2b00c17');
+const VERSION = new Version('6.1.10+14.sha-a251374');
 
 /**
  * @license
--- fesm5/http.js
+++ fesm5/http.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.10+13.sha-2b00c17
+ * @license Angular v6.1.10+14.sha-a251374
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1835,7 +1835,7 @@ var JsonpModule = /** @class */ (function () {
 /**
  * @deprecated see https://angular.io/guide/http
  */
-var VERSION = new Version('6.1.10+13.sha-2b00c17');
+var VERSION = new Version('6.1.10+14.sha-a251374');
 
 /**
  * @license
