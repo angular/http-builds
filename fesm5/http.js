a20f1f08565ced731683f6266e94d320aa437e38
George Kalpakas <kalpakas.g@gmail.com>
2018-10-15 20:13:25 +0000

    a720d8a build(docs-infra): upgrade cli command docs sources to 9b72649b8

--- fesm5/http.js
+++ fesm5/http.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.0.0-rc.1+40.sha-b821218
+ * @license Angular v7.0.0-rc.1+40.sha-a720d8a
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1835,7 +1835,7 @@ var JsonpModule = /** @class */ (function () {
 /**
  * @deprecated see https://angular.io/guide/http
  */
-var VERSION = new Version('7.0.0-rc.1+40.sha-b821218');
+var VERSION = new Version('7.0.0-rc.1+40.sha-a720d8a');
 
 /**
  * @license
