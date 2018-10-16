80705792be434fc85d2a4294ac6443be2edde298
Miško Hevery <misko@hevery.com>
2018-10-16 22:12:25 +0000

    f6a2dbf docs: updated text to match the Getting started guide (#26421)

--- bundles/http.umd.js
+++ bundles/http.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.0.0-rc.1+52.sha-b115374
+ * @license Angular v7.0.0-rc.1+53.sha-f6a2dbf
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1876,7 +1876,7 @@
     /**
      * @deprecated see https://angular.io/guide/http
      */
-    var VERSION = new core.Version('7.0.0-rc.1+52.sha-b115374');
+    var VERSION = new core.Version('7.0.0-rc.1+53.sha-f6a2dbf');
 
     /**
      * @license
