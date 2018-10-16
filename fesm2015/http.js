dd53eaa845ef3b226b82c1f810ee1cb5668621c5
Miško Hevery <misko@hevery.com>
2018-10-16 22:18:23 +0000

    be0382b docs: fix links to setup and cli docs (#26463)

--- fesm2015/http.js
+++ fesm2015/http.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.0.0-rc.1+52.sha-96f3856
+ * @license Angular v7.0.0-rc.1+55.sha-be0382b
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2102,7 +2102,7 @@ JsonpModule.decorators = [
 /** *
  * @deprecated see https://angular.io/guide/http
   @type {?} */
-const VERSION = new Version('7.0.0-rc.1+52.sha-96f3856');
+const VERSION = new Version('7.0.0-rc.1+55.sha-be0382b');
 
 /**
  * @fileoverview added by tsickle
--- fesm5/http.js
+++ fesm5/http.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.0.0-rc.1+52.sha-96f3856
+ * @license Angular v7.0.0-rc.1+55.sha-be0382b
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1835,7 +1835,7 @@ var JsonpModule = /** @class */ (function () {
 /**
  * @deprecated see https://angular.io/guide/http
  */
-var VERSION = new Version('7.0.0-rc.1+52.sha-96f3856');
+var VERSION = new Version('7.0.0-rc.1+55.sha-be0382b');
 
 /**
  * @license
