0c091980f8e583d38c0c9be4bb211ee4937ca9f5
Miško Hevery <misko@hevery.com>
2018-10-16 22:19:31 +0000

    be0382b docs: fix links to setup and cli docs (#26463)

--- fesm5/http.js
+++ fesm5/http.js
@@ -1,10 +1,10 @@
 /**
- * @license Angular v7.0.0-rc.1+52.sha-96f3856
+ * @license Angular v7.0.0-rc.1+55.sha-be0382b
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
 
-import { __decorate, __metadata, __extends, __spread, __read } from 'tslib';
+import { __extends, __decorate, __metadata, __spread, __read } from 'tslib';
 import { Injectable, NgModule, Version } from '@angular/core';
 import { Observable } from 'rxjs';
 import { ɵgetDOM } from '@angular/platform-browser';
@@ -1835,7 +1835,7 @@ var JsonpModule = /** @class */ (function () {
 /**
  * @deprecated see https://angular.io/guide/http
  */
-var VERSION = new Version('7.0.0-rc.1+52.sha-96f3856');
+var VERSION = new Version('7.0.0-rc.1+55.sha-be0382b');
 
 /**
  * @license
