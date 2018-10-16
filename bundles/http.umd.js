9d3876ec7a5f79d43514c5414d825343093c76b2
Miško Hevery <misko@hevery.com>
2018-10-16 00:21:51 +0000

    eeebe62 docs: Indicate that PRs should have an associated issue (#25436) PR Close #25436

--- bundles/http.umd.js
+++ bundles/http.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.10+17.sha-05f279d
+ * @license Angular v6.1.10+18.sha-eeebe62
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1876,7 +1876,7 @@
     /**
      * @deprecated see https://angular.io/guide/http
      */
-    var VERSION = new core.Version('6.1.10+17.sha-05f279d');
+    var VERSION = new core.Version('6.1.10+18.sha-eeebe62');
 
     /**
      * @license
