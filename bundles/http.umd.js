f9a12788fa88f3d274cdef6bd4ec89c9bdf02b85
Misko Hevery <misko@hevery.com>
2018-10-17 19:31:39 +0000

    a43b80a ci: add triage PRs config for the ngbot (#26477)

--- bundles/http.umd.js
+++ bundles/http.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.0.0-rc.1+84.sha-65b2093
+ * @license Angular v7.0.0-rc.1+76.sha-a43b80a
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1876,7 +1876,7 @@
     /**
      * @deprecated see https://angular.io/guide/http
      */
-    var VERSION = new core.Version('7.0.0-rc.1+84.sha-65b2093');
+    var VERSION = new core.Version('7.0.0-rc.1+76.sha-a43b80a');
 
     /**
      * @license
