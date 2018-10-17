ee32b70ab85f09ad601547cabf9289fa1f9be066
Misko Hevery <misko@hevery.com>
2018-10-17 19:23:14 +0000

    d769b44 docs: change links to cli wiki to link to new aio docs (#26489)

--- fesm5/http.js
+++ fesm5/http.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.0.0-rc.1+61.sha-017a087
+ * @license Angular v7.0.0-rc.1+64.sha-d769b44
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1835,7 +1835,7 @@ var JsonpModule = /** @class */ (function () {
 /**
  * @deprecated see https://angular.io/guide/http
  */
-var VERSION = new Version('7.0.0-rc.1+61.sha-017a087');
+var VERSION = new Version('7.0.0-rc.1+64.sha-d769b44');
 
 /**
  * @license
