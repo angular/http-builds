4e1fd4f285999084b94b377d110db1dbd757e030
Misko Hevery <misko@hevery.com>
2018-10-17 04:09:21 +0000

    4d164b6 fix(ivy): make defineComponent tree shakable by Closure Compiler (#26425)

--- fesm5/http.js
+++ fesm5/http.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.0.0-rc.1+60.sha-fdfedce
+ * @license Angular v7.0.0-rc.1+62.sha-4d164b6
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1835,7 +1835,7 @@ var JsonpModule = /** @class */ (function () {
 /**
  * @deprecated see https://angular.io/guide/http
  */
-var VERSION = new Version('7.0.0-rc.1+60.sha-fdfedce');
+var VERSION = new Version('7.0.0-rc.1+62.sha-4d164b6');
 
 /**
  * @license
