1bfdcbf14950139b3d25e1c22e50ce0886c0316e
Misko Hevery <misko@hevery.com>
2018-10-17 19:20:30 +0000

    4b52257 fix(docs-infra): fix top menu logo position (#26473)

--- fesm5/http.js
+++ fesm5/http.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.0.0-rc.1+58.sha-34aff0b
+ * @license Angular v7.0.0-rc.1+60.sha-4b52257
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1835,7 +1835,7 @@ var JsonpModule = /** @class */ (function () {
 /**
  * @deprecated see https://angular.io/guide/http
  */
-var VERSION = new Version('7.0.0-rc.1+58.sha-34aff0b');
+var VERSION = new Version('7.0.0-rc.1+60.sha-4b52257');
 
 /**
  * @license
