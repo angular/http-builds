44b87dcc2dabe5e54cb6f7d8daa27be14ec3bb68
Miško Hevery <misko@hevery.com>
2018-10-17 03:48:36 +0000

    2326b9c fix(service-worker): clean up caches from old SW versions (#26319)

--- fesm5/http.js
+++ fesm5/http.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.0.0-rc.1+43.sha-e76a570
+ * @license Angular v7.0.0-rc.1+55.sha-2326b9c
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1835,7 +1835,7 @@ var JsonpModule = /** @class */ (function () {
 /**
  * @deprecated see https://angular.io/guide/http
  */
-var VERSION = new Version('7.0.0-rc.1+43.sha-e76a570');
+var VERSION = new Version('7.0.0-rc.1+55.sha-2326b9c');
 
 /**
  * @license
