0cb51909fa1ba30e53affcc1978a3633a97001cf
Misko Hevery <misko@hevery.com>
2018-10-17 04:01:17 +0000

    fdfedce feat(router): add prioritizedGuardValue operator optimization and allowing UrlTree return from guard (#26478)

--- bundles/http.umd.js
+++ bundles/http.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.0.0-rc.1+61.sha-371ffef
+ * @license Angular v7.0.0-rc.1+60.sha-fdfedce
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1876,7 +1876,7 @@
     /**
      * @deprecated see https://angular.io/guide/http
      */
-    var VERSION = new core.Version('7.0.0-rc.1+61.sha-371ffef');
+    var VERSION = new core.Version('7.0.0-rc.1+60.sha-fdfedce');
 
     /**
      * @license
