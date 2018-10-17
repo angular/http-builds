d5b1af4ad89aaeeaf3ddc833206405e385892f39
Misko Hevery <misko@hevery.com>
2018-10-17 04:09:28 +0000

    fa8e633 feat(ivy): enhance [style] and [class] bindings to be animation aware (#26096)

--- fesm5/http.js
+++ fesm5/http.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.0.0-rc.1+62.sha-4d164b6
+ * @license Angular v7.0.0-rc.1+64.sha-fa8e633
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1835,7 +1835,7 @@ var JsonpModule = /** @class */ (function () {
 /**
  * @deprecated see https://angular.io/guide/http
  */
-var VERSION = new Version('7.0.0-rc.1+62.sha-4d164b6');
+var VERSION = new Version('7.0.0-rc.1+64.sha-fa8e633');
 
 /**
  * @license
