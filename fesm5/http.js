319b1bcf5c2753f1ff7cdb1a13dc02a8fe0969ea
Misko Hevery <misko@hevery.com>
2018-10-17 19:25:05 +0000

    b29e709 fix(docs-infra): rename "title" by "header" to avoid unwanted tooltips (#26396)

--- fesm5/http.js
+++ fesm5/http.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.0.0-rc.1+64.sha-d769b44
+ * @license Angular v7.0.0-rc.1+62.sha-b29e709
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1835,7 +1835,7 @@ var JsonpModule = /** @class */ (function () {
 /**
  * @deprecated see https://angular.io/guide/http
  */
-var VERSION = new Version('7.0.0-rc.1+64.sha-d769b44');
+var VERSION = new Version('7.0.0-rc.1+62.sha-b29e709');
 
 /**
  * @license
