2af870cdfdc92a81b67f20df8018785243577a3d
Alex Rickabaugh <alx+alxhub@alxandria.net>
2018-10-18 19:44:53 +0000

    8d28fe9 docs: update http to https where possible (#26509)

--- fesm2015/http.js
+++ fesm2015/http.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.0.0+1.sha-a403c4f
+ * @license Angular v7.0.0+4.sha-8d28fe9
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1760,7 +1760,7 @@ JsonpModule = __decorate([
 /**
  * @deprecated see https://angular.io/guide/http
  */
-const VERSION = new Version('7.0.0+1.sha-a403c4f');
+const VERSION = new Version('7.0.0+4.sha-8d28fe9');
 
 /**
  * @license
