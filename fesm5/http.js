270a83fcd522eca661e74948e3991b770e7c12a3
Alex Rickabaugh <alx+alxhub@alxandria.net>
2018-10-18 19:44:29 +0000

    7634c1c docs: fix links to browserlist (#26531)

--- fesm5/http.js
+++ fesm5/http.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.0.0-rc.1+98.sha-1a666de
+ * @license Angular v7.0.0-rc.1+97.sha-7634c1c
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1835,7 +1835,7 @@ var JsonpModule = /** @class */ (function () {
 /**
  * @deprecated see https://angular.io/guide/http
  */
-var VERSION = new Version('7.0.0-rc.1+98.sha-1a666de');
+var VERSION = new Version('7.0.0-rc.1+97.sha-7634c1c');
 
 /**
  * @license
