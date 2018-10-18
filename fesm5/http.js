a78990a8af37f91c8c81c48458a3fcbe08c0933e
Alex Rickabaugh <alx+alxhub@alxandria.net>
2018-10-18 19:45:57 +0000

    d0a1e42 ci: add forms-overview guide to forms approval list (#26549)

--- fesm5/http.js
+++ fesm5/http.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.0.0+2.sha-32c018c
+ * @license Angular v7.0.0+6.sha-d0a1e42
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1835,7 +1835,7 @@ var JsonpModule = /** @class */ (function () {
 /**
  * @deprecated see https://angular.io/guide/http
  */
-var VERSION = new Version('7.0.0+2.sha-32c018c');
+var VERSION = new Version('7.0.0+6.sha-d0a1e42');
 
 /**
  * @license
