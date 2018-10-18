6dfd8945df38d28eb0b5d89ec001b3bc44dc6efb
Miško Hevery <misko@hevery.com>
2018-10-18 17:26:40 +0000

    d557f1d build(docs-infra): update git ref for cli command docs (#26545)

--- fesm5/http.js
+++ fesm5/http.js
@@ -1,10 +1,10 @@
 /**
- * @license Angular v7.0.0-rc.1+87.sha-665627e
+ * @license Angular v7.0.0-rc.1+88.sha-d557f1d
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
 
-import { __decorate, __read, __metadata, __extends, __spread } from 'tslib';
+import { __decorate, __metadata, __extends, __read, __spread } from 'tslib';
 import { Injectable, NgModule, Version } from '@angular/core';
 import { Observable } from 'rxjs';
 import { ɵgetDOM } from '@angular/platform-browser';
@@ -1835,7 +1835,7 @@ var JsonpModule = /** @class */ (function () {
 /**
  * @deprecated see https://angular.io/guide/http
  */
-var VERSION = new Version('7.0.0-rc.1+87.sha-665627e');
+var VERSION = new Version('7.0.0-rc.1+88.sha-d557f1d');
 
 /**
  * @license
