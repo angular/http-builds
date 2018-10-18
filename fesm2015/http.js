686669590af61e306926e385c4544599e466afc3
Alex Rickabaugh <alx+alxhub@alxandria.net>
2018-10-18 19:44:14 +0000

    a403c4f docs: type fix (#26386)

--- fesm2015/http.js
+++ fesm2015/http.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.0.0
+ * @license Angular v7.0.0+1.sha-a403c4f
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1760,7 +1760,7 @@ JsonpModule = __decorate([
 /**
  * @deprecated see https://angular.io/guide/http
  */
-const VERSION = new Version('7.0.0');
+const VERSION = new Version('7.0.0+1.sha-a403c4f');
 
 /**
  * @license
