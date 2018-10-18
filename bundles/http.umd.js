a45a3aa74e63c4f414ce1cd598edf30c08ba6764
Alex Rickabaugh <alx+alxhub@alxandria.net>
2018-10-18 19:46:50 +0000

    4f14a09 docs: fix links to browserlist (#26531)

--- bundles/http.umd.js
+++ bundles/http.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.0.0+6.sha-d0a1e42
+ * @license Angular v7.0.0+5.sha-4f14a09
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1876,7 +1876,7 @@
     /**
      * @deprecated see https://angular.io/guide/http
      */
-    var VERSION = new core.Version('7.0.0+6.sha-d0a1e42');
+    var VERSION = new core.Version('7.0.0+5.sha-4f14a09');
 
     /**
      * @license
