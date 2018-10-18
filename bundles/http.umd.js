3d639bd057de6d9b8df767cae6346a62e892a125
Miško Hevery <misko@hevery.com>
2018-10-18 17:30:33 +0000

    3b183ce build(docs-infra): allow `""` as empty region in `{@example}` tags (#26514)

--- bundles/http.umd.js
+++ bundles/http.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.0.0-rc.1+75.sha-1035c6a
+ * @license Angular v7.0.0-rc.1+78.sha-3b183ce
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1876,7 +1876,7 @@
     /**
      * @deprecated see https://angular.io/guide/http
      */
-    var VERSION = new core.Version('7.0.0-rc.1+75.sha-1035c6a');
+    var VERSION = new core.Version('7.0.0-rc.1+78.sha-3b183ce');
 
     /**
      * @license
