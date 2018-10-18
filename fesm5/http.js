48b16d1c064464cc6797f19abf5a31e24aa2c865
Alex Rickabaugh <alx+alxhub@alxandria.net>
2018-10-18 19:43:55 +0000

    32c018c docs: removing errant double-quote (#26483) PR Close #26483

--- fesm5/http.js
+++ fesm5/http.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.0.0+4.sha-8d28fe9
+ * @license Angular v7.0.0+2.sha-32c018c
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1835,7 +1835,7 @@ var JsonpModule = /** @class */ (function () {
 /**
  * @deprecated see https://angular.io/guide/http
  */
-var VERSION = new Version('7.0.0+4.sha-8d28fe9');
+var VERSION = new Version('7.0.0+2.sha-32c018c');
 
 /**
  * @license
