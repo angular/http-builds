a67758a2f92ce67a0a722de91128cbbaaeaec851
Alex Rickabaugh <arick@arick-macbookpro2.roam.corp.google.com>
2018-10-19 20:56:08 +0000

    31022cb feat(ivy): generate .ngsummary.js shims (#26495)

--- fesm5/http.js
+++ fesm5/http.js
@@ -1,10 +1,10 @@
 /**
- * @license Angular v7.0.0-rc.1+115.sha-1700bd6
+ * @license Angular v7.0.0-rc.1+118.sha-31022cb
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
 
-import { __decorate, __metadata, __extends, __read, __spread } from 'tslib';
+import { __decorate, __metadata, __read, __extends, __spread } from 'tslib';
 import { Injectable, NgModule, Version } from '@angular/core';
 import { Observable } from 'rxjs';
 import { ɵgetDOM } from '@angular/platform-browser';
@@ -1835,7 +1835,7 @@ var JsonpModule = /** @class */ (function () {
 /**
  * @deprecated see https://angular.io/guide/http
  */
-var VERSION = new Version('7.0.0-rc.1+115.sha-1700bd6');
+var VERSION = new Version('7.0.0-rc.1+118.sha-31022cb');
 
 /**
  * @license
