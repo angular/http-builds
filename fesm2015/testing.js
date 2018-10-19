86c159c21b40cfaf044e5e2841ea53a2dc22b1e3
Alex Rickabaugh <arick@arick-macbookpro2.roam.corp.google.com>
2018-10-19 22:00:26 +0000

    4c2ce4e docs: update animations to use `@publicApi` tags (#26595)

--- fesm2015/testing.js
+++ fesm2015/testing.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.0.0-rc.1+118.sha-31022cb
+ * @license Angular v7.0.0-rc.1+137.sha-4c2ce4e
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -38,6 +38,7 @@ import { take } from 'rxjs/operators';
  * ```
  *
  * @deprecated see https://angular.io/guide/http
+ * \@publicApi
  */
 class MockConnection {
     /**
@@ -176,6 +177,7 @@ class MockConnection {
  * ```
  *
  * @deprecated see https://angular.io/guide/http
+ * \@publicApi
  */
 class MockBackend {
     constructor() {
