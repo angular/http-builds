4d8292891f5f8e9d9b5542732ffb2647ad43e68e
Igor Minar <igor@angular.io>
2018-10-21 04:26:09 +0000

    100c7ef build: fix ivy package-dist generation (#26629)

--- fesm5/http.js
+++ fesm5/http.js
@@ -1,10 +1,10 @@
 /**
- * @license Angular v7.0.0-rc.1+137.sha-4c2ce4e
+ * @license Angular v7.0.0-rc.1+156.sha-100c7ef.with-local-changes
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
 
-import { __decorate, __metadata, __read, __extends, __spread } from 'tslib';
+import { __decorate, __metadata, __extends, __read, __spread } from 'tslib';
 import { Injectable, NgModule, Version } from '@angular/core';
 import { Observable } from 'rxjs';
 import { ɵgetDOM } from '@angular/platform-browser';
@@ -1862,7 +1862,7 @@ var JsonpModule = /** @class */ (function () {
  * @deprecated see https://angular.io/guide/http
  * @publicApi
  */
-var VERSION = new Version('7.0.0-rc.1+137.sha-4c2ce4e');
+var VERSION = new Version('7.0.0-rc.1+156.sha-100c7ef.with-local-changes');
 
 /**
  * @license
