71fb41496a2de514764838b22157297a9152965c
Matias Niemelä <matias@yearofmoo.com>
2018-10-24 23:04:04 +0000

    f6c2db8 fix(ivy): ensure styling pipes are allocated before used in bindings (#26593)

--- fesm5/http.js
+++ fesm5/http.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.1.0-beta.0
+ * @license Angular v7.1.0-beta.0+1.sha-f6c2db8
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1804,7 +1804,7 @@ var JsonpModule = /** @class */ (function () {
  * @deprecated see https://angular.io/guide/http
  * @publicApi
  */
-var VERSION = new Version('7.1.0-beta.0');
+var VERSION = new Version('7.1.0-beta.0+1.sha-f6c2db8');
 
 /**
  * @license
