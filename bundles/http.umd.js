95f4e8a275a3ebc6f798511194712a8d70eab8bf
Matias Niemelä <matias@yearofmoo.com>
2018-10-26 21:55:24 +0000

    d50d400 build: fix formatting issue in host_binding_spec file

--- bundles/http.umd.js
+++ bundles/http.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.1.0-beta.0+35.sha-19fcfc3
+ * @license Angular v7.1.0-beta.0+36.sha-d50d400
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1830,7 +1830,7 @@
      * @deprecated see https://angular.io/guide/http
      * @publicApi
      */
-    var VERSION = new i0.Version('7.1.0-beta.0+35.sha-19fcfc3');
+    var VERSION = new i0.Version('7.1.0-beta.0+36.sha-d50d400');
 
     /**
      * @license
--- fesm5/http.js
+++ fesm5/http.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.1.0-beta.0+35.sha-19fcfc3
+ * @license Angular v7.1.0-beta.0+36.sha-d50d400
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1804,7 +1804,7 @@ var JsonpModule = /** @class */ (function () {
  * @deprecated see https://angular.io/guide/http
  * @publicApi
  */
-var VERSION = new Version('7.1.0-beta.0+35.sha-19fcfc3');
+var VERSION = new Version('7.1.0-beta.0+36.sha-d50d400');
 
 /**
  * @license
