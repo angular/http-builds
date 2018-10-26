a0a9fc73609c7c69e5d613bf157be35605b9bb2c
Matias Niemelä <matias@yearofmoo.com>
2018-10-26 17:40:15 +0000

    7c730fe build(docs-infra): include inherited members in search index (#26676)

--- fesm5/http.js
+++ fesm5/http.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.1.0-beta.0+18.sha-aefa06f
+ * @license Angular v7.1.0-beta.0+20.sha-7c730fe
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1804,7 +1804,7 @@ var JsonpModule = /** @class */ (function () {
  * @deprecated see https://angular.io/guide/http
  * @publicApi
  */
-var VERSION = new Version('7.1.0-beta.0+18.sha-aefa06f');
+var VERSION = new Version('7.1.0-beta.0+20.sha-7c730fe');
 
 /**
  * @license
