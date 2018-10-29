2b5e3c5087b464c23c0cf3efd4745c7ef68fffce
Matias Niemelä <matias@yearofmoo.com>
2018-10-29 18:24:03 +0000

    2a86927 fix(ivy): move HostListeners generation to factory function (#26480)

--- bundles/http.umd.js
+++ bundles/http.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.1.0-beta.0+55.sha-c0bf222
+ * @license Angular v7.1.0-beta.0+56.sha-2a86927
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1830,7 +1830,7 @@
      * @deprecated see https://angular.io/guide/http
      * @publicApi
      */
-    var VERSION = new i0.Version('7.1.0-beta.0+55.sha-c0bf222');
+    var VERSION = new i0.Version('7.1.0-beta.0+56.sha-2a86927');
 
     /**
      * @license
