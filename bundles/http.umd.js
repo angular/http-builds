1e74b11a97b1c51b4af97b75ed0a81cd64e2680a
Matias Niemelä <matias@yearofmoo.com>
2018-10-29 17:33:24 +0000

    c0bf222 ci: exclude ngcc from g3sync check (#26740)

--- bundles/http.umd.js
+++ bundles/http.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.1.0-beta.0+54.sha-d72d50d
+ * @license Angular v7.1.0-beta.0+55.sha-c0bf222
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1830,7 +1830,7 @@
      * @deprecated see https://angular.io/guide/http
      * @publicApi
      */
-    var VERSION = new i0.Version('7.1.0-beta.0+54.sha-d72d50d');
+    var VERSION = new i0.Version('7.1.0-beta.0+55.sha-c0bf222');
 
     /**
      * @license
