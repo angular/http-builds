023ee6edf7bd3c2c88b03d0c4c336c14305525b7
Matias Niemelä <matias@yearofmoo.com>
2018-10-30 18:21:14 +0000

    91d2368 docs: update example dependencies to Angular/CLI/Universal V7 releases (#26820)

--- fesm5/http.js
+++ fesm5/http.js
@@ -1,10 +1,10 @@
 /**
- * @license Angular v7.1.0-beta.0+59.sha-578e4c7
+ * @license Angular v7.1.0-beta.0+61.sha-91d2368
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
 
-import { __decorate, __metadata, __read, __extends, __spread } from 'tslib';
+import { __decorate, __metadata, __extends, __read, __spread } from 'tslib';
 import { Injectable, NgModule, Version } from '@angular/core';
 import { Observable } from 'rxjs';
 import { ɵgetDOM } from '@angular/platform-browser';
@@ -1862,7 +1862,7 @@ var JsonpModule = /** @class */ (function () {
  * @deprecated see https://angular.io/guide/http
  * @publicApi
  */
-var VERSION = new Version('7.1.0-beta.0+59.sha-578e4c7');
+var VERSION = new Version('7.1.0-beta.0+61.sha-91d2368');
 
 /**
  * @license
