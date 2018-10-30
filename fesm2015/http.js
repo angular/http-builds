a94025c053e917bb4ae94a2198aa1eb9d89f17f9
Matias Niemelä <matias@yearofmoo.com>
2018-10-30 18:26:29 +0000

    23648b0 docs: configuration file reference (#26484)

--- fesm2015/http.js
+++ fesm2015/http.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.1.0-beta.0+61.sha-91d2368
+ * @license Angular v7.1.0-beta.0+62.sha-23648b0
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2125,7 +2125,7 @@ JsonpModule.decorators = [
  * @deprecated see https://angular.io/guide/http
  * \@publicApi
   @type {?} */
-const VERSION = new Version('7.1.0-beta.0+61.sha-91d2368');
+const VERSION = new Version('7.1.0-beta.0+62.sha-23648b0');
 
 /**
  * @fileoverview added by tsickle
