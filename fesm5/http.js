9a33c3131d488affc04e3f8df8f680a122ff358c
Matias Niemelä <matias@yearofmoo.com>
2018-10-30 20:54:43 +0000

    634e9e9 docs: Removing duplicate lines (#26754)

--- fesm5/http.js
+++ fesm5/http.js
@@ -1,10 +1,10 @@
 /**
- * @license Angular v7.1.0-beta.0+70.sha-87a7f2d
+ * @license Angular v7.1.0-beta.0+72.sha-634e9e9
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
 
-import { __read, __decorate, __metadata, __extends, __spread } from 'tslib';
+import { __decorate, __metadata, __extends, __read, __spread } from 'tslib';
 import { Injectable, NgModule, Version } from '@angular/core';
 import { Observable } from 'rxjs';
 import { ɵgetDOM } from '@angular/platform-browser';
@@ -1862,7 +1862,7 @@ var JsonpModule = /** @class */ (function () {
  * @deprecated see https://angular.io/guide/http
  * @publicApi
  */
-var VERSION = new Version('7.1.0-beta.0+70.sha-87a7f2d');
+var VERSION = new Version('7.1.0-beta.0+72.sha-634e9e9');
 
 /**
  * @license
