9c5f01812d7960ce36fd3e8d155c8b5f787985d0
Matias Niemelä <matias@yearofmoo.com>
2018-10-31 23:28:42 +0000

    ec29fd3 build: remove problematic `yarn` prefix for running bazel

--- fesm5/http.js
+++ fesm5/http.js
@@ -1,11 +1,11 @@
 /**
- * @license Angular v7.1.0-beta.0+77.sha-d042c4a
+ * @license Angular v7.1.0-beta.0+78.sha-ec29fd3
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
 
 import { Version, defineInjectable, ɵdefineNgModule, defineInjector, inject } from '@angular/core';
-import { __extends, __read, __spread } from 'tslib';
+import { __extends, __spread, __read } from 'tslib';
 import { Observable } from 'rxjs';
 import { ɵgetDOM } from '@angular/platform-browser';
 
@@ -1804,7 +1804,7 @@ var JsonpModule = /** @class */ (function () {
  * @deprecated see https://angular.io/guide/http
  * @publicApi
  */
-var VERSION = new Version('7.1.0-beta.0+77.sha-d042c4a');
+var VERSION = new Version('7.1.0-beta.0+78.sha-ec29fd3');
 
 /**
  * @license
