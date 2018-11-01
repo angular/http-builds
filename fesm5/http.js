8bc2bdc108518cd8408e007d9838b40d5772520c
Kara Erickson <karakara@google.com>
2018-11-01 21:13:25 +0000

    eb5d308 build: update `canonical-path` dependency (#26719)

--- fesm5/http.js
+++ fesm5/http.js
@@ -1,10 +1,10 @@
 /**
- * @license Angular v7.1.0-beta.1+18.sha-683d53d
+ * @license Angular v7.1.0-beta.1+19.sha-eb5d308
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
 
-import { __decorate, __metadata, __read, __extends, __spread } from 'tslib';
+import { __decorate, __metadata, __extends, __read, __spread } from 'tslib';
 import { Injectable, NgModule, Version } from '@angular/core';
 import { Observable } from 'rxjs';
 import { ɵgetDOM } from '@angular/platform-browser';
@@ -1862,7 +1862,7 @@ var JsonpModule = /** @class */ (function () {
  * @deprecated see https://angular.io/guide/http
  * @publicApi
  */
-var VERSION = new Version('7.1.0-beta.1+18.sha-683d53d');
+var VERSION = new Version('7.1.0-beta.1+19.sha-eb5d308');
 
 /**
  * @license
