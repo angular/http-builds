802d7d6f1205728f1818ba4db3a5cee35daeebd1
Kara Erickson <karakara@google.com>
2018-11-01 21:48:00 +0000

    3b9bc73 fix(ivy): host bindings after dirs without host bindings should work (#26801)

--- fesm5/http.js
+++ fesm5/http.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.1.0-beta.1+31.sha-bc68b59
+ * @license Angular v7.1.0-beta.1+37.sha-3b9bc73
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1875,7 +1875,7 @@ var JsonpModule = /** @class */ (function () {
  * @deprecated see https://angular.io/guide/http
  * @publicApi
  */
-var VERSION = new Version('7.1.0-beta.1+31.sha-bc68b59');
+var VERSION = new Version('7.1.0-beta.1+37.sha-3b9bc73');
 
 /**
  * @license
