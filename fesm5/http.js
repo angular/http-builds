4563e8a62d021df6efe83ebf9b6feb4074c7a269
GitHub <noreply@github.com>
2018-11-01 00:20:59 +0000

    904b91d fixup! docs: add `Function Inlining` rules to `PERF_NOTES.md`

--- fesm5/http.js
+++ fesm5/http.js
@@ -1,10 +1,10 @@
 /**
- * @license Angular v7.1.0-beta.0+84.sha-094b9fc
+ * @license Angular v7.1.0-beta.0+85.sha-904b91d
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
 
-import { __decorate, __metadata, __read, __extends, __spread } from 'tslib';
+import { __decorate, __metadata, __extends, __read, __spread } from 'tslib';
 import { Injectable, NgModule, Version } from '@angular/core';
 import { Observable } from 'rxjs';
 import { ɵgetDOM } from '@angular/platform-browser';
@@ -1862,7 +1862,7 @@ var JsonpModule = /** @class */ (function () {
  * @deprecated see https://angular.io/guide/http
  * @publicApi
  */
-var VERSION = new Version('7.1.0-beta.0+84.sha-094b9fc');
+var VERSION = new Version('7.1.0-beta.0+85.sha-904b91d');
 
 /**
  * @license
