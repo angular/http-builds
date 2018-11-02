85213bee90ce13f66847129b8a9f0a59fbee8ced
Kara Erickson <karakara@google.com>
2018-11-02 20:41:46 +0000

    f536261 docs: minor edits and corrections to cli intro (#26654)

--- fesm2015/http.js
+++ fesm2015/http.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.0.2+18.sha-315d95c
+ * @license Angular v7.0.2+20.sha-f536261
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2125,7 +2125,7 @@ JsonpModule.decorators = [
  * @deprecated see https://angular.io/guide/http
  * \@publicApi
   @type {?} */
-const VERSION = new Version('7.0.2+18.sha-315d95c');
+const VERSION = new Version('7.0.2+20.sha-f536261');
 
 /**
  * @fileoverview added by tsickle
