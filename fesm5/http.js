55ced3627070d396fd75543e97c782abba3df334
Kara Erickson <karakara@google.com>
2018-11-02 18:08:12 +0000

    c016066 fix(ivy): ngcc should not break lifecycle hooks (#26856)

--- fesm5/http.js
+++ fesm5/http.js
@@ -1,10 +1,10 @@
 /**
- * @license Angular v7.1.0-beta.1+60.sha-beabfb7
+ * @license Angular v7.1.0-beta.1+61.sha-c016066
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
 
-import { __read, __decorate, __metadata, __extends, __spread } from 'tslib';
+import { __decorate, __metadata, __read, __extends, __spread } from 'tslib';
 import { Injectable, NgModule, Version } from '@angular/core';
 import { Observable } from 'rxjs';
 import { ɵgetDOM } from '@angular/platform-browser';
@@ -1862,7 +1862,7 @@ var JsonpModule = /** @class */ (function () {
  * @deprecated see https://angular.io/guide/http
  * @publicApi
  */
-var VERSION = new Version('7.1.0-beta.1+60.sha-beabfb7');
+var VERSION = new Version('7.1.0-beta.1+61.sha-c016066');
 
 /**
  * @license
