ae04778ebb81a5decfdc754b23c70d87e2214730
Kara Erickson <karakara@google.com>
2018-11-05 18:18:09 +0000

    d568d7a ci: make `integration_test` job logs less verbose (#26929)

--- fesm5/http.js
+++ fesm5/http.js
@@ -1,10 +1,10 @@
 /**
- * @license Angular v7.1.0-beta.1+68.sha-86d41a1
+ * @license Angular v7.1.0-beta.1+69.sha-d568d7a
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
 
-import { __decorate, __metadata, __read, __extends, __spread } from 'tslib';
+import { __decorate, __metadata, __extends, __read, __spread } from 'tslib';
 import { Injectable, NgModule, Version } from '@angular/core';
 import { Observable } from 'rxjs';
 import { ɵgetDOM } from '@angular/platform-browser';
@@ -1862,7 +1862,7 @@ var JsonpModule = /** @class */ (function () {
  * @deprecated see https://angular.io/guide/http
  * @publicApi
  */
-var VERSION = new Version('7.1.0-beta.1+68.sha-86d41a1');
+var VERSION = new Version('7.1.0-beta.1+69.sha-d568d7a');
 
 /**
  * @license
