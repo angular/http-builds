f057cd415f10283b423c4ae377ff1003189ac54e
Kara Erickson <karakara@google.com>
2018-11-05 23:30:09 +0000

    14ff340 docs(forms): update reactive form directives API reference (#26823)

--- fesm5/http.js
+++ fesm5/http.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.0.2+24.sha-c8e7507
+ * @license Angular v7.0.2+25.sha-14ff340
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1862,7 +1862,7 @@ var JsonpModule = /** @class */ (function () {
  * @deprecated see https://angular.io/guide/http
  * @publicApi
  */
-var VERSION = new Version('7.0.2+24.sha-c8e7507');
+var VERSION = new Version('7.0.2+25.sha-14ff340');
 
 /**
  * @license
