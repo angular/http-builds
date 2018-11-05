89186dafcc6ca7af2900db857848001ea098024a
Kara Erickson <karakara@google.com>
2018-11-05 18:15:44 +0000

    e6a0c45 style(compiler): typo fix (#26934) PR Close #26934

--- fesm5/http.js
+++ fesm5/http.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.1.0-beta.1+66.sha-7d2a746
+ * @license Angular v7.1.0-beta.1+67.sha-e6a0c45
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1862,7 +1862,7 @@ var JsonpModule = /** @class */ (function () {
  * @deprecated see https://angular.io/guide/http
  * @publicApi
  */
-var VERSION = new Version('7.1.0-beta.1+66.sha-7d2a746');
+var VERSION = new Version('7.1.0-beta.1+67.sha-e6a0c45');
 
 /**
  * @license
