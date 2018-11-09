5d9d28ba1f0c9fec32b6716a9059ce91a63e012b
Andrew Kushnir <akushnir@google.com>
2018-11-09 18:11:00 +0000

    cbd4206 docs(core): cleanup todo notes in ContentChild documentation examples (#26543)

--- fesm5/http.js
+++ fesm5/http.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.0.3+12.sha-f33307b
+ * @license Angular v7.0.3+13.sha-cbd4206
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1862,7 +1862,7 @@ var JsonpModule = /** @class */ (function () {
  * @deprecated see https://angular.io/guide/http
  * @publicApi
  */
-var VERSION = new Version('7.0.3+12.sha-f33307b');
+var VERSION = new Version('7.0.3+13.sha-cbd4206');
 
 /**
  * @license
