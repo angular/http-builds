bd6e55e5f58aaee3e2bf9a11219dec60ef3d3ab0
Andrew Kushnir <akushnir@google.com>
2018-11-09 18:07:36 +0000

    fb0d93d docs: override code formatting for CLI commands (#26619)

--- fesm2015/http.js
+++ fesm2015/http.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.0.3+8.sha-682034f
+ * @license Angular v7.0.3+10.sha-fb0d93d
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2125,7 +2125,7 @@ JsonpModule.decorators = [
  * @deprecated see https://angular.io/guide/http
  * \@publicApi
   @type {?} */
-const VERSION = new Version('7.0.3+8.sha-682034f');
+const VERSION = new Version('7.0.3+10.sha-fb0d93d');
 
 /**
  * @fileoverview added by tsickle
