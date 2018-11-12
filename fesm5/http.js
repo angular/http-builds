399eb072a16e9aa5448de5b2efc38ccdfa13e0a8
Andrew Kushnir <akushnir@google.com>
2018-11-12 21:17:14 +0000

    53197de docs: Fix wrong quoting in SSR guide (#27057) PR Close #27057

--- fesm5/http.js
+++ fesm5/http.js
@@ -1,10 +1,10 @@
 /**
- * @license Angular v7.0.3+14.sha-7a1b876
+ * @license Angular v7.0.3+17.sha-53197de
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
 
-import { __decorate, __metadata, __extends, __read, __spread } from 'tslib';
+import { __decorate, __metadata, __read, __extends, __spread } from 'tslib';
 import { Injectable, NgModule, Version } from '@angular/core';
 import { Observable } from 'rxjs';
 import { ɵgetDOM } from '@angular/platform-browser';
@@ -1862,7 +1862,7 @@ var JsonpModule = /** @class */ (function () {
  * @deprecated see https://angular.io/guide/http
  * @publicApi
  */
-var VERSION = new Version('7.0.3+14.sha-7a1b876');
+var VERSION = new Version('7.0.3+17.sha-53197de');
 
 /**
  * @license
