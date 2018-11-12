d0bdcaa34ebb91fcb21ff41edc761aff5aaffd6a
Andrew Kushnir <akushnir@google.com>
2018-11-12 21:23:01 +0000

    f80c600 docs: Fix wrong quoting in SSR guide (#27057) PR Close #27057

--- fesm5/http.js
+++ fesm5/http.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.1.0-beta.2+22.sha-99c5db1
+ * @license Angular v7.1.0-beta.2+26.sha-f80c600
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1862,7 +1862,7 @@ var JsonpModule = /** @class */ (function () {
  * @deprecated see https://angular.io/guide/http
  * @publicApi
  */
-var VERSION = new Version('7.1.0-beta.2+22.sha-99c5db1');
+var VERSION = new Version('7.1.0-beta.2+26.sha-f80c600');
 
 /**
  * @license
