ecbd957a8d7b20ebb3dd03c38b67e0c60c4d6811
Andrew Kushnir <akushnir@google.com>
2018-11-13 23:14:22 +0000

    a63fd2d feat(ivy): support for i18n & ICU expressions (#26275)

--- fesm5/http.js
+++ fesm5/http.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.1.0-beta.2+42.sha-d97994b
+ * @license Angular v7.1.0-beta.2+43.sha-a63fd2d
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1875,7 +1875,7 @@ var JsonpModule = /** @class */ (function () {
  * @deprecated see https://angular.io/guide/http
  * @publicApi
  */
-var VERSION = new Version('7.1.0-beta.2+42.sha-d97994b');
+var VERSION = new Version('7.1.0-beta.2+43.sha-a63fd2d');
 
 /**
  * @license
