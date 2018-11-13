d96daf3defe11e82f89f4de05f6de153845ab426
Andrew Kushnir <akushnir@google.com>
2018-11-13 19:15:23 +0000

    f5a0ec0 fix(ivy): ngcc should not fail on invalid package.json (#26539)

--- fesm5/http.js
+++ fesm5/http.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.1.0-beta.2+28.sha-095b6e8
+ * @license Angular v7.1.0-beta.2+30.sha-f5a0ec0
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1875,7 +1875,7 @@ var JsonpModule = /** @class */ (function () {
  * @deprecated see https://angular.io/guide/http
  * @publicApi
  */
-var VERSION = new Version('7.1.0-beta.2+28.sha-095b6e8');
+var VERSION = new Version('7.1.0-beta.2+30.sha-f5a0ec0');
 
 /**
  * @license
