89b2fe1ab92d25bc4ad3bd89bc1f994dd538cab6
Andrew Kushnir <akushnir@google.com>
2018-11-14 20:46:31 +0000

    9c7428e build(bazel): turn on --nolegacy-external-runfiles (#26770)

--- fesm2015/http.js
+++ fesm2015/http.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.0.3+23.sha-4348c47
+ * @license Angular v7.0.3+25.sha-9c7428e
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2125,7 +2125,7 @@ JsonpModule.decorators = [
  * @deprecated see https://angular.io/guide/http
  * \@publicApi
   @type {?} */
-const VERSION = new Version('7.0.3+23.sha-4348c47');
+const VERSION = new Version('7.0.3+25.sha-9c7428e');
 
 /**
  * @fileoverview added by tsickle
--- fesm5/http.js
+++ fesm5/http.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.0.3+23.sha-4348c47
+ * @license Angular v7.0.3+25.sha-9c7428e
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1862,7 +1862,7 @@ var JsonpModule = /** @class */ (function () {
  * @deprecated see https://angular.io/guide/http
  * @publicApi
  */
-var VERSION = new Version('7.0.3+23.sha-4348c47');
+var VERSION = new Version('7.0.3+25.sha-9c7428e');
 
 /**
  * @license
