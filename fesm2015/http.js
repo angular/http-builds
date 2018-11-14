ce5a30a88e02565f5111169adc196dfd1b0b50b7
Miško Hevery <misko@hevery.com>
2018-11-14 18:48:16 +0000

    f8f1168 Revert "feat(ivy): support for i18n & ICU expressions (#26275)"

--- fesm2015/http.js
+++ fesm2015/http.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.1.0-beta.2+45.sha-a752971
+ * @license Angular v7.1.0-beta.2+46.sha-f8f1168
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2281,7 +2281,7 @@ JsonpModule.ngInjectorDef = defineInjector({ factory: function JsonpModule_Facto
  * @deprecated see https://angular.io/guide/http
  * \@publicApi
   @type {?} */
-const VERSION = new Version('7.1.0-beta.2+45.sha-a752971');
+const VERSION = new Version('7.1.0-beta.2+46.sha-f8f1168');
 
 /**
  * @fileoverview added by tsickle
