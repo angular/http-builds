b3bf71d6ac9b4e4aec306ce657c0e5565d3bfe0d
Misko Hevery <misko@hevery.com>
2018-11-16 20:46:12 +0000

    b07bd30 feat(bazel): Bazel workspace schematics (#26971)

--- fesm5/http.js
+++ fesm5/http.js
@@ -1,10 +1,10 @@
 /**
- * @license Angular v7.1.0-rc.0+7.sha-4f965ad
+ * @license Angular v7.1.0-rc.0+8.sha-b07bd30
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
 
-import { __decorate, __metadata, __read, __extends, __spread } from 'tslib';
+import { __decorate, __metadata, __extends, __read, __spread } from 'tslib';
 import { Injectable, NgModule, Version } from '@angular/core';
 import { Observable } from 'rxjs';
 import { ɵgetDOM } from '@angular/platform-browser';
@@ -1862,7 +1862,7 @@ var JsonpModule = /** @class */ (function () {
  * @deprecated see https://angular.io/guide/http
  * @publicApi
  */
-var VERSION = new Version('7.1.0-rc.0+7.sha-4f965ad');
+var VERSION = new Version('7.1.0-rc.0+8.sha-b07bd30');
 
 /**
  * @license
