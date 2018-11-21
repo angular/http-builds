6b190732d34a6d7b0e66758b5eb3737d12500f8e
Miško Hevery <misko@hevery.com>
2018-11-21 16:10:51 +0000

    a72250b build(bazel): Add Bazel builders (#27141)

--- bundles/http.umd.js
+++ bundles/http.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.1.0-rc.0+31.sha-026b7e3
+ * @license Angular v7.1.0-rc.0+32.sha-a72250b
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1901,7 +1901,7 @@
      * @deprecated see https://angular.io/guide/http
      * @publicApi
      */
-    var VERSION = new i0.Version('7.1.0-rc.0+31.sha-026b7e3');
+    var VERSION = new i0.Version('7.1.0-rc.0+32.sha-a72250b');
 
     /**
      * @license
