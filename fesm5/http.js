829bdee500316a161b16279d7bc61de5573d8a1e
Miško Hevery <misko@hevery.com>
2018-11-21 17:44:34 +0000

    8ce59a5 test(ivy): run router tests with ivy on CI (#27195)

--- fesm5/http.js
+++ fesm5/http.js
@@ -1,10 +1,10 @@
 /**
- * @license Angular v7.1.0-rc.0+32.sha-a72250b
+ * @license Angular v7.1.0-rc.0+35.sha-8ce59a5
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
 
-import { __decorate, __metadata, __extends, __read, __spread } from 'tslib';
+import { __decorate, __metadata, __read, __extends, __spread } from 'tslib';
 import { Injectable, NgModule, Version } from '@angular/core';
 import { Observable } from 'rxjs';
 import { ɵgetDOM } from '@angular/platform-browser';
@@ -1862,7 +1862,7 @@ var JsonpModule = /** @class */ (function () {
  * @deprecated see https://angular.io/guide/http
  * @publicApi
  */
-var VERSION = new Version('7.1.0-rc.0+32.sha-a72250b');
+var VERSION = new Version('7.1.0-rc.0+35.sha-8ce59a5');
 
 /**
  * @license
