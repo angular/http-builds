c033702cfb3c37841da86f5619db7bf623f49d30
Miško Hevery <misko@hevery.com>
2018-11-21 17:46:35 +0000

    573fb78 docs: add formatting productivity hints to `DEVELOPER.md` (#27211)

--- fesm5/http.js
+++ fesm5/http.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.1.0-rc.0+37.sha-0d9b27f
+ * @license Angular v7.1.0-rc.0+34.sha-573fb78
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1875,7 +1875,7 @@ var JsonpModule = /** @class */ (function () {
  * @deprecated see https://angular.io/guide/http
  * @publicApi
  */
-var VERSION = new Version('7.1.0-rc.0+37.sha-0d9b27f');
+var VERSION = new Version('7.1.0-rc.0+34.sha-573fb78');
 
 /**
  * @license
