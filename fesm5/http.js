f0d4020fc616400ae0d24efab3c90749394837fd
Misko Hevery <misko@hevery.com>
2018-11-21 23:55:43 +0000

    dc300c5 feat(ivy): render flags support in host bindings function (FW-649) (#27204)

--- fesm5/http.js
+++ fesm5/http.js
@@ -1,10 +1,10 @@
 /**
- * @license Angular v7.1.0
+ * @license Angular v7.1.0+1.sha-dc300c5
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
 
-import { Injectable, Version, NgModule, defineInjectable, ɵsetClassMetadata, inject, ɵdefineNgModule, defineInjector } from '@angular/core';
+import { Injectable, Version, NgModule, defineInjectable, ɵsetClassMetadata, ɵdefineNgModule, defineInjector, inject } from '@angular/core';
 import { __extends, __spread, __read } from 'tslib';
 import { Observable } from 'rxjs';
 import { ɵgetDOM } from '@angular/platform-browser';
@@ -1875,7 +1875,7 @@ var JsonpModule = /** @class */ (function () {
  * @deprecated see https://angular.io/guide/http
  * @publicApi
  */
-var VERSION = new Version('7.1.0');
+var VERSION = new Version('7.1.0+1.sha-dc300c5');
 
 /**
  * @license
