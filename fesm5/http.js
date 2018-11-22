27f509304c95ea6af71c5295c724f0f7ac8ad68f
Filipe Silva <filipematossilva@gmail.com>
2018-11-22 15:32:09 +0000

    d3ddd31 ci: test ts-api-guardian on windows

--- fesm5/http.js
+++ fesm5/http.js
@@ -1,10 +1,10 @@
 /**
- * @license Angular v7.1.0-rc.0+17.sha-83b1c63.with-local-changes
+ * @license Angular v7.1.0-rc.0+17.sha-d3ddd31.with-local-changes
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
 
-import { __decorate, __metadata, __read, __extends, __spread } from 'tslib';
+import { __decorate, __metadata, __extends, __read, __spread } from 'tslib';
 import { Injectable, NgModule, Version } from '@angular/core';
 import { Observable } from 'rxjs';
 import { ɵgetDOM } from '@angular/platform-browser';
@@ -1862,7 +1862,7 @@ var JsonpModule = /** @class */ (function () {
  * @deprecated see https://angular.io/guide/http
  * @publicApi
  */
-var VERSION = new Version('7.1.0-rc.0+17.sha-83b1c63.with-local-changes');
+var VERSION = new Version('7.1.0-rc.0+17.sha-d3ddd31.with-local-changes');
 
 /**
  * @license
