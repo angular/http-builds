efe53911a4ad6b09d8b7953b3079b4340748b504
Jason Aden <jasonaden@google.com>
2018-11-26 20:43:58 +0000

    f45aedc build(docs-infra): upgrade `npm-run-all` to latest version for security (#27274)

--- fesm2015/http.js
+++ fesm2015/http.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.1.0+1.sha-dc300c5
+ * @license Angular v7.1.0+11.sha-f45aedc
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2125,7 +2125,7 @@ JsonpModule.decorators = [
  * @deprecated see https://angular.io/guide/http
  * \@publicApi
   @type {?} */
-const VERSION = new Version('7.1.0+1.sha-dc300c5');
+const VERSION = new Version('7.1.0+11.sha-f45aedc');
 
 /**
  * @fileoverview added by tsickle
