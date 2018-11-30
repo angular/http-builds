f3b7831ef3faf7e4d113eb9a9e7bf0b6e064ec43
Igor Minar <igor@angular.io>
2018-11-30 06:19:14 +0000

    eb99753 test(ivy): enable @angular/service-worker targets to run on CI (#27310)

--- fesm5/http.js
+++ fesm5/http.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.1.0+60.sha-1402175
+ * @license Angular v7.1.0+63.sha-eb99753
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1875,7 +1875,7 @@ var JsonpModule = /** @class */ (function () {
  * @deprecated see https://angular.io/guide/http
  * @publicApi
  */
-var VERSION = new Version('7.1.0+60.sha-1402175');
+var VERSION = new Version('7.1.0+63.sha-eb99753');
 
 /**
  * @license
