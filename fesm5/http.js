e95413fac61873e3f4ffb22cb4cda5c7386ac4d0
Igor Minar <igor@angular.io>
2018-11-30 06:20:09 +0000

    c331fc6 docs(forms): update API reference for common APIs and template-driven directives (#27033)

--- fesm5/http.js
+++ fesm5/http.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.1.0+62.sha-4effb89
+ * @license Angular v7.1.0+68.sha-c331fc6
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1862,7 +1862,7 @@ var JsonpModule = /** @class */ (function () {
  * @deprecated see https://angular.io/guide/http
  * @publicApi
  */
-var VERSION = new Version('7.1.0+62.sha-4effb89');
+var VERSION = new Version('7.1.0+68.sha-c331fc6');
 
 /**
  * @license
