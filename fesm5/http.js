07ad7c5ea428b4a05e788540010586661fc2bf9d
Igor Minar <igor@angular.io>
2018-11-30 06:14:14 +0000

    23bc8ed test: move remaining fixmeIvy to test level (#27354)

--- fesm5/http.js
+++ fesm5/http.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.1.0+56.sha-36e7bf1
+ * @license Angular v7.1.0+57.sha-23bc8ed
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1875,7 +1875,7 @@ var JsonpModule = /** @class */ (function () {
  * @deprecated see https://angular.io/guide/http
  * @publicApi
  */
-var VERSION = new Version('7.1.0+56.sha-36e7bf1');
+var VERSION = new Version('7.1.0+57.sha-23bc8ed');
 
 /**
  * @license
