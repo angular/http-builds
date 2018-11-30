38d5d535174a6779aacece061bca3be1a229459d
Igor Minar <igor@angular.io>
2018-11-30 06:11:58 +0000

    816ec0b refactor(ivy): treate LView as the primary global state (#27282)

--- fesm5/http.js
+++ fesm5/http.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.1.0+52.sha-2d51579
+ * @license Angular v7.1.0+55.sha-816ec0b
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1875,7 +1875,7 @@ var JsonpModule = /** @class */ (function () {
  * @deprecated see https://angular.io/guide/http
  * @publicApi
  */
-var VERSION = new Version('7.1.0+52.sha-2d51579');
+var VERSION = new Version('7.1.0+55.sha-816ec0b');
 
 /**
  * @license
