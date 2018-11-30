4840d9d2ebbca4114f3952125a38d9ff928ba0f6
Igor Minar <igor@angular.io>
2018-11-30 06:18:33 +0000

    4effb89 test(ivy): update //packages/compiler tests for Ivy (#27301)

--- fesm2015/http.js
+++ fesm2015/http.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.1.0+63.sha-eb99753
+ * @license Angular v7.1.0+62.sha-4effb89
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2125,7 +2125,7 @@ JsonpModule.decorators = [
  * @deprecated see https://angular.io/guide/http
  * \@publicApi
   @type {?} */
-const VERSION = new Version('7.1.0+63.sha-eb99753');
+const VERSION = new Version('7.1.0+62.sha-4effb89');
 
 /**
  * @fileoverview added by tsickle
