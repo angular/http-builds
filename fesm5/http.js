e9872718928e43c3960b326a66cafaf7a2ee11cf
Igor Minar <igor@angular.io>
2018-11-30 06:17:14 +0000

    1402175 test(upgrade): re-enable tests that have been fixed (#27305)

--- fesm5/http.js
+++ fesm5/http.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.1.0+59.sha-65775a3
+ * @license Angular v7.1.0+60.sha-1402175
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1875,7 +1875,7 @@ var JsonpModule = /** @class */ (function () {
  * @deprecated see https://angular.io/guide/http
  * @publicApi
  */
-var VERSION = new Version('7.1.0+59.sha-65775a3');
+var VERSION = new Version('7.1.0+60.sha-1402175');
 
 /**
  * @license
