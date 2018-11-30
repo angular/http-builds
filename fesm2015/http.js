2891e78a0310330f2d0dde8d241e02a6085c701e
Igor Minar <igor@angular.io>
2018-11-30 19:12:19 +0000

    1b84b11 Revert "fix(forms): apply unicode flag to pattern attribute when supported (#20819)" (#27375)

--- fesm2015/http.js
+++ fesm2015/http.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.1.0+72.sha-32c5be9
+ * @license Angular v7.1.0+75.sha-1b84b11
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2281,7 +2281,7 @@ JsonpModule.ngInjectorDef = defineInjector({ factory: function JsonpModule_Facto
  * @deprecated see https://angular.io/guide/http
  * \@publicApi
   @type {?} */
-const VERSION = new Version('7.1.0+72.sha-32c5be9');
+const VERSION = new Version('7.1.0+75.sha-1b84b11');
 
 /**
  * @fileoverview added by tsickle
