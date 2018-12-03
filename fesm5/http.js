46c03666380765ec0c4c658ae08e831ef234c945
Igor Minar <igor@angular.io>
2018-12-03 16:53:58 +0000

    931a636 docs: fix typo in toh-pt6.md (#27403)

--- fesm5/http.js
+++ fesm5/http.js
@@ -1,10 +1,10 @@
 /**
- * @license Angular v7.1.0+106.sha-a082f64
+ * @license Angular v7.1.0+107.sha-931a636
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
 
-import { __decorate, __metadata, __read, __extends, __spread } from 'tslib';
+import { __decorate, __metadata, __extends, __read, __spread } from 'tslib';
 import { Injectable, NgModule, Version } from '@angular/core';
 import { Observable } from 'rxjs';
 import { ɵgetDOM } from '@angular/platform-browser';
@@ -1862,7 +1862,7 @@ var JsonpModule = /** @class */ (function () {
  * @deprecated see https://angular.io/guide/http
  * @publicApi
  */
-var VERSION = new Version('7.1.0+106.sha-a082f64');
+var VERSION = new Version('7.1.0+107.sha-931a636');
 
 /**
  * @license
