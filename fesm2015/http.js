e1af4b1c6ed654a84957f6c60f0a077a0eb14870
Alex Rickabaugh <alx+alxhub@alxandria.net>
2018-12-03 23:11:17 +0000

    6552471 fix(ivy): account for `useValue: undefined` providers in module injector (#27035)

--- fesm2015/http.js
+++ fesm2015/http.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.1.0+114.sha-75723d5
+ * @license Angular v7.1.0+115.sha-6552471
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2281,7 +2281,7 @@ JsonpModule.ngInjectorDef = defineInjector({ factory: function JsonpModule_Facto
  * @deprecated see https://angular.io/guide/http
  * \@publicApi
   @type {?} */
-const VERSION = new Version('7.1.0+114.sha-75723d5');
+const VERSION = new Version('7.1.0+115.sha-6552471');
 
 /**
  * @fileoverview added by tsickle
