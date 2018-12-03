c4f9e7667f17837583e31f27b4432e852d9fefec
Alex Rickabaugh <alx+alxhub@alxandria.net>
2018-12-03 23:19:22 +0000

    0cf49c8 test(ivy): run router/upgrade tests with ivy on CI (#27410)

--- fesm5/http.js
+++ fesm5/http.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.1.0+111.sha-8eb102a
+ * @license Angular v7.1.0+116.sha-0cf49c8
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1875,7 +1875,7 @@ var JsonpModule = /** @class */ (function () {
  * @deprecated see https://angular.io/guide/http
  * @publicApi
  */
-var VERSION = new Version('7.1.0+111.sha-8eb102a');
+var VERSION = new Version('7.1.0+116.sha-0cf49c8');
 
 /**
  * @license
