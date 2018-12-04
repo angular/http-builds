8533299036ac4e80b324cc360cfae927ec620235
Igor Minar <igor@angular.io>
2018-12-04 22:39:10 +0000

    1597886 fix(ivy): resolve resources using TS module resolution semantics (#27357)

--- fesm5/http.js
+++ fesm5/http.js
@@ -1,10 +1,10 @@
 /**
- * @license Angular v7.1.0+128.sha-8e644d9
+ * @license Angular v7.1.0+127.sha-1597886
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
 
-import { __decorate, __metadata, __extends, __read, __spread } from 'tslib';
+import { __decorate, __metadata, __read, __extends, __spread } from 'tslib';
 import { Injectable, NgModule, Version } from '@angular/core';
 import { Observable } from 'rxjs';
 import { ɵgetDOM } from '@angular/platform-browser';
@@ -1862,7 +1862,7 @@ var JsonpModule = /** @class */ (function () {
  * @deprecated see https://angular.io/guide/http
  * @publicApi
  */
-var VERSION = new Version('7.1.0+128.sha-8e644d9');
+var VERSION = new Version('7.1.0+127.sha-1597886');
 
 /**
  * @license
