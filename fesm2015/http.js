9b7711dc205414f7ebdefab3262d9fed7728aa37
Igor Minar <igor@angular.io>
2018-12-05 21:14:29 +0000

    24ebdbd ci: test ts-api-guardian on windows (#27205)

--- fesm2015/http.js
+++ fesm2015/http.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.1.0+163.sha-821fecb
+ * @license Angular v7.1.0+164.sha-24ebdbd
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2125,7 +2125,7 @@ JsonpModule.decorators = [
  * @deprecated see https://angular.io/guide/http
  * \@publicApi
   @type {?} */
-const VERSION = new Version('7.1.0+163.sha-821fecb');
+const VERSION = new Version('7.1.0+164.sha-24ebdbd');
 
 /**
  * @fileoverview added by tsickle
