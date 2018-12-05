382cb8d5d81e5c283533a2f89aa0c579f9518b58
Igor Minar <igor@angular.io>
2018-12-05 04:43:53 +0000

    f0b0d64 fix(ivy): adding `projectDef` instructions to all templates where <ng-content> is present (FW-745) (#27384)

--- fesm2015/http.js
+++ fesm2015/http.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.1.0+130.sha-b2d6f43
+ * @license Angular v7.1.0+129.sha-f0b0d64
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2281,7 +2281,7 @@ JsonpModule.ngInjectorDef = defineInjector({ factory: function JsonpModule_Facto
  * @deprecated see https://angular.io/guide/http
  * \@publicApi
   @type {?} */
-const VERSION = new Version('7.1.0+130.sha-b2d6f43');
+const VERSION = new Version('7.1.0+129.sha-f0b0d64');
 
 /**
  * @fileoverview added by tsickle
