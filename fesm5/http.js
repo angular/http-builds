677f4360aa2a41ec534fe9ad43931d1d351f2e1b
Igor Minar <igor@angular.io>
2018-12-05 04:49:55 +0000

    711ef2d build: update to newest ibazel (#27441)

--- fesm5/http.js
+++ fesm5/http.js
@@ -1,10 +1,10 @@
 /**
- * @license Angular v7.1.1+25.sha-5c57920
+ * @license Angular v7.1.1+26.sha-711ef2d
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
 
-import { __decorate, __metadata, __extends, __read, __spread } from 'tslib';
+import { __decorate, __metadata, __read, __extends, __spread } from 'tslib';
 import { Injectable, NgModule, Version } from '@angular/core';
 import { Observable } from 'rxjs';
 import { ɵgetDOM } from '@angular/platform-browser';
@@ -1862,7 +1862,7 @@ var JsonpModule = /** @class */ (function () {
  * @deprecated see https://angular.io/guide/http
  * @publicApi
  */
-var VERSION = new Version('7.1.1+25.sha-5c57920');
+var VERSION = new Version('7.1.1+26.sha-711ef2d');
 
 /**
  * @license
