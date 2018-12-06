dbd40b66d7f9d4b7f777c3c3148bafef35f7a104
Igor Minar <igor@angular.io>
2018-12-06 17:52:31 +0000

    ca1c430 fix(compiler-cli): ngtsc shim files not being generated on case-insensitive platforms (#27466)

--- fesm2015/http.js
+++ fesm2015/http.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.1.0+196.sha-091a504
+ * @license Angular v7.1.0+197.sha-ca1c430
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2281,7 +2281,7 @@ JsonpModule.ngInjectorDef = defineInjector({ factory: function JsonpModule_Facto
  * @deprecated see https://angular.io/guide/http
  * \@publicApi
   @type {?} */
-const VERSION = new Version('7.1.0+196.sha-091a504');
+const VERSION = new Version('7.1.0+197.sha-ca1c430');
 
 /**
  * @fileoverview added by tsickle
--- fesm5/http.js
+++ fesm5/http.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.1.0+196.sha-091a504
+ * @license Angular v7.1.0+197.sha-ca1c430
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1875,7 +1875,7 @@ var JsonpModule = /** @class */ (function () {
  * @deprecated see https://angular.io/guide/http
  * @publicApi
  */
-var VERSION = new Version('7.1.0+196.sha-091a504');
+var VERSION = new Version('7.1.0+197.sha-ca1c430');
 
 /**
  * @license
