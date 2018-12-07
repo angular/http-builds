0703867bfea9f36e5fcb395b2373e1d6f7347403
Alex Rickabaugh <alx+alxhub@alxandria.net>
2018-12-07 18:00:53 +0000

    dcb44e6 fix(ivy): generate proper event listener names for animation events (FW-800) (#27525)

--- fesm5/http.js
+++ fesm5/http.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.0-beta.1+5.sha-6316051
+ * @license Angular v7.2.0-beta.1+6.sha-dcb44e6
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1862,7 +1862,7 @@ var JsonpModule = /** @class */ (function () {
  * @deprecated see https://angular.io/guide/http
  * @publicApi
  */
-var VERSION = new Version('7.2.0-beta.1+5.sha-6316051');
+var VERSION = new Version('7.2.0-beta.1+6.sha-dcb44e6');
 
 /**
  * @license
