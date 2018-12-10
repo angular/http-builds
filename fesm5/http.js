1299e001233ab635e155a3f97f8434108764d7f0
Alex Rickabaugh <alx+alxhub@alxandria.net>
2018-12-10 18:58:48 +0000

    9e7a8f6 refactor(ivy): dedup render3 NodeInjector (#27541)

--- fesm5/http.js
+++ fesm5/http.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.0-beta.1+18.sha-54e4bdb
+ * @license Angular v7.2.0-beta.1+17.sha-9e7a8f6
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1862,7 +1862,7 @@ var JsonpModule = /** @class */ (function () {
  * @deprecated see https://angular.io/guide/http
  * @publicApi
  */
-var VERSION = new Version('7.2.0-beta.1+18.sha-54e4bdb');
+var VERSION = new Version('7.2.0-beta.1+17.sha-9e7a8f6');
 
 /**
  * @license
