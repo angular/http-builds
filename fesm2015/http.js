f1bcb8050a597fb6759ff276894b8292748b5387
Alex Rickabaugh <alx+alxhub@alxandria.net>
2018-12-11 22:32:04 +0000

    b69ec48 release: cut the v7.2.0-beta.2 release

--- fesm2015/http.js
+++ fesm2015/http.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.0-beta.1+33.sha-098a787
+ * @license Angular v7.2.0-beta.2
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2281,7 +2281,7 @@ JsonpModule.ngInjectorDef = defineInjector({ factory: function JsonpModule_Facto
  * @deprecated see https://angular.io/guide/http
  * \@publicApi
   @type {?} */
-const VERSION = new Version('7.2.0-beta.1+33.sha-098a787');
+const VERSION = new Version('7.2.0-beta.2');
 
 /**
  * @fileoverview added by tsickle
