a7692e5c4ebffe743c18c39c04231575d5e67d88
Alex Rickabaugh <alx+alxhub@alxandria.net>
2018-12-11 22:30:48 +0000

    b69ec48 release: cut the v7.2.0-beta.2 release

--- fesm5/http.js
+++ fesm5/http.js
@@ -1,10 +1,10 @@
 /**
- * @license Angular v7.2.0-beta.1+33.sha-098a787
+ * @license Angular v7.2.0-beta.2
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
 
-import { __decorate, __metadata, __extends, __read, __spread } from 'tslib';
+import { __decorate, __metadata, __read, __extends, __spread } from 'tslib';
 import { Injectable, NgModule, Version } from '@angular/core';
 import { Observable } from 'rxjs';
 import { ɵgetDOM } from '@angular/platform-browser';
@@ -1862,7 +1862,7 @@ var JsonpModule = /** @class */ (function () {
  * @deprecated see https://angular.io/guide/http
  * @publicApi
  */
-var VERSION = new Version('7.2.0-beta.1+33.sha-098a787');
+var VERSION = new Version('7.2.0-beta.2');
 
 /**
  * @license
