7efdaf0f835c0c4aaa2c4faf4e50ac4758ebf69f
Alex Rickabaugh <alx+alxhub@alxandria.net>
2018-12-12 01:05:10 +0000

    0b8db92 docs: update SAVED_REPLIES and CONTRIBUTING with new issue templates (#27608)

--- fesm5/http.js
+++ fesm5/http.js
@@ -1,10 +1,10 @@
 /**
- * @license Angular v7.1.3+3.sha-d0e6eeb
+ * @license Angular v7.1.3+4.sha-0b8db92
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
 
-import { __decorate, __metadata, __read, __extends, __spread } from 'tslib';
+import { __decorate, __metadata, __extends, __read, __spread } from 'tslib';
 import { Injectable, NgModule, Version } from '@angular/core';
 import { Observable } from 'rxjs';
 import { ɵgetDOM } from '@angular/platform-browser';
@@ -1862,7 +1862,7 @@ var JsonpModule = /** @class */ (function () {
  * @deprecated see https://angular.io/guide/http
  * @publicApi
  */
-var VERSION = new Version('7.1.3+3.sha-d0e6eeb');
+var VERSION = new Version('7.1.3+4.sha-0b8db92');
 
 /**
  * @license
