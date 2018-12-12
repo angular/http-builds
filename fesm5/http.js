1733ca6a7215792d06603c922422a33f9809103e
Alex Rickabaugh <alx+alxhub@alxandria.net>
2018-12-12 19:32:40 +0000

    9c7fb0d docs: add Zenika trainings in resources.json (#27216)

--- fesm5/http.js
+++ fesm5/http.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.0-beta.2+4.sha-042463f
+ * @license Angular v7.2.0-beta.2+8.sha-9c7fb0d
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1875,7 +1875,7 @@ var JsonpModule = /** @class */ (function () {
  * @deprecated see https://angular.io/guide/http
  * @publicApi
  */
-var VERSION = new Version('7.2.0-beta.2+4.sha-042463f');
+var VERSION = new Version('7.2.0-beta.2+8.sha-9c7fb0d');
 
 /**
  * @license
