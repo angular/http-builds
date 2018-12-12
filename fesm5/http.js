7ed5e17a80c34018885df627ac241c6d747fa4ec
Miško Hevery <misko@hevery.com>
2018-12-12 21:31:19 +0000

    7fabe44 fix(ivy): add support for optional nullable injection tokens (#27552)

--- fesm5/http.js
+++ fesm5/http.js
@@ -1,10 +1,10 @@
 /**
- * @license Angular v7.2.0-beta.2+9.sha-37c05bd
+ * @license Angular v7.2.0-beta.2+10.sha-7fabe44
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
 
-import { __decorate, __metadata, __read, __extends, __spread } from 'tslib';
+import { __decorate, __metadata, __extends, __read, __spread } from 'tslib';
 import { Injectable, NgModule, Version } from '@angular/core';
 import { Observable } from 'rxjs';
 import { ɵgetDOM } from '@angular/platform-browser';
@@ -1862,7 +1862,7 @@ var JsonpModule = /** @class */ (function () {
  * @deprecated see https://angular.io/guide/http
  * @publicApi
  */
-var VERSION = new Version('7.2.0-beta.2+9.sha-37c05bd');
+var VERSION = new Version('7.2.0-beta.2+10.sha-7fabe44');
 
 /**
  * @license
