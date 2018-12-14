90568492d183a5933ebcbe8805923a7fffe7ec8b
Miško Hevery <misko@hevery.com>
2018-12-14 23:58:47 +0000

    78bd3c7 docs(animations): rename animateChild() to animate() (#26184)

--- fesm5/http.js
+++ fesm5/http.js
@@ -1,10 +1,10 @@
 /**
- * @license Angular v7.1.3+29.sha-b224057
+ * @license Angular v7.1.3+30.sha-78bd3c7
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
 
-import { __decorate, __metadata, __read, __extends, __spread } from 'tslib';
+import { __decorate, __metadata, __extends, __read, __spread } from 'tslib';
 import { Injectable, NgModule, Version } from '@angular/core';
 import { Observable } from 'rxjs';
 import { ɵgetDOM } from '@angular/platform-browser';
@@ -1862,7 +1862,7 @@ var JsonpModule = /** @class */ (function () {
  * @deprecated see https://angular.io/guide/http
  * @publicApi
  */
-var VERSION = new Version('7.1.3+29.sha-b224057');
+var VERSION = new Version('7.1.3+30.sha-78bd3c7');
 
 /**
  * @license
