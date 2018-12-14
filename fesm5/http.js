98d6e154da3d0dd7a51f5d7ec33f30746467f492
Miško Hevery <misko@hevery.com>
2018-12-14 19:04:46 +0000

    7fae911 docs(common): Fix typo in KeyValuePipe docs (#27580) PR Close #27580

--- fesm5/http.js
+++ fesm5/http.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.0-beta.2+38.sha-a954345
+ * @license Angular v7.2.0-beta.2+36.sha-7fae911
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1862,7 +1862,7 @@ var JsonpModule = /** @class */ (function () {
  * @deprecated see https://angular.io/guide/http
  * @publicApi
  */
-var VERSION = new Version('7.2.0-beta.2+38.sha-a954345');
+var VERSION = new Version('7.2.0-beta.2+36.sha-7fae911');
 
 /**
  * @license
