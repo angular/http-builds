c3f01da0fa171994588d0de1643c0e9d6194d421
Miško Hevery <misko@hevery.com>
2018-12-14 23:58:03 +0000

    b224057 docs(common): fix documentation for getLocaleCurrencyName (#27087) PR Close #27087

--- fesm5/http.js
+++ fesm5/http.js
@@ -1,10 +1,10 @@
 /**
- * @license Angular v7.1.3+27.sha-453589f
+ * @license Angular v7.1.3+29.sha-b224057
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
 
-import { __decorate, __metadata, __extends, __read, __spread } from 'tslib';
+import { __decorate, __metadata, __read, __extends, __spread } from 'tslib';
 import { Injectable, NgModule, Version } from '@angular/core';
 import { Observable } from 'rxjs';
 import { ɵgetDOM } from '@angular/platform-browser';
@@ -1862,7 +1862,7 @@ var JsonpModule = /** @class */ (function () {
  * @deprecated see https://angular.io/guide/http
  * @publicApi
  */
-var VERSION = new Version('7.1.3+27.sha-453589f');
+var VERSION = new Version('7.1.3+29.sha-b224057');
 
 /**
  * @license
