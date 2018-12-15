459024e0ed82793b3155b6585418998fc6881498
Miško Hevery <misko@hevery.com>
2018-12-15 00:07:04 +0000

    7dab458 docs(common): fix documentation for getLocaleCurrencyName (#27087) PR Close #27087

--- fesm5/http.js
+++ fesm5/http.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.0-beta.2+43.sha-f9545d1
+ * @license Angular v7.2.0-beta.2+51.sha-7dab458
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1862,7 +1862,7 @@ var JsonpModule = /** @class */ (function () {
  * @deprecated see https://angular.io/guide/http
  * @publicApi
  */
-var VERSION = new Version('7.2.0-beta.2+43.sha-f9545d1');
+var VERSION = new Version('7.2.0-beta.2+51.sha-7dab458');
 
 /**
  * @license
