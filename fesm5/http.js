d41452b5c6d816f945bb8b31a742b1af2494f36c
Miško Hevery <misko@hevery.com>
2018-12-15 00:04:49 +0000

    b39efdd fix(common): KeyValuePipe should return empty array for empty objects (#27258)

--- fesm5/http.js
+++ fesm5/http.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.0-beta.2+42.sha-0b78f54
+ * @license Angular v7.2.0-beta.2+44.sha-b39efdd
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1875,7 +1875,7 @@ var JsonpModule = /** @class */ (function () {
  * @deprecated see https://angular.io/guide/http
  * @publicApi
  */
-var VERSION = new Version('7.2.0-beta.2+42.sha-0b78f54');
+var VERSION = new Version('7.2.0-beta.2+44.sha-b39efdd');
 
 /**
  * @license
