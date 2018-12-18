1dbebc2b7774ee2dc9b152bbf64e7d6db9ff0e9c
Miško Hevery <misko@hevery.com>
2018-12-18 01:15:57 +0000

    85866de fix(bazel): Set module_name and enable ng test (#27715)

--- fesm5/http.js
+++ fesm5/http.js
@@ -1,10 +1,10 @@
 /**
- * @license Angular v7.2.0-beta.2+66.sha-c986d3d
+ * @license Angular v7.2.0-beta.2+67.sha-85866de
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
 
-import { __decorate, __metadata, __read, __extends, __spread } from 'tslib';
+import { __decorate, __metadata, __extends, __read, __spread } from 'tslib';
 import { Injectable, NgModule, Version } from '@angular/core';
 import { Observable } from 'rxjs';
 import { ɵgetDOM } from '@angular/platform-browser';
@@ -1862,7 +1862,7 @@ var JsonpModule = /** @class */ (function () {
  * @deprecated see https://angular.io/guide/http
  * @publicApi
  */
-var VERSION = new Version('7.2.0-beta.2+66.sha-c986d3d');
+var VERSION = new Version('7.2.0-beta.2+67.sha-85866de');
 
 /**
  * @license
