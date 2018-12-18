3428f027e92fd5d2fee594921c0f8f6022e6dda4
Miško Hevery <misko@hevery.com>
2018-12-18 21:45:23 +0000

    b04bc5d test(ivy): re-enable passing upgrade tests (#27736)

--- fesm5/http.js
+++ fesm5/http.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.0-beta.2+82.sha-1c93afe
+ * @license Angular v7.2.0-beta.2+83.sha-b04bc5d
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1875,7 +1875,7 @@ var JsonpModule = /** @class */ (function () {
  * @deprecated see https://angular.io/guide/http
  * @publicApi
  */
-var VERSION = new Version('7.2.0-beta.2+82.sha-1c93afe');
+var VERSION = new Version('7.2.0-beta.2+83.sha-b04bc5d');
 
 /**
  * @license
