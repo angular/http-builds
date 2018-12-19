284c9dec4497310f5c3a31432d2ab23fe2c71ad4
Matias Niemelä <matias@yearofmoo.com>
2018-12-19 21:33:24 +0000

    3ab25ab refactor: fix broken linting rules due to revert

--- fesm5/http.js
+++ fesm5/http.js
@@ -1,10 +1,10 @@
 /**
- * @license Angular v7.2.0-rc.0+4.sha-0604527
+ * @license Angular v7.2.0-rc.0+5.sha-3ab25ab
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
 
-import { __decorate, __metadata, __extends, __read, __spread } from 'tslib';
+import { __decorate, __metadata, __read, __extends, __spread } from 'tslib';
 import { Injectable, NgModule, Version } from '@angular/core';
 import { Observable } from 'rxjs';
 import { ɵgetDOM } from '@angular/platform-browser';
@@ -1862,7 +1862,7 @@ var JsonpModule = /** @class */ (function () {
  * @deprecated see https://angular.io/guide/http
  * @publicApi
  */
-var VERSION = new Version('7.2.0-rc.0+4.sha-0604527');
+var VERSION = new Version('7.2.0-rc.0+5.sha-3ab25ab');
 
 /**
  * @license
