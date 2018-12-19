572e0868698b723438dc8a183d2b6faa1f53da6a
Matias Niemelä <matias@yearofmoo.com>
2018-12-19 20:41:36 +0000

    b1560f7 build: fix our copy of Array#find typing (#27742)

--- fesm5/http.js
+++ fesm5/http.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.0-rc.0+2.sha-a3debf6
+ * @license Angular v7.2.0-rc.0+1.sha-b1560f7
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1862,7 +1862,7 @@ var JsonpModule = /** @class */ (function () {
  * @deprecated see https://angular.io/guide/http
  * @publicApi
  */
-var VERSION = new Version('7.2.0-rc.0+2.sha-a3debf6');
+var VERSION = new Version('7.2.0-rc.0+1.sha-b1560f7');
 
 /**
  * @license
