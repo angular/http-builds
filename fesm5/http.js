f4b17514febd54c1c9250d2f2676cc44b0365a0a
Matias Niemelä <matias@yearofmoo.com>
2018-12-20 21:47:14 +0000

    07ada7f fix(router): ensure URL is updated after second redirect with UrlUpdateStrategy="eager" (#27523)

--- fesm5/http.js
+++ fesm5/http.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.0-rc.0+13.sha-880e8a5
+ * @license Angular v7.2.0-rc.0+14.sha-07ada7f
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1862,7 +1862,7 @@ var JsonpModule = /** @class */ (function () {
  * @deprecated see https://angular.io/guide/http
  * @publicApi
  */
-var VERSION = new Version('7.2.0-rc.0+13.sha-880e8a5');
+var VERSION = new Version('7.2.0-rc.0+14.sha-07ada7f');
 
 /**
  * @license
