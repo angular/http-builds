e628a697479fc7efc3d1b33cc3634e79603f56b4
Matias Niemelä <matias@yearofmoo.com>
2018-12-20 17:41:31 +0000

    880e8a5 refactor: fix broken formatting

--- fesm5/http.js
+++ fesm5/http.js
@@ -1,10 +1,10 @@
 /**
- * @license Angular v7.2.0-rc.0+11.sha-a833b98
+ * @license Angular v7.2.0-rc.0+13.sha-880e8a5
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
 
-import { __decorate, __metadata, __read, __extends, __spread } from 'tslib';
+import { __decorate, __metadata, __extends, __read, __spread } from 'tslib';
 import { Injectable, NgModule, Version } from '@angular/core';
 import { Observable } from 'rxjs';
 import { ɵgetDOM } from '@angular/platform-browser';
@@ -1862,7 +1862,7 @@ var JsonpModule = /** @class */ (function () {
  * @deprecated see https://angular.io/guide/http
  * @publicApi
  */
-var VERSION = new Version('7.2.0-rc.0+11.sha-a833b98');
+var VERSION = new Version('7.2.0-rc.0+13.sha-880e8a5');
 
 /**
  * @license
