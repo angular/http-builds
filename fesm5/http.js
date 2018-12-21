fe9ed7d7921b0a1f8a0dbe4e59d68b88d638bcaf
Matias Niemelä <matias@yearofmoo.com>
2018-12-21 20:37:37 +0000

    cdd737e test(ivy): mark tests using getDebugContext as obsolete in ivy (#27790)

--- fesm5/http.js
+++ fesm5/http.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.0-rc.0+20.sha-5da55d6
+ * @license Angular v7.2.0-rc.0+21.sha-cdd737e
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1862,7 +1862,7 @@ var JsonpModule = /** @class */ (function () {
  * @deprecated see https://angular.io/guide/http
  * @publicApi
  */
-var VERSION = new Version('7.2.0-rc.0+20.sha-5da55d6');
+var VERSION = new Version('7.2.0-rc.0+21.sha-cdd737e');
 
 /**
  * @license
