df35e7d0d949f53c5a78fb038ea23c1f13f48d03
Matias Niemelä <matias@yearofmoo.com>
2018-12-25 01:19:53 +0000

    9129112 test(bazel): fix `bazel-workspace` schematics test

--- fesm5/http.js
+++ fesm5/http.js
@@ -1,10 +1,10 @@
 /**
- * @license Angular v7.1.3+35.sha-be08611
+ * @license Angular v7.1.4+1.sha-9129112
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
 
-import { __decorate, __metadata, __read, __extends, __spread } from 'tslib';
+import { __decorate, __metadata, __extends, __read, __spread } from 'tslib';
 import { Injectable, NgModule, Version } from '@angular/core';
 import { Observable } from 'rxjs';
 import { ɵgetDOM } from '@angular/platform-browser';
@@ -1862,7 +1862,7 @@ var JsonpModule = /** @class */ (function () {
  * @deprecated see https://angular.io/guide/http
  * @publicApi
  */
-var VERSION = new Version('7.1.3+35.sha-be08611');
+var VERSION = new Version('7.1.4+1.sha-9129112');
 
 /**
  * @license
