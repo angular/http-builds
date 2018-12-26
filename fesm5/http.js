1f5ee7349b38189474ce2c85c37f6d2940fb6f0a
Ben Lesh <ben@benlesh.com>
2018-12-26 20:16:08 +0000

    c4f7727 docs: add api doc for viewport scroller (#27381)

--- fesm5/http.js
+++ fesm5/http.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.0-rc.0+34.sha-d1de9ff
+ * @license Angular v7.2.0-rc.0+35.sha-c4f7727
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1862,7 +1862,7 @@ var JsonpModule = /** @class */ (function () {
  * @deprecated see https://angular.io/guide/http
  * @publicApi
  */
-var VERSION = new Version('7.2.0-rc.0+34.sha-d1de9ff');
+var VERSION = new Version('7.2.0-rc.0+35.sha-c4f7727');
 
 /**
  * @license
