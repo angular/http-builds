7038244982da0ca4c3c799497f80df1138c52b77
Ben Lesh <ben@benlesh.com>
2018-12-26 19:55:23 +0000

    707c682 docs(elements): add "Firefox" for support of custom elements (#27789)

--- fesm2015/http.js
+++ fesm2015/http.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.0-rc.0+32.sha-3f64e87
+ * @license Angular v7.2.0-rc.0+33.sha-707c682
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2327,7 +2327,7 @@ JsonpModule.ngInjectorDef = defineInjector({ factory: function JsonpModule_Facto
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('7.2.0-rc.0+32.sha-3f64e87');
+const VERSION = new Version('7.2.0-rc.0+33.sha-707c682');
 
 /**
  * @fileoverview added by tsickle
