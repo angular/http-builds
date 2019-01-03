5fbf57225d8c138620419d742885795d22c8cb39
Ben Lesh <ben@benlesh.com>
2019-01-03 18:17:57 +0000

    5638c1d docs: fix npm description in architecture modules guide (#27818)

--- fesm5/http.js
+++ fesm5/http.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.0-rc.0+43.sha-a0a3648
+ * @license Angular v7.2.0-rc.0+41.sha-5638c1d
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1862,7 +1862,7 @@ var JsonpModule = /** @class */ (function () {
  * @deprecated see https://angular.io/guide/http
  * @publicApi
  */
-var VERSION = new Version('7.2.0-rc.0+43.sha-a0a3648');
+var VERSION = new Version('7.2.0-rc.0+41.sha-5638c1d');
 
 /**
  * @license
