b2527b58a2386e60927304287a2d3eb52936a733
Ben Lesh <ben@benlesh.com>
2019-01-03 18:23:12 +0000

    1e6c9be fix(bazel): unable to launch protractor test on windows (#27850)

--- fesm2015/http.js
+++ fesm2015/http.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.0-rc.0+48.sha-880b4aa
+ * @license Angular v7.2.0-rc.0+46.sha-1e6c9be
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2327,7 +2327,7 @@ JsonpModule.ngInjectorDef = defineInjector({ factory: function JsonpModule_Facto
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('7.2.0-rc.0+48.sha-880b4aa');
+const VERSION = new Version('7.2.0-rc.0+46.sha-1e6c9be');
 
 /**
  * @fileoverview added by tsickle
