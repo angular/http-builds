5bc7938aab5c84d11fb9a462b30b4476e3d94d62
Ben Lesh <ben@benlesh.com>
2019-01-03 18:00:58 +0000

    e9de47c docs(service-worker): update service worker configuration doc (#27868)

--- fesm5/http.js
+++ fesm5/http.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.0-rc.0+16.sha-213e01b
+ * @license Angular v7.2.0-rc.0+18.sha-e9de47c
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1862,7 +1862,7 @@ var JsonpModule = /** @class */ (function () {
  * @deprecated see https://angular.io/guide/http
  * @publicApi
  */
-var VERSION = new Version('7.2.0-rc.0+16.sha-213e01b');
+var VERSION = new Version('7.2.0-rc.0+18.sha-e9de47c');
 
 /**
  * @license
