36810b2cafbd6f809411706dea07346b38e8e296
Kara Erickson <karakara@google.com>
2019-01-04 21:51:36 +0000

    0bd9deb fix(ivy): pipes should overwrite pipes with later entry in the pipes array (#27910)

--- fesm5/http.js
+++ fesm5/http.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.0-rc.0+60.sha-13d23f3
+ * @license Angular v7.2.0-rc.0+61.sha-0bd9deb
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1875,7 +1875,7 @@ var JsonpModule = /** @class */ (function () {
  * @deprecated see https://angular.io/guide/http
  * @publicApi
  */
-var VERSION = new Version('7.2.0-rc.0+60.sha-13d23f3');
+var VERSION = new Version('7.2.0-rc.0+61.sha-0bd9deb');
 
 /**
  * @license
