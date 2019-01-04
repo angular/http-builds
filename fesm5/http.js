814d0712d110fbed4045a71b1811b4bd68d44351
Kara Erickson <karakara@google.com>
2019-01-04 20:26:53 +0000

    970e3a0 test: remove bower and polymer benchmarks (#27931)

--- fesm5/http.js
+++ fesm5/http.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.0-rc.0+25.sha-791d865
+ * @license Angular v7.2.0-rc.0+26.sha-970e3a0
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1862,7 +1862,7 @@ var JsonpModule = /** @class */ (function () {
  * @deprecated see https://angular.io/guide/http
  * @publicApi
  */
-var VERSION = new Version('7.2.0-rc.0+25.sha-791d865');
+var VERSION = new Version('7.2.0-rc.0+26.sha-970e3a0');
 
 /**
  * @license
