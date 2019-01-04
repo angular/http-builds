1da64319a43042e11cdc54e38f8bde2133674543
Kara Erickson <karakara@google.com>
2019-01-04 20:50:53 +0000

    3f2ebbd fix(ivy): support bindings in ICUs in nested contexts (#27914)

--- fesm5/http.js
+++ fesm5/http.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.0-rc.0+57.sha-7eb2c41
+ * @license Angular v7.2.0-rc.0+56.sha-3f2ebbd
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1862,7 +1862,7 @@ var JsonpModule = /** @class */ (function () {
  * @deprecated see https://angular.io/guide/http
  * @publicApi
  */
-var VERSION = new Version('7.2.0-rc.0+57.sha-7eb2c41');
+var VERSION = new Version('7.2.0-rc.0+56.sha-3f2ebbd');
 
 /**
  * @license
