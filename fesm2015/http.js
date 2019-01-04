fecb76f7d95cdee854f5903f63bd837f320dfe65
Ben Lesh <ben@benlesh.com>
2019-01-04 00:36:17 +0000

    51a0bd2 test(ivy): correct attribute marker in broken test (#27919)

--- fesm2015/http.js
+++ fesm2015/http.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.0-rc.0+46.sha-1e6c9be
+ * @license Angular v7.2.0-rc.0+52.sha-51a0bd2
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2327,7 +2327,7 @@ JsonpModule.ngInjectorDef = defineInjector({ factory: function JsonpModule_Facto
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('7.2.0-rc.0+46.sha-1e6c9be');
+const VERSION = new Version('7.2.0-rc.0+52.sha-51a0bd2');
 
 /**
  * @fileoverview added by tsickle
