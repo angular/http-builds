afdb14ff3ebc93c6e80ef0134e90d372b9937916
Kara Erickson <karakara@google.com>
2019-01-05 03:03:43 +0000

    6cc864a build: enforce minimum Bazel version of 0.21.0 in WORKSPACE so cache works with `yarn bazel` (#27935)

--- fesm5/http.js
+++ fesm5/http.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.0-rc.0+27.sha-0a54825
+ * @license Angular v7.2.0-rc.0+29.sha-6cc864a
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1862,7 +1862,7 @@ var JsonpModule = /** @class */ (function () {
  * @deprecated see https://angular.io/guide/http
  * @publicApi
  */
-var VERSION = new Version('7.2.0-rc.0+27.sha-0a54825');
+var VERSION = new Version('7.2.0-rc.0+29.sha-6cc864a');
 
 /**
  * @license
