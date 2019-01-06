5392856da95c3bd2a5e5cd460de79b0a77fef3f7
Kara Erickson <karakara@google.com>
2019-01-06 02:12:35 +0000

    6b0c1a7 test(ivy): turn on platform-browser-dynamic tests on CI (#27940)

--- fesm5/http.js
+++ fesm5/http.js
@@ -1,10 +1,10 @@
 /**
- * @license Angular v7.2.0-rc.0+70.sha-4c9aff5
+ * @license Angular v7.2.0-rc.0+71.sha-6b0c1a7
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
 
-import { __decorate, __metadata, __read, __extends, __spread } from 'tslib';
+import { __decorate, __metadata, __extends, __read, __spread } from 'tslib';
 import { Injectable, NgModule, Version } from '@angular/core';
 import { Observable } from 'rxjs';
 import { ɵgetDOM } from '@angular/platform-browser';
@@ -1862,7 +1862,7 @@ var JsonpModule = /** @class */ (function () {
  * @deprecated see https://angular.io/guide/http
  * @publicApi
  */
-var VERSION = new Version('7.2.0-rc.0+70.sha-4c9aff5');
+var VERSION = new Version('7.2.0-rc.0+71.sha-6b0c1a7');
 
 /**
  * @license
