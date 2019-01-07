a33e74413c6bb488d24af7ed16fe4d73d844e855
Kara Erickson <karakara@google.com>
2019-01-07 23:17:44 +0000

    4613864 test(ivy): add i18n integration tests via TestBed (#27852)

--- fesm5/http.js
+++ fesm5/http.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.0+10.sha-a75c734
+ * @license Angular v7.2.0+15.sha-4613864
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1862,7 +1862,7 @@ var JsonpModule = /** @class */ (function () {
  * @deprecated see https://angular.io/guide/http
  * @publicApi
  */
-var VERSION = new Version('7.2.0+10.sha-a75c734');
+var VERSION = new Version('7.2.0+15.sha-4613864');
 
 /**
  * @license
