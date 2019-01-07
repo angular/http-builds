7498a09958260d1d06420fd8139fbb7d28019acf
Kara Erickson <karakara@google.com>
2019-01-07 23:18:56 +0000

    4613864 test(ivy): add i18n integration tests via TestBed (#27852)

--- fesm2015/http.js
+++ fesm2015/http.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.0+10.sha-a75c734
+ * @license Angular v7.2.0+15.sha-4613864
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2327,7 +2327,7 @@ JsonpModule.ngInjectorDef = defineInjector({ factory: function JsonpModule_Facto
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('7.2.0+10.sha-a75c734');
+const VERSION = new Version('7.2.0+15.sha-4613864');
 
 /**
  * @fileoverview added by tsickle
