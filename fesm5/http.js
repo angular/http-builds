456b3a575c4f73da5497d0bcc28f670074bf7226
Kara Erickson <karakara@google.com>
2019-01-07 23:19:08 +0000

    ac5f5ed test(bazel): re-enable ng_package golden testing on ci (#27829)

--- fesm5/http.js
+++ fesm5/http.js
@@ -1,10 +1,10 @@
 /**
- * @license Angular v7.2.0+16.sha-9de9c8a
+ * @license Angular v7.2.0+14.sha-ac5f5ed
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
 
-import { __decorate, __metadata, __extends, __read, __spread } from 'tslib';
+import { __decorate, __metadata, __read, __extends, __spread } from 'tslib';
 import { Injectable, NgModule, Version } from '@angular/core';
 import { Observable } from 'rxjs';
 import { ɵgetDOM } from '@angular/platform-browser';
@@ -1862,7 +1862,7 @@ var JsonpModule = /** @class */ (function () {
  * @deprecated see https://angular.io/guide/http
  * @publicApi
  */
-var VERSION = new Version('7.2.0+16.sha-9de9c8a');
+var VERSION = new Version('7.2.0+14.sha-ac5f5ed');
 
 /**
  * @license
