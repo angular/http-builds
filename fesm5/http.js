b531342fcd97f88bf021221cf292f40dce37468d
Kara Erickson <karakara@google.com>
2019-01-07 23:11:56 +0000

    a75c734 fix(ivy): restore NG defs after running tests in TestBed (#27786)

--- fesm5/http.js
+++ fesm5/http.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.0+9.sha-e775313
+ * @license Angular v7.2.0+10.sha-a75c734
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1875,7 +1875,7 @@ var JsonpModule = /** @class */ (function () {
  * @deprecated see https://angular.io/guide/http
  * @publicApi
  */
-var VERSION = new Version('7.2.0+9.sha-e775313');
+var VERSION = new Version('7.2.0+10.sha-a75c734');
 
 /**
  * @license
