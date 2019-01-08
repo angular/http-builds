13dc4142c55dc34cfe29b72ee696d513ed3023b5
Kara Erickson <karakara@google.com>
2019-01-08 22:21:19 +0000

    1a7f92c test(ivy): re-enable passing animation tests (#27997)

--- fesm5/http.js
+++ fesm5/http.js
@@ -1,10 +1,10 @@
 /**
- * @license Angular v7.2.0+43.sha-a084024
+ * @license Angular v7.2.0+44.sha-1a7f92c
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
 
-import { __decorate, __metadata, __read, __extends, __spread } from 'tslib';
+import { __decorate, __metadata, __extends, __read, __spread } from 'tslib';
 import { Injectable, NgModule, Version } from '@angular/core';
 import { Observable } from 'rxjs';
 import { ɵgetDOM } from '@angular/platform-browser';
@@ -1862,7 +1862,7 @@ var JsonpModule = /** @class */ (function () {
  * @deprecated see https://angular.io/guide/http
  * @publicApi
  */
-var VERSION = new Version('7.2.0+43.sha-a084024');
+var VERSION = new Version('7.2.0+44.sha-1a7f92c');
 
 /**
  * @license
