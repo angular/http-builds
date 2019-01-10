b2c2e848af6f5a53109c187577c0354d48009cac
Andrew Kushnir <akushnir@google.com>
2019-01-10 19:22:01 +0000

    6003145 fix(ivy): properly rewrite imports in generated factory shims (#27998)

--- fesm5/http.js
+++ fesm5/http.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.0+85.sha-5a0deb8
+ * @license Angular v7.2.0+87.sha-6003145
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1862,7 +1862,7 @@ var JsonpModule = /** @class */ (function () {
  * @deprecated see https://angular.io/guide/http
  * @publicApi
  */
-var VERSION = new Version('7.2.0+85.sha-5a0deb8');
+var VERSION = new Version('7.2.0+87.sha-6003145');
 
 /**
  * @license
