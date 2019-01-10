a0f45ea4eb45b5e2c2fed709b670ba835826f327
Andrew Kushnir <akushnir@google.com>
2019-01-10 00:17:50 +0000

    8934b73 test(ivy): re-enable passing upgrade tests (#28030)

--- fesm5/http.js
+++ fesm5/http.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.0+80.sha-11325ba
+ * @license Angular v7.2.0+81.sha-8934b73
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1875,7 +1875,7 @@ var JsonpModule = /** @class */ (function () {
  * @deprecated see https://angular.io/guide/http
  * @publicApi
  */
-var VERSION = new Version('7.2.0+80.sha-11325ba');
+var VERSION = new Version('7.2.0+81.sha-8934b73');
 
 /**
  * @license
