9ab5c8a94290dd06b67bacb1a808e69d7fe39010
Andrew Kushnir <akushnir@google.com>
2019-01-10 19:33:32 +0000

    e8a57f0 test(upgrade): log more info to help debug CI flakes (#28045)

--- fesm5/http.js
+++ fesm5/http.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.0+95.sha-dffcb9c
+ * @license Angular v7.2.0+99.sha-e8a57f0
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1862,7 +1862,7 @@ var JsonpModule = /** @class */ (function () {
  * @deprecated see https://angular.io/guide/http
  * @publicApi
  */
-var VERSION = new Version('7.2.0+95.sha-dffcb9c');
+var VERSION = new Version('7.2.0+99.sha-e8a57f0');
 
 /**
  * @license
