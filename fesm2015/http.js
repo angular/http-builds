a2c60e24c96c9c29bc1062bab2e473cfafa0a0a2
Andrew Kushnir <akushnir@google.com>
2019-01-10 19:30:32 +0000

    c5ab3e8 fix(ivy): proper resolution of Enums in Component decorator (#27971)

--- fesm2015/http.js
+++ fesm2015/http.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.0+88.sha-142553a
+ * @license Angular v7.2.0+89.sha-c5ab3e8
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2327,7 +2327,7 @@ JsonpModule.ngInjectorDef = defineInjector({ factory: function JsonpModule_Facto
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('7.2.0+88.sha-142553a');
+const VERSION = new Version('7.2.0+89.sha-c5ab3e8');
 
 /**
  * @fileoverview added by tsickle
