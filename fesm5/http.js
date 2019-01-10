083d90eae747630cfd6acf4278e984e87d65cdfa
Andrew Kushnir <akushnir@google.com>
2019-01-10 19:26:41 +0000

    142553a feat(ivy): accept multiple values for exportAs in the compiler (#28001)

--- fesm5/http.js
+++ fesm5/http.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.0+87.sha-6003145
+ * @license Angular v7.2.0+88.sha-142553a
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1862,7 +1862,7 @@ var JsonpModule = /** @class */ (function () {
  * @deprecated see https://angular.io/guide/http
  * @publicApi
  */
-var VERSION = new Version('7.2.0+87.sha-6003145');
+var VERSION = new Version('7.2.0+88.sha-142553a');
 
 /**
  * @license
