0bb5fb50e18c219797b620397213b4ae543d7368
Andrew Kushnir <akushnir@google.com>
2019-01-10 19:32:19 +0000

    ee60c76 test(ivy): turn on passing i18n test in CI (#28035)

--- fesm5/http.js
+++ fesm5/http.js
@@ -1,10 +1,10 @@
 /**
- * @license Angular v7.2.0+92.sha-65e72e9
+ * @license Angular v7.2.0+94.sha-ee60c76
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
 
-import { __decorate, __metadata, __extends, __read, __spread } from 'tslib';
+import { __decorate, __metadata, __read, __extends, __spread } from 'tslib';
 import { Injectable, NgModule, Version } from '@angular/core';
 import { Observable } from 'rxjs';
 import { ɵgetDOM } from '@angular/platform-browser';
@@ -1862,7 +1862,7 @@ var JsonpModule = /** @class */ (function () {
  * @deprecated see https://angular.io/guide/http
  * @publicApi
  */
-var VERSION = new Version('7.2.0+92.sha-65e72e9');
+var VERSION = new Version('7.2.0+94.sha-ee60c76');
 
 /**
  * @license
