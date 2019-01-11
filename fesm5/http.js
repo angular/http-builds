d0496d60a9a021e13e5e4ce39f0c2a27b099d4ea
Andrew Kushnir <akushnir@google.com>
2019-01-11 19:41:28 +0000

    0465dc6 build: update `version` in `package.json` (#28075)

--- fesm5/http.js
+++ fesm5/http.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.0-rc.0+87.sha-0d9afd8
+ * @license Angular v7.2.0-rc.0+93.sha-0465dc6
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1862,7 +1862,7 @@ var JsonpModule = /** @class */ (function () {
  * @deprecated see https://angular.io/guide/http
  * @publicApi
  */
-var VERSION = new Version('7.2.0-rc.0+87.sha-0d9afd8');
+var VERSION = new Version('7.2.0-rc.0+93.sha-0465dc6');
 
 /**
  * @license
