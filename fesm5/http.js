bb0d914c9853c9c976b73b04bd68f6dff5709151
Andrew Kushnir <akushnir@google.com>
2019-01-11 19:28:20 +0000

    d505468 docs: re-write interpolation section and add example (#25170)

--- fesm5/http.js
+++ fesm5/http.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.0+112.sha-f854eb7
+ * @license Angular v7.2.0+113.sha-d505468
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1862,7 +1862,7 @@ var JsonpModule = /** @class */ (function () {
  * @deprecated see https://angular.io/guide/http
  * @publicApi
  */
-var VERSION = new Version('7.2.0+112.sha-f854eb7');
+var VERSION = new Version('7.2.0+113.sha-d505468');
 
 /**
  * @license
