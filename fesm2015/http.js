649876527fc75ecb41880d428ae74a5d4a1bb352
Andrew Kushnir <akushnir@google.com>
2019-01-11 01:32:35 +0000

    96f4969 docs: fix broken html for deployment.md (#27851)

--- fesm2015/http.js
+++ fesm2015/http.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.0+106.sha-aebec4b
+ * @license Angular v7.2.0+107.sha-96f4969
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2327,7 +2327,7 @@ JsonpModule.ngInjectorDef = defineInjector({ factory: function JsonpModule_Facto
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('7.2.0+106.sha-aebec4b');
+const VERSION = new Version('7.2.0+107.sha-96f4969');
 
 /**
  * @fileoverview added by tsickle
