43f752e8319349da545068ce5a4a61057c58e621
Andrew Kushnir <akushnir@google.com>
2019-01-11 01:09:26 +0000

    9277142 fix(ivy): support multiple exportAs (#27996)

--- fesm2015/http.js
+++ fesm2015/http.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.0+104.sha-b78351c
+ * @license Angular v7.2.0+105.sha-9277142
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2327,7 +2327,7 @@ JsonpModule.ngInjectorDef = defineInjector({ factory: function JsonpModule_Facto
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('7.2.0+104.sha-b78351c');
+const VERSION = new Version('7.2.0+105.sha-9277142');
 
 /**
  * @fileoverview added by tsickle
