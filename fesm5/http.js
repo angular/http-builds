83dd18a1e60b668685c97f3850ef9e42f6640567
Andrew Kushnir <akushnir@google.com>
2019-01-11 01:34:37 +0000

    26a8c09 docs(router): fixed a typo in CanLoad title text (#27894)

--- fesm5/http.js
+++ fesm5/http.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.0+108.sha-acb2cac
+ * @license Angular v7.2.0+110.sha-26a8c09
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1862,7 +1862,7 @@ var JsonpModule = /** @class */ (function () {
  * @deprecated see https://angular.io/guide/http
  * @publicApi
  */
-var VERSION = new Version('7.2.0+108.sha-acb2cac');
+var VERSION = new Version('7.2.0+110.sha-26a8c09');
 
 /**
  * @license
