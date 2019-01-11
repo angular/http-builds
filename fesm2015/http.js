a8e356789f49223f31db2df57ddf0bbfdbc2e5c1
Andrew Kushnir <akushnir@google.com>
2019-01-11 01:34:24 +0000

    acb2cac docs: fix typo (#27865) PR Close #27865

--- fesm2015/http.js
+++ fesm2015/http.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.0+107.sha-96f4969
+ * @license Angular v7.2.0+108.sha-acb2cac
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2327,7 +2327,7 @@ JsonpModule.ngInjectorDef = defineInjector({ factory: function JsonpModule_Facto
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('7.2.0+107.sha-96f4969');
+const VERSION = new Version('7.2.0+108.sha-acb2cac');
 
 /**
  * @fileoverview added by tsickle
