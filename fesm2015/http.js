420e13550da4f86c8a931dd8811a53319fcd5be6
Andrew Kushnir <akushnir@google.com>
2019-01-11 01:36:02 +0000

    bccf8da docs(router): fix misnamed path (#27879)

--- fesm2015/http.js
+++ fesm2015/http.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.0+110.sha-26a8c09
+ * @license Angular v7.2.0+109.sha-bccf8da
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2327,7 +2327,7 @@ JsonpModule.ngInjectorDef = defineInjector({ factory: function JsonpModule_Facto
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('7.2.0+110.sha-26a8c09');
+const VERSION = new Version('7.2.0+109.sha-bccf8da');
 
 /**
  * @fileoverview added by tsickle
