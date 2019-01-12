bdb23f731374bfbc5af51483eabd6d03bbdc10b0
Andrew Kushnir <akushnir@google.com>
2019-01-12 00:40:46 +0000

    31556fa docs: add library doc to guides in aio (#27581)

--- fesm5/http.js
+++ fesm5/http.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.0-rc.0+96.sha-01d475d
+ * @license Angular v7.2.0-rc.0+97.sha-31556fa
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1862,7 +1862,7 @@ var JsonpModule = /** @class */ (function () {
  * @deprecated see https://angular.io/guide/http
  * @publicApi
  */
-var VERSION = new Version('7.2.0-rc.0+96.sha-01d475d');
+var VERSION = new Version('7.2.0-rc.0+97.sha-31556fa');
 
 /**
  * @license
