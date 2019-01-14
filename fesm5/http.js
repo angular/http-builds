2dd868e155c36b685cf89bf21eb82fd9fb0943bc
Andrew Kushnir <akushnir@google.com>
2019-01-14 18:32:42 +0000

    c5b67b9 docs: update link in universal guide (#26628)

--- fesm5/http.js
+++ fesm5/http.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.0+148.sha-78bc21c
+ * @license Angular v7.2.0+149.sha-c5b67b9
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1862,7 +1862,7 @@ var JsonpModule = /** @class */ (function () {
  * @deprecated see https://angular.io/guide/http
  * @publicApi
  */
-var VERSION = new Version('7.2.0+148.sha-78bc21c');
+var VERSION = new Version('7.2.0+149.sha-c5b67b9');
 
 /**
  * @license
