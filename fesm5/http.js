0e98b0e503f94fc28e0cc20acdcabe6e8aa45638
Andrew Kushnir <akushnir@google.com>
2019-01-14 19:01:37 +0000

    638375b docs: fix typo in testing guide (#26828)

--- fesm5/http.js
+++ fesm5/http.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.0+149.sha-c5b67b9
+ * @license Angular v7.2.0+150.sha-638375b
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1875,7 +1875,7 @@ var JsonpModule = /** @class */ (function () {
  * @deprecated see https://angular.io/guide/http
  * @publicApi
  */
-var VERSION = new Version('7.2.0+149.sha-c5b67b9');
+var VERSION = new Version('7.2.0+150.sha-638375b');
 
 /**
  * @license
