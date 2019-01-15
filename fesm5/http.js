40965949941f4bac05adb01d4c1ae231690bf7cc
Andrew Kushnir <akushnir@google.com>
2019-01-15 19:43:24 +0000

    ce3a746 perf(platform-server): use shared `DomElementSchemaRegistry` instance (#28150) (#28151)

--- fesm5/http.js
+++ fesm5/http.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.0+187.sha-bc02e31
+ * @license Angular v7.2.0+188.sha-ce3a746
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1862,7 +1862,7 @@ var JsonpModule = /** @class */ (function () {
  * @deprecated see https://angular.io/guide/http
  * @publicApi
  */
-var VERSION = new Version('7.2.0+187.sha-bc02e31');
+var VERSION = new Version('7.2.0+188.sha-ce3a746');
 
 /**
  * @license
