d9d44158ecd70f8709c61bd0ce9f5dba25a2e19d
Andrew Kushnir <akushnir@google.com>
2019-01-15 19:10:21 +0000

    850b867 docs: add api doc for switch directives (#27378)

--- fesm5/http.js
+++ fesm5/http.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.0+183.sha-a241200
+ * @license Angular v7.2.0+184.sha-850b867
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1862,7 +1862,7 @@ var JsonpModule = /** @class */ (function () {
  * @deprecated see https://angular.io/guide/http
  * @publicApi
  */
-var VERSION = new Version('7.2.0+183.sha-a241200');
+var VERSION = new Version('7.2.0+184.sha-850b867');
 
 /**
  * @license
