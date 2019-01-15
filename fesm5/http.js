18926873255cb1e98f375fde62c4dfa6577820e3
Andrew Kushnir <akushnir@google.com>
2019-01-15 18:10:30 +0000

    9faad4b docs(core): fix typo (#28042) PR Close #28042

--- fesm5/http.js
+++ fesm5/http.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.0-rc.0+123.sha-42ccea5
+ * @license Angular v7.2.0-rc.0+124.sha-9faad4b
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1862,7 +1862,7 @@ var JsonpModule = /** @class */ (function () {
  * @deprecated see https://angular.io/guide/http
  * @publicApi
  */
-var VERSION = new Version('7.2.0-rc.0+123.sha-42ccea5');
+var VERSION = new Version('7.2.0-rc.0+124.sha-9faad4b');
 
 /**
  * @license
