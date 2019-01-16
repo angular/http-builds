284321220a0e1aa8343cb737cebe5c3688aaa327
Andrew Kushnir <akushnir@google.com>
2019-01-16 18:02:22 +0000

    3b7a571 test(upgrade): clean up global state after test (#28181)

--- fesm5/http.js
+++ fesm5/http.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.0+192.sha-f983e99
+ * @license Angular v7.2.0+193.sha-3b7a571
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1862,7 +1862,7 @@ var JsonpModule = /** @class */ (function () {
  * @deprecated see https://angular.io/guide/http
  * @publicApi
  */
-var VERSION = new Version('7.2.0+192.sha-f983e99');
+var VERSION = new Version('7.2.0+193.sha-3b7a571');
 
 /**
  * @license
