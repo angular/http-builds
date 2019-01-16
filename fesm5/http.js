03123c08cc1b8acd1901f93861765a5825be7df4
Andrew Kushnir <akushnir@google.com>
2019-01-16 18:02:46 +0000

    35ec2dc fix(ivy): normalize summary and factory shim files paths (#28173)

--- fesm5/http.js
+++ fesm5/http.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.0-rc.0+128.sha-332dcb2
+ * @license Angular v7.2.0-rc.0+129.sha-35ec2dc
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1862,7 +1862,7 @@ var JsonpModule = /** @class */ (function () {
  * @deprecated see https://angular.io/guide/http
  * @publicApi
  */
-var VERSION = new Version('7.2.0-rc.0+128.sha-332dcb2');
+var VERSION = new Version('7.2.0-rc.0+129.sha-35ec2dc');
 
 /**
  * @license
