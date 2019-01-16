4034634c26662b1f0066aa95bad3eeb4f9523fee
Andrew Kushnir <akushnir@google.com>
2019-01-16 18:12:54 +0000

    bac71ef fix(ivy): componentFactories not populated in ModuleWithComponentFactories (#28112)

--- fesm5/http.js
+++ fesm5/http.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.0+196.sha-9a81f0d
+ * @license Angular v7.2.0+198.sha-bac71ef
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1862,7 +1862,7 @@ var JsonpModule = /** @class */ (function () {
  * @deprecated see https://angular.io/guide/http
  * @publicApi
  */
-var VERSION = new Version('7.2.0+196.sha-9a81f0d');
+var VERSION = new Version('7.2.0+198.sha-bac71ef');
 
 /**
  * @license
