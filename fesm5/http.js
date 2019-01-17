8405ff92a0b4d6cc12901f7807364b1467416a2a
Andrew Kushnir <akushnir@google.com>
2019-01-17 00:43:35 +0000

    a570fdf fix(bazel): Fix integration test after v8 bump (#28194)

--- fesm5/http.js
+++ fesm5/http.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.0+199.sha-1e5e62e
+ * @license Angular v8.0.0-beta.0+1.sha-a570fdf
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1875,7 +1875,7 @@ var JsonpModule = /** @class */ (function () {
  * @deprecated see https://angular.io/guide/http
  * @publicApi
  */
-var VERSION = new Version('7.2.0+199.sha-1e5e62e');
+var VERSION = new Version('8.0.0-beta.0+1.sha-a570fdf');
 
 /**
  * @license
