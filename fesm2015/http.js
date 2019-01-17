c2b83d5c4ff77dd1fb3535e21b57880de802741a
Alex Rickabaugh <alx+alxhub@alxandria.net>
2019-01-17 01:57:52 +0000

    6d40ef2 fix(docs-infra): improve a11y (#28086)

--- fesm2015/http.js
+++ fesm2015/http.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.1+3.sha-4ea6c27
+ * @license Angular v7.2.1+4.sha-6d40ef2
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2165,7 +2165,7 @@ JsonpModule.decorators = [
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('7.2.1+3.sha-4ea6c27');
+const VERSION = new Version('7.2.1+4.sha-6d40ef2');
 
 /**
  * @fileoverview added by tsickle
--- fesm5/http.js
+++ fesm5/http.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.1+3.sha-4ea6c27
+ * @license Angular v7.2.1+4.sha-6d40ef2
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1862,7 +1862,7 @@ var JsonpModule = /** @class */ (function () {
  * @deprecated see https://angular.io/guide/http
  * @publicApi
  */
-var VERSION = new Version('7.2.1+3.sha-4ea6c27');
+var VERSION = new Version('7.2.1+4.sha-6d40ef2');
 
 /**
  * @license
