f23e50aebd856540265d08a84fe92b8b69c7f86a
Alex Rickabaugh <alx+alxhub@alxandria.net>
2019-01-17 02:36:43 +0000

    8a08ff1 docs: fix comments in services example (#26194)

--- fesm5/http.js
+++ fesm5/http.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.0+5.sha-80967ce
+ * @license Angular v8.0.0-beta.0+7.sha-8a08ff1
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1875,7 +1875,7 @@ var JsonpModule = /** @class */ (function () {
  * @deprecated see https://angular.io/guide/http
  * @publicApi
  */
-var VERSION = new Version('8.0.0-beta.0+5.sha-80967ce');
+var VERSION = new Version('8.0.0-beta.0+7.sha-8a08ff1');
 
 /**
  * @license
