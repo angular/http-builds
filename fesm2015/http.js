16d54672350ea35a8f53e44fcdd0ffb83b7c9518
Alex Rickabaugh <alx+alxhub@alxandria.net>
2019-01-17 22:30:50 +0000

    49bee4c ci(docs-infra): do not automatically post preview comments on PRs for team (#28211)

--- fesm2015/http.js
+++ fesm2015/http.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.1+5.sha-d8c75f1
+ * @license Angular v7.2.1+6.sha-49bee4c
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2165,7 +2165,7 @@ JsonpModule.decorators = [
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('7.2.1+5.sha-d8c75f1');
+const VERSION = new Version('7.2.1+6.sha-49bee4c');
 
 /**
  * @fileoverview added by tsickle
