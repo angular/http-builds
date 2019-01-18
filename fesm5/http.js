d9fb850d8b9a56e3ffd44cfdc897b248fef896df
Alex Rickabaugh <alx+alxhub@alxandria.net>
2019-01-18 18:11:23 +0000

    2b9cc85 fix(ivy): Ensure proper namespace is used to create elements in JIT (#28144)

--- fesm5/http.js
+++ fesm5/http.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.0+18.sha-a58fd21
+ * @license Angular v8.0.0-beta.0+19.sha-2b9cc85
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1862,7 +1862,7 @@ var JsonpModule = /** @class */ (function () {
  * @deprecated see https://angular.io/guide/http
  * @publicApi
  */
-var VERSION = new Version('8.0.0-beta.0+18.sha-a58fd21');
+var VERSION = new Version('8.0.0-beta.0+19.sha-2b9cc85');
 
 /**
  * @license
