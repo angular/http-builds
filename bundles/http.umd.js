837b88f5f09c5a6d3cde9d753a6d0f52a4725ae2
Alex Rickabaugh <alx+alxhub@alxandria.net>
2019-01-22 19:55:09 +0000

    f99082f build: hide vscode settings e.g. debug launch config (#28299)

--- bundles/http.umd.js
+++ bundles/http.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.0+39.sha-197676a
+ * @license Angular v8.0.0-beta.0+41.sha-f99082f
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1901,7 +1901,7 @@
      * @deprecated see https://angular.io/guide/http
      * @publicApi
      */
-    var VERSION = new i0.Version('8.0.0-beta.0+39.sha-197676a');
+    var VERSION = new i0.Version('8.0.0-beta.0+41.sha-f99082f');
 
     /**
      * @license
