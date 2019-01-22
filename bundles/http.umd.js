89ad2604d046050099a7a9a3415954be2b0e1ce9
Alex Rickabaugh <alx+alxhub@alxandria.net>
2019-01-22 17:43:52 +0000

    9dabeea ci: add a rebase check to the merge-pr script (#28250)

--- bundles/http.umd.js
+++ bundles/http.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.0+24.sha-73dcd72
+ * @license Angular v8.0.0-beta.0+25.sha-9dabeea
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1901,7 +1901,7 @@
      * @deprecated see https://angular.io/guide/http
      * @publicApi
      */
-    var VERSION = new i0.Version('8.0.0-beta.0+24.sha-73dcd72');
+    var VERSION = new i0.Version('8.0.0-beta.0+25.sha-9dabeea');
 
     /**
      * @license
