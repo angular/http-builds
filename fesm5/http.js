f6724655f876036621ce8dc3ddc0afaf361e8a66
Alex Rickabaugh <alx+alxhub@alxandria.net>
2019-01-22 19:59:05 +0000

    d336bff fix(ivy): add missging getDirectives export (#28259)

--- fesm5/http.js
+++ fesm5/http.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.0+41.sha-f99082f
+ * @license Angular v8.0.0-beta.0+42.sha-d336bff
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1862,7 +1862,7 @@ var JsonpModule = /** @class */ (function () {
  * @deprecated see https://angular.io/guide/http
  * @publicApi
  */
-var VERSION = new Version('8.0.0-beta.0+41.sha-f99082f');
+var VERSION = new Version('8.0.0-beta.0+42.sha-d336bff');
 
 /**
  * @license
