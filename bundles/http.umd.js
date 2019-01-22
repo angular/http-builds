d755d38bfb6a5c45d25c9d461090a868ab707766
Alex Rickabaugh <alx+alxhub@alxandria.net>
2019-01-22 19:38:20 +0000

    1fb6731 docs: fix crisis-detail milestone files (#27025)

--- bundles/http.umd.js
+++ bundles/http.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.0+37.sha-1df8be5
+ * @license Angular v8.0.0-beta.0+38.sha-1fb6731
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1901,7 +1901,7 @@
      * @deprecated see https://angular.io/guide/http
      * @publicApi
      */
-    var VERSION = new i0.Version('8.0.0-beta.0+37.sha-1df8be5');
+    var VERSION = new i0.Version('8.0.0-beta.0+38.sha-1fb6731');
 
     /**
      * @license
