2dda05f3d25aea90e4626d9b84b9dd1ceb8fb0b6
Alex Rickabaugh <alx+alxhub@alxandria.net>
2019-01-22 18:35:11 +0000

    7439b46 docs: fix explanation of Injectable decorator in architecture guide (#27480)

--- bundles/http.umd.js
+++ bundles/http.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.0+34.sha-9a965c9
+ * @license Angular v8.0.0-beta.0+35.sha-7439b46
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1899,7 +1899,7 @@
      * @deprecated see https://angular.io/guide/http
      * @publicApi
      */
-    var VERSION = new core.Version('8.0.0-beta.0+34.sha-9a965c9');
+    var VERSION = new core.Version('8.0.0-beta.0+35.sha-7439b46');
 
     /**
      * @license
