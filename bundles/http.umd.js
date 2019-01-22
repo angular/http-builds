e55865d9bcb2a897afe10d869dc414579776eadd
Alex Rickabaugh <alx+alxhub@alxandria.net>
2019-01-22 17:58:46 +0000

    5b08a88 style(docs-infra): Active tslint rule `semicolon` (#28282)

--- bundles/http.umd.js
+++ bundles/http.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.0+26.sha-7db0358
+ * @license Angular v8.0.0-beta.0+27.sha-5b08a88
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1901,7 +1901,7 @@
      * @deprecated see https://angular.io/guide/http
      * @publicApi
      */
-    var VERSION = new i0.Version('8.0.0-beta.0+26.sha-7db0358');
+    var VERSION = new i0.Version('8.0.0-beta.0+27.sha-5b08a88');
 
     /**
      * @license
