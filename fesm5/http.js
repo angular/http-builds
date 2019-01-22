6498d569dfe15d3255e79fa7255b81aa0ea62f1e
Alex Rickabaugh <alx+alxhub@alxandria.net>
2019-01-22 20:42:49 +0000

    ea1b5c1 fix(ivy): not applying camelCased style properties (#28276)

--- fesm5/http.js
+++ fesm5/http.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.0+62.sha-18a9afc
+ * @license Angular v8.0.0-beta.0+63.sha-ea1b5c1
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1862,7 +1862,7 @@ var JsonpModule = /** @class */ (function () {
  * @deprecated see https://angular.io/guide/http
  * @publicApi
  */
-var VERSION = new Version('8.0.0-beta.0+62.sha-18a9afc');
+var VERSION = new Version('8.0.0-beta.0+63.sha-ea1b5c1');
 
 /**
  * @license
