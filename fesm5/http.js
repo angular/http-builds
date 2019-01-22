d680aed9db032ac17140b19e0383c62df8115c46
Alex Rickabaugh <alx+alxhub@alxandria.net>
2019-01-22 20:18:56 +0000

    94e305f build: upgrade `cli-hello-world[-ivy]` integration projects to @angular/cli@7.2.1 (#27697)

--- fesm5/http.js
+++ fesm5/http.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.0+43.sha-070fca1
+ * @license Angular v8.0.0-beta.0+50.sha-94e305f
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1875,7 +1875,7 @@ var JsonpModule = /** @class */ (function () {
  * @deprecated see https://angular.io/guide/http
  * @publicApi
  */
-var VERSION = new Version('8.0.0-beta.0+43.sha-070fca1');
+var VERSION = new Version('8.0.0-beta.0+50.sha-94e305f');
 
 /**
  * @license
