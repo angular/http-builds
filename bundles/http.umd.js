293bfb5b47b1d88879b494f210b4d0dc86536599
Alex Rickabaugh <alx+alxhub@alxandria.net>
2019-01-23 19:20:36 +0000

    cf8770f fix(ivy): don't create TNodes for native projectable nodes (#28275)

--- bundles/http.umd.js
+++ bundles/http.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.1
+ * @license Angular v8.0.0-beta.1+3.sha-cf8770f
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1901,7 +1901,7 @@
      * @deprecated see https://angular.io/guide/http
      * @publicApi
      */
-    var VERSION = new i0.Version('8.0.0-beta.1');
+    var VERSION = new i0.Version('8.0.0-beta.1+3.sha-cf8770f');
 
     /**
      * @license
