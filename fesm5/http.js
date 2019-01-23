24983f02ea24ba845c71cc12d4649fab425bc789
Alex Rickabaugh <alx+alxhub@alxandria.net>
2019-01-23 21:16:37 +0000

    9f9024b fix(ivy): handle namespaces in attributes (#28242)

--- fesm5/http.js
+++ fesm5/http.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.1+7.sha-030350f
+ * @license Angular v8.0.0-beta.1+12.sha-9f9024b
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1875,7 +1875,7 @@ var JsonpModule = /** @class */ (function () {
  * @deprecated see https://angular.io/guide/http
  * @publicApi
  */
-var VERSION = new Version('8.0.0-beta.1+7.sha-030350f');
+var VERSION = new Version('8.0.0-beta.1+12.sha-9f9024b');
 
 /**
  * @license
