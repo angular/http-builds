e39b14ac9b51764147f9bb81365b4dd030b71654
Jason Aden <jasonaden@google.com>
2019-01-24 18:32:24 +0000

    beacbfc build(docs-infra): upgrade cli command docs sources to 699743282 (#28329)

--- fesm5/http.js
+++ fesm5/http.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.2+4.sha-f72319c
+ * @license Angular v7.2.2+5.sha-beacbfc
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1862,7 +1862,7 @@ var JsonpModule = /** @class */ (function () {
  * @deprecated see https://angular.io/guide/http
  * @publicApi
  */
-var VERSION = new Version('7.2.2+4.sha-f72319c');
+var VERSION = new Version('7.2.2+5.sha-beacbfc');
 
 /**
  * @license
