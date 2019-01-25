4fd8ff75a41516b949101886da3a4a74ca1d25e7
Jason Aden <jasonaden@google.com>
2019-01-25 22:49:37 +0000

    3d5a919 refactor(ivy): clean up TNode not depending on LView (#28354)

--- fesm5/http.js
+++ fesm5/http.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.0+21.sha-45bf911
+ * @license Angular v8.0.0-beta.1+43.sha-3d5a919
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1862,7 +1862,7 @@ var JsonpModule = /** @class */ (function () {
  * @deprecated see https://angular.io/guide/http
  * @publicApi
  */
-var VERSION = new Version('8.0.0-beta.0+21.sha-45bf911');
+var VERSION = new Version('8.0.0-beta.1+43.sha-3d5a919');
 
 /**
  * @license
