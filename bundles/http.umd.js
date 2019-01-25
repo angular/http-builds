e6e2ed61d7ee717fab6737dc0323c21c766d4c60
Jason Aden <jasonaden@google.com>
2019-01-25 21:26:35 +0000

    2da82db fix(ivy): proper i18n postprocessing in case of nested templates (#28209)

--- bundles/http.umd.js
+++ bundles/http.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.1+39.sha-7496630
+ * @license Angular v8.0.0-beta.1+41.sha-2da82db
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1901,7 +1901,7 @@
      * @deprecated see https://angular.io/guide/http
      * @publicApi
      */
-    var VERSION = new i0.Version('8.0.0-beta.1+39.sha-7496630');
+    var VERSION = new i0.Version('8.0.0-beta.1+41.sha-2da82db');
 
     /**
      * @license
