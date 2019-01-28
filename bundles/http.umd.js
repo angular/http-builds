3a490eb39ff8d60c11fa2f7351e7079e4cdd0ba0
Jason Aden <jasonaden@google.com>
2019-01-28 22:19:19 +0000

    6e16338 test: log cli version in `cli-hello-world[-ivy]` integration projects (#26947)

--- bundles/http.umd.js
+++ bundles/http.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.1+56.sha-fd8dbd5
+ * @license Angular v8.0.0-beta.1+68.sha-6e16338
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1901,7 +1901,7 @@
      * @deprecated see https://angular.io/guide/http
      * @publicApi
      */
-    var VERSION = new i0.Version('8.0.0-beta.1+56.sha-fd8dbd5');
+    var VERSION = new i0.Version('8.0.0-beta.1+68.sha-6e16338');
 
     /**
      * @license
