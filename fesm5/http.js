5bb6be8fb51814b4f815c8d43a070733979d2de3
Jason Aden <jasonaden@google.com>
2019-01-30 01:06:22 +0000

    d11c2f9 docs: fix typo in lifecycle-hooks.md (#28206)

--- fesm5/http.js
+++ fesm5/http.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.2+44.sha-0ff48a1
+ * @license Angular v7.2.2+47.sha-d11c2f9
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1862,7 +1862,7 @@ var JsonpModule = /** @class */ (function () {
  * @deprecated see https://angular.io/guide/http
  * @publicApi
  */
-var VERSION = new Version('7.2.2+44.sha-0ff48a1');
+var VERSION = new Version('7.2.2+47.sha-d11c2f9');
 
 /**
  * @license
