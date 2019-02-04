73284935faad3b7eb4b228feda667bda419a9d13
Matias Niemelä <matias@yearofmoo.com>
2019-02-04 20:19:29 +0000

    3d52271 fix(docs-infra): remove polyfill web-animations since we use Angular > 6 and we don't use `AnimationBuilder` (#28514)

--- fesm5/http.js
+++ fesm5/http.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.2+25.sha-a98d660
+ * @license Angular v8.0.0-beta.2+23.sha-3d52271
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1875,7 +1875,7 @@ var JsonpModule = /** @class */ (function () {
  * @deprecated see https://angular.io/guide/http
  * @publicApi
  */
-var VERSION = new Version('8.0.0-beta.2+25.sha-a98d660');
+var VERSION = new Version('8.0.0-beta.2+23.sha-3d52271');
 
 /**
  * @license
