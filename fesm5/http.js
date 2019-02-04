4a986eb46aef91968691f402eca4dc59a33d2499
Matias Niemelä <matias@yearofmoo.com>
2019-02-04 21:54:21 +0000

    dad1bc7 build: bump year (#28220)

--- fesm5/http.js
+++ fesm5/http.js
@@ -1,6 +1,6 @@
 /**
- * @license Angular v7.2.3+18.sha-2109c30
- * (c) 2010-2018 Google, Inc. https://angular.io/
+ * @license Angular v7.2.3+19.sha-dad1bc7
+ * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
 
@@ -1862,7 +1862,7 @@ var JsonpModule = /** @class */ (function () {
  * @deprecated see https://angular.io/guide/http
  * @publicApi
  */
-var VERSION = new Version('7.2.3+18.sha-2109c30');
+var VERSION = new Version('7.2.3+19.sha-dad1bc7');
 
 /**
  * @license
