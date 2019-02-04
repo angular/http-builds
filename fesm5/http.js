e8bb24cf180cb6e0035c0f52ef29bb972dec091b
Matias Niemelä <matias@yearofmoo.com>
2019-02-04 22:11:19 +0000

    f2621db fix(core): remove createInjector() from public API (#28509)

--- fesm5/http.js
+++ fesm5/http.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.2+27.sha-463a789
+ * @license Angular v8.0.0-beta.2+35.sha-f2621db
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1875,7 +1875,7 @@ var JsonpModule = /** @class */ (function () {
  * @deprecated see https://angular.io/guide/http
  * @publicApi
  */
-var VERSION = new Version('8.0.0-beta.2+27.sha-463a789');
+var VERSION = new Version('8.0.0-beta.2+35.sha-f2621db');
 
 /**
  * @license
