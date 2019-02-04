c911679137d7bed3626ecccda422c540d5049136
Matias Niemelä <matias@yearofmoo.com>
2019-02-04 20:16:49 +0000

    2109c30 docs: fix shadow variable in tutorial example (#28499) PR Close #28499

--- fesm5/http.js
+++ fesm5/http.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.3+16.sha-c64e666
+ * @license Angular v7.2.3+18.sha-2109c30
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1862,7 +1862,7 @@ var JsonpModule = /** @class */ (function () {
  * @deprecated see https://angular.io/guide/http
  * @publicApi
  */
-var VERSION = new Version('7.2.3+16.sha-c64e666');
+var VERSION = new Version('7.2.3+18.sha-2109c30');
 
 /**
  * @license
