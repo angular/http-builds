b6f28762dc75e3b9c963da0943708494c09de0d7
Matias Niemelä <matias@yearofmoo.com>
2019-02-04 20:16:58 +0000

    a98d660 docs(bazel): Explain explicit version requirement (#28482)

--- fesm5/http.js
+++ fesm5/http.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.2+22.sha-52d3795
+ * @license Angular v8.0.0-beta.2+25.sha-a98d660
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1862,7 +1862,7 @@ var JsonpModule = /** @class */ (function () {
  * @deprecated see https://angular.io/guide/http
  * @publicApi
  */
-var VERSION = new Version('8.0.0-beta.2+22.sha-52d3795');
+var VERSION = new Version('8.0.0-beta.2+25.sha-a98d660');
 
 /**
  * @license
