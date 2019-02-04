5e9d3b3ee4d5eae20340ed890223bb8c623e3df5
Matias Niemelä <matias@yearofmoo.com>
2019-02-04 16:54:06 +0000

    4dfcbc6 build(docs-infra): update terser version in lockfile (#28512)

--- fesm5/http.js
+++ fesm5/http.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.2+17.sha-72c3695
+ * @license Angular v8.0.0-beta.2+21.sha-4dfcbc6
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1875,7 +1875,7 @@ var JsonpModule = /** @class */ (function () {
  * @deprecated see https://angular.io/guide/http
  * @publicApi
  */
-var VERSION = new Version('8.0.0-beta.2+17.sha-72c3695');
+var VERSION = new Version('8.0.0-beta.2+21.sha-4dfcbc6');
 
 /**
  * @license
