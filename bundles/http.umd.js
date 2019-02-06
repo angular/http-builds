9b642a204e4ddacb742b5a3d196f4da161feca1f
Matias Niemelä <matias@yearofmoo.com>
2019-02-06 07:38:22 +0000

    ae8b7b1 test(ivy): root cause analysis for mat-selection-list (#28525)

--- bundles/http.umd.js
+++ bundles/http.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.3+16.sha-62a13e7
+ * @license Angular v8.0.0-beta.3+17.sha-ae8b7b1
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1901,7 +1901,7 @@
      * @deprecated see https://angular.io/guide/http
      * @publicApi
      */
-    var VERSION = new i0.Version('8.0.0-beta.3+16.sha-62a13e7');
+    var VERSION = new i0.Version('8.0.0-beta.3+17.sha-ae8b7b1');
 
     /**
      * @license
