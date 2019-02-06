23e0908f17262826cbfc9c67960bc90dfcb0e641
Matias Niemelä <matias@yearofmoo.com>
2019-02-06 00:56:56 +0000

    fe4d811 revert: build: switch to typescript's es2015 typings (#28134)

--- fesm5/http.js
+++ fesm5/http.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.2+3.sha-35e45dc
+ * @license Angular v8.0.0-beta.2+58.sha-fe4d811
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1862,7 +1862,7 @@ var JsonpModule = /** @class */ (function () {
  * @deprecated see https://angular.io/guide/http
  * @publicApi
  */
-var VERSION = new Version('8.0.0-beta.2+3.sha-35e45dc');
+var VERSION = new Version('8.0.0-beta.2+58.sha-fe4d811');
 
 /**
  * @license
