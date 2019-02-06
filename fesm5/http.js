5dc77866a8c6d0c5c3c7b4d16ddebbcab9af1ae6
Matias Niemelä <matias@yearofmoo.com>
2019-02-06 04:56:33 +0000

    d42f32c feat: optionally save complete performance log in chrome benchpress tests (#27551)

--- fesm5/http.js
+++ fesm5/http.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.3+6.sha-baf103c
+ * @license Angular v8.0.0-beta.3+10.sha-d42f32c
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1875,7 +1875,7 @@ var JsonpModule = /** @class */ (function () {
  * @deprecated see https://angular.io/guide/http
  * @publicApi
  */
-var VERSION = new Version('8.0.0-beta.3+6.sha-baf103c');
+var VERSION = new Version('8.0.0-beta.3+10.sha-d42f32c');
 
 /**
  * @license
