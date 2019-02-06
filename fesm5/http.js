f3a15517249dab303949786f900360b2fa5b4518
Matias Niemelä <matias@yearofmoo.com>
2019-02-06 20:30:28 +0000

    851cf16 docs(service-worker): note about isStable/setInterval (#28102)

--- fesm5/http.js
+++ fesm5/http.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.3+26.sha-0e6f799
+ * @license Angular v8.0.0-beta.3+28.sha-851cf16
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1862,7 +1862,7 @@ var JsonpModule = /** @class */ (function () {
  * @deprecated see https://angular.io/guide/http
  * @publicApi
  */
-var VERSION = new Version('8.0.0-beta.3+26.sha-0e6f799');
+var VERSION = new Version('8.0.0-beta.3+28.sha-851cf16');
 
 /**
  * @license
