85ed43d6f812a82f6f2767189036a134635892d1
Matias Niemelä <matias@yearofmoo.com>
2019-02-06 05:39:28 +0000

    62a13e7 refactor(ivy): pass host into createLView explicitly (#28461)

--- fesm5/http.js
+++ fesm5/http.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.3+15.sha-22d3226
+ * @license Angular v8.0.0-beta.3+16.sha-62a13e7
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1875,7 +1875,7 @@ var JsonpModule = /** @class */ (function () {
  * @deprecated see https://angular.io/guide/http
  * @publicApi
  */
-var VERSION = new Version('8.0.0-beta.3+15.sha-22d3226');
+var VERSION = new Version('8.0.0-beta.3+16.sha-62a13e7');
 
 /**
  * @license
