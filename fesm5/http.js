bfb9a624ed179b940f65ea8b7b66f122d39b4e97
Matias Niemelä <matias@yearofmoo.com>
2019-02-06 19:52:26 +0000

    0e6f799 test(ivy): update e2e tests to use Renderer2 instead of Renderer (#28558)

--- fesm5/http.js
+++ fesm5/http.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.3+25.sha-6709db9
+ * @license Angular v8.0.0-beta.3+26.sha-0e6f799
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1862,7 +1862,7 @@ var JsonpModule = /** @class */ (function () {
  * @deprecated see https://angular.io/guide/http
  * @publicApi
  */
-var VERSION = new Version('8.0.0-beta.3+25.sha-6709db9');
+var VERSION = new Version('8.0.0-beta.3+26.sha-0e6f799');
 
 /**
  * @license
