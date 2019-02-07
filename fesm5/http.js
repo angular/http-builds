abe1defb8f5cf5eae95c65d272740bd90e00af76
Miško Hevery <misko@hevery.com>
2019-02-07 20:56:19 +0000

    264ef72 build: update render3 section of integration test readme (#28372)

--- fesm5/http.js
+++ fesm5/http.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.3+35.sha-50732e1
+ * @license Angular v8.0.0-beta.3+42.sha-264ef72
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1875,7 +1875,7 @@ var JsonpModule = /** @class */ (function () {
  * @deprecated see https://angular.io/guide/http
  * @publicApi
  */
-var VERSION = new Version('8.0.0-beta.3+35.sha-50732e1');
+var VERSION = new Version('8.0.0-beta.3+42.sha-264ef72');
 
 /**
  * @license
