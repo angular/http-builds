1ee48948b50d6ac48cfaef68bbfba6b8bd75c4ea
Miško Hevery <misko@hevery.com>
2019-02-08 17:47:18 +0000

    0e4705a test(ivy): run view_injector_integration tests on node (#28593)

--- fesm5/http.js
+++ fesm5/http.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.3+57.sha-ceada77
+ * @license Angular v8.0.0-beta.3+59.sha-0e4705a
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1875,7 +1875,7 @@ var JsonpModule = /** @class */ (function () {
  * @deprecated see https://angular.io/guide/http
  * @publicApi
  */
-var VERSION = new Version('8.0.0-beta.3+57.sha-ceada77');
+var VERSION = new Version('8.0.0-beta.3+59.sha-0e4705a');
 
 /**
  * @license
