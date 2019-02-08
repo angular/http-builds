d19d4726c12fc7112a280aedfaf2d7a72f79a182
Miško Hevery <misko@hevery.com>
2019-02-08 01:23:30 +0000

    4b7264f ci: use fallback caches (#28545)

--- fesm5/http.js
+++ fesm5/http.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.3+49.sha-6f9dd1b
+ * @license Angular v8.0.0-beta.3+50.sha-4b7264f
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1875,7 +1875,7 @@ var JsonpModule = /** @class */ (function () {
  * @deprecated see https://angular.io/guide/http
  * @publicApi
  */
-var VERSION = new Version('8.0.0-beta.3+49.sha-6f9dd1b');
+var VERSION = new Version('8.0.0-beta.3+50.sha-4b7264f');
 
 /**
  * @license
