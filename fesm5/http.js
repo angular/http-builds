e5f7add816fbeb0ee3376d460cf950e8d0f3cda7
Miško Hevery <misko@hevery.com>
2019-02-08 01:07:12 +0000

    bd9da26 docs: update HttpHeader documentation (#26233)

--- fesm5/http.js
+++ fesm5/http.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.4+29.sha-1e58a21
+ * @license Angular v7.2.4+30.sha-bd9da26
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1862,7 +1862,7 @@ var JsonpModule = /** @class */ (function () {
  * @deprecated see https://angular.io/guide/http
  * @publicApi
  */
-var VERSION = new Version('7.2.4+29.sha-1e58a21');
+var VERSION = new Version('7.2.4+30.sha-bd9da26');
 
 /**
  * @license
