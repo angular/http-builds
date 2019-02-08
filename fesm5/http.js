015ff9b2a1bdc61617a82761869a75183c726cb8
Miško Hevery <misko@hevery.com>
2019-02-08 22:16:12 +0000

    fb9a4a6 fix(ivy): support web worker nodes in unknown property checks (#28610)

--- fesm5/http.js
+++ fesm5/http.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.3+68.sha-7115e7c
+ * @license Angular v8.0.0-beta.3+64.sha-fb9a4a6
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1875,7 +1875,7 @@ var JsonpModule = /** @class */ (function () {
  * @deprecated see https://angular.io/guide/http
  * @publicApi
  */
-var VERSION = new Version('8.0.0-beta.3+68.sha-7115e7c');
+var VERSION = new Version('8.0.0-beta.3+64.sha-fb9a4a6');
 
 /**
  * @license
