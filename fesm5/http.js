405d6ebbc36e9ca1368288c7e3b589ad6d1ccede
Miško Hevery <misko@hevery.com>
2019-02-11 17:32:44 +0000

    8bd7d5b docs: clarify library types integration (#28410)

--- fesm5/http.js
+++ fesm5/http.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.4+45.sha-aa163be
+ * @license Angular v7.2.4+46.sha-8bd7d5b
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1862,7 +1862,7 @@ var JsonpModule = /** @class */ (function () {
  * @deprecated see https://angular.io/guide/http
  * @publicApi
  */
-var VERSION = new Version('7.2.4+45.sha-aa163be');
+var VERSION = new Version('7.2.4+46.sha-8bd7d5b');
 
 /**
  * @license
