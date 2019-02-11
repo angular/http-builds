eacfbf9823bef25650e2f973f64937f3deb4f4c3
Miško Hevery <misko@hevery.com>
2019-02-11 17:29:18 +0000

    9ec8aa5 docs: re-enable E2E tests for Http guide (#28592)

--- fesm5/http.js
+++ fesm5/http.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.3+71.sha-81329c8
+ * @license Angular v8.0.0-beta.3+88.sha-9ec8aa5
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1875,7 +1875,7 @@ var JsonpModule = /** @class */ (function () {
  * @deprecated see https://angular.io/guide/http
  * @publicApi
  */
-var VERSION = new Version('8.0.0-beta.3+71.sha-81329c8');
+var VERSION = new Version('8.0.0-beta.3+88.sha-9ec8aa5');
 
 /**
  * @license
