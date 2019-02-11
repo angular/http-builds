6d1415c73bb81d9a83719fce758ad4ddcd92dbf4
Miško Hevery <misko@hevery.com>
2019-02-11 17:29:56 +0000

    aa163be refactor(compiler): allow disabling external symbol factory reexports (#28594)

--- fesm2015/http.js
+++ fesm2015/http.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.4+44.sha-2f73c55
+ * @license Angular v7.2.4+45.sha-aa163be
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -2165,7 +2165,7 @@ JsonpModule.decorators = [
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('7.2.4+44.sha-2f73c55');
+const VERSION = new Version('7.2.4+45.sha-aa163be');
 
 /**
  * @fileoverview added by tsickle
