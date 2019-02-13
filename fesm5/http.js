a0523b9acb37bcb304cf28db5654a0ae2ec90795
Misko Hevery <misko@hevery.com>
2019-02-13 05:15:52 +0000

    d68a98f test(ivy): add template source mapping tests (#28055)

--- fesm5/http.js
+++ fesm5/http.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.3+108.sha-6050cd0
+ * @license Angular v8.0.0-beta.3+125.sha-d68a98f
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1862,7 +1862,7 @@ var JsonpModule = /** @class */ (function () {
  * @deprecated see https://angular.io/guide/http
  * @publicApi
  */
-var VERSION = new Version('8.0.0-beta.3+108.sha-6050cd0');
+var VERSION = new Version('8.0.0-beta.3+125.sha-d68a98f');
 
 /**
  * @license
