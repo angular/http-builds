5876091a2b18ef1b4379473796be99c046842d09
Miško Hevery <misko@hevery.com>
2019-02-13 20:55:51 +0000

    6fa4235 test(ivy): enable passing Material tests (#28687)

--- fesm5/http.js
+++ fesm5/http.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.3+152.sha-dba2a40
+ * @license Angular v8.0.0-beta.3+146.sha-6fa4235
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1875,7 +1875,7 @@ var JsonpModule = /** @class */ (function () {
  * @deprecated see https://angular.io/guide/http
  * @publicApi
  */
-var VERSION = new Version('8.0.0-beta.3+152.sha-dba2a40');
+var VERSION = new Version('8.0.0-beta.3+146.sha-6fa4235');
 
 /**
  * @license
