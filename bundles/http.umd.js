acbacfa3c15a39d9e8af7426977dee265842e368
Miško Hevery <misko@hevery.com>
2019-02-13 20:29:13 +0000

    39d0311 refactor(ivy): combine contentQueries and contentQueriesRefresh functions (#28503)

--- bundles/http.umd.js
+++ bundles/http.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.3+135.sha-644e7a2
+ * @license Angular v8.0.0-beta.3+136.sha-39d0311
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1901,7 +1901,7 @@
      * @deprecated see https://angular.io/guide/http
      * @publicApi
      */
-    var VERSION = new i0.Version('8.0.0-beta.3+135.sha-644e7a2');
+    var VERSION = new i0.Version('8.0.0-beta.3+136.sha-39d0311');
 
     /**
      * @license
