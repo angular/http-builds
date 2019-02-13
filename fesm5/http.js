ad7e7e1e6e6a5c86e12248f55e05fcb5ebe5d822
Miško Hevery <misko@hevery.com>
2019-02-13 20:45:57 +0000

    77eee42 fix(core): improve global variable detection (#28679)

--- fesm5/http.js
+++ fesm5/http.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.3+141.sha-5cafd44
+ * @license Angular v8.0.0-beta.3+143.sha-77eee42
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1875,7 +1875,7 @@ var JsonpModule = /** @class */ (function () {
  * @deprecated see https://angular.io/guide/http
  * @publicApi
  */
-var VERSION = new Version('8.0.0-beta.3+141.sha-5cafd44');
+var VERSION = new Version('8.0.0-beta.3+143.sha-77eee42');
 
 /**
  * @license
