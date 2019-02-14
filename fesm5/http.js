b9c3f482c0b1afa868ba5a4806864c0941b6571d
Misko Hevery <misko@hevery.com>
2019-02-14 05:44:17 +0000

    65c2dea refactor(bazel): Fix warning about overridden tsconfig options (#28674)

--- fesm5/http.js
+++ fesm5/http.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.3+167.sha-1df3aef
+ * @license Angular v8.0.0-beta.3+173.sha-65c2dea
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1875,7 +1875,7 @@ var JsonpModule = /** @class */ (function () {
  * @deprecated see https://angular.io/guide/http
  * @publicApi
  */
-var VERSION = new Version('8.0.0-beta.3+167.sha-1df3aef');
+var VERSION = new Version('8.0.0-beta.3+173.sha-65c2dea');
 
 /**
  * @license
