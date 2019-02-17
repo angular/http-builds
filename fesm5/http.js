6998aefc2f54eaa33dcde0b718435fdeecd0d273
Misko Hevery <misko@hevery.com>
2019-02-17 05:27:23 +0000

    04a1923 ci: add /packages/* to the @angular/fw-dev-infra CODEOWNERS group (#28745)

--- fesm5/http.js
+++ fesm5/http.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.4+2.sha-c1094cf
+ * @license Angular v8.0.0-beta.4+4.sha-04a1923
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1875,7 +1875,7 @@ var JsonpModule = /** @class */ (function () {
  * @deprecated see https://angular.io/guide/http
  * @publicApi
  */
-var VERSION = new Version('8.0.0-beta.4+2.sha-c1094cf');
+var VERSION = new Version('8.0.0-beta.4+4.sha-04a1923');
 
 /**
  * @license
