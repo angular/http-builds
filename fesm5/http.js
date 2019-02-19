45fc60d206ed66aa6ce94e743bd5252c5735e445
Igor Minar <igor@angular.io>
2019-02-19 21:29:37 +0000

    d8ae899 docs(common): fix a typo in a debounce requests example header (#28829)

--- fesm5/http.js
+++ fesm5/http.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.4+28.sha-19afb79
+ * @license Angular v8.0.0-beta.4+29.sha-d8ae899
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1875,7 +1875,7 @@ var JsonpModule = /** @class */ (function () {
  * @deprecated see https://angular.io/guide/http
  * @publicApi
  */
-var VERSION = new Version('8.0.0-beta.4+28.sha-19afb79');
+var VERSION = new Version('8.0.0-beta.4+29.sha-d8ae899');
 
 /**
  * @license
