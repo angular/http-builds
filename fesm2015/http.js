1f92253ae068777d8a9e72135655d67679687647
Igor Minar <igor@angular.io>
2019-02-19 20:43:17 +0000

    2a23eca docs: display the beginning label in interpolation guide example (#28602)

--- fesm2015/http.js
+++ fesm2015/http.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.5+4.sha-b5da4c6
+ * @license Angular v7.2.5+5.sha-2a23eca
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -2165,7 +2165,7 @@ JsonpModule.decorators = [
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('7.2.5+4.sha-b5da4c6');
+const VERSION = new Version('7.2.5+5.sha-2a23eca');
 
 /**
  * @fileoverview added by tsickle
