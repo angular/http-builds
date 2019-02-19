f992f108174f9f1767b9fec47885b59e9dbb0034
Igor Minar <igor@angular.io>
2019-02-19 19:30:44 +0000

    55b144b ci: add renovate configuration (#28121)

--- fesm5/http.js
+++ fesm5/http.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.4+9.sha-980bb52
+ * @license Angular v8.0.0-beta.4+10.sha-55b144b
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1875,7 +1875,7 @@ var JsonpModule = /** @class */ (function () {
  * @deprecated see https://angular.io/guide/http
  * @publicApi
  */
-var VERSION = new Version('8.0.0-beta.4+9.sha-980bb52');
+var VERSION = new Version('8.0.0-beta.4+10.sha-55b144b');
 
 /**
  * @license
