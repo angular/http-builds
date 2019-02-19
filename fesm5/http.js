78c7ed0aa4ce99bc0ff0935d9f84cf4581ca2215
Igor Minar <igor@angular.io>
2019-02-19 19:38:58 +0000

    b5da4c6 docs(aio): add Keilla Fernandes to GDE resources (#28437)

--- fesm5/http.js
+++ fesm5/http.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.5+2.sha-75cf86e
+ * @license Angular v7.2.5+4.sha-b5da4c6
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1862,7 +1862,7 @@ var JsonpModule = /** @class */ (function () {
  * @deprecated see https://angular.io/guide/http
  * @publicApi
  */
-var VERSION = new Version('7.2.5+2.sha-75cf86e');
+var VERSION = new Version('7.2.5+4.sha-b5da4c6');
 
 /**
  * @license
