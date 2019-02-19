12136c6ab23f25bcdbff1d5b6232efa11d7082fa
Igor Minar <igor@angular.io>
2019-02-19 20:59:57 +0000

    cdcbb53 docs: capitalize karma (#28775)

--- fesm5/http.js
+++ fesm5/http.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.5+7.sha-1df6b16
+ * @license Angular v7.2.5+9.sha-cdcbb53
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1862,7 +1862,7 @@ var JsonpModule = /** @class */ (function () {
  * @deprecated see https://angular.io/guide/http
  * @publicApi
  */
-var VERSION = new Version('7.2.5+7.sha-1df6b16');
+var VERSION = new Version('7.2.5+9.sha-cdcbb53');
 
 /**
  * @license
