5d77fcba73658f78d073262f302d50af4c9fe726
Igor Minar <igor@angular.io>
2019-02-20 00:15:30 +0000

    ad6475f fix(ivy): re-create node links after creating/moving/removing nodes with i18n (#28827)

--- fesm5/http.js
+++ fesm5/http.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.4+34.sha-3c1a162
+ * @license Angular v8.0.0-beta.4+35.sha-ad6475f
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1875,7 +1875,7 @@ var JsonpModule = /** @class */ (function () {
  * @deprecated see https://angular.io/guide/http
  * @publicApi
  */
-var VERSION = new Version('8.0.0-beta.4+34.sha-3c1a162');
+var VERSION = new Version('8.0.0-beta.4+35.sha-ad6475f');
 
 /**
  * @license
