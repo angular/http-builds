fd3b5c245f2e0b0200e45ef7e1eeda94e0963587
Igor Minar <igor@angular.io>
2019-02-20 19:16:19 +0000

    b9eb662 test(bazel): Upgrade CLI dependencies for schematics test (#28872)

--- fesm5/http.js
+++ fesm5/http.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.4+51.sha-04ae125
+ * @license Angular v8.0.0-beta.4+52.sha-b9eb662
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1862,7 +1862,7 @@ var JsonpModule = /** @class */ (function () {
  * @deprecated see https://angular.io/guide/http
  * @publicApi
  */
-var VERSION = new Version('8.0.0-beta.4+51.sha-04ae125');
+var VERSION = new Version('8.0.0-beta.4+52.sha-b9eb662');
 
 /**
  * @license
