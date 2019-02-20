ee0e29d6e3b810a54df098cd998ef26264085c59
Igor Minar <igor@angular.io>
2019-02-20 22:12:14 +0000

    cfb2d17 feat(ivy): convert [ngStyle] and [ngClass] to use ivy styling bindings (#28711)

--- fesm5/http.js
+++ fesm5/http.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.4+56.sha-a7e1c0c
+ * @license Angular v8.0.0-beta.5+2.sha-cfb2d17
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1875,7 +1875,7 @@ var JsonpModule = /** @class */ (function () {
  * @deprecated see https://angular.io/guide/http
  * @publicApi
  */
-var VERSION = new Version('8.0.0-beta.4+56.sha-a7e1c0c');
+var VERSION = new Version('8.0.0-beta.5+2.sha-cfb2d17');
 
 /**
  * @license
