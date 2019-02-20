54d9edbc3ab9fef1bfd6aca029e165bf6f85798b
Igor Minar <igor@angular.io>
2019-02-20 00:38:16 +0000

    295a143 docs: add Sharon DiOrio to contributors (#28671)

--- fesm5/http.js
+++ fesm5/http.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.4+35.sha-ad6475f
+ * @license Angular v8.0.0-beta.4+36.sha-295a143
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1875,7 +1875,7 @@ var JsonpModule = /** @class */ (function () {
  * @deprecated see https://angular.io/guide/http
  * @publicApi
  */
-var VERSION = new Version('8.0.0-beta.4+35.sha-ad6475f');
+var VERSION = new Version('8.0.0-beta.4+36.sha-295a143');
 
 /**
  * @license
