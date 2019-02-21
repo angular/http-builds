f737d78f16cf8d899b82585fd4deb5a1ec0498dd
Ben Lesh <ben@benlesh.com>
2019-02-21 21:02:13 +0000

    3f2b51b ci: make yarn globally available and independent of `$BASH_ENV` (#28889)

--- fesm5/http.js
+++ fesm5/http.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.5+19.sha-d8704a3
+ * @license Angular v8.0.0-beta.5+20.sha-3f2b51b.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1875,7 +1875,7 @@ var JsonpModule = /** @class */ (function () {
  * @deprecated see https://angular.io/guide/http
  * @publicApi
  */
-var VERSION = new Version('8.0.0-beta.5+19.sha-d8704a3');
+var VERSION = new Version('8.0.0-beta.5+20.sha-3f2b51b.with-local-changes');
 
 /**
  * @license
