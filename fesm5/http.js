131085f769059213563f83b61fa04e9d70f6157c
Ben Lesh <ben@benlesh.com>
2019-02-21 02:47:18 +0000

    58436fd fix(ivy): unable to import shim factory files on case-insensitive platforms (#28831)

--- fesm5/http.js
+++ fesm5/http.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.5+7.sha-d0b6622
+ * @license Angular v8.0.0-beta.5+9.sha-58436fd
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1875,7 +1875,7 @@ var JsonpModule = /** @class */ (function () {
  * @deprecated see https://angular.io/guide/http
  * @publicApi
  */
-var VERSION = new Version('8.0.0-beta.5+7.sha-d0b6622');
+var VERSION = new Version('8.0.0-beta.5+9.sha-58436fd');
 
 /**
  * @license
