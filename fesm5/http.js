6502fb78799c2fe1636d6a1b8c13114bd6e93e2f
Igor Minar <igor@angular.io>
2019-02-21 17:55:22 +0000

    e1aaa7e fix(ivy): component destroy hook called twice when configured as provider (#28470)

--- fesm5/http.js
+++ fesm5/http.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.5+15.sha-ebffde7
+ * @license Angular v8.0.0-beta.5+17.sha-e1aaa7e
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1875,7 +1875,7 @@ var JsonpModule = /** @class */ (function () {
  * @deprecated see https://angular.io/guide/http
  * @publicApi
  */
-var VERSION = new Version('8.0.0-beta.5+15.sha-ebffde7');
+var VERSION = new Version('8.0.0-beta.5+17.sha-e1aaa7e');
 
 /**
  * @license
