374a2066866490fee97abc34c042e23b34c05d6e
Igor Minar <igor@angular.io>
2019-02-21 01:11:59 +0000

    d0b6622 test(ivy): turn on more passing Material tests (#28876)

--- fesm5/http.js
+++ fesm5/http.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.5+6.sha-13b96ac
+ * @license Angular v8.0.0-beta.5+7.sha-d0b6622
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1875,7 +1875,7 @@ var JsonpModule = /** @class */ (function () {
  * @deprecated see https://angular.io/guide/http
  * @publicApi
  */
-var VERSION = new Version('8.0.0-beta.5+6.sha-13b96ac');
+var VERSION = new Version('8.0.0-beta.5+7.sha-d0b6622');
 
 /**
  * @license
