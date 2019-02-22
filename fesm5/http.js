7270d9c3277e81f834c7bb5592a3765324d8d523
Ben Lesh <ben@benlesh.com>
2019-02-22 19:13:18 +0000

    7f3e3a8 docs(bazel): Use CLI 8 for Bazel schematics (#28908)

--- fesm5/http.js
+++ fesm5/http.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.5+41.sha-230a941.with-local-changes
+ * @license Angular v8.0.0-beta.5+43.sha-7f3e3a8.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1862,7 +1862,7 @@ var JsonpModule = /** @class */ (function () {
  * @deprecated see https://angular.io/guide/http
  * @publicApi
  */
-var VERSION = new Version('8.0.0-beta.5+41.sha-230a941.with-local-changes');
+var VERSION = new Version('8.0.0-beta.5+43.sha-7f3e3a8.with-local-changes');
 
 /**
  * @license
