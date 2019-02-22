dc2ad3f2caa95e698b1bed242804f529d290bfa5
Igor Minar <igor@angular.io>
2019-02-22 02:09:35 +0000

    f4d6525 build: use uname instead of relying on sed --version (#28900)

--- fesm5/http.js
+++ fesm5/http.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.5+20.sha-3f2b51b.with-local-changes
+ * @license Angular v8.0.0-beta.5+22.sha-f4d6525.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1875,7 +1875,7 @@ var JsonpModule = /** @class */ (function () {
  * @deprecated see https://angular.io/guide/http
  * @publicApi
  */
-var VERSION = new Version('8.0.0-beta.5+20.sha-3f2b51b.with-local-changes');
+var VERSION = new Version('8.0.0-beta.5+22.sha-f4d6525.with-local-changes');
 
 /**
  * @license
