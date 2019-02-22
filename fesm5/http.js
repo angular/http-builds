0cc1e278d91a58494f8b76fc72e540f3083c231b
Igor Minar <igor@angular.io>
2019-02-22 07:49:28 +0000

    599e2e2 feat(docs-infra): add meta[name]=Description to angular.io's index.html (#28916)

--- fesm5/http.js
+++ fesm5/http.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.5+28.sha-94223a0.with-local-changes
+ * @license Angular v8.0.0-beta.5+32.sha-599e2e2.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1875,7 +1875,7 @@ var JsonpModule = /** @class */ (function () {
  * @deprecated see https://angular.io/guide/http
  * @publicApi
  */
-var VERSION = new Version('8.0.0-beta.5+28.sha-94223a0.with-local-changes');
+var VERSION = new Version('8.0.0-beta.5+32.sha-599e2e2.with-local-changes');
 
 /**
  * @license
