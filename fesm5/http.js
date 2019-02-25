5055c431a0cc6ac35fb0b5153d214fa70d2f5dc4
Ben Lesh <ben@benlesh.com>
2019-02-25 18:49:29 +0000

    65d3dda docs: add Human Coders trainings in resources.json (#28840) PR Close #28840

--- fesm5/http.js
+++ fesm5/http.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.5+69.sha-0071048.with-local-changes
+ * @license Angular v8.0.0-beta.5+71.sha-65d3dda.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1862,7 +1862,7 @@ var JsonpModule = /** @class */ (function () {
  * @deprecated see https://angular.io/guide/http
  * @publicApi
  */
-var VERSION = new Version('8.0.0-beta.5+69.sha-0071048.with-local-changes');
+var VERSION = new Version('8.0.0-beta.5+71.sha-65d3dda.with-local-changes');
 
 /**
  * @license
