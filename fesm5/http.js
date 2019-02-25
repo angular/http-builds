e5fb9e8ba7b1b22249918eaf0c2cfb81af1b9e59
Ben Lesh <ben@benlesh.com>
2019-02-25 18:50:12 +0000

    0b8aad2 docs: add ngVikings 2019 to events page (#28501)

--- fesm5/http.js
+++ fesm5/http.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.5+72.sha-ef1b9e6.with-local-changes
+ * @license Angular v8.0.0-beta.5+70.sha-0b8aad2.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1862,7 +1862,7 @@ var JsonpModule = /** @class */ (function () {
  * @deprecated see https://angular.io/guide/http
  * @publicApi
  */
-var VERSION = new Version('8.0.0-beta.5+72.sha-ef1b9e6.with-local-changes');
+var VERSION = new Version('8.0.0-beta.5+70.sha-0b8aad2.with-local-changes');
 
 /**
  * @license
