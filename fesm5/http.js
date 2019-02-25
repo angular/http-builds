1b83f461605b8678a482862e9ea3739f2514ddec
Ben Lesh <ben@benlesh.com>
2019-02-25 18:36:34 +0000

    481f4b7 docs: Updated TOH Heros documentation (#28347)

--- fesm5/http.js
+++ fesm5/http.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.6+27.sha-4bc0084.with-local-changes
+ * @license Angular v7.2.6+28.sha-481f4b7.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1862,7 +1862,7 @@ var JsonpModule = /** @class */ (function () {
  * @deprecated see https://angular.io/guide/http
  * @publicApi
  */
-var VERSION = new Version('7.2.6+27.sha-4bc0084.with-local-changes');
+var VERSION = new Version('7.2.6+28.sha-481f4b7.with-local-changes');
 
 /**
  * @license
