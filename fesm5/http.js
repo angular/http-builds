0cae2367b348cb2f74b3bbaf272071039781bcb8
Ben Lesh <ben@benlesh.com>
2019-02-25 21:12:47 +0000

    0977d95 docs(core): export the `...Decorator` interfaces to ensure they are documented (#28836) (#28961)

--- fesm5/http.js
+++ fesm5/http.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.6+34.sha-d374787.with-local-changes
+ * @license Angular v7.2.6+35.sha-0977d95.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1862,7 +1862,7 @@ var JsonpModule = /** @class */ (function () {
  * @deprecated see https://angular.io/guide/http
  * @publicApi
  */
-var VERSION = new Version('7.2.6+34.sha-d374787.with-local-changes');
+var VERSION = new Version('7.2.6+35.sha-0977d95.with-local-changes');
 
 /**
  * @license
