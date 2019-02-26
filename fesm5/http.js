591ed6acdd42079e857cd54c642cdee62d706786
Ben Lesh <ben@benlesh.com>
2019-02-26 20:08:28 +0000

    25a2fef fix(ivy): ViewRef.rootNodes not including projected nodes (#28951)

--- fesm5/http.js
+++ fesm5/http.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.5+78.sha-edb6c2d.with-local-changes
+ * @license Angular v8.0.0-beta.5+82.sha-25a2fef.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1862,7 +1862,7 @@ var JsonpModule = /** @class */ (function () {
  * @deprecated see https://angular.io/guide/http
  * @publicApi
  */
-var VERSION = new Version('8.0.0-beta.5+78.sha-edb6c2d.with-local-changes');
+var VERSION = new Version('8.0.0-beta.5+82.sha-25a2fef.with-local-changes');
 
 /**
  * @license
