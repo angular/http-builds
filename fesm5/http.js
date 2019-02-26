daff6276f5cbabaa461b13a6b77d3561b1475c73
Miško Hevery <misko@hevery.com>
2019-02-26 02:24:19 +0000

    edb6c2d feat(bazel): add ts_config extending support for ng_module (#21883)

--- fesm5/http.js
+++ fesm5/http.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.5+77.sha-3cb497c.with-local-changes
+ * @license Angular v8.0.0-beta.5+78.sha-edb6c2d.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1875,7 +1875,7 @@ var JsonpModule = /** @class */ (function () {
  * @deprecated see https://angular.io/guide/http
  * @publicApi
  */
-var VERSION = new Version('8.0.0-beta.5+77.sha-3cb497c.with-local-changes');
+var VERSION = new Version('8.0.0-beta.5+78.sha-edb6c2d.with-local-changes');
 
 /**
  * @license
