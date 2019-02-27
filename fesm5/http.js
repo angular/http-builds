865afec486c7e902722d809ab3ad276175ccb3e5
Ben Lesh <ben@benlesh.com>
2019-02-27 19:02:46 +0000

    034de06 fix(ivy): avoid duplicate i18n consts to be present in generated output (#28967)

--- fesm5/http.js
+++ fesm5/http.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.6+10.sha-2dd44d7.with-local-changes
+ * @license Angular v8.0.0-beta.6+11.sha-034de06.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1875,7 +1875,7 @@ var JsonpModule = /** @class */ (function () {
  * @deprecated see https://angular.io/guide/http
  * @publicApi
  */
-var VERSION = new Version('8.0.0-beta.6+10.sha-2dd44d7.with-local-changes');
+var VERSION = new Version('8.0.0-beta.6+11.sha-034de06.with-local-changes');
 
 /**
  * @license
