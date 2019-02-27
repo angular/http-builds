659295c52e34ab5a77f83bcf12e7c260b34b8e0f
Andrew Kushnir <akushnir@google.com>
2019-02-27 22:21:37 +0000

    daf8251 test(ivy): update root causes for expansion panel and accordion (#29011)

--- fesm5/http.js
+++ fesm5/http.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.6+14.sha-827e89c.with-local-changes
+ * @license Angular v8.0.0-beta.6+16.sha-daf8251.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1862,7 +1862,7 @@ var JsonpModule = /** @class */ (function () {
  * @deprecated see https://angular.io/guide/http
  * @publicApi
  */
-var VERSION = new Version('8.0.0-beta.6+14.sha-827e89c.with-local-changes');
+var VERSION = new Version('8.0.0-beta.6+16.sha-daf8251.with-local-changes');
 
 /**
  * @license
