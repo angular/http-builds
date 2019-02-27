e8d36bf2a846bc93912f16c554b22d6edf53712e
Andrew Kushnir <akushnir@google.com>
2019-02-27 22:28:30 +0000

    91a161a refactor(ivy): simplify logic of projectable nodes insertion (#29008)

--- fesm5/http.js
+++ fesm5/http.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.6+16.sha-daf8251.with-local-changes
+ * @license Angular v8.0.0-beta.6+18.sha-91a161a.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1862,7 +1862,7 @@ var JsonpModule = /** @class */ (function () {
  * @deprecated see https://angular.io/guide/http
  * @publicApi
  */
-var VERSION = new Version('8.0.0-beta.6+16.sha-daf8251.with-local-changes');
+var VERSION = new Version('8.0.0-beta.6+18.sha-91a161a.with-local-changes');
 
 /**
  * @license
