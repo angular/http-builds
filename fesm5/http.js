2387918c6b251fe72ab910daf8cc7a9e0aeb88ec
Igor Minar <igor@angular.io>
2019-02-28 11:04:44 +0000

    a54a752 build: add VSCode extension recommendations (#28784)

--- fesm5/http.js
+++ fesm5/http.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.7+3.sha-86981b3.with-local-changes
+ * @license Angular v7.2.7+5.sha-a54a752.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1862,7 +1862,7 @@ var JsonpModule = /** @class */ (function () {
  * @deprecated see https://angular.io/guide/http
  * @publicApi
  */
-var VERSION = new Version('7.2.7+3.sha-86981b3.with-local-changes');
+var VERSION = new Version('7.2.7+5.sha-a54a752.with-local-changes');
 
 /**
  * @license
