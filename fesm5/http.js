887bad1a9667fa2e85f1e8c0054f03dc1b243116
Andrew Kushnir <akushnir@google.com>
2019-03-01 23:35:56 +0000

    b50283e fix(ivy): support dynamic host attribute bindings (#29033)

--- fesm5/http.js
+++ fesm5/http.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.6+42.sha-ff8e4dd.with-local-changes
+ * @license Angular v8.0.0-beta.6+45.sha-b50283e.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1862,7 +1862,7 @@ var JsonpModule = /** @class */ (function () {
  * @deprecated see https://angular.io/guide/http
  * @publicApi
  */
-var VERSION = new Version('8.0.0-beta.6+42.sha-ff8e4dd.with-local-changes');
+var VERSION = new Version('8.0.0-beta.6+45.sha-b50283e.with-local-changes');
 
 /**
  * @license
