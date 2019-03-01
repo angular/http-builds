21e0010f0978372123ca808fa6b7f24027a078c8
Andrew Kushnir <akushnir@google.com>
2019-03-01 23:45:17 +0000

    ba602db build: update Bazel to 0.23 (#29058)

--- fesm5/http.js
+++ fesm5/http.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.6+47.sha-b1df9a3.with-local-changes
+ * @license Angular v8.0.0-beta.6+48.sha-ba602db.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1875,7 +1875,7 @@ var JsonpModule = /** @class */ (function () {
  * @deprecated see https://angular.io/guide/http
  * @publicApi
  */
-var VERSION = new Version('8.0.0-beta.6+47.sha-b1df9a3.with-local-changes');
+var VERSION = new Version('8.0.0-beta.6+48.sha-ba602db.with-local-changes');
 
 /**
  * @license
