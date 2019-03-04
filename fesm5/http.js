35367d31a051af230ac62171a276d127f41acc4b
Andrew Kushnir <akushnir@google.com>
2019-03-04 18:41:43 +0000

    d2f015f docs: fix minor typo (#29100)

--- fesm5/http.js
+++ fesm5/http.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.6+60.sha-c875851.with-local-changes
+ * @license Angular v8.0.0-beta.6+59.sha-d2f015f.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1875,7 +1875,7 @@ var JsonpModule = /** @class */ (function () {
  * @deprecated see https://angular.io/guide/http
  * @publicApi
  */
-var VERSION = new Version('8.0.0-beta.6+60.sha-c875851.with-local-changes');
+var VERSION = new Version('8.0.0-beta.6+59.sha-d2f015f.with-local-changes');
 
 /**
  * @license
