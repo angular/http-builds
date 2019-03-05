2a137c766d3d498a26c3569a53e41cf7595824d2
Andrew Kushnir <akushnir@google.com>
2019-03-05 18:05:44 +0000

    7d17496 ci: update packageFiles to be renovated (#29071)

--- fesm5/http.js
+++ fesm5/http.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.6+70.sha-586234b.with-local-changes
+ * @license Angular v8.0.0-beta.6+72.sha-7d17496.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1875,7 +1875,7 @@ var JsonpModule = /** @class */ (function () {
  * @deprecated see https://angular.io/guide/http
  * @publicApi
  */
-var VERSION = new Version('8.0.0-beta.6+70.sha-586234b.with-local-changes');
+var VERSION = new Version('8.0.0-beta.6+72.sha-7d17496.with-local-changes');
 
 /**
  * @license
