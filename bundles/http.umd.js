24203d8f171599029842b76996c465fa6234aaf2
George Kalpakas <kalpakas.g@gmail.com>
2019-03-06 23:26:29 +0000

    2fa61e8 build(docs-infra): use pinned dependencies when possible in `ng-packages-installer` (#28510)

--- bundles/http.umd.js
+++ bundles/http.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.2.11-d8b63d0
+ * @license Angular v5.2.11-2fa61e8
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -36,7 +36,7 @@ function __extends(d, b) {
 }
 
 /**
- * @license Angular v5.2.11-d8b63d0
+ * @license Angular v5.2.11-2fa61e8
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2751,7 +2751,7 @@ var JsonpModule = /** @class */ (function () {
 /**
  * @deprecated use \@angular/common/http instead
  */
-var VERSION = new _angular_core.Version('5.2.11-d8b63d0');
+var VERSION = new _angular_core.Version('5.2.11-2fa61e8');
 
 exports.BrowserXhr = BrowserXhr;
 exports.JSONPBackend = JSONPBackend;
--- esm5/http.js
+++ esm5/http.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.2.11-d8b63d0
+ * @license Angular v5.2.11-2fa61e8
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2719,7 +2719,7 @@ var JsonpModule = /** @class */ (function () {
 /**
  * @deprecated use \@angular/common/http instead
  */
-var VERSION = new Version('5.2.11-d8b63d0');
+var VERSION = new Version('5.2.11-2fa61e8');
 
 /**
  * @fileoverview added by tsickle
