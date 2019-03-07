a41791a105720714c5f4797fb0f90310a6dca572
Kara Erickson <karakara@google.com>
2019-03-07 17:57:33 +0000

    479ae51 test(ivy): update native table root causes (#29161)

--- fesm5/http.js
+++ fesm5/http.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.7+17.sha-eccbc78.with-local-changes
+ * @license Angular v8.0.0-beta.7+18.sha-479ae51.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1862,7 +1862,7 @@ var JsonpModule = /** @class */ (function () {
  * @deprecated see https://angular.io/guide/http
  * @publicApi
  */
-var VERSION = new Version('8.0.0-beta.7+17.sha-eccbc78.with-local-changes');
+var VERSION = new Version('8.0.0-beta.7+18.sha-479ae51.with-local-changes');
 
 /**
  * @license
