5e5480de87c0b61cac0bb85fbd843ff03e6c7fac
Kara Erickson <karakara@google.com>
2019-03-08 03:12:12 +0000

    014841d test(ivy): fix failing view ref test (#29178)

--- bundles/http.umd.js
+++ bundles/http.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.7+39.sha-63d1806.with-local-changes
+ * @license Angular v8.0.0-beta.7+40.sha-014841d.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1883,7 +1883,7 @@
      * @deprecated see https://angular.io/guide/http
      * @publicApi
      */
-    var VERSION = new i0.Version('8.0.0-beta.7+39.sha-63d1806.with-local-changes');
+    var VERSION = new i0.Version('8.0.0-beta.7+40.sha-014841d.with-local-changes');
 
     /**
      * @license
