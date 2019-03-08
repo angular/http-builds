4443f3c3c2122baa21c852dd88a2c907252ed08c
Kara Erickson <karakara@google.com>
2019-03-08 17:53:44 +0000

    142ac41 fix(ivy): ngcc - handle prototype pseudo-member from typings file in ESM5 host (#29158)

--- fesm5/http.js
+++ fesm5/http.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.7+41.sha-a68b1a1.with-local-changes
+ * @license Angular v8.0.0-beta.7+43.sha-142ac41.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1862,7 +1862,7 @@ var JsonpModule = /** @class */ (function () {
  * @deprecated see https://angular.io/guide/http
  * @publicApi
  */
-var VERSION = new Version('8.0.0-beta.7+41.sha-a68b1a1.with-local-changes');
+var VERSION = new Version('8.0.0-beta.7+43.sha-142ac41.with-local-changes');
 
 /**
  * @license
