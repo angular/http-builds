ec333384e799777193ca2a44f3d63bac45aa9a21
Kara Erickson <karakara@google.com>
2019-03-11 18:36:34 +0000

    1d88c2b fix(ivy): handle aliased Angular decorators (#29195)

--- fesm5/http.js
+++ fesm5/http.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.7+59.sha-99aa967.with-local-changes
+ * @license Angular v8.0.0-beta.7+60.sha-1d88c2b.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1857,7 +1857,7 @@ var JsonpModule = /** @class */ (function () {
  * @deprecated see https://angular.io/guide/http
  * @publicApi
  */
-var VERSION = new Version('8.0.0-beta.7+59.sha-99aa967.with-local-changes');
+var VERSION = new Version('8.0.0-beta.7+60.sha-1d88c2b.with-local-changes');
 
 /**
  * @license
