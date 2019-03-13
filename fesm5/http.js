f95eed1c462167c64e38c55a30a0640927f571d2
Kara Erickson <karakara@google.com>
2019-03-13 02:07:42 +0000

    955e4e7 test(ivy): update blocklist to turn on table tests (#29271)

--- fesm5/http.js
+++ fesm5/http.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.8+6.sha-73da279.with-local-changes
+ * @license Angular v8.0.0-beta.8+7.sha-955e4e7.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1857,7 +1857,7 @@ var JsonpModule = /** @class */ (function () {
  * @deprecated see https://angular.io/guide/http
  * @publicApi
  */
-var VERSION = new Version('8.0.0-beta.8+6.sha-73da279.with-local-changes');
+var VERSION = new Version('8.0.0-beta.8+7.sha-955e4e7.with-local-changes');
 
 /**
  * @license
