d0f31714713f5a0605ac9d1b542eb655a5a8d74b
Matias Niemelä <matias@yearofmoo.com>
2019-03-14 17:37:49 +0000

    a5c747f build: enable shard_count for some jasmine tests that have many specs (#29196)

--- bundles/http.umd.js
+++ bundles/http.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.8+25.sha-410ccac.with-local-changes
+ * @license Angular v8.0.0-beta.8+26.sha-a5c747f.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1883,7 +1883,7 @@
      * @deprecated see https://angular.io/guide/http
      * @publicApi
      */
-    var VERSION = new i0.Version('8.0.0-beta.8+25.sha-410ccac.with-local-changes');
+    var VERSION = new i0.Version('8.0.0-beta.8+26.sha-a5c747f.with-local-changes');
 
     /**
      * @license
