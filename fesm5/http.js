2b63b57e60f15e633d945f39ea402b45aba2a873
Filipe Silva <filipematossilva@gmail.com>
2019-03-14 13:20:00 +0000

    4b7ed54 ci: add codefresh

--- fesm5/http.js
+++ fesm5/http.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.7+14.sha-8210bea.with-local-changes
+ * @license Angular v8.0.0-beta.8+18.sha-4b7ed54.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1862,7 +1862,7 @@ var JsonpModule = /** @class */ (function () {
  * @deprecated see https://angular.io/guide/http
  * @publicApi
  */
-var VERSION = new Version('8.0.0-beta.7+14.sha-8210bea.with-local-changes');
+var VERSION = new Version('8.0.0-beta.8+18.sha-4b7ed54.with-local-changes');
 
 /**
  * @license
