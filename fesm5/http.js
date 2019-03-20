49f9e93d47a8a14f4fb82ce872e21c7bcd6e3abe
Matias Niemelä <matias@yearofmoo.com>
2019-03-20 19:01:15 +0000

    64e5628 feat(ivy): ngcc - support creating a new copy of the entry-point format (#29092)

--- fesm5/http.js
+++ fesm5/http.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.9+20.sha-0d04450.with-local-changes
+ * @license Angular v8.0.0-beta.9+40.sha-64e5628.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1862,7 +1862,7 @@ var JsonpModule = /** @class */ (function () {
  * @deprecated see https://angular.io/guide/http
  * @publicApi
  */
-var VERSION = new Version('8.0.0-beta.9+20.sha-0d04450.with-local-changes');
+var VERSION = new Version('8.0.0-beta.9+40.sha-64e5628.with-local-changes');
 
 /**
  * @license
