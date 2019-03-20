ec4b993fdbe8a16b5f1e656d200e6d9b958a5911
Matias Niemelä <matias@yearofmoo.com>
2019-03-20 20:28:14 +0000

    f64de2e docs: add Justin Schwartzenberger to GDE page (#29430)

--- fesm5/http.js
+++ fesm5/http.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.10+3.sha-86ad77c.with-local-changes
+ * @license Angular v7.2.10+4.sha-f64de2e.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1862,7 +1862,7 @@ var JsonpModule = /** @class */ (function () {
  * @deprecated see https://angular.io/guide/http
  * @publicApi
  */
-var VERSION = new Version('7.2.10+3.sha-86ad77c.with-local-changes');
+var VERSION = new Version('7.2.10+4.sha-f64de2e.with-local-changes');
 
 /**
  * @license
