01b02436430103d1dfaddfb471db32c9a991c74c
Matias Niemelä <matias@yearofmoo.com>
2019-03-20 03:55:31 +0000

    2d7435d build(bazel): fix router test failure (#29375)

--- fesm5/http.js
+++ fesm5/http.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.9+2.sha-dafbbf8.with-local-changes
+ * @license Angular v8.0.0-beta.9+9.sha-2d7435d.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1862,7 +1862,7 @@ var JsonpModule = /** @class */ (function () {
  * @deprecated see https://angular.io/guide/http
  * @publicApi
  */
-var VERSION = new Version('8.0.0-beta.9+2.sha-dafbbf8.with-local-changes');
+var VERSION = new Version('8.0.0-beta.9+9.sha-2d7435d.with-local-changes');
 
 /**
  * @license
