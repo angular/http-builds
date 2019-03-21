3d04fd4cfb776183863323a4d1c42a9efae578ee
Matias Niemelä <matias@yearofmoo.com>
2019-03-21 07:02:05 +0000

    41737bb docs: fix html tags in changelog (#29411)

--- fesm5/http.js
+++ fesm5/http.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.9+47.sha-afe3e72.with-local-changes
+ * @license Angular v8.0.0-beta.9+48.sha-41737bb.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1862,7 +1862,7 @@ var JsonpModule = /** @class */ (function () {
  * @deprecated see https://angular.io/guide/http
  * @publicApi
  */
-var VERSION = new Version('8.0.0-beta.9+47.sha-afe3e72.with-local-changes');
+var VERSION = new Version('8.0.0-beta.9+48.sha-41737bb.with-local-changes');
 
 /**
  * @license
