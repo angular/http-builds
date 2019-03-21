6639567f40437afa19bd049f50bec4d3cfac5193
Matias Niemelä <matias@yearofmoo.com>
2019-03-21 16:31:31 +0000

    dc10355 build(compiler-cli): enable full TypeScript strictness (#29436)

--- fesm5/http.js
+++ fesm5/http.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.9+55.sha-6cd3743.with-local-changes
+ * @license Angular v8.0.0-beta.9+56.sha-dc10355.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1862,7 +1862,7 @@ var JsonpModule = /** @class */ (function () {
  * @deprecated see https://angular.io/guide/http
  * @publicApi
  */
-var VERSION = new Version('8.0.0-beta.9+55.sha-6cd3743.with-local-changes');
+var VERSION = new Version('8.0.0-beta.9+56.sha-dc10355.with-local-changes');
 
 /**
  * @license
