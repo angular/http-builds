b5f72164488b41803b051768f2adf5a2c46ffab4
Miško Hevery <misko@hevery.com>
2019-03-27 19:58:56 +0000

    9745f55 feat: remove @angular/http dependency from @angular/platform-server (#29408)

--- fesm5/http.js
+++ fesm5/http.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.10+21.sha-96b800c.with-local-changes
+ * @license Angular v8.0.0-beta.10+25.sha-9745f55.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1862,7 +1862,7 @@ var JsonpModule = /** @class */ (function () {
  * @deprecated see https://angular.io/guide/http
  * @publicApi
  */
-var VERSION = new Version('8.0.0-beta.10+21.sha-96b800c.with-local-changes');
+var VERSION = new Version('8.0.0-beta.10+25.sha-9745f55.with-local-changes');
 
 /**
  * @license
