bccf609e5a1f5ae7e6e7b6a083f60c41ae4b18d2
Jason Aden <jasonaden@google.com>
2019-04-01 19:23:59 +0000

    ab72e06 docs: add custom-element ref to library overview (#28476)

--- fesm5/http.js
+++ fesm5/http.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.11+43.sha-5218f5a.with-local-changes
+ * @license Angular v7.2.11+44.sha-ab72e06.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1862,7 +1862,7 @@ var JsonpModule = /** @class */ (function () {
  * @deprecated see https://angular.io/guide/http
  * @publicApi
  */
-var VERSION = new Version('7.2.11+43.sha-5218f5a.with-local-changes');
+var VERSION = new Version('7.2.11+44.sha-ab72e06.with-local-changes');
 
 /**
  * @license
