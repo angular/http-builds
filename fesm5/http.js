16d9dd236e7fba3574232d7c4b97560528149447
Jason Aden <jasonaden@google.com>
2019-04-01 18:24:38 +0000

    a52123f docs: add missing picture of Jason Bedard (#29596)

--- fesm5/http.js
+++ fesm5/http.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.11+25.sha-60b5bf7.with-local-changes
+ * @license Angular v7.2.11+37.sha-a52123f.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1862,7 +1862,7 @@ var JsonpModule = /** @class */ (function () {
  * @deprecated see https://angular.io/guide/http
  * @publicApi
  */
-var VERSION = new Version('7.2.11+25.sha-60b5bf7.with-local-changes');
+var VERSION = new Version('7.2.11+37.sha-a52123f.with-local-changes');
 
 /**
  * @license
