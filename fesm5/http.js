30497820e10251fd770fff2f8dd205c1fc3dca34
Jason Aden <jasonaden@google.com>
2019-04-02 18:02:36 +0000

    3487055 refactor(compiler): rename INTERPOLATION_BLACKLIST_REGEXPS (#29593)

--- fesm5/http.js
+++ fesm5/http.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.10+100.sha-1293da1.with-local-changes
+ * @license Angular v8.0.0-beta.10+103.sha-3487055.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1862,7 +1862,7 @@ var JsonpModule = /** @class */ (function () {
  * @deprecated see https://angular.io/guide/http
  * @publicApi
  */
-var VERSION = new Version('8.0.0-beta.10+100.sha-1293da1.with-local-changes');
+var VERSION = new Version('8.0.0-beta.10+103.sha-3487055.with-local-changes');
 
 /**
  * @license
