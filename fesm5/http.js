0ad4e1724e8b234a226dcdf1a44cbf17587e1088
Jason Aden <jasonaden@google.com>
2019-04-02 17:57:28 +0000

    1c07061 refactor: change error message (#29594)

--- fesm5/http.js
+++ fesm5/http.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.10+99.sha-7c1f73a.with-local-changes
+ * @license Angular v8.0.0-beta.10+102.sha-1c07061.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1857,7 +1857,7 @@ var JsonpModule = /** @class */ (function () {
  * @deprecated see https://angular.io/guide/http
  * @publicApi
  */
-var VERSION = new Version('8.0.0-beta.10+99.sha-7c1f73a.with-local-changes');
+var VERSION = new Version('8.0.0-beta.10+102.sha-1c07061.with-local-changes');
 
 /**
  * @license
