55dd259cc0154fd586e980bb7ae27544fc94c1fe
Jason Aden <jasonaden@google.com>
2019-04-02 17:56:45 +0000

    d11b0c0 refactor(compiler): update docs (#29599)

--- fesm5/http.js
+++ fesm5/http.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.10+102.sha-1c07061.with-local-changes
+ * @license Angular v8.0.0-beta.10+101.sha-d11b0c0.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1862,7 +1862,7 @@ var JsonpModule = /** @class */ (function () {
  * @deprecated see https://angular.io/guide/http
  * @publicApi
  */
-var VERSION = new Version('8.0.0-beta.10+102.sha-1c07061.with-local-changes');
+var VERSION = new Version('8.0.0-beta.10+101.sha-d11b0c0.with-local-changes');
 
 /**
  * @license
