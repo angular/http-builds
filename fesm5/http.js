bdbb55d68ea5f62e943994a0a0e08c5aafd84a73
Jason Aden <jasonaden@google.com>
2019-04-02 23:29:11 +0000

    ec4dced docs: update examples region to match the description in the guide (#29259)

--- fesm5/http.js
+++ fesm5/http.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.11+59.sha-69786c2.with-local-changes
+ * @license Angular v7.2.11+60.sha-ec4dced.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1862,7 +1862,7 @@ var JsonpModule = /** @class */ (function () {
  * @deprecated see https://angular.io/guide/http
  * @publicApi
  */
-var VERSION = new Version('7.2.11+59.sha-69786c2.with-local-changes');
+var VERSION = new Version('7.2.11+60.sha-ec4dced.with-local-changes');
 
 /**
  * @license
