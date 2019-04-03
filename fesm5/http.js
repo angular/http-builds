c7aa0674c90f8c45ce74298689755b9d8bdaa6ea
Jason Aden <jasonaden@google.com>
2019-04-03 22:52:56 +0000

    736afea docs: remove trailing comma in BAZEL.md (#29682)

--- fesm5/http.js
+++ fesm5/http.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.11+61.sha-a357dae.with-local-changes
+ * @license Angular v7.2.12+1.sha-736afea.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1862,7 +1862,7 @@ var JsonpModule = /** @class */ (function () {
  * @deprecated see https://angular.io/guide/http
  * @publicApi
  */
-var VERSION = new Version('7.2.11+61.sha-a357dae.with-local-changes');
+var VERSION = new Version('7.2.12+1.sha-736afea.with-local-changes');
 
 /**
  * @license
