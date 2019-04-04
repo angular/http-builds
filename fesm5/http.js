37999cb8a2aefdd3df58f90f05a22f64a4dcd8c3
Jason Aden <jasonaden@google.com>
2019-04-04 18:16:30 +0000

    29481a1 test(docs-infra): fix eslint warnings (#29673)

--- fesm5/http.js
+++ fesm5/http.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.12+7.sha-b4c77d5.with-local-changes
+ * @license Angular v7.2.12+10.sha-29481a1.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1862,7 +1862,7 @@ var JsonpModule = /** @class */ (function () {
  * @deprecated see https://angular.io/guide/http
  * @publicApi
  */
-var VERSION = new Version('7.2.12+7.sha-b4c77d5.with-local-changes');
+var VERSION = new Version('7.2.12+10.sha-29481a1.with-local-changes');
 
 /**
  * @license
