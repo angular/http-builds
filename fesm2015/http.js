bc6500f24419ba9ebdf0cd5c436eb997ee65df37
Igor Minar <igor@angular.io>
2019-04-08 17:10:02 +0000

    895b045 refactor(docs-infra): rename properties (blacklisted --> ignored) (#29754)

--- fesm2015/http.js
+++ fesm2015/http.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.12+14.sha-7d6d4c4.with-local-changes
+ * @license Angular v7.2.12+16.sha-895b045.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -2165,7 +2165,7 @@ JsonpModule.decorators = [
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('7.2.12+14.sha-7d6d4c4.with-local-changes');
+const VERSION = new Version('7.2.12+16.sha-895b045.with-local-changes');
 
 /**
  * @fileoverview added by tsickle
