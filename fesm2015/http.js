5d0df777529a7087383a19f8cea3f0875cc9bc32
Igor Minar <igor@angular.io>
2019-04-10 19:41:44 +0000

    42b7f85 docs: redirect to schematics-for-libraries (#29780)

--- fesm2015/http.js
+++ fesm2015/http.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.12+24.sha-7089c20.with-local-changes
+ * @license Angular v7.2.12+26.sha-42b7f85.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -2165,7 +2165,7 @@ JsonpModule.decorators = [
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('7.2.12+24.sha-7089c20.with-local-changes');
+const VERSION = new Version('7.2.12+26.sha-42b7f85.with-local-changes');
 
 /**
  * @fileoverview added by tsickle
