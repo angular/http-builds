8b9a49520653d31ecae46a5fc6c470168005961a
Igor Minar <igor@angular.io>
2019-04-10 19:42:40 +0000

    7f1c5f6 docs: fix grammatical errors in the style guide (#29769)

--- fesm2015/http.js
+++ fesm2015/http.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.12+26.sha-42b7f85.with-local-changes
+ * @license Angular v7.2.12+25.sha-7f1c5f6.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -2165,7 +2165,7 @@ JsonpModule.decorators = [
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('7.2.12+26.sha-42b7f85.with-local-changes');
+const VERSION = new Version('7.2.12+25.sha-7f1c5f6.with-local-changes');
 
 /**
  * @fileoverview added by tsickle
--- fesm5/http.js
+++ fesm5/http.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.12+26.sha-42b7f85.with-local-changes
+ * @license Angular v7.2.12+25.sha-7f1c5f6.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1862,7 +1862,7 @@ var JsonpModule = /** @class */ (function () {
  * @deprecated see https://angular.io/guide/http
  * @publicApi
  */
-var VERSION = new Version('7.2.12+26.sha-42b7f85.with-local-changes');
+var VERSION = new Version('7.2.12+25.sha-7f1c5f6.with-local-changes');
 
 /**
  * @license
